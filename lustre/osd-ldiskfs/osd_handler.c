4d56f32b3ca845c534ce4012477f4bded03ab867
Alex Zhuravlev <alexey.zhuravlev@intel.com>
2014-03-19 12:20:16 +0400

    LU-3680 osd: reduce osd_thread_info in ldiskfs osd
    by unioning few rarely used fields. now the structure should
    fit a page:
    
    (gdb) p sizeof(struct osd_thread_info)
    $1 = 3296
    
    Lustre-commit: 56875fde8c66a4a937b173ccb065a9a6a3c67419
    Lustre-change: http://review.whamcloud.com/9726
    
    Includes the change from
    Lustre-commit: c65d83dd93fc61c44a1f0503e196a123d57dfb88
    Lustre-change: http://review.whamcloud.com/13955
    to silence a verbose CLASSERT()
    
    Signed-off-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Signed-off-by: Bob Glossman <bob.glossman@intel.com>
    Change-Id: I75d5c6fefa41884390ce155781e0963884a3ad2c
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Fan Yong <fan.yong@intel.com>
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-on: http://review.whamcloud.com/15312
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -129,7 +129,6 @@ int osd_trans_declare_op2rb[] = {
 	[OSD_OT_WRITE]		= OSD_OT_WRITE,
 	[OSD_OT_INSERT]		= OSD_OT_DELETE,
 	[OSD_OT_DELETE]		= OSD_OT_INSERT,
-	[OSD_OT_UPDATE]		= OSD_OT_MAX,
 	[OSD_OT_QUOTA]		= OSD_OT_MAX,
 };
 
@@ -987,7 +986,6 @@ static struct thandle *osd_trans_create(const struct lu_env *env,
 		oh->ot_credits = 0;
 		atomic_set(&th->th_refc, 1);
 		th->th_alloc_size = sizeof(*oh);
-		oti->oti_dev = osd_dt_dev(d);
 		INIT_LIST_HEAD(&oh->ot_dcb_list);
 		osd_th_alloced(oh);
 
@@ -1134,7 +1132,8 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 	struct osd_thandle     *oh;
 	struct osd_thread_info *oti = osd_oti_get(env);
 	struct osd_iobuf       *iobuf = &oti->oti_iobuf;
-	struct qsd_instance    *qsd = oti->oti_dev->od_quota_slave;
+	struct osd_device      *osd = osd_dt_dev(th->th_dev);
+	struct qsd_instance    *qsd = osd->od_quota_slave;
 	struct lquota_trans    *qtrans;
 	ENTRY;
 
@@ -1158,19 +1157,20 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
                 oti->oti_txns--;
                 rc = dt_txn_hook_stop(env, th);
                 if (rc != 0)
-                        CERROR("Failure in transaction hook: %d\n", rc);
+			CERROR("%s: failed in transaction hook: rc = %d\n",
+			       osd_name(osd), rc);
 
 		/* hook functions might modify th_sync */
 		hdl->h_sync = th->th_sync;
 
-                oh->ot_handle = NULL;
-                OSD_CHECK_SLOW_TH(oh, oti->oti_dev,
-                                  rc = ldiskfs_journal_stop(hdl));
-                if (rc != 0)
-                        CERROR("Failure to stop transaction: %d\n", rc);
-        } else {
+		oh->ot_handle = NULL;
+		OSD_CHECK_SLOW_TH(oh, osd, rc = ldiskfs_journal_stop(hdl));
+		if (rc != 0)
+			CERROR("%s: failed to stop transaction: rc = %d\n",
+			       osd_name(osd), rc);
+	} else {
 		thandle_put(&oh->ot_super);
-        }
+	}
 
 	/* inform the quota slave device that the transaction is stopping */
 	qsd_op_end(env, qsd, qtrans);
@@ -1186,7 +1186,7 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 	 */
 	wait_event(iobuf->dr_wait,
 		       atomic_read(&iobuf->dr_numreqs) == 0);
-	osd_fini_iobuf(oti->oti_dev, iobuf);
+	osd_fini_iobuf(osd, iobuf);
 	if (!rc)
 		rc = iobuf->dr_error;
 
@@ -5873,6 +5873,8 @@ static void osd_key_fini(const struct lu_context *ctx,
 {
 	struct osd_thread_info *info = data;
 
+	if (info->oti_inode != NULL)
+		OBD_FREE_PTR(info->oti_inode);
 	if (info->oti_hlock != NULL)
 		ldiskfs_htree_lock_free(info->oti_hlock);
 	OBD_FREE(info->oti_it_ea_buf, OSD_IT_EA_BUFSIZE);
@@ -6508,6 +6510,11 @@ static struct obd_ops osd_obd_device_ops = {
 static int __init osd_mod_init(void)
 {
 	int rc;
+
+#if !defined(CONFIG_DEBUG_MUTEXES) && !defined(CONFIG_DEBUG_SPINLOCK)
+	/* please, try to keep osd_thread_info smaller than a page */
+	CLASSERT(sizeof(struct osd_thread_info) <= PAGE_SIZE);
+#endif
 
 	osd_oi_mod_init();
 
