a09a1aac5b2f22899068617dec7b9ce48b554065
Sebastien Buisson <sebastien.buisson@bull.net>
2014-10-17 23:34:33 -0700

    LU-4044 build: fix 'resource leak' errors
    Fix 'resource leak' defects found by Coverity version 6.6.1:
    Resource leak (RESOURCE_LEAK)
    Variable going out of scope leaks the storage it points to.
    
    This patch is back-ported from the following one:
    Lustre-commit: a0e74aa58234752ec01c7a056af41fcefd2e43b6
    Lustre-change: http://review.whamcloud.com/7820
    
    Test-Parameters: alwaysuploadlogs \
    envdefinitions=SLOW=yes,ENABLE_QUOTA=yes \
    mdtfilesystemtype=zfs mdsfilesystemtype=zfs ostfilesystemtype=zfs \
    testlist=sanity-hsm
    
    Change-Id: Ieb697165ceedd26520531a4a2eafc527bb8716c5
    Signed-off-by: Sebastien Buisson <sebastien.buisson@bull.net>
    Signed-off-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-on: http://review.whamcloud.com/12338
    Tested-by: Jenkins
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: James Nunez <james.a.nunez@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/mdt/mdt_handler.c
+++ lustre/mdt/mdt_handler.c
@@ -4564,7 +4564,7 @@ static int mdt_stack_init(const struct lu_env *env, struct mdt_device *mdt,
 	strcpy(name, dev);
 	p = strstr(name, "-MDT");
 	if (p == NULL)
-		GOTO(cleanup_mem, rc = -ENOMEM);
+		GOTO(free_bufs, rc = -ENOMEM);
 	p[3] = 'D';
 
 	snprintf(uuid, MAX_OBD_NAME, "%s_UUID", name);
@@ -4573,7 +4573,7 @@ static int mdt_stack_init(const struct lu_env *env, struct mdt_device *mdt,
 	if (lprof == NULL || lprof->lp_dt == NULL) {
 		CERROR("can't find the profile: %s\n",
 		       lustre_cfg_string(cfg, 0));
-		GOTO(cleanup_mem, rc = -EINVAL);
+		GOTO(free_bufs, rc = -EINVAL);
 	}
 
 	lustre_cfg_bufs_reset(bufs, name);
@@ -4612,7 +4612,7 @@ static int mdt_stack_init(const struct lu_env *env, struct mdt_device *mdt,
 	/* connect to MDD we just setup */
 	rc = mdt_connect_to_next(env, mdt, name, &mdt->mdt_child_exp);
 	if (rc)
-		RETURN(rc);
+		GOTO(class_detach, rc);
 
 	site = mdt->mdt_child_exp->exp_obd->obd_lu_dev->ld_site;
 	LASSERT(site);
@@ -4626,7 +4626,7 @@ static int mdt_stack_init(const struct lu_env *env, struct mdt_device *mdt,
 	snprintf(name, MAX_OBD_NAME, "%s-osd", dev);
 	rc = mdt_connect_to_next(env, mdt, name, &mdt->mdt_bottom_exp);
 	if (rc)
-		RETURN(rc);
+		GOTO(class_detach, rc);
 	mdt->mdt_bottom =
 		lu2dt_dev(mdt->mdt_bottom_exp->exp_obd->obd_lu_dev);
 
