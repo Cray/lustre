d40db9a611261851aa656c22c305b066e8b424a6
Bob Glossman <bob.glossman@intel.com>
2015-03-05 09:14:50 -0800

    LU-5363 build: add ll_d_count wrapper to fix build problem
    Create and use ll_d_count() wrapper to solve
    build problem caused by linux #include changes.
    
    This comes from
    
    Lustre-commit: 3000a83dcd3c201f383c0507d33e7861fc695997
    Lustre-change: http://review.whamcloud.com/11133
    
    It was left out of the previous backport of this mod as
    it wasn't needed in the strictly v2.5 based code.
    It is needed now due to the many additional changes added
    for new kernel support.
    
    Signed-off-by: Bob Glossman <bob.glossman@intel.com>
    Change-Id: I871a30655bf0f1deaa72ae98c097e8200a054da4

--- lustre/llite/dcache.c
+++ lustre/llite/dcache.c
@@ -112,7 +112,7 @@ int ll_dcompare(struct dentry *parent, struct qstr *d_name, struct qstr *name)
 
 	CDEBUG(D_DENTRY, "found name %.*s(%p) flags %#x refc %d\n",
 	       name->len, name->name, dentry, dentry->d_flags,
-	       d_count(dentry));
+	       ll_d_count(dentry));
 
 	/* mountpoint is always valid */
 	if (d_mountpoint((struct dentry *)dentry))
@@ -179,10 +179,10 @@ static int ll_ddelete(HAVE_D_DELETE_CONST struct dentry *de)
 	       list_empty(&de->d_subdirs) ? "" : "subdirs");
 
 #ifdef HAVE_DCACHE_LOCK
-	LASSERT(d_count(de) == 0);
+	LASSERT(ll_d_count(de) == 0);
 #else
 	/* kernel >= 2.6.38 last refcount is decreased after this function. */
-	LASSERT(d_count(de) == 1);
+	LASSERT(ll_d_count(de) == 1);
 #endif
 
 	/* Disable this piece of code temproarily because this is called
@@ -208,7 +208,7 @@ int ll_d_init(struct dentry *de)
 
 	CDEBUG(D_DENTRY, "ldd on dentry %.*s (%p) parent %p inode %p refc %d\n",
 		de->d_name.len, de->d_name.name, de, de->d_parent, de->d_inode,
-		d_count(de));
+		ll_d_count(de));
 
 	if (de->d_fsdata == NULL) {
 		struct ll_dentry_data *lld;
@@ -530,7 +530,7 @@ out:
 		CDEBUG(D_DENTRY, "revalidated dentry %.*s (%p) parent %p "
 		       "inode %p refc %d\n", de->d_name.len,
 		       de->d_name.name, de, de->d_parent, de->d_inode,
-		       d_count(de));
+		       ll_d_count(de));
 
 		ll_set_lock_data(exp, de->d_inode, it, &bits);
 
