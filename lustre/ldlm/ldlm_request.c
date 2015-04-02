cc42ecf015b5dfc73794b81d68368dcd4433c99f
Vitaly Fertman <vitaly.fertman@seagate.com>
2015-04-02 20:35:32 +0300

    LU-6390 ldlm: restore the ELC for enqueue
    after LU-4300 enqueue does not ELC anymore, however if enqueue is
    agressive (ls -la of a large dir) we may exceed lru-resize limit
    quickly because LRUR shrinker and recalc are called not so often.
    
    ELC is to be restored in enqueue.
    ELC also should check for the lock weight, in addition to LRUR.
    ELC can also keep "skipped" locks, i.e. once checked for the weight
    and left in the lru - let LRUR take care about them later.
    LRUR is to be left untouched, no weight logic, otherwise LU-5727
    appears and OPEN locks do not get canceled.
    
    Lustre-commit: ac5abd46e95edd97316ff0e9563288636e7c42bc
    Lustre-change: http://review.whamcloud.com/14342
    
    Xyratex-bug-id: MRP-2550
    Signed-off-by: Vitaly Fertman <vitaly.fertman@seagate.com>
    Signed-off-by: Bob Glossman <bob.glossman@intel.com>
    Change-Id: I19fb7c367d9d38107a4fb6d100a47c2e7b7ad640
    Reviewed-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Reviewed-by: Niu Yawei <yawei.niu@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Reviewed-on: https://review.whamcloud.com/27983
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/ldlm/ldlm_request.c
+++ lustre/ldlm/ldlm_request.c
@@ -784,10 +784,9 @@ int ldlm_prep_elc_req(struct obd_export *exp, struct ptlrpc_request *req,
                 req_capsule_filled_sizes(pill, RCL_CLIENT);
                 avail = ldlm_capsule_handles_avail(pill, RCL_CLIENT, canceloff);
 
-                flags = ns_connect_lru_resize(ns) ?
-                        LDLM_CANCEL_LRUR : LDLM_CANCEL_AGED;
-                to_free = !ns_connect_lru_resize(ns) &&
-                          opc == LDLM_ENQUEUE ? 1 : 0;
+		flags = ns_connect_lru_resize(ns) ?
+			LDLM_CANCEL_LRUR_NO_WAIT : LDLM_CANCEL_AGED;
+		to_free = !ns_connect_lru_resize(ns) && opc == LDLM_ENQUEUE;
 
 		/* Cancel LRU locks here _only_ if the server supports
 		 * EARLY_CANCEL. Otherwise we have to send extra CANCEL
@@ -1514,6 +1513,21 @@ static ldlm_policy_res_t ldlm_cancel_lrur_policy(struct ldlm_namespace *ns,
 	return LDLM_POLICY_CANCEL_LOCK;
 }
 
+static ldlm_policy_res_t
+ldlm_cancel_lrur_no_wait_policy(struct ldlm_namespace *ns,
+				struct ldlm_lock *lock,
+				int unused, int added,
+				int count)
+{
+	ldlm_policy_res_t result;
+
+	result = ldlm_cancel_lrur_policy(ns, lock, unused, added, count);
+	if (result == LDLM_POLICY_KEEP_LOCK)
+		return result;
+
+	return ldlm_cancel_no_wait_policy(ns, lock, unused, added, count);
+}
+
 /**
  * Callback function for proc used policy. Makes decision whether to keep
  * \a lock in LRU for current \a LRU size \a unused, added in current scan \a
@@ -1594,6 +1608,8 @@ ldlm_cancel_lru_policy(struct ldlm_namespace *ns, int flags)
                         return ldlm_cancel_lrur_policy;
                 else if (flags & LDLM_CANCEL_PASSED)
                         return ldlm_cancel_passed_policy;
+		else if (flags & LDLM_CANCEL_LRUR_NO_WAIT)
+			return ldlm_cancel_lrur_no_wait_policy;
         } else {
                 if (flags & LDLM_CANCEL_AGED)
                         return ldlm_cancel_aged_policy;
@@ -1642,6 +1658,7 @@ static int ldlm_prepare_lru_list(struct ldlm_namespace *ns,
 	ldlm_cancel_lru_policy_t pf;
 	struct ldlm_lock *lock, *next;
 	int added = 0, unused, remained;
+	int no_wait = flags & (LDLM_CANCEL_NO_WAIT | LDLM_CANCEL_LRUR_NO_WAIT);
 	ENTRY;
 
 	spin_lock(&ns->ns_lock);
@@ -1671,8 +1688,7 @@ static int ldlm_prepare_lru_list(struct ldlm_namespace *ns,
                         /* No locks which got blocking requests. */
 			LASSERT(!ldlm_is_bl_ast(lock));
 
-			if (flags & LDLM_CANCEL_NO_WAIT &&
-			    ldlm_is_skipped(lock))
+			if (no_wait && ldlm_is_skipped(lock))
 				/* already processed */
 				continue;
 
