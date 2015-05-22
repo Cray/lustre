a076095998503ad58b436a3ab85037ed906bbfcc
Ulka Vaze <ulka.vaze@yahoo.in>
2015-05-22 19:14:28 +0530

    LU-6523 lmv: Error not handled for lmv_find_target
    This issue is found by smatch; has been reported as-
    Unchecked usage of potential ERR_PTR result in lmv_hsm_req_count
    and lmv_hsm_req_build.
    Added ERR_PTR in both functions and also return value check added.
    
    Signed-off-by: Ulka Vaze <ulka.vaze@yahoo.in>
    Signed-off-by: Aditya Pandit <panditadityashreesh@yahoo.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Change-Id: Icca9231f72077398e9ded4067f6b664549349ed6

--- lustre/lmv/lmv_obd.c
+++ lustre/lmv/lmv_obd.c
@@ -786,13 +786,15 @@ static int lmv_hsm_req_count(struct lmv_obd *lmv,
 	/* count how many requests must be sent to the given target */
 	for (i = 0; i < hur->hur_request.hr_itemcount; i++) {
 		curr_tgt = lmv_find_target(lmv, &hur->hur_user_item[i].hui_fid);
+		if (IS_ERR(curr_tgt))
+			return PTR_ERR(curr_tgt);
 		if (obd_uuid_equals(&curr_tgt->ltd_uuid, &tgt_mds->ltd_uuid))
 			nr++;
 	}
 	return nr;
 }
 
-static void lmv_hsm_req_build(struct lmv_obd *lmv,
+static int lmv_hsm_req_build(struct lmv_obd *lmv,
 			      struct hsm_user_request *hur_in,
 			      const struct lmv_tgt_desc *tgt_mds,
 			      struct hsm_user_request *hur_out)
@@ -806,6 +808,8 @@ static void lmv_hsm_req_build(struct lmv_obd *lmv,
 	for (i = 0; i < hur_in->hur_request.hr_itemcount; i++) {
 		curr_tgt = lmv_find_target(lmv,
 					   &hur_in->hur_user_item[i].hui_fid);
+		if (IS_ERR(curr_tgt))
+			return PTR_ERR(curr_tgt);
 		if (obd_uuid_equals(&curr_tgt->ltd_uuid, &tgt_mds->ltd_uuid)) {
 			hur_out->hur_user_item[nr_out] =
 						hur_in->hur_user_item[i];
@@ -815,6 +819,8 @@ static void lmv_hsm_req_build(struct lmv_obd *lmv,
 	hur_out->hur_request.hr_itemcount = nr_out;
 	memcpy(hur_data(hur_out), hur_data(hur_in),
 	       hur_in->hur_request.hr_data_len);
+
+	return 0;
 }
 
 static int lmv_hsm_ct_unregister(struct lmv_obd *lmv, unsigned int cmd, int len,
@@ -1102,8 +1108,8 @@ static int lmv_iocontrol(unsigned int cmd, struct obd_export *exp,
 		} else {
 			/* split fid list to their respective MDS */
 			for (i = 0; i < count; i++) {
-				unsigned int		nr, reqlen;
-				int			rc1;
+				int nr, rc1;
+				size_t reqlen;
 				struct hsm_user_request *req;
 
 				tgt = lmv->tgts[i];
@@ -1111,6 +1117,8 @@ static int lmv_iocontrol(unsigned int cmd, struct obd_export *exp,
 					continue;
 
 				nr = lmv_hsm_req_count(lmv, hur, tgt);
+				if (nr < 0)
+					RETURN(nr);
 				if (nr == 0) /* nothing for this MDS */
 					continue;
 
@@ -1121,11 +1129,12 @@ static int lmv_iocontrol(unsigned int cmd, struct obd_export *exp,
 				OBD_ALLOC_LARGE(req, reqlen);
 				if (req == NULL)
 					RETURN(-ENOMEM);
-
-				lmv_hsm_req_build(lmv, hur, tgt, req);
-
+				rc1 = lmv_hsm_req_build(lmv, hur, tgt, req);
+				if (rc1 < 0)
+					GOTO(hsm_req_err, rc1);
 				rc1 = obd_iocontrol(cmd, tgt->ltd_exp, reqlen,
 						    req, uarg);
+hsm_req_err:
 				if (rc1 != 0 && rc == 0)
 					rc = rc1;
 				OBD_FREE_LARGE(req, reqlen);
