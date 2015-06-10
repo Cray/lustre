07908c59c74b1be757cbdf63c77faf1f17ecac05
Jian Yu <jian.yu@intel.com>
2015-06-10 10:15:24 -0700

    LDEV-94 tests: add ignored error ticket into sanity test 237
    While using error_ignore(), the first parameter should be an
    ignored error ticket number. This patch adds this into sanity
    test 237 to fix the following error:
    
    Error: 'error() without useful message, please fix'
    
    The patch also renames sanity test 237 to test 188.
    
    Test-Parameters: alwaysuploadlogs envdefinitions=ONLY=188 \
    testlist=sanity,sanity,sanity,sanity
    
    Change-Id: I4c1add0c9d5967b0a6d3f5decf7e678693cb5014
    Signed-off-by: Jian Yu <jian.yu@intel.com>
    Reviewed-on: http://review.whamcloud.com/15199
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -10621,6 +10621,63 @@ test_187b() {
 }
 run_test 187b "Test data version change on volatile file"
 
+test_188() {	# LDEV-6
+	local my_error=error
+
+	# This test case is time sensitive and maloo uses kvm to run auto test.
+	# Therefore the complete time of I/O task is unreliable and depends on
+	# the work load on the host machine when the task is running.
+	which virt-what 2> /dev/null && [ "$(virt-what)" != "kvm" ] ||
+		{ echo "no virt-what installed or running in kvm; ignore error";
+		  my_error="error_ignore LDEV-6"; }
+
+	# create a large file for fast read verification
+	dd if=/dev/zero of=$DIR/$tfile bs=128M count=1 > /dev/null 2>&1
+
+	# make sure the file is created correctly
+	$CHECKSTAT -s $((128*1024*1024)) $DIR/$tfile ||
+		{ rm -f $DIR/$tfile; skip "file creation error" && return; }
+
+	local saved_fast_read=$($LCTL get_param -n llite.*.fast_read)
+
+	echo "Test 1: verify that fast read is 4 times faster on cache read"
+
+	# small read with fast read enabled
+	$LCTL set_param -n llite.*.fast_read=1
+	local s1=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=4k 2>&1 |
+		   awk '/real/ { print $2 }')
+
+	# small read with fast read disabled
+	$LCTL set_param -n llite.*.fast_read=0
+	local s2=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=4k 2>&1 |
+		   awk '/real/ { print $2 }')
+
+	# verify that fast read is 4 times faster for cache read
+	[ $(bc <<< "4 * $s1 < $s2") -eq 1 ] ||
+		eval $my_error "fast read was not 4 times faster: $s1 vs $s2"
+
+	echo "Test 2: verify the performance between big and small read"
+	$LCTL set_param -n llite.*.fast_read=1
+
+	# 1k non-cache read
+	cancel_lru_locks osc
+	s1=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=1k 2>&1 |
+	     awk '/real/ { print $2 }')
+
+	# 1M non-cache read
+	cancel_lru_locks osc
+	s1=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=1M 2>&1 |
+	     awk '/real/ { print $2 }')
+
+	# verify that big IO is not 4 times faster than small IO
+	[ $(bc <<< "4 * $s2 >= $s1") -eq 1 ] ||
+		eval $my_error "bigger IO is way too fast: $s1 vs $s2"
+
+	$LCTL set_param -n llite.*.fast_read=$saved_fast_read
+	rm -f $DIR/$tfile
+}
+run_test 188 "fast read verification"
+
 # OST pools tests
 check_file_in_pool()
 {
@@ -12219,63 +12276,6 @@ test_236() {
 	rm -rf $DIR/$tdir
 }
 run_test 236 "Layout swap on open unlinked file"
-
-test_237() {
-	local my_error=error
-
-	# This test case is time sensitive and maloo uses kvm to run auto test.
-	# Therefore the complete time of I/O task is unreliable and depends on
-	# the work load on the host machine when the task is running.
-	which virt-what 2> /dev/null && [ "$(virt-what)" != "kvm" ] ||
-		{ echo "no virt-what installed or running in kvm; ignore error";
-		  my_error=error_ignore; }
-
-	# create a large file for fast read verification
-	dd if=/dev/zero of=$DIR/$tfile bs=128M count=1 > /dev/null 2>&1
-
-	# make sure the file is created correctly
-	$CHECKSTAT -s $((128*1024*1024)) $DIR/$tfile ||
-		{ rm -f $DIR/$tfile; skip "file creation error" && return; }
-
-	local saved_fast_read=$($LCTL get_param -n llite.*.fast_read)
-
-	echo "Test 1: verify that fast read is 4 times faster on cache read"
-
-	# small read with fast read enabled
-	$LCTL set_param -n llite.*.fast_read=1
-	local s1=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=4k 2>&1 |
-		   awk '/real/ { print $2 }')
-
-	# small read with fast read disabled
-	$LCTL set_param -n llite.*.fast_read=0
-	local s2=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=4k 2>&1 |
-		   awk '/real/ { print $2 }')
-
-	# verify that fast read is 4 times faster for cache read
-	[ $(bc <<< "4 * $s1 < $s2") -eq 1 ] ||
-		$my_error "fast read was not 4 times faster: $s1 vs $s2"
-
-	echo "Test 2: verify the performance between big and small read"
-	$LCTL set_param -n llite.*.fast_read=1
-
-	# 1k non-cache read
-	cancel_lru_locks osc
-	s1=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=1k 2>&1 |
-	     awk '/real/ { print $2 }')
-
-	# 1M non-cache read
-	cancel_lru_locks osc
-	s1=$(eval time -p dd if=$DIR/$tfile of=/dev/null bs=1M 2>&1 |
-	     awk '/real/ { print $2 }')
-
-	# verify that big IO is not 4 times faster than small IO
-	[ $(bc <<< "4 * $s2 >= $s1") -eq 1 ] ||
-		$my_error "bigger IO is way too fast: $s1 vs $s2"
-
-	$LCTL set_param -n llite.*.fast_read=$saved_fast_read
-	rm -f $DIR/$tfile
-}
-run_test 237 "fast read verification"
 
 # LU-4659 linkea consistency
 test_238() {
