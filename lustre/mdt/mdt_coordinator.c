3261f9a93bf584b0ed300cfb69d91b31ef3ba375
John L. Hammond <john.hammond@intel.com>
2015-06-17 15:46:09 -0700

    LDEV-87 hsm: convert cdt_llog_lock to a rw semaphore
    Convert struct coordinator:cdt_llog_lock from a mutex to a rw
    semaphore. Add a flag to cdt_llog_process() to indicate whether a read
    or write lock is needed.
    
    This patch is back-ported from the following one:
    Lustre-commit: 0a7fc3e64cad646d027f32cc76aa55a7ee4a889d
    Lustre-change: http://review.whamcloud.com/15035
    
    Change-Id: Ie5c14d8eccda22731ad0ad9594bee7db99fe9cfd
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Signed-off-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-on: http://review.whamcloud.com/18354
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Faccini Bruno <bruno.faccini@intel.com>

--- lustre/mdt/mdt_coordinator.c
+++ lustre/mdt/mdt_coordinator.c
@@ -498,8 +498,8 @@ static int mdt_coordinator(void *data)
 
 		hsd.request_cnt = 0;
 
-		rc = cdt_llog_process(mti->mti_env, mdt,
-				      mdt_coordinator_cb, &hsd);
+		rc = cdt_llog_process(mti->mti_env, mdt, mdt_coordinator_cb,
+				      &hsd, WRITE);
 		if (rc < 0)
 			goto clean_cb_alloc;
 
@@ -745,7 +745,7 @@ static int mdt_hsm_pending_restore(struct mdt_thread_info *mti)
 	hrd.hrd_mti = mti;
 
 	rc = cdt_llog_process(mti->mti_env, mti->mti_mdt,
-			      hsm_restore_cb, &hrd);
+			      hsm_restore_cb, &hrd, READ);
 
 	RETURN(rc);
 }
@@ -810,7 +810,7 @@ int mdt_hsm_cdt_init(struct mdt_device *mdt)
 	cdt->cdt_state = CDT_STOPPED;
 
 	init_waitqueue_head(&cdt->cdt_thread.t_ctl_waitq);
-	mutex_init(&cdt->cdt_llog_lock);
+	init_rwsem(&cdt->cdt_llog_lock);
 	init_rwsem(&cdt->cdt_agent_lock);
 	init_rwsem(&cdt->cdt_request_lock);
 	mutex_init(&cdt->cdt_restore_lock);
@@ -1729,7 +1729,7 @@ int hsm_cancel_all_actions(struct mdt_device *mdt,
 	hcad.mdt = mdt;
 
 	rc = cdt_llog_process(mti->mti_env, mti->mti_mdt,
-			      mdt_cancel_all_cb, &hcad);
+			      mdt_cancel_all_cb, &hcad, WRITE);
 out:
 	/* Put down the obj ref count, normally incase
 	 * of evicted client and for restore operation */
