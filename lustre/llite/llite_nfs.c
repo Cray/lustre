5210887f8a0bfc064a83a9462fadd8ea8e13a593
Andrew Perepechko <andrew.perepechko@seagate.com>
2015-06-25 15:07:58 +0300

    LU-6794 nfs: ASSERTION( atomic_read(&d->ld_ref) == 0 ) failed
    ll_iget_for_nfs() can call unbalanced iput() causing memory
    leaks. This patch removes this iput() call.
    
    Also, avoid unhashing disconnected dentries in
    d_lustre_invalidate(), which is another source of memory
    leaks.
    
    One of the symptoms of the leak is the following crash pattern:
    <0>LustreError: 14812:0:(lu_object.c:1251:lu_device_fini())
    ASSERTION( atomic_read(&d->ld_ref) == 0 ) failed: Refcount is 1
    <0>LustreError: 14812:0:(lu_object.c:1251:lu_device_fini()) LBUG
    <4>Pid: 14812, comm: umount
    <4>
    <4>Call Trace:
    <4> [<ffffffffa11bc895>] libcfs_debug_dumpstack+0x55/0x80 [libcfs]
    <4> [<ffffffffa11bce97>] lbug_with_loc+0x47/0xb0 [libcfs]
    <4> [<ffffffffa1458a48>] lu_device_fini+0xb8/0xc0 [obdclass]
    <4> [<ffffffffa08e9ab2>] lovsub_device_free+0x52/0x220 [lov]
    <4> [<ffffffffa145c64e>] lu_stack_fini+0x7e/0xc0 [obdclass]
    <4> [<ffffffffa146356e>] cl_stack_fini+0xe/0x10 [obdclass]
    <4> [<ffffffffa08bc1a8>] lov_device_fini+0x58/0x120 [lov]
    <4> [<ffffffffa145c619>] lu_stack_fini+0x49/0xc0 [obdclass]
    <4> [<ffffffffa146356e>] cl_stack_fini+0xe/0x10 [obdclass]
    <4> [<ffffffffa0e1279d>] cl_sb_fini+0x6d/0x190 [lustre]
    <4> [<ffffffffa0dd34bc>] ll_put_super+0x1bc/0x11e0 [lustre]
    <4> [<ffffffff811cd0f2>] ? fsnotify_clear_marks_by_inode+0x32/0xf0
    <4> [<ffffffff811a59df>] ? destroy_inode+0x2f/0x60
    <4> [<ffffffff811a5eac>] ? dispose_list+0xfc/0x120
    <4> [<ffffffff811a62a6>] ? invalidate_inodes+0xf6/0x190
    <4> [<ffffffff8118b35b>] generic_shutdown_super+0x5b/0xe0
    <4> [<ffffffff8118b446>] kill_anon_super+0x16/0x60
    <4> [<ffffffffa144e7ba>] lustre_kill_super+0x4a/0x60 [obdclass]
    <4> [<ffffffff8118bbe7>] deactivate_super+0x57/0x80
    <4> [<ffffffff811aabef>] mntput_no_expire+0xbf/0x110
    <4> [<ffffffff811ab73b>] sys_umount+0x7b/0x3a0
    <4> [<ffffffff8100b072>] system_call_fastpath+0x16/0x1b
    
    Lustre-commit: a37a5a9e5e309f87db9497d187df41d0fede8d29
    Lustre-change: http://review.whamcloud.com/15480
    
    Xyratex-bug-id: MRP-2414
    Signed-off-by: Andrew Perepechko <andrew.perepechko@seagate.com>
    Signed-off-by: Bob Glossman <bob.glossman@intel.com>
    Change-Id: Ie8bc428014f3897dc108568f55db9a9ed93c932b
    Reviewed-by: Lai Siyao <lai.siyao@intel.com>
    Reviewed-by: Bobi Jam <bobijam@hotmail.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Reviewed-on: http://review.whamcloud.com/16055
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>

--- lustre/llite/llite_nfs.c
+++ lustre/llite/llite_nfs.c
@@ -153,13 +153,10 @@ ll_iget_for_nfs(struct super_block *sb, struct lu_fid *fid, struct lu_fid *paren
 		spin_unlock(&lli->lli_lock);
 	}
 
+	/* N.B. d_obtain_alias() drops inode ref on error */
 	result = d_obtain_alias(inode);
-	if (IS_ERR(result)) {
-		iput(inode);
-		RETURN(result);
-	}
 
-        RETURN(result);
+	RETURN(result);
 }
 
 #ifndef FILEID_INVALID
