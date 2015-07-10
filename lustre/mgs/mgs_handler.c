c91405a078e0daa2949e017919379c71aefba93b
Mikhail Pershin <mike.pershin@intel.com>
2015-07-10 00:13:21 -0700

    LU-4772 mgs: free MGS fsdb before export barrier
    The mgs_fsc keeps export reference and should be cleaned up
    before the waiting for exports cleanup. Patch moves FSDB cleanup
    functions which cleanups also mgs_fsc ahead of obd_export_barried()
    
    This patch is back-ported from the following one:
    Lustre-commit: 319acc84feeb66d08c7db408ad732889b46765e0
    Lustre-change: http://review.whamcloud.com/14443
    
    Signed-off-by: Mikhail Pershin <mike.pershin@intel.com>
    Change-Id: I2913ec2bf8492b12c444ca2e11048adc6b9b38ef
    Reviewed-on: http://review.whamcloud.com/15553
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/mgs/mgs_handler.c
+++ lustre/mgs/mgs_handler.c
@@ -1384,12 +1384,13 @@ static struct lu_device *mgs_device_fini(const struct lu_env *env,
 	ptlrpc_unregister_service(mgs->mgs_service);
 	mutex_unlock(&mgs->mgs_health_mutex);
 
+	mgs_params_fsdb_cleanup(env, mgs);
+	mgs_cleanup_fsdb_list(mgs);
+
 	obd_exports_barrier(obd);
 	obd_zombie_barrier();
 
 	tgt_fini(env, &mgs->mgs_lut);
-	mgs_params_fsdb_cleanup(env, mgs);
-	mgs_cleanup_fsdb_list(mgs);
 	lproc_mgs_cleanup(mgs);
 
 	ctxt = llog_get_context(mgs->mgs_obd, LLOG_CONFIG_ORIG_CTXT);
