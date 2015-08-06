8447cd38bae8f13f9e849e262474f3439c3c2c1d
John L. Hammond <john.hammond@intel.com>
2015-08-06 10:20:30 -0500

    LDEV-137 hsm: use a new environment during HSM purge
    In hsm_cancel_all_actions() use a newly allocated environment instead
    of borrowing it from the coordinator.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Change-Id: Idd5f05d1d5629f4e4e627e137e3addc1a51f8a96
    Reviewed-on: http://review.whamcloud.com/18029
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>

--- lustre/mdt/mdt_coordinator.c
+++ lustre/mdt/mdt_coordinator.c
@@ -1584,6 +1584,8 @@ static int mdt_cancel_all_cb(const struct lu_env *env,
 int hsm_cancel_all_actions(struct mdt_device *mdt,
 	const struct obd_uuid *uuid, int cl_evicted, int agent_unregistered)
 {
+	struct lu_env			 env;
+	struct lu_context		 session;
 	struct mdt_thread_info		*mti;
 	struct coordinator		*cdt = &mdt->mdt_coordinator;
 	struct cdt_agent_req		*car;
@@ -1597,8 +1599,25 @@ int hsm_cancel_all_actions(struct mdt_device *mdt,
 
 	ENTRY;
 
-	/* retrieve coordinator context */
-	mti = lu_context_key_get(&cdt->cdt_env.le_ctx, &mdt_thread_key);
+	rc = lu_env_init(&env, LCT_MD_THREAD);
+	if (rc < 0)
+		RETURN(rc);
+
+	/* for mdt_ucred(), lu_ucred stored in lu_ucred_key */
+	rc = lu_context_init(&session, LCT_SERVER_SESSION);
+	if (rc < 0)
+		GOTO(out_env, rc);
+
+	lu_context_enter(&session);
+	env.le_ses = &session;
+
+	mti = lu_context_key_get(&env.le_ctx, &mdt_thread_key);
+	LASSERT(mti != NULL);
+
+	mti->mti_env = &env;
+	mti->mti_mdt = mdt;
+
+	hsm_init_ucred(mdt_ucred(mti));
 
 	/* disable coordinator */
 	save_state = cdt->cdt_state;
@@ -1638,7 +1657,7 @@ int hsm_cancel_all_actions(struct mdt_device *mdt,
 			if (hal == NULL) {
 				mdt_cdt_put_request(car);
 				up_read(&cdt->cdt_request_lock);
-				GOTO(out, rc = -ENOMEM);
+				GOTO(out_cdt_state, rc = -ENOMEM);
 			}
 		}
 
@@ -1666,7 +1685,7 @@ int hsm_cancel_all_actions(struct mdt_device *mdt,
 			obj = mdt_hsm_get_md_hsm(mti, &car->car_hai->hai_fid, &mh);
 			if (IS_ERR(obj))
 				/* object removed */
-				goto out;
+				goto out_cdt_state;
 
 			mutex_lock(&cdt->cdt_restore_lock);
 			crh = hsm_restore_hdl_find(cdt, &car->car_hai->hai_fid);
@@ -1727,7 +1746,7 @@ int hsm_cancel_all_actions(struct mdt_device *mdt,
 
 	rc = cdt_llog_process(mti->mti_env, mti->mti_mdt,
 			      mdt_cancel_all_cb, &hcad, WRITE);
-out:
+out_cdt_state:
 	/* Put down the obj ref count, normally incase
 	 * of evicted client and for restore operation */
 	if (obj != NULL && !IS_ERR(obj))
@@ -1735,6 +1754,10 @@ out:
 
 	/* enable coordinator */
 	cdt->cdt_state = save_state;
+	lu_context_exit(&session);
+	lu_context_fini(&session);
+out_env:
+	lu_env_fini(&env);
 
 	RETURN(rc);
 }
