91d18aca7a086a312422d85adf3a0e113ada5867
Emoly Liu <emoly.liu@intel.com>
2015-08-31 03:33:37 +0800

    LU-5580 ptlrpc: policy switch directly in tbf
    Former version can't change nid to jobid(or reversely) directly in
    tbf, it need to change to FIFO(or any other policy) first. This patch
    fix this problem. It save the policy argument(like "nid" in tbf),
    and if the policy start with a new argument(like "jobid"), it will
    stop the former policy and start a new one.
    
    This is also adapt to any other nrs policies if they have arguments.
    
    This patch is backported from the following one:
    Lustre-commit: e7ab554c1ca887e1a3fa9da5250b2debb4eee2d6
    Lustre-Change: http://review.whamcloud.com/11749
    
    Change-Id: I35e3aa377b1a727a8316d75ed4b0112517ce2c36
    Signed-off-by: Wu Libin <lwu@ddn.com>
    Signed-off-by: Emoly Liu <emoly.liu@intel.com>
    Reviewed-on: http://review.whamcloud.com/16156
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

--- lustre/include/lustre_net.h
+++ lustre/include/lustre_net.h
@@ -1106,6 +1106,7 @@ struct ptlrpc_nrs {
 };
 
 #define NRS_POL_NAME_MAX		16
+#define NRS_POL_ARG_MAX			16
 
 struct ptlrpc_nrs_pol_desc;
 
@@ -1275,6 +1276,10 @@ struct ptlrpc_nrs_pol_info {
 	 */
 	char				pi_name[NRS_POL_NAME_MAX];
 	/**
+	 * Policy argument
+	 */
+	char				pi_arg[NRS_POL_ARG_MAX];
+	/**
 	 * Current policy state
 	 */
 	enum ptlrpc_nrs_pol_state	pi_state;
@@ -1329,6 +1334,10 @@ struct ptlrpc_nrs_policy {
 	 * Usage Reference count taken on the policy instance
 	 */
 	long				pol_ref;
+        /**
+	 * Human-readable policy argument
+	 */
+	char				pol_arg[NRS_POL_ARG_MAX];
 	/**
 	 * The NRS head this policy has been created at
 	 */
