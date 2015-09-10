f37c0684a4e9cb1a0623d1b8e8b95852cf48a259
Alexander Zarochentsev <alexander.zarochentsev@seagate.com>
2015-09-10 09:26:43 +0300

    MRP-2939 libcfs: do not call empty lnet upcall
    An attempt to call lnet upcall in lbug_with_loc
    may cause lbug thread to hang even if lnet upcall
    is not set or set to non-existing path.
    The patch allows to skip lnet upcall if it is set
    to "NONE" keyword.
    
    Change-Id: I529adc1d2c3c19745d60a7846c4248307c527af0
    Signed-off-by: Alexander Zarochentsev <alexander.zarochentsev@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/8430
    Tested-by: Jenkins
    Reviewed-by: Artem Blagodarenko <artem.blagodarenko@seagate.com>
    Reviewed-by: Andrew Perepechko <andrew.perepechko@seagate.com>
    Tested-by: Alexander Lezhoev <alexander.lezhoev@seagate.com>

--- libcfs/libcfs/linux/linux-debug.c
+++ libcfs/libcfs/linux/linux-debug.c
@@ -68,7 +68,7 @@
 
 #include <linux/kallsyms.h>
 
-char lnet_upcall[1024] = "/usr/lib/lustre/lnet_upcall";
+char lnet_upcall[1024] = "NONE";
 char lnet_debug_log_upcall[1024] = "/usr/lib/lustre/lnet_debug_log_upcall";
 
 /**
@@ -116,6 +116,9 @@ void libcfs_run_upcall(char **argv)
                 NULL};
         ENTRY;
 
+	if (!strcmp(lnet_upcall, "NONE"))
+		RETURN_EXIT;
+
         argv[0] = lnet_upcall;
         argc = 1;
         while (argv[argc] != NULL)
@@ -140,6 +143,7 @@ void libcfs_run_upcall(char **argv)
                        argc < 5 ? "" : ",", argc < 5 ? "" : argv[4],
                        argc < 6 ? "" : ",...");
         }
+	EXIT;
 }
 
 void libcfs_run_lbug_upcall(struct libcfs_debug_msg_data *msgdata)
@@ -157,6 +161,7 @@ void libcfs_run_lbug_upcall(struct libcfs_debug_msg_data *msgdata)
         argv[5] = NULL;
 
         libcfs_run_upcall (argv);
+	EXIT;
 }
 
 /* coverity[+kill] */
