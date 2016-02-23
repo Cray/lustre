c540b8cb5b93a2d0483e96a8ff3d27b0b04cb81c
John L. Hammond <john.hammond@intel.com>
2016-02-23 11:26:00 -0600

    LDEV-355 hsm: cache agent record locations
    Create a cookie indexed hash of record locations for the agent actions
    log. Use this cache to avoid linear scans of the log in HSM RPC
    handlers that must update given entries of the log.
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Change-Id: I1b9bb0746e88fb0bed81fe1174b5d3c7abac1211
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-by: Faccini Bruno <bruno.faccini@intel.com>
    Reviewed-on: http://review.whamcloud.com/18579
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Robert Read <robert.read@intel.com>

--- lustre/mdt/mdt_coordinator.c
+++ lustre/mdt/mdt_coordinator.c
@@ -283,6 +283,11 @@ static int mdt_coordinator_cb(const struct lu_env *env,
 		hsd->request[found].hal_used_sz +=
 						   cfs_size_round(hai->hai_len);
 		hsd->request[found].hal->hal_count++;
+
+		if (hai->hai_action != HSMA_CANCEL)
+			cdt_agent_record_hash_add(cdt, hai->hai_cookie,
+						  llh->lgh_hdr->llh_cat_idx,
+						  hdr->lrh_index);
 		break;
 	}
 	case ARS_STARTED: {
@@ -338,6 +343,8 @@ static int mdt_coordinator_cb(const struct lu_env *env,
 			 * to the client, for which an error will be
 			 * sent back, leading to an endless cycle of
 			 * cancellation. */
+			cdt_agent_record_hash_del(cdt,
+						  larr->arr_hai.hai_cookie);
 			RETURN(LLOG_DEL_RECORD);
 		}
 
@@ -358,8 +365,11 @@ static int mdt_coordinator_cb(const struct lu_env *env,
 	case ARS_CANCELED:
 	case ARS_SUCCEED:
 		if ((larr->arr_req_change + cdt->cdt_grace_delay) <
-		    cfs_time_current_sec())
+		    cfs_time_current_sec()) {
+			cdt_agent_record_hash_del(cdt,
+						  larr->arr_hai.hai_cookie);
 			RETURN(LLOG_DEL_RECORD);
+		}
 		break;
 	}
 	RETURN(0);
@@ -499,7 +509,7 @@ static int mdt_coordinator(void *data)
 		hsd.request_cnt = 0;
 
 		rc = cdt_llog_process(mti->mti_env, mdt, mdt_coordinator_cb,
-				      &hsd, WRITE);
+				      &hsd, 0, 0, WRITE);
 		if (rc < 0)
 			goto clean_cb_alloc;
 
@@ -754,8 +764,8 @@ static int mdt_hsm_pending_restore(struct mdt_thread_info *mti)
 
 	hrd.hrd_mti = mti;
 
-	rc = cdt_llog_process(mti->mti_env, mti->mti_mdt,
-			      hsm_restore_cb, &hrd, READ);
+	rc = cdt_llog_process(mti->mti_env, mti->mti_mdt, hsm_restore_cb, &hrd,
+			      0, 0, READ);
 
 	RETURN(rc);
 }
@@ -841,9 +851,21 @@ int mdt_hsm_cdt_init(struct mdt_device *mdt)
 	if (cdt->cdt_request_cookie_hash == NULL)
 		RETURN(-ENOMEM);
 
+	cdt->cdt_agent_record_hash = cfs_hash_create("AGENT_RECORD_HASH",
+						     CFS_HASH_BITS_MIN,
+						     CFS_HASH_BITS_MAX,
+						     CFS_HASH_BKT_BITS,
+						     0 /* extra bytes */,
+						     CFS_HASH_MIN_THETA,
+						     CFS_HASH_MAX_THETA,
+						     &cdt_agent_record_hash_ops,
+						     CFS_HASH_DEFAULT);
+	if (cdt->cdt_agent_record_hash == NULL)
+		GOTO(out_request_cookie_hash, rc = -ENOMEM);
+
 	rc = lu_env_init(&cdt->cdt_env, LCT_MD_THREAD);
 	if (rc < 0)
-		GOTO(out_request_cookie_hash, rc);
+		GOTO(out_agent_record_hash, rc);
 
 	/* for mdt_ucred(), lu_ucred stored in lu_ucred_key */
 	rc = lu_context_init(&cdt->cdt_session, LCT_SERVER_SESSION);
@@ -874,6 +896,9 @@ int mdt_hsm_cdt_init(struct mdt_device *mdt)
 
 out_env:
 	lu_env_fini(&cdt->cdt_env);
+out_agent_record_hash:
+	cfs_hash_putref(cdt->cdt_agent_record_hash);
+	cdt->cdt_agent_record_hash = NULL;
 out_request_cookie_hash:
 	cfs_hash_putref(cdt->cdt_request_cookie_hash);
 	cdt->cdt_request_cookie_hash = NULL;
@@ -894,6 +919,9 @@ int  mdt_hsm_cdt_fini(struct mdt_device *mdt)
 	lu_context_fini(cdt->cdt_env.le_ses);
 
 	lu_env_fini(&cdt->cdt_env);
+
+	cfs_hash_putref(cdt->cdt_agent_record_hash);
+	cdt->cdt_agent_record_hash = NULL;
 
 	cfs_hash_putref(cdt->cdt_request_cookie_hash);
 	cdt->cdt_request_cookie_hash = NULL;
@@ -1686,8 +1714,8 @@ static int hsm_cancel_all_actions(struct mdt_device *mdt)
 	/* cancel all on-disk records */
 	hcad.mdt = mdt;
 
-	rc = cdt_llog_process(mti->mti_env, mti->mti_mdt,
-			      mdt_cancel_all_cb, &hcad, WRITE);
+	rc = cdt_llog_process(mti->mti_env, mti->mti_mdt, mdt_cancel_all_cb,
+			      &hcad, 0, 0, WRITE);
 out_cdt_state:
 	/* enable coordinator */
 	cdt->cdt_state = save_state;
