bcae543f04c963368cb043c21bdf218567d7411f
Bruno Faccini <bruno.faccini@intel.com>
2016-02-26 17:59:30 +0100

    LU-7975 lod: fix delayed stripe error path & Client resend
    This patch is a follow-on to LU-4260/LU-4971/LU-5346 to fix
    cleanup in error path during layout/stripe creation.
    It is intended to fix special case when layout creation has been
    delayed and will be with a non-0 size (ie truncate).
    
    Problem has been unveiled due to the current limitation where a
    delayed layout creation with a non-0 size/truncate will not be
    resent if connection with OST is down, but is rather returning
    -EAGAIN/-EWOULDBLOCK upon next open, and finally MDS will crash for
    "(lod_object.c:700:lod_ah_init()) ASSERTION( lc->ldo_stripenr == 0 )"
    LBUG.
    
    Thus, patch also fixes this limitation by instead returning
    -EINPROGRESS,this to avoid using costly resend operation from MDS
    (will keep a ptlrpc thread busy) but offload this responsibility to
    Client side (in after_reply()) which will handle this process in a
    much less agressive way.
    
    Specific sanity/test_27D sub-test has been created to test both
    layout creation error path correct cleanup and Client resend.
    
    This b_ieel2_0 backport of master patch:
    Lustre-commit: bb8a376656f6b3626a47c32cbc1855045a47d929
    Lustre-change: http://review.whamcloud.com/19302/
    
    Signed-off-by: Bruno Faccini <bruno.faccini@intel.com>
    Change-Id: Ibc1734f52c071c66cb5974e3fe21d8819d725c2a
    Reviewed-on: http://review.whamcloud.com/18689
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Johann Lombardi <johann.lombardi@intel.com>
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -1925,6 +1925,46 @@ test_27C() { #LU-2871
 }
 run_test 27C "check full striping across all OSTs"
 
+test_27D() { # LU-5346/LU-7975
+
+	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return
+
+	[[ $(lustre_version_code $SINGLEMDS) -lt $(version_code 2.5.42) ]] &&
+		skip "Need MDS version at least 2.5.42" && return
+
+	test_mkdir -p $DIR/$tdir
+	rm -f $DIR/$tdir/f0
+	$SETSTRIPE -c 2 $DIR/$tdir
+
+	# stop all OSTs to reproduce situation for LU-7975 ticket
+	for num in $(seq $OSTCOUNT); do
+		stop ost$num
+	done
+
+	# open/create f0 with O_LOV_DELAY_CREATE
+	# truncate f0 to a non-0 size
+	# close
+	multiop $DIR/$tdir/f0 oO_RDWR:O_CREAT:O_LOV_DELAY_CREATE:T1050000c
+
+	$CHECKSTAT -s 1050000 $DIR/$tdir/f0 || error "checkstat failed"
+	# open/write it again to force delayed layout creation
+	cat /etc/hosts > $DIR/$tdir/f0 &
+	catpid=$!
+
+	# restart OSTs
+	for num in $(seq $OSTCOUNT); do
+		start ost$num $(ostdevname $num) $OST_MOUNT_OPTS ||
+			error "ost$num failed to start"
+	done
+
+	wait $catpid || error "cat failed"
+
+	cmp /etc/hosts $DIR/$tdir/f0 || error "cmp failed"
+	[[ $($GETSTRIPE -c $DIR/$tdir/f0) == 2 ]] || error "wrong stripecount"
+
+}
+run_test 27D "Client resend delayed layout creation with non-zero size"
+
 # createtest also checks that device nodes are created and
 # then visible correctly (#2091)
 test_28() { # bug 2091
