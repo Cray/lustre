0030dbb456692a4ac8d21c63c0586c2d838bd1a1
Andrew Perepechko <andrew.perepechko@seagate.com>
2016-04-20 00:34:58 +0300

    MRP-2634: ASSERTION(osp->opd_connects == 1) failed
    writeconf on an MDT with index > 0000 will cause
    "add mdc" to be added to $FSNAME-client config
    and "add osp" to be added to $FSNAME-MDTXXXX configs.
    
    However, the configs may already contain these
    directives. Duplicating the OSP device will
    cause the assertion failure, unlike duplicating
    the MDC which will just return -EEXIST.
    
    A possible solution is to check configs for duplicates
    before writing to them. However, sometimes we
    would like to change nids which are part of
    "add mdc" and "add osp".
    
    Another solution is to mark previous entries with
    SKIP flags. This patch implements this approach.
    Since after revoking the config lock, the clients
    and the MDTs will receive the updated log and
    apply its newer entries, we still have to handle
    OSP duplication, but this is only an issue
    immediately after writeconf processing.
    
    Change-Id: I45fac2502ae1d9d24f9e21813176d49e40d4255a
    Signed-off-by: Andrew Perepechko <andrew.perepechko@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/6531
    Tested-by: Jenkins
    Reviewed-by: Artem Blagodarenko <artem.blagodarenko@seagate.com>
    Reviewed-by: Alexey Leonidovich Lyashkov <alexey.lyashkov@seagate.com>
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -5535,6 +5535,42 @@ test_87() { # MRP-153
 }
 run_test 87 "check tunefs correctly handles parameter addition and removal"
 
+test_89() {
+	local had_config
+
+	[ "$MDSCOUNT" -lt 2 ] && { skip "mdt count < 2"; return 0; }
+
+	had_config=$(do_facet mds1 "$LCTL get_param debug|grep config")
+	do_facet mds1 "$LCTL set_param debug=+config"
+	do_facet mds1 "$LCTL dk > /dev/null"
+
+	setup
+	do_facet mds2 "$TUNEFS --writeconf $(mdsdevname 2)" > /dev/null 2>&1
+	# mount after writeconf will make "add osp" added to mdt0 config:
+	# 53 (224)marker  60 (flags=0x01, v2.5.1.0) lustre-MDT0001  'add osp'
+	# 54 (080)add_uuid  nid=...  0:  1:...
+	# 55 (144)attach    0:lustre-MDT0001-osp-MDT0000  1:osp  2:...
+	# 56 (144)setup     0:lustre-MDT0001-osp-MDT0000  1:...  2:...
+	# 57 (136)modify_mdc_tgts add 0:lustre-MDT0000-mdtlov  1:...  2:1  3:1
+	# duplicate modify_mds_tgts caused crashes
+	for i in `seq 1 3`; do
+		stop_mdt 2
+		# though config processing stops after failed attach and setup
+		# it will proceed after the failed command after each writeconf
+		# this is the original scenario of the issue
+		do_facet mds2 "$TUNEFS --writeconf $(mdsdevname 2)" > /dev/null 2>&1
+		start_mdt 2
+		while [ -z "$(do_facet mds1 $LCTL dk|grep Processed\ log\ $FSNAME-MDT0000)" ] ; do
+			sleep 1
+		done
+	done
+
+	[ -z "$had_config" ] && do_facet mds1 lctl set_param debug=-config
+
+	reformat
+}
+run_test 89 "writeconf on mdt>0 shouldn't duplicate mdc/osp and crash"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi
