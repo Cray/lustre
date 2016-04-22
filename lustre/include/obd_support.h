Xyratex/lustre-stable

MRP-1800 tests: Fix race, fail to start or shutdown vs re-enqueue lock.
Porting/adding the test (conf-sanity.sh: 27c) to check if MRP-1800 is
fixed. The test checks that there is no race between with fail to start
or shutdown vs reenqueue lock.

Signed-off-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
Signed-off-by: Rahul Deshmukh <rahul_deshmukh@xyratex.com>
Change-Id: I0a26ab1e8d608e6b5a603adf05b8aeed7c932f67
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/3320
Reviewed-by: Alexander Zarochentsev <alexander_zarochentsev@xyratex.com>
Tested-by: Jenkins
Tested-by: Alexander Lezhoev <alexander_lezhoev@xyratex.com>
Reviewed-by: Vitaly Fertman <vitaly_fertman@xyratex.com>

lustre/include/obd_support.h
@@ -446,6 +446,8 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_MGS_TARGET_REG_NET	 0x90a
 #define OBD_FAIL_MGS_TARGET_DEL_NET	 0x90b
 #define OBD_FAIL_MGS_CONFIG_READ_NET	 0x90c
+#define OBD_FAIL_MGC_FAIL_NET		 0x90d
+#define OBD_FAIL_MGC_FS_CLEANUP_RACE	 0x90e
 
 #define OBD_FAIL_QUOTA_DQACQ_NET			0xA01
 #define OBD_FAIL_QUOTA_EDQUOT            0xA02

Cray/lustre

lustre/include/obd_support.h
@@ -465,6 +465,8 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_MGS_TARGET_DEL_NET	 0x90b
 #define OBD_FAIL_MGS_CONFIG_READ_NET	 0x90c
 #define OBD_FAIL_MGS_LDLM_REPLY_NET	 0x90d
+#define OBD_FAIL_MGC_FAIL_NET		 0x90e
+#define OBD_FAIL_MGC_FS_CLEANUP_RACE	 0x90f
 
 #define OBD_FAIL_QUOTA_DQACQ_NET			0xA01
 #define OBD_FAIL_QUOTA_EDQUOT            0xA02