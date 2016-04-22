Xyratex/lustre-stable

MRP-2536 ldlm: ASSERTION(flock->blocking_export!=((void *)0)) failed
Hash lock protects only during .hs_put_locked.
Switch to atomic blocking_refs.

Change-Id: I4f0533d7838b7e25c5a0d7d36780643607a25bd6
Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/5613
Tested-by: Jenkins
Reviewed-by: Alexander Boyko <alexander.boyko@seagate.com>
Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@seagate.com>

lustre/include/lustre_dlm.h
@@ -664,8 +664,7 @@ struct ldlm_flock {
         __u64 owner;
         __u64 blocking_owner;
         struct obd_export *blocking_export;
-        /* Protected by the hash lock */
-        __u32 blocking_refs;
+        cfs_atomic_t blocking_refs;
         __u32 pid;
 };
 

lustre/ldlm/ldlm_flock.c
@@ -107,7 +107,7 @@ static inline void ldlm_flock_blocking_link(struct ldlm_lock *req,
                 lock->l_policy_data.l_flock.owner;
         req->l_policy_data.l_flock.blocking_export =
                 lock->l_export;
-        req->l_policy_data.l_flock.blocking_refs = 0;
+	cfs_atomic_set(&req->l_policy_data.l_flock.blocking_refs, 0);
 
         cfs_hash_add(req->l_export->exp_flock_hash,
                      &req->l_policy_data.l_flock.owner,
@@ -1040,7 +1040,7 @@ ldlm_export_flock_get(cfs_hash_t *hs, cfs_hlist_node_t *hnode)
         flock = &lock->l_policy_data.l_flock;
         LASSERT(flock->blocking_export != NULL);
         class_export_get(flock->blocking_export);
-        flock->blocking_refs++;
+	cfs_atomic_inc(&flock->blocking_refs);
 }
 
 static void
@@ -1050,15 +1050,16 @@ ldlm_export_flock_put(cfs_hash_t *hs, cfs_hlist_node_t *hnode)
         struct ldlm_flock *flock;
 
         lock = cfs_hlist_entry(hnode, struct ldlm_lock, l_exp_flock_hash);
-        LDLM_LOCK_RELEASE(lock);
 
         flock = &lock->l_policy_data.l_flock;
         LASSERT(flock->blocking_export != NULL);
+
         class_export_put(flock->blocking_export);
-        if (--flock->blocking_refs == 0) {
+	if (cfs_atomic_dec_and_test(&flock->blocking_refs)) {
                 flock->blocking_owner = 0;
                 flock->blocking_export = NULL;
         }
+	LDLM_LOCK_RELEASE(lock);
 }
 
 static cfs_hash_ops_t ldlm_export_flock_ops = {

Cray/lustre

lustre/include/lustre_dlm.h
@@ -622,8 +622,7 @@ struct ldlm_flock {
         __u64 owner;
         __u64 blocking_owner;
         struct obd_export *blocking_export;
-	/* Protected by the hash lock */
-	__u32 blocking_refs;
+        atomic_t blocking_refs;
         __u32 pid;
 };
 

lustre/ldlm/ldlm_flock.c
@@ -117,7 +117,7 @@ static inline void ldlm_flock_blocking_link(struct ldlm_lock *req,
                 lock->l_policy_data.l_flock.owner;
         req->l_policy_data.l_flock.blocking_export =
 		lock->l_export;
-	req->l_policy_data.l_flock.blocking_refs = 0;
+	atomic_set(&req->l_policy_data.l_flock.blocking_refs, 0);
 
 	cfs_hash_add(req->l_export->exp_flock_hash,
 		     &req->l_policy_data.l_flock.owner,
@@ -1094,7 +1094,7 @@ ldlm_export_flock_get(cfs_hash_t *hs, struct hlist_node *hnode)
 	flock = &lock->l_policy_data.l_flock;
 	LASSERT(flock->blocking_export != NULL);
 	class_export_get(flock->blocking_export);
-	flock->blocking_refs++;
+	atomic_inc(&flock->blocking_refs);
 }
 
 static void
@@ -1104,15 +1104,16 @@ ldlm_export_flock_put(cfs_hash_t *hs, struct hlist_node *hnode)
 	struct ldlm_flock *flock;
 
 	lock = hlist_entry(hnode, struct ldlm_lock, l_exp_flock_hash);
-	LDLM_LOCK_RELEASE(lock);
 
 	flock = &lock->l_policy_data.l_flock;
 	LASSERT(flock->blocking_export != NULL);
+
 	class_export_put(flock->blocking_export);
-	if (--flock->blocking_refs == 0) {
+	if (atomic_dec_and_test(&flock->blocking_refs)) {
 		flock->blocking_owner = 0;
 		flock->blocking_export = NULL;
 	}
+	LDLM_LOCK_RELEASE(lock);
 }
 
 static cfs_hash_ops_t ldlm_export_flock_ops = {