Xyratex/lustre-stable

MRP-3061 tests: make recovery-small,test_10d more accurate
Multiop utility is used in order to be able to
control failed blocking ast in a better way.
Client1 fails more accurately responding to
blocking ast from ost.

Signed-off-by: Vladimir Saveliev <vladimir_saveliev@xyratex.com>
Signed-off-by: Kshipra Namjoshi <kshipra.namjoshi@seagate.com>
Change-Id: I1ee9b70bb43c34beb0d3ca757f2e29fd3c33710b
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/9326
Tested-by: Jenkins
Reviewed-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
Reviewed-by: Ujjwal Lanjewar <ujjwal.lanjewar@seagate.com>
Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

lustre/tests/recovery-small.sh
@@ -246,16 +246,25 @@ test_10d() {
 	local evict
 	# sleep 1 is to make sure that BEFORE is not equal to EVICTED below
 	sleep 1
-	echo -n ", world" | dd of=$TMP/$tfile bs=1c seek=5
+	local rec1len=6
+	local rec2len=5
+	$MULTIOP $TMP/$tfile oO_CREAT:O_WRONLY:T0z${rec1len}w${rec2len}c
 
 	mount_client $MOUNT2
 
 	$LFS setstripe -i 0 -c 1 $DIR1/$tfile
-	echo -n hello > $DIR1/$tfile
 
-	$LCTL set_param fail_val=71
-	drop_bl_callback_once "echo -n \\\", world\\\" >> $DIR2/$tfile"
-	$LCTL set_param fail_val=0
+	# dd is to guarantee that first multiop will not submit data to
+	# server due to ar_force_sync flag set
+	dd if=/dev/zero of=$DIR1/$tfile bs=4k count=1
+	$MULTIOP $DIR1/$tfile oO_CREAT:O_WRONLY:T0w${rec1len}c
+	$MULTIOP $DIR2/$tfile oO_WRONLY:z${rec1len}_w${rec2len}c &
+	PID1=$!
+#define OBD_FAIL_LDLM_BL_CALLBACK_NET			0x305
+	do_facet client $LCTL set_param fail_loc=0x80000305
+	do_facet client $LCTL set_param fail_val=71
+	kill -USR1 $PID1
+	wait $PID1
 
 	client_reconnect
 

Cray/lustre

lustre/tests/recovery-small.sh
@@ -253,16 +253,25 @@ test_10d() {
 	# sleep 1 is to make sure that BEFORE is not equal to EVICTED below
 	sleep 1
 	rm -f $TMP/$tfile
-	echo -n ", world" | dd of=$TMP/$tfile bs=1c seek=5
+	local rec1len=6
+	local rec2len=5
+	$MULTIOP $TMP/$tfile oO_CREAT:O_WRONLY:T0z${rec1len}w${rec2len}c
 
 	mount_client $MOUNT2
 
 	$LFS setstripe -i 0 -c 1 $DIR1/$tfile
-	echo -n hello > $DIR1/$tfile
 
-	stat $DIR2/$tfile >& /dev/null
-	$LCTL set_param fail_err=71
-	drop_bl_callback_once "echo -n \\\", world\\\" >> $DIR2/$tfile"
+	# dd is to guarantee that first multiop will not submit data to
+	# server due to ar_force_sync flag set
+	dd if=/dev/zero of=$DIR1/$tfile bs=4k count=1
+	$MULTIOP $DIR1/$tfile oO_CREAT:O_WRONLY:T0w${rec1len}c
+	$MULTIOP $DIR2/$tfile oO_WRONLY:z${rec1len}_w${rec2len}c &
+	PID1=$!
+#define OBD_FAIL_LDLM_BL_CALLBACK_NET			0x305
+	do_facet client $LCTL set_param fail_loc=0x80000305
+	do_facet client $LCTL set_param fail_err=71
+	kill -USR1 $PID1
+	wait $PID1
 
 	client_reconnect
 