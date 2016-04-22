Xyratex/lustre-stable

MRP-2017 test: ASSERTION(args != ((void *)0)) failed
The lock hits granted vs blocked granted race on a client.

Change-Id: I8dc877023e2bc9702a6f28d96f217b354246ff15
Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/4353
Tested-by: Jenkins
Reviewed-by: Elena Gryaznova <elena.gryaznova@seagate.com>
Tested-by: Elena Gryaznova <elena.gryaznova@seagate.com>
Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

lustre/include/obd_support.h
@@ -499,6 +499,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_GLIMPSE_DELAY			    0x1404
 #define OBD_FAIL_LLITE_XATTR_ENOMEM		    0x1405
 #define OBD_FAIL_LLITE_FLOCK_UNLOCK_RACE            0x1406
+#define OBD_FAIL_LLITE_FLOCK_BL_GRANT_RACE	    0x1407
 
 #define OBD_FAIL_FID_INDIR	0x1501
 #define OBD_FAIL_FID_INLMA	0x1502

lustre/tests/flock-nfs.sh
@@ -170,6 +170,24 @@ test_4() {
 }
 run_test 4 "kernel flock unlock race"
 
+test_5() {
+	nfs_start || return 1
+	do_node $lustre_client \
+		"flocks_test 5 set write sleep 10 $LUSTRE_MNT/$LOCKF" &
+	sleep 1
+#define OBD_FAIL_LLITE_FLOCK_BL_GRANT_RACE     0x1407
+	do_node $lustre_client "lctl set_param fail_loc=0x1407"
+	do_node $NFS_CLIENT "flocks_test 5 set write sleep 5 $LUSTRE_MNT/$LOCKF"
+	wait
+	do_node $lustre_client "lctl set_param fail_loc=0"
+	nfs_stop
+	do_node $lustre_client "umount $LUSTRE_MNT" || error "umount failed"
+	stopall -f || error "cleanup failed"
+	check_and_setup_lustre
+}
+run_test 5 "flock grant blocked race"
+
+
 do_node $lustre_client "umount -f $LUSTRE_MNT || true"
 restore_mount $MOUNT
 

Cray/lustre

lustre/include/obd_support.h
@@ -514,6 +514,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_MAKE_LOVEA_HOLE		    0x1406
 #define OBD_FAIL_LLITE_LOST_LAYOUT		    0x1407
 #define OBD_FAIL_LLITE_FLOCK_UNLOCK_RACE            0x140a
+#define OBD_FAIL_LLITE_FLOCK_BL_GRANT_RACE	    0x140b
 
 #define OBD_FAIL_FID_INDIR	0x1501
 #define OBD_FAIL_FID_INLMA	0x1502

lustre/tests/flock-nfs.sh
@@ -170,6 +170,24 @@ test_4() {
 }
 run_test 4 "kernel flock unlock race"
 
+test_5() {
+	nfs_start || return 1
+	do_node $lustre_client \
+		"flocks_test 5 set write sleep 10 $LUSTRE_MNT/$LOCKF" &
+	sleep 1
+#define OBD_FAIL_LLITE_FLOCK_BL_GRANT_RACE     0x140b
+	do_node $lustre_client "lctl set_param fail_loc=0x140b"
+	do_node $NFS_CLIENT "flocks_test 5 set write sleep 5 $LUSTRE_MNT/$LOCKF"
+	wait
+	do_node $lustre_client "lctl set_param fail_loc=0"
+	nfs_stop
+	do_node $lustre_client "umount $LUSTRE_MNT" || error "umount failed"
+	stopall -f || error "cleanup failed"
+	check_and_setup_lustre
+}
+run_test 5 "flock grant blocked race"
+
+
 do_node $lustre_client "umount -f $LUSTRE_MNT || true"
 restore_mount $MOUNT
 