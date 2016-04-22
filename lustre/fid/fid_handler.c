Xyratex/lustre-stable

MRP-353 Resolve lock depend issues
This commit tryes to make Lusre compatible
with linux lockdep checks.

Don't disable lockdep during mount.
Set lockdep classes for cli->cl_sem, cfs_trace
to resolve warnings.

Change-Id: If92461649242c7077a0ee9f6a692443ebfa44f13
Signed-off-by: Andriy Skulysh <Andriy_Skulysh@xyratex.com>
Reviewed-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
Reviewed-by: Bruce Korb <bruce_korb@xyratex.com>
Reviewed-on: http://morpheus.xyus.xyratex.com:8443/83
Tested-by: Jenkins
Tested-by: Alexander Lezhoev <Alexander_Lezhoev@xyratex.com>
Reviewed-by: Vitaly Fertman <Vitaly_Fertman@xyratex.com>

lustre/fid/fid_handler.c
@@ -149,7 +149,7 @@ int seq_server_alloc_super(struct lu_server_seq *seq,
         int rc;
         ENTRY;
 
-        cfs_mutex_lock(&seq->lss_mutex);
+	cfs_mutex_lock_nested(&seq->lss_mutex, SEQ_ALLOC_SUPER);
         rc = __seq_server_alloc_super(seq, out, env);
         cfs_mutex_unlock(&seq->lss_mutex);
 
@@ -287,7 +287,7 @@ int seq_server_alloc_meta(struct lu_server_seq *seq,
         int rc;
         ENTRY;
 
-        cfs_mutex_lock(&seq->lss_mutex);
+	cfs_mutex_lock_nested(&seq->lss_mutex, SEQ_ALLOC_META);
         rc = __seq_server_alloc_meta(seq, out, env);
         cfs_mutex_unlock(&seq->lss_mutex);
 

lustre/ldlm/ldlm_resource.c
@@ -1008,7 +1008,7 @@ struct ldlm_namespace *ldlm_namespace_first_locked(ldlm_side_t client)
                 struct ldlm_namespace, ns_list_chain);
 }
 
-static struct ldlm_resource *ldlm_resource_new(void)
+static struct ldlm_resource *ldlm_resource_new(int lock_lvb)
 {
         struct ldlm_resource *res;
         int idx;
@@ -1034,8 +1034,10 @@ static struct ldlm_resource *ldlm_resource_new(void)
 
         /* one who creates the resource must unlock
          * the mutex after lvb initialization */
-        cfs_mutex_init(&res->lr_lvb_mutex);
-        cfs_mutex_lock(&res->lr_lvb_mutex);
+	if (lock_lvb) {
+		cfs_mutex_init(&res->lr_lvb_mutex);
+		cfs_mutex_lock(&res->lr_lvb_mutex);
+	}
 
         return res;
 }
@@ -1078,7 +1080,7 @@ ldlm_resource_get(struct ldlm_namespace *ns, struct ldlm_resource *parent,
 
         LASSERTF(type >= LDLM_MIN_TYPE && type < LDLM_MAX_TYPE,
                  "type: %d\n", type);
-        res = ldlm_resource_new();
+	res = ldlm_resource_new(ns->ns_lvbo && ns->ns_lvbo->lvbo_init);
         if (!res)
                 return NULL;
 

Cray/lustre

lustre/fid/fid_handler.c
@@ -143,7 +143,7 @@ int seq_server_alloc_super(struct lu_server_seq *seq,
         int rc;
         ENTRY;
 
-	mutex_lock(&seq->lss_mutex);
+	mutex_lock_nested(&seq->lss_mutex, SEQ_ALLOC_SUPER);
         rc = __seq_server_alloc_super(seq, out, env);
 	mutex_unlock(&seq->lss_mutex);
 
@@ -318,7 +318,7 @@ int seq_server_alloc_meta(struct lu_server_seq *seq,
         int rc;
         ENTRY;
 
-	mutex_lock(&seq->lss_mutex);
+	mutex_lock_nested(&seq->lss_mutex, SEQ_ALLOC_META);
         rc = __seq_server_alloc_meta(seq, out, env);
 	mutex_unlock(&seq->lss_mutex);
 

lustre/ldlm/ldlm_resource.c
@@ -1026,7 +1026,7 @@ struct ldlm_namespace *ldlm_namespace_first_locked(ldlm_side_t client)
 }
 
 /** Create and initialize new resource. */
-static struct ldlm_resource *ldlm_resource_new(void)
+static struct ldlm_resource *ldlm_resource_new(int lock_lvb)
 {
 	struct ldlm_resource *res;
 	int idx;
@@ -1052,8 +1052,10 @@ static struct ldlm_resource *ldlm_resource_new(void)
 
 	/* Since LVB init can be delayed now, there is no longer need to
 	 * immediatelly acquire mutex here. */
-	mutex_init(&res->lr_lvb_mutex);
-	res->lr_lvb_initialized = false;
+	if (lock_lvb) {
+		mutex_init(&res->lr_lvb_mutex);
+		res->lr_lvb_initialized = false;
+	}
 
 	return res;
 }
@@ -1094,7 +1096,7 @@ ldlm_resource_get(struct ldlm_namespace *ns, struct ldlm_resource *parent,
 
 	LASSERTF(type >= LDLM_MIN_TYPE && type < LDLM_MAX_TYPE,
 		 "type: %d\n", type);
-	res = ldlm_resource_new();
+	res = ldlm_resource_new(ns->ns_lvbo && ns->ns_lvbo->lvbo_init);
 	if (res == NULL)
 		return ERR_PTR(-ENOMEM);
 