Xyratex/lustre-stable

MRP-153 utils: add erase-param change param in tunefs.lustre
A new option is added to tunefs.lustre, "--erase-param", which allows
to remove all instances of a specific parameter.

This patch should come with a warning, as it also introduces a change
in current behaviour: option "--param" is modified to erase all
previously stored instances of the parameter, and store all new
instances of the parameter specified in the command line.

Signed-off-by: Nikitas Angelinas <nikitas.angelinas@seagate.com>
Change-Id: Ia981d4b964af07adcd0d9f03be96dcb31711b970
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/4330
Tested-by: Jenkins
Reviewed-by: Artem Blagodarenko <artem.blagodarenko@seagate.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@seagate.com>
Tested-by: Elena Gryaznova <elena.gryaznova@seagate.com>
Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

lustre/tests/conf-sanity.sh
@@ -5068,6 +5068,51 @@ test_86() {
 }
 run_test 86 "Replacing mkfs.lustre -G option"
 
+test_87() { # MRP-153
+	local key=failover.node
+	local val1=192.0.2.254@tcp0 # Reserved IPs, see RFC 5735
+	local val2=192.0.2.255@tcp0
+	local mdsdev=$(mdsdevname 1)
+	local params
+
+	stopall
+
+	do_facet mds "$TUNEFS --erase-params $mdsdev >/dev/null" ||
+		error "tunefs failed"
+
+	# Check that parameters are added correctly
+	do_facet mds "$TUNEFS --param $key=$val1 $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --print $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=$val1) = "1" ] ||
+		error "on-disk parameter not added correctly via tunefs"
+
+	# Check that parameters replace existing instances when added
+	do_facet mds "$TUNEFS --param $key=$val2 $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --print $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=) = "1" ] ||
+		error "on-disk parameter not replaced via tunefs"
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=$val2) = "1" ] ||
+		error "on-disk parameter not replaced correctly via tunefs"
+
+	# Check that a parameter is erased properly
+	do_facet mds "$TUNEFS --erase-param $key $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --print $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=) = "0" ] ||
+		error "on-disk parameter not erased correctly via tunefs"
+
+	reformat
+}
+run_test 87 "check tunefs correctly handles parameter addition and removal"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi

Cray/lustre

lustre/tests/conf-sanity.sh
@@ -5478,6 +5478,51 @@ test_86() {
 }
 run_test 86 "Replacing mkfs.lustre -G option"
 
+test_87() { # MRP-153
+	local key=failover.node
+	local val1=192.0.2.254@tcp0 # Reserved IPs, see RFC 5735
+	local val2=192.0.2.255@tcp0
+	local mdsdev=$(mdsdevname 1)
+	local params
+
+	stopall
+
+	do_facet mds "$TUNEFS --erase-params $mdsdev >/dev/null" ||
+		error "tunefs failed"
+
+	# Check that parameters are added correctly
+	do_facet mds "$TUNEFS --param $key=$val1 $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --dryrun $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=$val1) = "1" ] ||
+		error "on-disk parameter not added correctly via tunefs"
+
+	# Check that parameters replace existing instances when added
+	do_facet mds "$TUNEFS --param $key=$val2 $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --dryrun $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=) = "1" ] ||
+		error "on-disk parameter not replaced via tunefs"
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=$val2) = "1" ] ||
+		error "on-disk parameter not replaced correctly via tunefs"
+
+	# Check that a parameter is erased properly
+	do_facet mds "$TUNEFS --erase-param $key $mdsdev >/dev/null" ||
+		error "tunefs failed"
+	params=$(do_facet mds $TUNEFS --dryrun $mdsdev) || error "tunefs failed"
+	params=${params##*Parameters:}
+	params=${params%%exiting*}
+	[ $(echo $params | tr ' ' '\n' | grep -c $key=) = "0" ] ||
+		error "on-disk parameter not erased correctly via tunefs"
+
+	reformat
+}
+run_test 87 "check tunefs correctly handles parameter addition and removal"
+
 if ! combined_mgs_mds ; then
 	stop mgs
 fi