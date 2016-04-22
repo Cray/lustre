Xyratex/lustre-stable

MRP-509 obdclass: correct shutdown
add zombie barrier in 2 places:
1) echo_device_fini, it is possible the echo connect method is called
and echo is connected to some upper layer; as class_manual_cleanup()
is not called for echo, we are cleaning up from bottom to top, i.e.
zombee barrier in the bottom layer is not enough;

2) class_detach, it is possible that next setup starts before previous
cleanup finishes, we need to wait for all the self-exports to die;

also avoid use after free in class_handle_ioctl for obd_device found by index or name

Change-Id: I3cbf78c9000973190ab97b3d2a8ea5c2280c33b1
Signed-off-by: Alexey Lyashkov <alexey_lyashkov@xyratex.com>
Reviewed-on: http://morpheus.xyus.xyratex.com:8443/gerrit/166
Tested-by: Jenkins
Reviewed-by: Artem Blagodarenko <Artem_Blagodarenko@xyratex.com>
Reviewed-by: Alexander Zarochentsev <alexander_zarochentsev@xyratex.com>
Tested-by: Alexander Lezhoev <Alexander_Lezhoev@xyratex.com>
Reviewed-by: Vitaly Fertman <Vitaly_Fertman@xyratex.com>

lustre/include/obd_support.h
@@ -358,6 +358,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_OBD_DQACQ               0x604
 #define OBD_FAIL_OBD_LLOG_SETUP          0x605
 #define OBD_FAIL_OBD_LOG_CANCEL_REP      0x606
+#define OBD_FAIL_OBD_ZOMBIE_SLEEP	0x607
 
 #define OBD_FAIL_TGT_REPLY_NET           0x700
 #define OBD_FAIL_TGT_CONN_RACE           0x701

lustre/tests/sanity.sh
@@ -8380,15 +8380,23 @@ test_180b() {
         local rc=0
         local rmmod_remote=0
 
-        do_facet ost1 "lsmod | grep -q obdecho || "                      \
-                      "{ insmod ${LUSTRE}/obdecho/obdecho.ko || "        \
-                      "modprobe obdecho; }" && rmmod_remote=1
-        target=$(do_facet ost1 $LCTL dl | awk '/obdfilter/ {print $4;exit}')
-        [[ -n $target ]] && { obdecho_create_test $target ost1 || rc=1; }
-        [ $rmmod_remote -eq 1 ] && do_facet ost1 "rmmod obdecho"
-        return $rc
+	if ! do_rpc_nodes $(facet_host ost1) "module_loaded obdecho"; then
+		rmmod_remote=1
+		do_rpc_nodes $(facet_host ost1) "load_module obdecho/obdecho" 
+	fi
+	target=$(do_facet ost1 $LCTL dl | awk '/obdfilter/ {print $4;exit}')
+	if [ -n $target ]; then
+#define OBD_FAIL_OBD_ZOMBIE_SLEEP	0x607
+		do_facet ost1 $LCTL set_param fail_loc=0x80000607
+		obdecho_create_test $target ost1 || rc=1
+		obdecho_create_test $target ost1 || rc=1
+		do_facet ost1 $LCTL set_param fail_loc=0
+	fi
+	[ $rmmod_remote -eq 1 ] && do_facet ost1 "rmmod obdecho"
+
+	return $rc
 }
-run_test 180b "test obdecho directly on obdfilter"
+run_test 180b "test obdecho directly on obdfilter and correctly handle zombies"
 
 test_181() { # bug 22177
 	mkdir -p $DIR/$tdir || error "creating dir $DIR/$tdir"

Cray/lustre

lustre/include/obd_support.h
@@ -422,6 +422,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_OBD_IDX_READ_NET        0x607
 #define OBD_FAIL_OBD_IDX_READ_BREAK	 0x608
 #define OBD_FAIL_OBD_NO_LRU		 0x609
+#define OBD_FAIL_OBD_ZOMBIE_SLEEP	 0x650
 
 #define OBD_FAIL_TGT_REPLY_NET           0x700
 #define OBD_FAIL_TGT_CONN_RACE           0x701

lustre/tests/sanity.sh
@@ -10997,15 +10997,23 @@ test_180b() {
 	local rc=0
 	local rmmod_remote=0
 
-	do_facet ost1 "lsmod | grep -q obdecho || "                      \
-		      "{ insmod ${LUSTRE}/obdecho/obdecho.ko || "        \
-		      "modprobe obdecho; }" && rmmod_remote=1
+	if ! do_rpc_nodes $(facet_host ost1) "module_loaded obdecho"; then
+		rmmod_remote=1
+		do_rpc_nodes $(facet_host ost1) "load_module obdecho/obdecho" 
+	fi
 	target=$(do_facet ost1 $LCTL dl | awk '/obdfilter/ {print $4;exit}')
-	[[ -n $target ]] && { obdecho_test $target ost1 || rc=1; }
+	if [ -n $target ]; then
+#define OBD_FAIL_OBD_ZOMBIE_SLEEP	0x650
+		do_facet ost1 $LCTL set_param fail_loc=0x80000650
+		obdecho_test $target ost1 || rc=1
+		obdecho_test $target ost1 || rc=1
+		do_facet ost1 $LCTL set_param fail_loc=0
+	fi
 	[ $rmmod_remote -eq 1 ] && do_facet ost1 "rmmod obdecho"
+
 	return $rc
 }
-run_test 180b "test obdecho directly on obdfilter"
+run_test 180b "test obdecho directly on obdfilter and correctly handle zombies"
 
 test_180c() { # LU-2598
 	[ $PARALLEL == "yes" ] && skip "skip parallel run" && return