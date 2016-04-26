1f280907aabff502706441eb29e2b1991abaefb0
Niu Yawei <yawei.niu@intel.com>
2016-04-25 23:22:05 -0400

    LU-3534 ptlrpc: mbits is sent within ptlrpc_body
    ptlrpc is using rq_xid as matchbits of bulk data, which means it
    has to change rq_xid for bulk resend to avoid several bulk data
    landing into the same buffer from different resends.
    
    This patch uses one of reserved __u64 of ptlrpc_body to transfer
    mbits to peer, matchbits is now separated from xid. With this change,
    ptlrpc can keep rq_xid unchanged on resend, it only updates matchbits
    for bulk data.
    
    This protocol change is only applied if both sides of connection have
    OBD_CONNECT_BULK_MBITS, otherwise, ptlrpc still uses old approach and
    update xid while resending bulk.
    
    Lustre-commit: d099fdd6cd15d0d00d9b573da5d3bfd3e4bbcb9d
    Lustre-change: http://review.whamcloud.com/15421
    
    Signed-off-by: Liang Zhen <liang.zhen@intel.com>
    Signed-off-by: Niu Yawei <yawei.niu@intel.com>
    Change-Id: I6349c1b1539306b3110b5eca79f0d4a944de5cc9
    Reviewed-on: http://review.whamcloud.com/19780
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Alex Zhuravlev <alexey.zhuravlev@intel.com>
    Tested-by: Jenkins
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -229,7 +229,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 				  OBD_CONNECT_MAX_EASIZE |
 				  OBD_CONNECT_FLOCK_DEAD |
 				  OBD_CONNECT_DISP_STRIPE | OBD_CONNECT_LFSCK |
-				  OBD_CONNECT_OPEN_BY_FID;
+				  OBD_CONNECT_OPEN_BY_FID |
+				  OBD_CONNECT_BULK_MBITS;
 
         if (sbi->ll_flags & LL_SBI_SOM_PREVIEW)
                 data->ocd_connect_flags |= OBD_CONNECT_SOM;
@@ -443,7 +444,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 				  OBD_CONNECT_JOBSTATS | OBD_CONNECT_LVB_TYPE |
 				  OBD_CONNECT_LAYOUTLOCK |
 				  OBD_CONNECT_PINGLESS | OBD_CONNECT_LFSCK |
-				  OBD_CONNECT_LOCK_AHEAD;
+				  OBD_CONNECT_LOCK_AHEAD |
+				  OBD_CONNECT_BULK_MBITS;
 
 	if (sb->s_flags & MS_RDONLY)
 		data->ocd_connect_flags |= OBD_CONNECT_RDONLY;
