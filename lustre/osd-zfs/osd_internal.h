03c22b461c4a95e1e1d2c6faded522c0b047c5b1
Yang Sheng <yang.sheng@intel.com>
2016-05-18 23:52:29 +0800

    LU-8147 osd-zfs: fix osd_mount error path
    The osd_oi_fini will grab osl_seq_list_lock for cleanup.  But this
    lock is not initilized if osd_mount return and failed before
    osd_oi_init. Remove the od_seq_init field of struct osd_device as it
    is no longer needed.
    
    Lustre-commit: 60270c6488b01db756eb216548f83f2826972854
    Lustre-change: http://review.whamcloud.com/20309/
    
    Signed-off-by: Yang Sheng <yang.sheng@intel.com>
    Change-Id: Ia053e83e9d2704a8e79dd44ceb1d03b55a7256a5
    Reviewed-by: Mike Pershin <mike.pershin@intel.com>
    Reviewed-by: Bob Glossman <bob.glossman@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-on: http://review.whamcloud.com/21548
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>

--- lustre/osd-zfs/osd_internal.h
+++ lustre/osd-zfs/osd_internal.h
@@ -280,8 +280,7 @@ struct osd_device {
 				 od_xattr_in_sa:1,
 				 od_quota_iused_est:1,
 				 od_is_ost:1,
-				 od_posix_acl:1,
-				 od_seq_init:1;
+				 od_posix_acl:1;
 
 	char			 od_mntdev[128];
 	char			 od_svname[128];
