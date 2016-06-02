f2a31c10dbe51570c8a206e034b2d16e0cef5897
Johann Lombardi <johann.lombardi@intel.com>
2016-06-02 12:00:37 +0800

    LU-2049 grant: add support for OBD_CONNECT_GRANT_PARAM
    Add support for grant overhead calculation on the client side.
    To do so, clients track usage on a per-extent basis. An extent is
    composed of contiguous blocks.
    The OST now returns to the OSC layer several parameters to consume
    grant more accurately:
    - the backend filesystem block size which is the minimal grant
      allocation unit;
    - the maximum extent size;
    - the extent insertion cost.
    Clients now pack in bulk write how much grant space was consumed for
    the RPC. Dirty data accounting also adopts the same scheme.
    
    Moreover, each backend OSD now reports its own set of parameters:
    - For ldiskfs, we usually have a 4KB block size with a maximum extent
      size of 32MB (theoretical limit of 128MB) and an extent insertion
      cost of 6 x 4KB = 24KB
    - For ZFS, we report a block size of 128KB, an extent size of 128
      blocks (i.e. 16MB with 128KB block size) and a block insertion cost
      of 112KB.
    
    Besides, there is now no more generic metadata overhead reservation
    done inside each OSD. Instead grant space is inflated for clients
    that do not support the new grant parameters. That said, a tiny
    percentage (typically 0.76%) of the free space is still reserved
    inside each OSD to avoid fragmentation which might hurt performance
    and impact our grant calculation (e.g. extents are broken due to
    fragmentation).
    
    This patch also fixes several other issues:
    
    - Bulk write resent by ptlrpc after reconnection could trigger
    spurious error messages related to broken dirty accounting.
    The issue was that oa_dirty is discarded for resent requests
    (grant flag cleared in ost_brw_write()), so we can legitimately
    have grant > fed_dirty in ofd_grant_check().
    This was fixed by reseting fed_dirty on reconnection and skipping
    the dirty accounting check in ofd_grant_check() in the case of
    ptlrpc resend.
    
    - In obd_connect_data_seqprint(), the connection flags cannot fit
      in a 32-bit integer.
    
    - When merging two OSC extents, an extent tax should be released
      in both the merged extent and in the grant accounting.
    
    This patch is back-ported from the following one:
    Lustre-commit: bd1e41672c974b97148b65115185a57ca4b7bbde
    Lustre-change: http://review.whamcloud.com/7793
    
    Signed-off-by: Johann Lombardi <johann.lombardi@intel.com>
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Change-Id: I9c738235583324dfae7eade034db28a8161f8ef5
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -407,6 +407,10 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 		GOTO(out_md_fid, err = -ENODEV);
 	}
 
+	/* pass client page size via ocd_grant_blkbits, the server should report
+	 * back its backend blocksize for grant calculation purpose */
+	data->ocd_grant_blkbits = PAGE_SHIFT;
+
         data->ocd_connect_flags = OBD_CONNECT_GRANT     | OBD_CONNECT_VERSION  |
 				  OBD_CONNECT_REQPORTAL | OBD_CONNECT_BRW_SIZE |
                                   OBD_CONNECT_CANCELSET | OBD_CONNECT_FID      |
@@ -425,6 +429,9 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 		data->ocd_connect_flags |= OBD_CONNECT_RDONLY;
         if (sbi->ll_flags & LL_SBI_SOM_PREVIEW)
                 data->ocd_connect_flags |= OBD_CONNECT_SOM;
+
+	if (!OBD_FAIL_CHECK(OBD_FAIL_OSC_CONNECT_GRANT_PARAM))
+		data->ocd_connect_flags |= OBD_CONNECT_GRANT_PARAM;
 
         if (!OBD_FAIL_CHECK(OBD_FAIL_OSC_CONNECT_CKSUM)) {
                 /* OBD_CONNECT_CKSUM should always be set, even if checksums are
