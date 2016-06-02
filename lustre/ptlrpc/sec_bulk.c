5574311042a3c69f46430cc4db53381dc61fef96
Jinshan Xiong <jinshan.xiong@intel.com>
2016-06-02 12:11:50 +0800

    LU-7990 rpc: increase bulk size
    To make the ptlrpc be able to size 16MB IO
    
    This patch is back-ported from the following one:
    Lustre-commit: 51b32ac2b9b86a600e92ab03f387717e526153d3
    Lustre-change: http://review.whamcloud.com/19366
    
    Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
    Signed-off-by: Gu Zheng <gzheng@ddn.com>
    Change-Id: I32727beaed08cb0e9048e7fe0e27489e330fce60
    Reviewed-on: http://review.whamcloud.com/20550
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/ptlrpc/sec_bulk.c
+++ lustre/ptlrpc/sec_bulk.c
@@ -566,7 +566,7 @@ int sptlrpc_enc_pool_get_pages(struct ptlrpc_bulk_desc *desc)
 	if (GET_ENC_KIOV(desc) != NULL)
 		return 0;
 
-	OBD_ALLOC(GET_ENC_KIOV(desc),
+	OBD_ALLOC_LARGE(GET_ENC_KIOV(desc),
 		  desc->bd_iov_count * sizeof(*GET_ENC_KIOV(desc)));
 	if (GET_ENC_KIOV(desc) == NULL)
 		return -ENOMEM;
@@ -620,7 +620,7 @@ again:
 				 * will put request back in queue. */
 				page_pools.epp_st_outofmem++;
 				spin_unlock(&page_pools.epp_lock);
-				OBD_FREE(GET_ENC_KIOV(desc),
+				OBD_FREE_LARGE(GET_ENC_KIOV(desc),
 					 desc->bd_iov_count *
 					 sizeof(*GET_ENC_KIOV(desc)));
 				GET_ENC_KIOV(desc) = NULL;
@@ -722,7 +722,7 @@ void sptlrpc_enc_pool_put_pages(struct ptlrpc_bulk_desc *desc)
 
 	spin_unlock(&page_pools.epp_lock);
 
-	OBD_FREE(GET_ENC_KIOV(desc),
+	OBD_FREE_LARGE(GET_ENC_KIOV(desc),
 		 desc->bd_iov_count * sizeof(*GET_ENC_KIOV(desc)));
 	GET_ENC_KIOV(desc) = NULL;
 }
