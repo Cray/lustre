b720f8854d6d80455873ea1e2f371a2dee586403
Andreas Dilger <andreas.dilger@intel.com>
2016-06-28 23:19:59 +0800

    LU-7759 llite: handle inactive OSTs better in statfs
    Change the order of checks for inactive OSCs in lov_prep_statfs_set()
    so that administratively disabled OSTs do not generate any output in
    "lfs df" at all, to avoid needlessly cluttering the output.
    
    Enable the lazystatfs mount option by default, so that "df" does not
    hang when an OST is temporarily offline.
    
    Fix test-framework.sh to use "lfs df $MOUNT" instead of "df $MOUNT" to
    determine when recovery is complete, now that "df" does not block.
    
    This is a port patch from:
    Lustre-commit: 23fde1f89bec0adf4f7181ccce5a236eac371a38
    Lustre-change: http://review.whamcloud.com/19195
    
    Signed-off-by: Andreas Dilger <andreas.dilger@intel.com>
    Change-Id: I993761a7eb120a36a1b80c2822cb6d8011ccab07
    Reviewed-by: John L. Hammond <john.hammond@intel.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Reviewed-on: http://review.whamcloud.com/21034
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Dmitry Eremin <dmitry.eremin@intel.com>

--- lustre/obdclass/genops.c
+++ lustre/obdclass/genops.c
@@ -1105,6 +1105,7 @@ void __class_export_add_lock_ref(struct obd_export *exp, struct ldlm_lock *lock)
                lock, exp, lock->l_exp_refs_nr);
 	spin_unlock(&exp->exp_locks_list_guard);
 }
+EXPORT_SYMBOL(__class_export_add_lock_ref);
 
 void __class_export_del_lock_ref(struct obd_export *exp, struct ldlm_lock *lock)
 {
@@ -1123,6 +1124,7 @@ void __class_export_del_lock_ref(struct obd_export *exp, struct ldlm_lock *lock)
                lock, exp, lock->l_exp_refs_nr);
 	spin_unlock(&exp->exp_locks_list_guard);
 }
+EXPORT_SYMBOL(__class_export_del_lock_ref);
 #endif
 
 /* A connection defines an export context in which preallocation can
@@ -1511,6 +1513,7 @@ int obd_export_evict_by_uuid(struct obd_device *obd, const char *uuid)
 
 #if LUSTRE_TRACKS_LOCK_EXP_REFS
 void (*class_export_dump_hook)(struct obd_export*) = NULL;
+EXPORT_SYMBOL(class_export_dump_hook);
 #endif
 
 static void print_export_data(struct obd_export *exp, const char *status,
