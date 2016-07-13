ccb75af29b9ba6065aaa85ef7b6776f74a0cd670
Jadhav Vikram <jadhav.vikram@seagate.com>
2016-07-14 04:33:15 +0530

    MRP-2820 lod: do not ignore degraded flag of ost.
    clear ost->ltd_qos.ltq_usable before checking degraded ost flags
    and some other flags in lod_alloc_qos() while finding all ost's which
    are valid stripe candidates. Call to lod_alloc_qos before setting
    degraded flag sets ost->ltd_qos.ltq_usable to 1 for all valid ost
    but after setting degraded flag ost->ltd_qos.ltq_usable not getting
    cleared so there is need to clear it so that it will not get added
    into stripe array.
    
    Signed-off-by: Jadhav Vikram <jadhav.vikram@seagate.com>
    Change-Id: I58da02dadc2a1e60cca1245f76cd5bd956147d59
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/11620
    Tested-by: Jenkins
    Reviewed-by: Rahul Deshmukh <rahul.deshmukh@seagate.com>
    Reviewed-by: Ashish Purkar <ashish.purkar@seagate.com>
    Reviewed-by: Andriy Skulysh <andriy.skulysh@seagate.com>
    Tested-by: Parinay Vijayprakash Kondekar <parinay.kondekar@seagate.com>

--- lustre/lod/lod_qos.c
+++ lustre/lod/lod_qos.c
@@ -1419,6 +1419,9 @@ static int lod_alloc_qos(const struct lu_env *env, struct lod_object *lo,
 		if (!cfs_bitmap_check(m->lod_ost_bitmap, osts->op_array[i]))
 			continue;
 
+		ost = OST_TGT(m,osts->op_array[i]);
+		ost->ltd_qos.ltq_usable = 0;
+
 		rc = lod_statfs_and_check(env, m, osts->op_array[i], sfs);
 		if (rc) {
 			/* this OSP doesn't feel well */
@@ -1440,7 +1443,6 @@ static int lod_alloc_qos(const struct lu_env *env, struct lod_object *lo,
 				   osts->op_array[i] == 0)
 			continue;
 
-		ost = OST_TGT(m,osts->op_array[i]);
 		ost->ltd_qos.ltq_usable = 1;
 		lod_qos_calc_weight(m, osts->op_array[i]);
 		total_weight += ost->ltd_qos.ltq_weight;
