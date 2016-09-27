5a48654f6b350962d86cce1581dcfb5f1cbbcc8e
Sergey Cheremencev <sergey.cheremencev@seagate.com>
2016-09-27 16:59:59 +0300

    MRP-3892 tests: ENOMEM handling in osp_precreate_cleanup_orphans
    Check that error handling in osp_precreate_cleanup_orphans doesn't block
    objects reservation.
    
    Change-Id: I86e75a454a222e0baac949d8c3f01db473fc672e
    Signed-off-by: Sergey Cheremencev <sergey.cheremencev@seagate.com>
    Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/12362
    Reviewed-by: Alexandr Boyko <alexander.boyko@seagate.com>
    Reviewed-by: Alexey Leonidovich Lyashkov <alexey.lyashkov@seagate.com>
    Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -257,6 +257,7 @@ int obd_alloc_fail(const void *ptr, const char *name, const char *type,
 #define OBD_FAIL_MDS_TRACK_OVERFLOW	 0x162
 #define OBD_FAIL_MDS_LOV_CREATE_RACE	 0x163
 #define OBD_FAIL_MDS_HSM_CDT_DELAY	 0x164
+#define OBD_FAIL_MDS_OSP_CLP_ORNS_FAIL	 0x166
 
 /* layout lock */
 #define OBD_FAIL_MDS_NO_LL_GETATTR	 0x170
