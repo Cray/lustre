15b83f83b088967c6c9f90bbd3007829884b0388
Vitaly Fertman <vitaly.fertman@seagate.com>
2016-09-30 18:38:48 +0300

    Revert "LU-793 test: validate dropping resend request"
    This reverts commit 671efd509bd020144b301a8bf252462ffc047bd9.

--- lustre/ptlrpc/import.c
+++ lustre/ptlrpc/import.c
@@ -1114,12 +1114,6 @@ static int ptlrpc_connect_interpret(const struct lu_env *env,
 	if (rc != 0)
 		GOTO(out, rc);
 
-	/* The net statistics after (re-)connect is not valid anymore,
-	 * because may reflect other routing, etc. */
-	at_init(&imp->imp_at.iat_net_latency, 0, 0);
-	ptlrpc_at_adj_net_latency(request,
-			lustre_msg_get_service_time(request->rq_repmsg));
-
 	obd_import_event(imp->imp_obd, imp, IMP_EVENT_OCD);
 
 	if (aa->pcaa_initial_connect) {
