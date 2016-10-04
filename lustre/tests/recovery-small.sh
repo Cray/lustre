Xyratex/lustre-stable

MRP-2909 test: ASSERTION(flock->blocking_export != 0)) failed
Whole policy structure was zeroed twice.
Once during enqueue and second time during resend or replay.

Change-Id: I79a715560ffda476d37f3feae98806057a236011
Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/11219
Tested-by: Jenkins
Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>
Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>

lustre/tests/recovery-small.sh
@@ -1950,6 +1950,29 @@ test_120() {
 }
 run_test 120 "flock race: completion vs. evict"
 
+test_133() {
+	local list=$(comma_list $(mdts_nodes))
+
+	local t=$((TIMEOUT * 2))
+	touch $DIR/$tfile
+
+	flock $DIR/$tfile -c "echo bl lock;sleep $t;echo bl flock unlocked" &
+	sleep 1
+	multiop_bg_pause $DIR/$tfile O_Fc || return 1
+	PID=$!
+
+	#define OBD_FAIL_LDLM_REPLY              0x30c
+	do_nodes $list $LCTL set_param fail_loc=0x8000030c
+	kill -USR1 $PID
+	echo "waiting for multiop $PID"
+	wait $PID || return 2
+
+	rm -f $DIR/$tfile
+
+	return 0
+}
+run_test 133 "don't fail on flock resend"
+
 complete $SECONDS
 check_and_cleanup_lustre
 exit_status

Cray/lustre

lustre/tests/recovery-small.sh
@@ -2462,6 +2462,29 @@ test_132() {
 }
 run_test 132 "long punch"
 
+test_133() {
+	local list=$(comma_list $(mdts_nodes))
+
+	local t=$((TIMEOUT * 2))
+	touch $DIR/$tfile
+
+	flock $DIR/$tfile -c "echo bl lock;sleep $t;echo bl flock unlocked" &
+	sleep 1
+	multiop_bg_pause $DIR/$tfile O_jc || return 1
+	PID=$!
+
+	#define OBD_FAIL_LDLM_REPLY              0x30c
+	do_nodes $list $LCTL set_param fail_loc=0x8000030c
+	kill -USR1 $PID
+	echo "waiting for multiop $PID"
+	wait $PID || return 2
+
+	rm -f $DIR/$tfile
+
+	return 0
+}
+run_test 133 "don't fail on flock resend"
+
 complete $SECONDS
 check_and_cleanup_lustre
 exit_status