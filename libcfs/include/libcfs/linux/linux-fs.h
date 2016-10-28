e1c179ba33bcfdf567060dc29d3e0adcfe5a814b
Chris Horn <hornc@cray.com>
2016-10-28 16:09:52 -0500

    845771 libcfs: Get rid of dead "read ip2nets and routes from file" code
    Seagate has their own implementation of this functionality. The merge
    of b_neo_stable into feature-2.7 resulted in some dead code from our
    implementation. Remove it.
    
    Bugs Affected: 845771
    Bug Action: RESOLVE

--- libcfs/include/libcfs/linux/linux-fs.h
+++ libcfs/include/libcfs/linux/linux-fs.h
@@ -83,14 +83,6 @@
 #define flock_end(fl)			((fl)->fl_end)
 #define flock_set_end(fl, end)		do { (fl)->fl_end = (end); } while (0)
 
-#define cfs_filp_close(f)                   filp_close(f, NULL)
-
-typedef struct file cfs_file_t;
-cfs_file_t *cfs_filp_open (const char *name, int flags, int mode, int *err);
-
-ssize_t cfs_user_read(cfs_file_t *filp, char *buf, size_t count,
-                      loff_t *offset);
-
 ssize_t filp_user_write(struct file *filp, const void *buf, size_t count,
 			loff_t *offset);
 ssize_t filp_user_read(struct file *filp, char *buf, size_t count,
--- libcfs/libcfs/linux/linux-fs.c
+++ libcfs/libcfs/linux/linux-fs.c
@@ -43,29 +43,6 @@
 
 #include <libcfs/libcfs.h>
 
-cfs_file_t *
-cfs_filp_open (const char *name, int flags, int mode, int *err)
-{
-	/* XXX
-	* Maybe we need to handle flags and mode in the future
-	*/
-	cfs_file_t *filp = NULL;
-
-	filp = filp_open(name, flags, mode);
-	if (IS_ERR(filp)) {
-		int rc;
-
-		rc = PTR_ERR(filp);
-		printk(KERN_ERR "LustreError: can't open %s file: err %d\n",
-		       name, rc);
-		if (err)
-			*err = rc;
-		filp = NULL;
-	}
-	return filp;
-}
-EXPORT_SYMBOL(cfs_filp_open);
-
 /* write a userspace buffer to disk.
  * NOTE: this returns 0 on success, not the number of bytes written. */
 ssize_t
