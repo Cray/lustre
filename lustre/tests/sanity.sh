1e23b297ed4db1b2428a758b52b250a90e71a6ac
Fan Yong <fan.yong@intel.com>
2016-10-31 02:17:45 +0800

    LDEV-507 mgc: use remote llog under read only mode
    Under read only mode, we ignore to sync the server
    (MDT or OST) local llog copy with the remote llog,
    so the it may has no local llog copy or its local
    llog copy is incomplete. Under such case, we should
    use the remote llog directly when start the server.
    
    Signed-off-by: Fan Yong <fan.yong@intel.com>
    Change-Id: Ia9bec22f6dbbc2214ff0990c86fdfbcc722ab1c9
    Reviewed-on: https://review.whamcloud.com/22582
    Tested-by: Jenkins
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: John L. Hammond <john.hammond@intel.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -14751,6 +14751,7 @@ run_test 404 "validate manual {de}activated works properly for OSPs"
 	do_nodes $(comma_list $(facet_active_host mgs) $(mdts_nodes)) \
 	$LCTL set_param debug=-snapshot
 
+saved_MGS_MOUNT_OPTS=$MGS_MOUNT_OPTS
 saved_MDS_MOUNT_OPTS=$MDS_MOUNT_OPTS
 saved_OST_MOUNT_OPTS=$OST_MOUNT_OPTS
 
@@ -14758,6 +14759,7 @@ cleanup_420() {
 	trap 0
 
 	stopall
+	MGS_MOUNT_OPTS=$saved_MGS_MOUNT_OPTS
 	MDS_MOUNT_OPTS=$saved_MDS_MOUNT_OPTS
 	OST_MOUNT_OPTS=$saved_OST_MOUNT_OPTS
 	setupall
@@ -14774,10 +14776,14 @@ test_420() {
 
 	trap cleanup_420 EXIT
 
+	# sync by force before remount as readonly
+	sync; sync_all_data; sleep 3; sync_all_data
+
 	stopall
 
+	MGS_MOUNT_OPTS=$(csa_add "$MGS_MOUNT_OPTS" -o rdonly_dev)
 	MDS_MOUNT_OPTS=$(csa_add "$MDS_MOUNT_OPTS" -o rdonly_dev)
-	OSS_MOUNT_OPTS=$(csa_add "$OST_MOUNT_OPTS" -o rdonly_dev)
+	OST_MOUNT_OPTS=$(csa_add "$OST_MOUNT_OPTS" -o rdonly_dev)
 
 	echo "Mount the server as read only"
 	setupall server_only || error "(3) Fail to start servers"
