7db0daed5eb8bbe8f17afc25fd9cd256db9b3c8e
John L. Hammond <john.hammond@intel.com>
2017-05-09 12:57:22 -0500

    LU-9464 hsm: use OBD_ALLOC_LARGE() for hsm_scan_data array
    In mdt_coordinator() use OBD_ALLOC_LARGE() rather than OBD_ALLOC() for
    the hsm_scan_data request array.
    
    Lustre-commit: 2dd720a1ad43c4d3cf1d226a226e2ffa494866e4
    Lustre-change: https://review.whamcloud.com/27014
    
    Signed-off-by: John L. Hammond <john.hammond@intel.com>
    Signed-off-by: Bob Glossman <bob.glossman@intel.com>
    Change-Id: Ieb648dfb92e6019ab316c7643aa0c0b5cf1d86f7
    Reviewed-by: Andreas Dilger <andreas.dilger@intel.com>
    Reviewed-by: Li Xi <lixi@ddn.com>
    Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>
    Reviewed-on: https://review.whamcloud.com/27137
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Faccini Bruno <bruno.faccini@intel.com>

--- lustre/mdt/mdt_coordinator.c
+++ lustre/mdt/mdt_coordinator.c
@@ -452,7 +452,7 @@ static int mdt_coordinator(void *data)
 	 */
 	hsd.max_requests = cdt->cdt_max_requests;
 	hsd.request_sz = hsd.max_requests * sizeof(*hsd.request);
-	OBD_ALLOC(hsd.request, hsd.request_sz);
+	OBD_ALLOC_LARGE(hsd.request, hsd.request_sz);
 	if (!hsd.request)
 		GOTO(out, rc = -ENOMEM);
 
@@ -495,11 +495,11 @@ static int mdt_coordinator(void *data)
 			/* cdt_max_requests has changed,
 			 * we need to allocate a new buffer
 			 */
-			OBD_FREE(hsd.request, hsd.request_sz);
+			OBD_FREE_LARGE(hsd.request, hsd.request_sz);
 			hsd.max_requests = cdt->cdt_max_requests;
 			hsd.request_sz =
 				   hsd.max_requests * sizeof(*hsd.request);
-			OBD_ALLOC(hsd.request, hsd.request_sz);
+			OBD_ALLOC_LARGE(hsd.request, hsd.request_sz);
 			if (!hsd.request) {
 				rc = -ENOMEM;
 				break;
@@ -614,7 +614,7 @@ clean_cb_alloc:
 	EXIT;
 out:
 	if (hsd.request)
-		OBD_FREE(hsd.request, hsd.request_sz);
+		OBD_FREE_LARGE(hsd.request, hsd.request_sz);
 
 	if (cdt->cdt_state == CDT_STOPPING) {
 		/* request comes from /proc path, so we need to clean cdt
