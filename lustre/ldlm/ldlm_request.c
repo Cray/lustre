bbfd8417e92d660f698d320ab43247f75eac173a
Vitaly Fertman <c17818@cray.com>
2018-01-30 21:14:22 +0300

    Revert "MRP-3662 ldlm: ELC shouldn't wait on lock flush"
    This reverts commit 357b4291414fb790106b23c2d4427dfe42b9aa9a.
    
    To be replaced by LU-8578

--- lustre/ldlm/ldlm_request.c
+++ lustre/ldlm/ldlm_request.c
@@ -792,7 +792,7 @@ int ldlm_prep_elc_req(struct obd_export *exp, struct ptlrpc_request *req,
                 avail = ldlm_capsule_handles_avail(pill, RCL_CLIENT, canceloff);
 
 		flags = ns_connect_lru_resize(ns) ?
-                        LDLM_CANCEL_LRUR_NO_WAIT : LDLM_CANCEL_AGED_NO_WAIT;
+			LDLM_CANCEL_LRUR_NO_WAIT : LDLM_CANCEL_AGED;
 		to_free = !ns_connect_lru_resize(ns) &&
 			  opc == LDLM_ENQUEUE ? 1 : 0;
 
@@ -1582,20 +1582,6 @@ static ldlm_policy_res_t ldlm_cancel_aged_policy(struct ldlm_namespace *ns,
 	return LDLM_POLICY_CANCEL_LOCK;
 }
 
-static ldlm_policy_res_t
-ldlm_cancel_aged_no_wait_policy(struct ldlm_namespace *ns,
-				struct ldlm_lock *lock,
-				int unused, int added, int count)
-{
-	ldlm_policy_res_t result;
-
-	result = ldlm_cancel_aged_policy(ns, lock, unused, added, count);
-	if (result == LDLM_POLICY_KEEP_LOCK)
-		return result;
-
-	return ldlm_cancel_no_wait_policy(ns, lock, unused, added, count);
-}
-
 /**
  * Callback function for default policy. Makes decision whether to keep \a lock
  * in LRU for current LRU size \a unused, added in current scan \a added and
@@ -1639,8 +1625,6 @@ ldlm_cancel_lru_policy(struct ldlm_namespace *ns, int flags)
         } else {
                 if (flags & LDLM_CANCEL_AGED)
                         return ldlm_cancel_aged_policy;
-		else if (flags & LDLM_CANCEL_AGED_NO_WAIT)
-			return ldlm_cancel_aged_no_wait_policy;
         }
 
         return ldlm_cancel_default_policy;
@@ -1686,8 +1670,7 @@ static int ldlm_prepare_lru_list(struct ldlm_namespace *ns,
 	ldlm_cancel_lru_policy_t pf;
 	struct ldlm_lock *lock, *next;
 	int added = 0, unused, remained;
-	int no_wait = flags & (LDLM_CANCEL_NO_WAIT | LDLM_CANCEL_LRUR_NO_WAIT |
-			       LDLM_CANCEL_AGED_NO_WAIT);
+	int no_wait = flags & (LDLM_CANCEL_NO_WAIT | LDLM_CANCEL_LRUR_NO_WAIT);
 	ENTRY;
 
 	spin_lock(&ns->ns_lock);
