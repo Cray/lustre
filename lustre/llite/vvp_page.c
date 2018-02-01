Xyratex/lustre-stable

MRP-3434 llite: Lustre I/O hung waiting for page
A deadlock during lock_page()

Thread 1:
sleep_on_page
__wait_on_bit_lock
__lock_page
vvp_page_make_ready
cl_page_make_ready
osc_extent_make_ready
osc_io_unplug0

Thread 2:
__wait_on_bit
wait_on_page_bit
shrink_inactive_list
shrink_list
shrink_zone
do_try_to_free_pages
try_to_free_mem_cgroup_pages
mem_cgroup_hierarchical_reclaim
__mem_cgroup_try_charge
mem_cgroup_prepare_migration
migrate_pages
compact_zone
__compact_pgdat
compact_node
sysfs_compact_node

Thread 2 tries to migrate 2nd page from extent
and waits for 1st to complete writeback.

Lock all extent pages and modify them after.

Change-Id: I42c53058051db0ee7b40d43fb0416cac2c0c0cb8
Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/10522
Reviewed-by: Andrew Perepechko <andrew.perepechko@seagate.com>
Tested-by: Jenkins
Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>
Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

lustre/llite/vvp_page.c
@@ -315,27 +315,41 @@ static void vvp_page_completion_write(const struct lu_env *env,
 }
 
 /**
- * Implements cl_page_operations::cpo_make_ready() method.
+ * Implements cl_page_operations::cpo_make_ready_start() method.
  *
  * This is called to yank a page from the transfer cache and to send it out as
- * a part of transfer. This function try-locks the page. If try-lock failed,
- * page is owned by some concurrent IO, and should be skipped (this is bad,
- * but hopefully rare situation, as it usually results in transfer being
- * shorter than possible).
+ * a part of transfer. This function is used to lock all pages from the extent.
+ *
+ * \retval 0      success, page is locked
+ *
+ */
+static int vvp_page_make_ready_start(const struct lu_env *env,
+				     const struct cl_page_slice *slice)
+{
+	struct page *vmpage = cl2vm_page(slice);
+
+	lock_page(vmpage);
+
+	return 0;
+}
+
+/**
+ * Implements cl_page_operations::cpo_make_ready_end() method.
+ *
+ * This is called to yank a page from the transfer cache and to send it out as
+ * a part of transfer. The page is already locked by
+ * vvp_page_make_ready_start().
  *
  * \retval 0      success, page can be placed into transfer
  *
- * \retval -EAGAIN page is either used by concurrent IO has been
- * truncated. Skip it.
  */
-static int vvp_page_make_ready(const struct lu_env *env,
-			       const struct cl_page_slice *slice)
+static int vvp_page_make_ready_end(const struct lu_env *env,
+				   const struct cl_page_slice *slice)
 {
 	struct page *vmpage = cl2vm_page(slice);
 	struct cl_page *pg = slice->cpl_page;
 	int result = 0;
 
-	lock_page(vmpage);
 	if (clear_page_dirty_for_io(vmpage)) {
 		LASSERT(pg->cp_state == CPS_CACHED);
 		/* This actually clears the dirty bit in the radix
@@ -412,12 +426,14 @@ static const struct cl_page_operations vvp_page_ops = {
                 [CRT_READ] = {
                         .cpo_prep        = vvp_page_prep_read,
                         .cpo_completion  = vvp_page_completion_read,
-                        .cpo_make_ready  = ccc_fail,
+			.cpo_make_ready_start = ccc_fail,
+			.cpo_make_ready_end  = ccc_fail,
                 },
                 [CRT_WRITE] = {
                         .cpo_prep        = vvp_page_prep_write,
                         .cpo_completion  = vvp_page_completion_write,
-                        .cpo_make_ready  = vvp_page_make_ready,
+			.cpo_make_ready_start = vvp_page_make_ready_start,
+			.cpo_make_ready_end = vvp_page_make_ready_end,
                 }
         }
 };

Cray/lustre

lustre/llite/vvp_page.c
@@ -328,27 +328,41 @@ static void vvp_page_completion_write(const struct lu_env *env,
 }
 
 /**
- * Implements cl_page_operations::cpo_make_ready() method.
+ * Implements cl_page_operations::cpo_make_ready_start() method.
  *
  * This is called to yank a page from the transfer cache and to send it out as
- * a part of transfer. This function try-locks the page. If try-lock failed,
- * page is owned by some concurrent IO, and should be skipped (this is bad,
- * but hopefully rare situation, as it usually results in transfer being
- * shorter than possible).
+ * a part of transfer. This function is used to lock all pages from the extent.
+ *
+ * \retval 0      success, page is locked
+ *
+ */
+static int vvp_page_make_ready_start(const struct lu_env *env,
+				     const struct cl_page_slice *slice)
+{
+	struct page *vmpage = cl2vm_page(slice);
+
+	lock_page(vmpage);
+
+	return 0;
+}
+
+/**
+ * Implements cl_page_operations::cpo_make_ready_end() method.
+ *
+ * This is called to yank a page from the transfer cache and to send it out as
+ * a part of transfer. The page is already locked by
+ * vvp_page_make_ready_start().
  *
  * \retval 0      success, page can be placed into transfer
  *
- * \retval -EAGAIN page is either used by concurrent IO has been
- * truncated. Skip it.
  */
-static int vvp_page_make_ready(const struct lu_env *env,
-			       const struct cl_page_slice *slice)
+static int vvp_page_make_ready_end(const struct lu_env *env,
+				   const struct cl_page_slice *slice)
 {
 	struct page *vmpage = cl2vm_page(slice);
 	struct cl_page *pg = slice->cpl_page;
 	int result = 0;
 
-	lock_page(vmpage);
 	if (clear_page_dirty_for_io(vmpage)) {
 		LASSERT(pg->cp_state == CPS_CACHED);
 		/* This actually clears the dirty bit in the radix
@@ -421,12 +435,14 @@ static const struct cl_page_operations vvp_page_ops = {
 		[CRT_READ] = {
 			.cpo_prep       = vvp_page_prep_read,
 			.cpo_completion = vvp_page_completion_read,
-			.cpo_make_ready = vvp_page_fail,
+			.cpo_make_ready_start = vvp_page_fail,
+			.cpo_make_ready_end  = vvp_page_fail,
 		},
 		[CRT_WRITE] = {
 			.cpo_prep       = vvp_page_prep_write,
 			.cpo_completion = vvp_page_completion_write,
-			.cpo_make_ready = vvp_page_make_ready,
+			.cpo_make_ready_start = vvp_page_make_ready_start,
+			.cpo_make_ready_end = vvp_page_make_ready_end,
 		},
 	},
 };