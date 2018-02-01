Xyratex/lustre-stable

MRP-4018 test: signal vs resend race
ptlrpc_resend_req() can be called earlier than removal
from imp_sending_list.
Thus wrong return value is returned from
ptlrpc_queue_wait() (-EAGAIN instead of -EINTR)

Change-Id: I802e422483e9e31bc648af79b1adbb9d99c45e3d
Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/13128
Reviewed-by: Alexander Nikolaevich Boyko <alexander.boyko@seagate.com>
Reviewed-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>
Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

lustre/ptlrpc/client.c
@@ -1703,6 +1703,7 @@ int ptlrpc_check_set(const struct lu_env *env, struct ptlrpc_request_set *set)
                 if (req->rq_intr && (req->rq_timedout || req->rq_waiting ||
                                      req->rq_wait_ctx)) {
                         req->rq_status = -EINTR;
+			OBD_FAIL_TIMEOUT(OBD_FAIL_OST_WRITE_DELAY, cfs_fail_val);
                         ptlrpc_rqphase_move(req, RQ_PHASE_INTERPRET);
                         GOTO(interpret, req->rq_status);
                 }

Cray/lustre

lustre/ptlrpc/client.c
@@ -1860,6 +1860,7 @@ int ptlrpc_check_set(const struct lu_env *env, struct ptlrpc_request_set *set)
                 if (req->rq_intr && (req->rq_timedout || req->rq_waiting ||
                                      req->rq_wait_ctx)) {
                         req->rq_status = -EINTR;
+			OBD_FAIL_TIMEOUT(OBD_FAIL_PTLRPC_DELAY_INTR, cfs_fail_val);
                         ptlrpc_rqphase_move(req, RQ_PHASE_INTERPRET);
                         GOTO(interpret, req->rq_status);
                 }