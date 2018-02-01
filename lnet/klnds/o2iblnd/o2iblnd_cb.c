lustre/lustre-release

LU-10015 o2iblnd: fix race at kiblnd_connect_peer
cmid will be destroyed at OFED if kiblnd_cm_callback return error.
if error happen before the end of kiblnd_connect_peer, it will touch
destroyed cmid and fail as
(o2iblnd_cb.c:1315:kiblnd_connect_peer())
            ASSERTION( cmid->device != ((void *)0) ) failed:

Seagate-bug-id: MRP-4592
Signed-off-by: Alexander Boyko <alexander.boyko@seagate.com>
Change-Id: I83eb5bceeb567acef0316498b936d25d6c6ccd95
Reviewed-on: https://review.whamcloud.com/29134
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Doug Oucharek <dougso@me.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1296,11 +1296,6 @@ kiblnd_connect_peer (kib_peer_ni_t *peer_ni)
                 goto failed2;
         }
 
-        LASSERT (cmid->device != NULL);
-	CDEBUG(D_NET, "%s: connection bound to %s:%pI4h:%s\n",
-               libcfs_nid2str(peer_ni->ibp_nid), dev->ibd_ifname,
-	       &dev->ibd_ifip, cmid->device->name);
-
 	return;
 
  failed2:
@@ -2997,8 +2992,19 @@ kiblnd_cm_callback(struct rdma_cm_id *cmid, struct rdma_cm_event *event)
                 } else {
                         rc = rdma_resolve_route(
                                 cmid, *kiblnd_tunables.kib_timeout * 1000);
-                        if (rc == 0)
-                                return 0;
+			if (rc == 0) {
+				kib_net_t *net = peer_ni->ibp_ni->ni_data;
+				kib_dev_t *dev = net->ibn_dev;
+
+				CDEBUG(D_NET, "%s: connection bound to "\
+				       "%s:%pI4h:%s\n",
+				       libcfs_nid2str(peer_ni->ibp_nid),
+				       dev->ibd_ifname,
+				       &dev->ibd_ifip, cmid->device->name);
+
+				return 0;
+			}
+
                         /* Can't initiate route resolution */
                         CERROR("Can't resolve route for %s: %d\n",
                                libcfs_nid2str(peer_ni->ibp_nid), rc);

Cray/lustre

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1335,12 +1335,7 @@ kiblnd_connect_peer (kib_peer_t *peer)
                 goto failed2;
         }
 
-        LASSERT (cmid->device != NULL);
-        CDEBUG(D_NET, "%s: connection bound to %s:%u.%u.%u.%u:%s\n",
-               libcfs_nid2str(peer->ibp_nid), dev->ibd_ifname,
-               HIPQUAD(dev->ibd_ifip), cmid->device->name);
-
-        return;
+	return;
 
  failed2:
         kiblnd_peer_decref(peer);               /* cmid's ref */
@@ -3009,8 +3004,19 @@ kiblnd_cm_callback(struct rdma_cm_id *cmid, struct rdma_cm_event *event)
                 } else {
                         rc = rdma_resolve_route(
                                 cmid, *kiblnd_tunables.kib_timeout * 1000);
-                        if (rc == 0)
-                                return 0;
+			if (rc == 0) {
+				kib_net_t *net = peer->ibp_ni->ni_data;
+				kib_dev_t *dev = net->ibn_dev;
+
+				CDEBUG(D_NET, "%s: connection bound to "\
+				       "%s:%pI4h:%s\n",
+				       libcfs_nid2str(peer->ibp_nid),
+				       dev->ibd_ifname,
+				       &dev->ibd_ifip, cmid->device->name);
+
+				return 0;
+			}
+
                         /* Can't initiate route resolution */
                         CERROR("Can't resolve route for %s: %d\n",
                                libcfs_nid2str(peer->ibp_nid), rc);