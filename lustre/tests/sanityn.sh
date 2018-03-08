e996c681c57a7771da0b579982135da946158e55
Patrick Farrell <paf@cray.com>
2018-03-08 05:47:56 -0600

    LU-10337 mdt: Allow open of open orphans
    Standard open by handle behavior allows opening of open
    unlinked files files.  This currently only works in Lustre
    if the file is already open on the same node, which is
    insufficient.
    
    When an open file is unlinked, we make it an orphan.
    These files can be recognized by checking their open count
    (mod_count).  It's enough to just make opening these files
    possible, because the client cannot look them up to do an
    open except when using a file handle.
    
    Cray-bug-id: LUS-2626
    Change-Id: Idd7898cefcf60b28c682e578774411e476216c9e
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Reviewed-on: https://review.whamcloud.com/30405
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Jenkins
    Tested-by: Maloo <hpdd-maloo@intel.com>
    Reviewed-by: Alexey Lyashkov <c17817@cray.com>
    Reviewed-by: James Simmons <uja.ornl@yahoo.com>
    Reviewed-by: Andrew Perepechko <c17827@cray.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/sanityn.sh
+++ lustre/tests/sanityn.sh
@@ -4307,6 +4307,22 @@ test_101c() {
 }
 run_test 101c "Discard DoM data on close-unlink"
 
+# test to verify file handle related system calls
+# (name_to_handle_at/open_by_handle_at)
+# The new system calls are supported in glibc >= 2.14.
+
+# test to verify we can open by handle an unlinked file from > 1 client
+# This test opens the file normally on $DIR1, which is on one mount, and then
+# opens it by handle on $DIR2, which is on a different mount.
+test_102() {
+	echo "Test file_handle syscalls" > $DIR/$tfile ||
+		error "write failed"
+	check_fhandle_syscalls $DIR/$tfile $DIR2 ||
+		error "check_fhandle_syscalls failed"
+	rm -f $DIR2/$tfile
+}
+run_test 102 "Test open by handle of unlinked file"
+
 # Compare file size between first & second mount, ensuring the client correctly
 # glimpses even with unused speculative locks - LU-11670
 test_103() {
