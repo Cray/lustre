Xyratex/lustre-stable

LUS-2590 lnet: Turn on D_NETERROR printk
Turn on D_NETERROR printk and add a debugging output for lnet,
gnilnd debugging.

Signed-off-by: Chris Horn <hornc@cray.com>
Change-Id: Iab3c3f77169af453120d0fbda799b9523f1806ab
Reviewed-on: http://es-gerrit.dev.cray.com:8080/17257
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Alexander Zarochentsev <c17826@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lnet/lnet/lib-move.c
@@ -808,8 +808,13 @@ lnet_peer_is_alive(struct lnet_peer_ni *lp, time64_t now)
 {
 	int alive;
 	time64_t deadline;
+	ENTRY;
 
 	LASSERT (lnet_peer_aliveness_enabled(lp));
+
+	CDEBUG(D_NET, "lp 0x%p->%s alive %d count %d last_alive %llu stamp %llu\n",
+               lp, libcfs_nid2str(lp->lpni_nid), lp->lpni_alive,
+               lp->lpni_alive_count, lp->lpni_last_alive, lp->lpni_timestamp);
 
 	/*
 	 * Trust lnet_notify() if it has more recent aliveness news, but
@@ -819,7 +824,7 @@ lnet_peer_is_alive(struct lnet_peer_ni *lp, time64_t now)
 	if (!lp->lpni_alive && lp->lpni_alive_count > 0 &&
 	    lp->lpni_timestamp >= lp->lpni_last_alive) {
 		spin_unlock(&lp->lpni_lock);
-		return 0;
+                RETURN(0);
 	}
 
 	deadline = lp->lpni_last_alive +
@@ -839,7 +844,7 @@ lnet_peer_is_alive(struct lnet_peer_ni *lp, time64_t now)
 		spin_unlock(&lp->lpni_lock);
 	}
 
-	return alive;
+        RETURN(alive);
 }
 
 

Cray/lustre

lnet/lnet/lib-move.c
@@ -701,15 +701,20 @@ lnet_peer_is_alive (lnet_peer_t *lp, cfs_time_t now)
 {
         int        alive;
         cfs_time_t deadline;
+	ENTRY;
 
         LASSERT (lnet_peer_aliveness_enabled(lp));
+
+        CDEBUG(D_NET, "lp 0x%p->%s alive %d count %d last_alive %lu stamp %lu\n",
+               lp, libcfs_nid2str(lp->lp_nid), lp->lp_alive,
+               lp->lp_alive_count, lp->lp_last_alive, lp->lp_timestamp);
 
         /* Trust lnet_notify() if it has more recent aliveness news, but
          * ignore the initial assumed death (see lnet_peers_start_down()).
          */
         if (!lp->lp_alive && lp->lp_alive_count > 0 &&
             cfs_time_aftereq(lp->lp_timestamp, lp->lp_last_alive))
-                return 0;
+                RETURN(0);
 
         deadline = cfs_time_add(lp->lp_last_alive,
                                 cfs_time_seconds(lp->lp_ni->ni_peertimeout));
@@ -723,7 +728,7 @@ lnet_peer_is_alive (lnet_peer_t *lp, cfs_time_t now)
             !(lnet_isrouter(lp) && lp->lp_alive_count == 0))
                 lnet_notify_locked(lp, 0, 1, lp->lp_last_alive);
 
-        return alive;
+        RETURN(alive);
 }
 
 