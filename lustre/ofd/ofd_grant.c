Xyratex/lustre-stable

LUS-2146 ofd: force transaction commit on grant lack
The following test
	addr = mmap(.., MAP_SHARED, ..);
	for(;;)
		memcpy(addr, ...);

may end up with receiving SIGBUS. This is because ll_page_mkwrite()
(lustre implementation of page_mkwrite method of vm_operations_struct)
is unable to handle case of insufficient grants.

An OST allocates grants based on amount of space available on a
backend filesystem reduced by amount of space already granted to
clients (see ofd_grant_space_left()). The amount of space granted to
clients gets released in transaction commit hook (ofd_grant_commit()).
If the OST manages to grant almost all available space to clients in
time interval between subsequent transaction commits - it degrades
grants allocation so that clients end up with ll_page_mkwrite failure.

This failure steadily reproduces on testing 12mb long file on 640mb
OSTs.

The fix is to toughen the threshold of free space exhaustion at which
a sync is triggered to release grants. left >> 3 is used as
ofd_grant_alloc() does not grant more than 1/8th of the remaining free
space in one chunk.

ll_page_mkwrite() always uses pages which consume grants, so check for
OBD_BRW_FROM_GRANT of network buffers is removed.

Signed-off-by: Vladimir Saveliev <c17830@cray.com>
Change-Id: Icddca16de33a8101e660349d5c2ff2d90ef33489
Test-Parameters: testlist=ltp-fs
Reviewed-on: https://es-gerrit.dev.cray.com/17694
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/ofd/ofd_grant.c
@@ -960,26 +960,12 @@ refresh:
 	/* When close to free space exhaustion, trigger a sync to force
 	 * writeback cache to consume required space immediately and release as
 	 * much space as possible. */
-	if (!obd->obd_recovering && force != 2 && left < OFD_GRANT_CHUNK) {
-		bool from_grant = true;
-		int  i;
-
-		/* That said, it is worth running a sync only if some pages did
-		 * not consume grant space on the client and could thus fail
-		 * with ENOSPC later in ofd_grant_check() */
-		for (i = 0; i < niocount; i++)
-			if (!(rnb[i].rnb_flags & OBD_BRW_FROM_GRANT))
-				from_grant = false;
-
-		if (!from_grant) {
-			/* at least one network buffer requires acquiring grant
-			 * space on the server */
-			spin_unlock(&ofd->ofd_grant_lock);
-			/* discard errors, at least we tried ... */
-			rc = dt_sync(env, ofd->ofd_osd);
-			force = 2;
-			goto refresh;
-		}
+	if (!obd->obd_recovering && force != 2 && (left >> 3) < OFD_GRANT_CHUNK) {
+		spin_unlock(&ofd->ofd_grant_lock);
+		/* discard errors, at least we tried ... */
+		rc = dt_sync(env, ofd->ofd_osd);
+		force = 2;
+		goto refresh;
 	}
 
 	/* extract incoming grant information provided by the client */

Cray/lustre

lustre/ofd/ofd_grant.c
@@ -1097,26 +1097,12 @@ refresh:
 	/* When close to free space exhaustion, trigger a sync to force
 	 * writeback cache to consume required space immediately and release as
 	 * much space as possible. */
-	if (!obd->obd_recovering && force != 2 && left < chunk) {
-		bool from_grant = true;
-		int  i;
-
-		/* That said, it is worth running a sync only if some pages did
-		 * not consume grant space on the client and could thus fail
-		 * with ENOSPC later in ofd_grant_check() */
-		for (i = 0; i < niocount; i++)
-			if (!(rnb[i].rnb_flags & OBD_BRW_FROM_GRANT))
-				from_grant = false;
-
-		if (!from_grant) {
-			/* at least one network buffer requires acquiring grant
-			 * space on the server */
-			spin_unlock(&ofd->ofd_grant_lock);
-			/* discard errors, at least we tried ... */
-			rc = dt_sync(env, ofd->ofd_osd);
-			force = 2;
-			goto refresh;
-		}
+	if (!obd->obd_recovering && force != 2 && (left >> 3) < chunk) {
+		spin_unlock(&ofd->ofd_grant_lock);
+		/* discard errors, at least we tried ... */
+		rc = dt_sync(env, ofd->ofd_osd);
+		force = 2;
+		goto refresh;
 	}
 
 	/* extract incoming grant information provided by the client,