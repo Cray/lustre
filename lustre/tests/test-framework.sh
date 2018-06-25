39032cd5988ee7075746073c19ef3d2a6c9f1a6d
Elena Gryaznova <c17455@cray.com>
2018-06-25 20:42:07 +0300

    LUS-2361 tests: one more mgs failover fix
    mgs failover still does not work properly,
    the tests failed as:
    get_param: */MGS/recovery_status: Found no match
    
    init_facet_vars () sets mgsfailover_HOST=$mgs_HOST
    instead of mgsfailover_HOST=$mds1failover_HOST for
    setup, where mds1_HOST != mds1failover_HOST.
    
    start() does not export mgs_dev and mgsfailover_dev
    for combined_mds_mgs.
    
    Patch adds these additional fixes for landed
    MRP-3374/LUS-4858.
    
    Signed-off-by: Elena Gryaznova <c17455@cray.com>
    Change-Id: I04cd36ef75911a8a5f4f2a674b87d9202f9bb44d
    Reviewed-on: https://es-gerrit.dev.cray.com/153265
    Reviewed-by: Vladimir Saveliev <c17830@cray.com>
    Reviewed-by: Alexander Boyko <c17825@cray.com>

--- lustre/tests/test-framework.sh
+++ lustre/tests/test-framework.sh
@@ -1332,11 +1332,17 @@ start() {
     eval export ${facet}_dev=${device}
     eval export ${facet}_opt=\"$@\"
 
+    combined_mgs_mds && [[ ${dev_alias} == mds1 ]] &&
+        eval export mgs_dev=${device}
+
     local varname=${facet}failover_dev
     if [ -n "${!varname}" ] ; then
         eval export ${facet}failover_dev=${!varname}
     else
         eval export ${facet}failover_dev=$device
+        combined_mgs_mds && [[ ${dev_alias} == mds1 ]] &&
+            eval export mgsfailover_dev=${device}
+
     fi
 
 	local mntpt=$(facet_mntpt $facet)
@@ -2336,8 +2342,11 @@ wait_recovery_complete () {
     fi
     echo affected facets: $facets
 
-	# we can use "for" here because we are waiting the slowest
-	for facet in ${facets//,/ }; do
+	facets=${facets//,/ }
+	# We can use "for" here because we are waiting the slowest.
+	# The mgs not having the recovery_status proc entry, exclude it
+	# from the facet list.
+	for facet in ${facets//mgs/ }; do
 		local var_svc=${facet}_svc
 		local param="*.${!var_svc}.recovery_status"
 
@@ -2587,7 +2596,7 @@ facet_failover() {
 			list_member ${affecteds[index]} mgs; then
 			mount_facet mgs || error "Restart of mgs failed"
 			affecteds[index]=$(exclude_items_from_list \
-			    ${affecteds[index]} mgs)
+				${affecteds[index]} mgs)
 		fi
 		# FIXME; has to be changed to mount all facets concurrently
 		if [ -n "${affecteds[index]}" ]; then
@@ -2870,7 +2879,14 @@ facet_host() {
 		elif [ "${facet:0:3}" == "mdt" -o \
 			"${facet:0:3}" == "mds" -o \
 			"${facet:0:3}" == "mgs" ]; then
-			eval export ${facet}_HOST=${mds_HOST}
+			local temp
+			if [ "${facet}" == "mgsfailover" ] &&
+			   [ -n "$mds1failover_HOST" ]; then
+				temp=$mds1failover_HOST
+			else
+				temp=${mds_HOST}
+			fi
+			eval export ${facet}_HOST=$temp
 		fi
 	fi
 	echo -n ${!varname}
@@ -2879,7 +2895,6 @@ facet_host() {
 facet_failover_host() {
 	local facet=$1
 	local varname
-	local temp
 
 	var=${facet}failover_HOST
 	if [ -n "${!var}" ]; then
@@ -2887,9 +2902,9 @@ facet_failover_host() {
 		return
 	fi
 
-	if combined_mgs_mds && [ $facet == "mgs" ]; then
-		temp=mds1failover_HOST
-		echo ${!temp}
+	if combined_mgs_mds && [ $facet == "mgs" ] &&
+	   [ -n "$mds1failover_HOST" ]; then
+		echo $mds1failover_HOST
 		return
 	fi
 
@@ -3899,7 +3914,14 @@ init_facet_vars () {
 
 	local varname=${facet}failover_HOST
 	if [ -z "${!varname}" ]; then
-		eval export $varname=$(facet_host $facet)
+		local temp
+		if combined_mgs_mds && [ $facet == "mgs" ] &&
+		   [ -n "$mds1failover_HOST" ]; then
+			temp=$mds1failover_HOST
+		else
+			temp=$(facet_host $facet)
+		fi
+		eval export $varname=$temp
 	fi
 
 	varname=${facet}_HOST
