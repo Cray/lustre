400909731892015b2fc5fbf8f09cba71805cf17c
Alexey Lyashkov <c17817@cray.com>
2018-07-17 12:36:55 +0300

    LUS-5841 osp: fix race when lov objid update
    first thread can be delayed due reading from disk,
    so it will completed after second and overwrite an
    on disk data with stale data
    
    Change-Id: I0984e5f55d569260c1219bf87c82423cc5b8589b
    Signed-off-by: Alexey Lyashkov <c17817@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/153291
    Reviewed-by: Alexander Boyko <c17825@cray.com>
    Reviewed-by: Sergey Cheremencev <c17829@cray.com>
    Tested-by: Elena Gryaznova <c17455@cray.com>

--- lustre/osp/osp_object.c
+++ lustre/osp/osp_object.c
@@ -1440,9 +1440,8 @@ static int osp_declare_object_create(const struct lu_env *env,
 
 	if (unlikely(!fid_is_zero(fid))) {
 		/* replay case: caller knows fid */
-		osi->osi_off = sizeof(osi->osi_id) * d->opd_index;
-		osi->osi_lb.lb_len = sizeof(osi->osi_id);
-		osi->osi_lb.lb_buf = NULL;
+		osp_objid_buf_prep(&osi->osi_lb, &osi->osi_off, NULL,
+				d->opd_index);
 		rc = dt_declare_record_write(env, d->opd_last_used_oid_file,
 					     &osi->osi_lb, osi->osi_off, th);
 		RETURN(rc);
@@ -1465,9 +1464,8 @@ static int osp_declare_object_create(const struct lu_env *env,
 		o->opo_reserved = 1;
 
 		/* common for all OSPs file hystorically */
-		osi->osi_off = sizeof(osi->osi_id) * d->opd_index;
-		osi->osi_lb.lb_len = sizeof(osi->osi_id);
-		osi->osi_lb.lb_buf = NULL;
+		osp_objid_buf_prep(&osi->osi_lb, &osi->osi_off, NULL,
+				d->opd_index);
 		rc = dt_declare_record_write(env, d->opd_last_used_oid_file,
 					     &osi->osi_lb, osi->osi_off, th);
 	} else {
@@ -1509,7 +1507,6 @@ static int osp_object_create(const struct lu_env *env, struct dt_object *dt,
 	struct osp_object	*o = dt2osp_obj(dt);
 	int			rc = 0;
 	struct lu_fid		*fid = &osi->osi_fid;
-	struct lu_fid		*last_fid = &d->opd_last_used_fid;
 	ENTRY;
 
 	if (is_only_remote_trans(th) &&
@@ -1582,13 +1579,8 @@ static int osp_object_create(const struct lu_env *env, struct dt_object *dt,
 
 	/* Only need update last_used oid file, seq file will only be update
 	 * during seq rollover */
-	if (fid_is_idif((last_fid)))
-		osi->osi_id = fid_idif_id(fid_seq(last_fid),
-					  fid_oid(last_fid), fid_ver(last_fid));
-	else
-		osi->osi_id = fid_oid(last_fid);
 	osp_objid_buf_prep(&osi->osi_lb, &osi->osi_off,
-			   &osi->osi_id, d->opd_index);
+			   &d->opd_last_id, d->opd_index);
 
 	rc = dt_record_write(env, d->opd_last_used_oid_file, &osi->osi_lb,
 			     &osi->osi_off, th);
