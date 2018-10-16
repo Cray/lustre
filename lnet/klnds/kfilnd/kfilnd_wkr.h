8a27cf4ca8001a72dec48755a953173fbbebc1fa
Doug Oucharek <doucharek@cray.com>
2018-10-16 15:51:21 -0700

    LUS-6565 kfilnd: Initial Prototype from cassini-lnd
    This is a direct copy of the prototype in the cassini-lnd project.
    All patches that went into this prototype have been inspected and
    run on an emulator.  The prototype is being pushed into the Lustre
    repo where work will continue on it.
    
    Note: this first patch is a direct copy and does not build in the
    Lustre repo.  A following patch will introduce the changes required
    for building and running this prototype from the Lustre repo.
    
    Signed-off-by: Doug Oucharek <doucharek@cray.com>
    Change-Id: Ie92f3d6b8b062365b25064115def3f97598b05d6

---
+++ lnet/klnds/kfilnd/kfilnd_wkr.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * kfilnd worker thread pool.
+ * Copyright 2018 Cray Inc. All Rights Reserved.
+ *
+ */
+
+#ifndef _KFILND_WKR_
+#define _KFILND_WKR_
+
+/* The callbacks posted to this system need this format */
+typedef void (*kfilnd_wkr_func)(void *devctx, void *context, int status);
+
+/* Use this call after init and start to post work to the Workqueue */
+int kfilnd_wkr_post(unsigned int cpt, kfilnd_wkr_func work_func,
+		    void *dev_context, void *work_context, int status);
+
+/* Called when this API will no longer be used */
+void kfilnd_wkr_cleanup(void);
+
+/* Called once before kiflnd_wkr_cleanup() to stop the threads */
+int kfilnd_wkr_stop(void);
+
+/* Called once after kiflnd_wkr_init() to allow posting of work */
+int kfilnd_wkr_start(void);
+
+/* Called once to intialize this API */
+int kfilnd_wkr_init(unsigned int max_parallel, unsigned int max_work, bool use_workqueues);
+
+#endif /* _KFILND_WKR_ */
