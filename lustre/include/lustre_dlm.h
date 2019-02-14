85c2a9632f57b3ba1636ec99fdf768aefe859421
Mikhail Pershin <mpershin@whamcloud.com>
2019-02-15 00:51:00 +0300

    LU-11964 mdc: prevent glimpse lock count grow
    DOM locks matching tries to ignore locks with
    LDLM_FL_KMS_IGNORE flag during ldlm_lock_match() but
    checks that after ldlm_lock_match() call. Therefore if
    there is any lock with such flag in queue then all other
    locks after it are ignored and new lock is created causing
    big amount of locks on single resource in some access
    patterns.
    Patch extends lock_matches() function to check flags to
    exclude and adds ldlm_lock_match_with_skip()p to use that
    when needed.
    Corresponding test was added in sanity-dom.sh
    
    Test-Parameters: testlist=sanity-dom
    
    Lustre-change: https://review.whamcloud.com/34261
    Lustre-commit: b915221b6d0f3457fd9dd202a9d14c5f8385bf47
    
    Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
    Change-Id: Ic45ca10f0e603e79a3a00e4fde13a5fae15ea5fc
    Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
    Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
    Signed-off-by: Minh Diep <mdiep@whamcloud.com>
    Reviewed-on: https://review.whamcloud.com/34504
    Tested-by: Jenkins
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/include/lustre_dlm.h
+++ lustre/include/lustre_dlm.h
@@ -961,6 +961,7 @@ struct ldlm_match_data {
 	union ldlm_policy_data	*lmd_policy;
 	__u64			 lmd_flags;
 	int			 lmd_unref;
+	__u64			 lmd_skip_flags;
 	bool			 lmd_has_ast_data;
 };
 
@@ -1532,10 +1533,27 @@ void ldlm_lock_fail_match_locked(struct ldlm_lock *lock);
 void ldlm_lock_fail_match(struct ldlm_lock *lock);
 void ldlm_lock_allow_match(struct ldlm_lock *lock);
 void ldlm_lock_allow_match_locked(struct ldlm_lock *lock);
-enum ldlm_mode ldlm_lock_match(struct ldlm_namespace *ns, __u64 flags,
-			       const struct ldlm_res_id *, enum ldlm_type type,
-			       union ldlm_policy_data *, enum ldlm_mode mode,
-			       struct lustre_handle *, int unref);
+enum ldlm_mode ldlm_lock_match_with_skip(struct ldlm_namespace *ns,
+					 __u64 flags, __u64 skip_flags,
+					 const struct ldlm_res_id *res_id,
+					 enum ldlm_type type,
+					 union ldlm_policy_data *policy,
+					 enum ldlm_mode mode,
+					 struct lustre_handle *lh,
+					 int unref);
+static inline enum ldlm_mode ldlm_lock_match(struct ldlm_namespace *ns,
+					     __u64 flags,
+					     const struct ldlm_res_id *res_id,
+					     enum ldlm_type type,
+					     union ldlm_policy_data *policy,
+					     enum ldlm_mode mode,
+					     struct lustre_handle *lh,
+					     int unref)
+{
+	return ldlm_lock_match_with_skip(ns, flags, 0, res_id, type, policy,
+					 mode, lh, unref);
+}
+
 struct ldlm_lock *search_itree(struct ldlm_resource *res,
 			       struct ldlm_match_data *data);
 enum ldlm_mode ldlm_revalidate_lock_handle(const struct lustre_handle *lockh,
