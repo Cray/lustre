Cray/lustre

commit 16b5c5211b9c0bd188521b3092a473a2d24ef40b

LUS-2403 mdt: mdt_init0 failure handling
When mdt_init0 fails it has to wait until zombied thread has all
disconnected exports destroyed before mdt_device_alloc will free the
mdt_device. Otherwise, zombied thread refers (via
tgt_client_free) to freed mdt_device.

- mdt_init0
    call to target_recovery_fini is moved so that it is called on
    every failure after successful tgt_init.

    obd_zombie_barrier is to be called after
    target_recovery_fini->class_disconnect_exports

    obd->obd_fail is set so that mdt_export_cleanup->tgt_client_del
    did not clear client's slot in last_rcvd in case of server start
    failure

- mdt_quota_init
    class_manual_clean does class_detach, goto is added to avoid
    repeated call to class_detach

- qmt_device_init0
    start qmt rebalance thread with SVC_STARTING flag so that
    qmt_start_reba_thread waited until the thread has started.
    Otherwise, qmt_device may get freed before qmt rebalance thread is
    stopped

Change-Id: I1a5dd8bd8b90e0b3f54d1530eca7cead9d815bab
Signed-off-by: Vladimir Saveliev <vladimir.saveliev@seagate.com>
Reviewed-on: https://es-gerrit.dev.cray.com/154267
Tested-by: Elena Gryaznova <c17455@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Reviewed-by: Sergey Cheremencev <c17829@cray.com>

lustre/quota/qmt_dev.c
@@ -240,7 +240,7 @@ static int qmt_device_init0(const struct lu_env *env, struct qmt_device *qmt,
 		GOTO(out, rc);
 
 	/* set up and start rebalance thread */
-	thread_set_flags(&qmt->qmt_reba_thread, SVC_STOPPED);
+	thread_set_flags(&qmt->qmt_reba_thread, SVC_STARTING);
 	init_waitqueue_head(&qmt->qmt_reba_thread.t_ctl_waitq);
 	INIT_LIST_HEAD(&qmt->qmt_reba_list);
 	spin_lock_init(&qmt->qmt_reba_lock);

lustre/mdt/mdt_handler.c
@@ -4821,6 +4821,7 @@ class_cleanup:
 	if (rc) {
 		class_manual_cleanup(obd);
 		mdt->mdt_qmt_dev = NULL;
+		GOTO(lcfg_cleanup, rc);
 	}
 class_detach:
 	if (rc)
@@ -5297,12 +5298,16 @@ static int mdt_init0(const struct lu_env *env, struct mdt_device *m,
 err_procfs:
 	mdt_procfs_fini(m);
 err_recovery:
-	target_recovery_fini(obd);
 	upcall_cache_cleanup(m->mdt_identity_cache);
 	m->mdt_identity_cache = NULL;
 err_fs_cleanup:
 	mdt_fs_cleanup(env, m);
 err_tgt:
+	/* keep recoverable clients */
+	obd->obd_fail = 1;
+	target_recovery_fini(obd);
+	obd_exports_barrier(obd);
+	obd_zombie_barrier();
 	tgt_fini(env, &m->mdt_lut);
 err_free_hsm:
 	mdt_hsm_cdt_fini(m);

lustre/quota/qmt_lock.c
@@ -796,12 +796,15 @@ static int qmt_reba_thread(void *arg)
 	ENTRY;
 
 	OBD_ALLOC_PTR(env);
-	if (env == NULL)
+	if (env == NULL) {
+		thread_set_flags(thread, SVC_STOPPED);
 		RETURN(-ENOMEM);
+	}
 
 	rc = lu_env_init(env, LCT_MD_THREAD);
 	if (rc) {
 		CERROR("%s: failed to init env.", qmt->qmt_svname);
+		thread_set_flags(thread, SVC_STOPPED);
 		OBD_FREE_PTR(env);
 		RETURN(rc);
 	}

commit a2f4287b9fa66ef3ac38331b3135e41c7135a307

LUS-2403 quota: set qmt_reba_thread flag correctly
If mdt is mounted reaonly, qmt_reba_thread does not get started, so
qmt_stop_reba_thread waits forever until thread is stopped.

Set SVC_STARTING qmt_reba_thread flag only of qmt_reba_thread starts.

Typo in sanity.sh:test_802 is fixed.

Fixes: 16b5c5211b ("LUS-2403 mdt: mdt_init0 failure handling")

Signed-off-by: Vladimir Saveliev <c17830@cray.com>
Test-Parameters: testlist=sanity/802
Change-Id: Ifa109e007ab5df57c03129e482ca369f9c0e9e6b
Reviewed-on: https://es-gerrit.dev.cray.com/154606
Reviewed-by: Sergey Cheremencev <c17829@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/quota/qmt_dev.c
@@ -240,11 +240,12 @@ static int qmt_device_init0(const struct lu_env *env, struct qmt_device *qmt,
 		GOTO(out, rc);
 
 	/* set up and start rebalance thread */
-	thread_set_flags(&qmt->qmt_reba_thread, SVC_STARTING);
+	thread_set_flags(&qmt->qmt_reba_thread, SVC_STOPPED);
 	init_waitqueue_head(&qmt->qmt_reba_thread.t_ctl_waitq);
 	INIT_LIST_HEAD(&qmt->qmt_reba_list);
 	spin_lock_init(&qmt->qmt_reba_lock);
 	if (!qmt->qmt_child->dd_rdonly) {
+		thread_set_flags(&qmt->qmt_reba_thread, SVC_STARTING);
 		rc = qmt_start_reba_thread(qmt);
 		if (rc != 0) {
 			CERROR("%s: failed to start rebalance thread (%d)\n",