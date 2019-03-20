07ef12e1b1d44a7383f8a94833af3d5c49cbb294
Amir Shehata <ashehata@whamcloud.com>
2019-03-20 12:14:51 -0700

    LU-12080 lnet: clean mt_eqh properly
    There is a scenario where you have a peer on your recovery queue
    that's down. So you keep pinging it, but every ping times out
    after 10 seconds. In the middle of these 10 seconds you perform a
    shutdown. First you try to do the rsp_tracker_clean. It goes through
    and calls MDUnlink on the MD related to that ping. But because the
    message has a ref count on the MD, it doesn't go away. The MD gets
    zombied. And just waits for lnet_md_unlink to be called in
    lnet_finalize(). Then you hit clean_peer_ni_recovery. We see the peer
    on the queue, we try to call Unlink on it, but when we lookup the
    MD using lnet_handle2md() we can't find it. Afterwards we try to clean
    up the EQ and it asserts. Even if we remove the assert we end up with
    a resource leak since the EQ is not actually freed since we won't call
    LNetEQFree() again.
    
    The solution is to pull the EQ create in the LNetNIInit() and deletion
    happens in lnet_unprepare. By this point all the remaining messages
    would've been finalized and all references on the EQ are gone,
    allowing us to clean it up properly
    
    Test-Parameters: forbuildonly
    Signed-off-by: Amir Shehata <ashehata@whamcloud.com>
    Change-Id: I7fd6018ee2e57f82c649fc3658352e89a4309986
    Reviewed-on: https://review.whamcloud.com/34477
    Reviewed-by: Olaf Weber <olaf.weber@hpe.com>
    Reviewed-by: Chris Horn <hornc@cray.com>
    Tested-by: Jenkins

--- lnet/lnet/lib-move.c
+++ lnet/lnet/lib-move.c
@@ -3516,7 +3516,7 @@ lnet_handle_recovery_reply(struct lnet_mt_event_info *ev_info,
 	}
 }
 
-static void
+void
 lnet_mt_event_handler(struct lnet_event *event)
 {
 	struct lnet_mt_event_info *ev_info = event->md.user_ptr;
@@ -3594,12 +3594,6 @@ int lnet_monitor_thr_start(void)
 	if (rc)
 		goto clean_queues;
 
-	rc = LNetEQAlloc(0, lnet_mt_event_handler, &the_lnet.ln_mt_eqh);
-	if (rc != 0) {
-		CERROR("Can't allocate monitor thread EQ: %d\n", rc);
-		goto clean_queues;
-	}
-
 	/* Pre monitor thread start processing */
 	rc = lnet_router_pre_mt_start();
 	if (rc)
@@ -3632,7 +3626,6 @@ free_mem:
 	lnet_clean_local_ni_recoveryq();
 	lnet_clean_peer_ni_recoveryq();
 	lnet_clean_resendqs();
-	LNetEQFree(the_lnet.ln_mt_eqh);
 	LNetInvalidateEQHandle(&the_lnet.ln_mt_eqh);
 	return rc;
 clean_queues:
@@ -3645,8 +3638,6 @@ clean_queues:
 
 void lnet_monitor_thr_stop(void)
 {
-	int rc;
-
 	if (the_lnet.ln_mt_state == LNET_MT_STATE_SHUTDOWN)
 		return;
 
@@ -3666,8 +3657,7 @@ void lnet_monitor_thr_stop(void)
 	lnet_clean_local_ni_recoveryq();
 	lnet_clean_peer_ni_recoveryq();
 	lnet_clean_resendqs();
-	rc = LNetEQFree(the_lnet.ln_mt_eqh);
-	LASSERT(rc == 0);
+
 	return;
 }
 
