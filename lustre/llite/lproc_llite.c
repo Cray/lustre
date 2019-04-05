e5a33854c4590946dba28603929b057c0c99b882
Wang Shilong <wshilong@ddn.com>
2019-04-05 09:43:29 +0800

    LU-12043 llite: switch to use ll_fsname directly
    There are many places which try to access filesystem
    fsname, instead of parsing it everytime, just store
    it into @sbi, we could use @ll_fsname directly whenever we need.
    
    Change-Id: I25b8505b9aac5d4f6e97fe3691321be2b800f35d
    Signed-off-by: Wang Shilong <wshilong@ddn.com>
    Reviewed-on: https://review.whamcloud.com/34602
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: Jenkins
    Reviewed-by: Jian Yu <yujian@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- lustre/llite/lproc_llite.c
+++ lustre/llite/lproc_llite.c
@@ -341,7 +341,7 @@ ll_max_readahead_mb_seq_write(struct file *file, const char __user *buffer,
 	if (pages_number < 0 || pages_number > totalram_pages / 2) {
 		/* 1/2 of RAM */
 		CERROR("%s: can't set max_readahead_mb=%lu > %luMB\n",
-		       ll_get_fsname(sb, NULL, 0),
+		       sbi->ll_fsname,
 		       (unsigned long)pages_number >> (20 - PAGE_SHIFT),
 		       totalram_pages >> (20 - PAGE_SHIFT + 1));
 		return -ERANGE;
@@ -390,7 +390,7 @@ ll_max_readahead_per_file_mb_seq_write(struct file *file,
 
 	if (pages_number < 0 || pages_number > sbi->ll_ra_info.ra_max_pages) {
 		CERROR("%s: can't set max_readahead_per_file_mb=%lu > "
-		       "max_read_ahead_mb=%lu\n", ll_get_fsname(sb, NULL, 0),
+		       "max_read_ahead_mb=%lu\n", sbi->ll_fsname,
 		       (unsigned long)pages_number >> (20 - PAGE_SHIFT),
 		       sbi->ll_ra_info.ra_max_pages >> (20 - PAGE_SHIFT));
 		return -ERANGE;
@@ -444,7 +444,7 @@ ll_max_read_ahead_whole_mb_seq_write(struct file *file,
 		int pages_shift = 20 - PAGE_SHIFT;
 		CERROR("%s: can't set max_read_ahead_whole_mb=%lu > "
 		       "max_read_ahead_per_file_mb=%lu\n",
-		       ll_get_fsname(sb, NULL, 0),
+		       sbi->ll_fsname,
 		       (unsigned long)pages_number >> pages_shift,
 		       sbi->ll_ra_info.ra_max_pages_per_file >> pages_shift);
 		return -ERANGE;
@@ -517,7 +517,7 @@ static ssize_t ll_max_cached_mb_seq_write(struct file *file,
 
 	if (pages_number < 0 || pages_number > totalram_pages) {
 		CERROR("%s: can't set max cache more than %lu MB\n",
-		       ll_get_fsname(sb, NULL, 0),
+		       sbi->ll_fsname,
 		       totalram_pages >> (20 - PAGE_SHIFT));
 		RETURN(-ERANGE);
 	}
@@ -964,7 +964,7 @@ static int ll_sbi_flags_seq_show(struct seq_file *m, void *v)
 	while (flags != 0) {
 		if (ARRAY_SIZE(str) <= i) {
 			CERROR("%s: Revise array LL_SBI_FLAGS to match sbi "
-				"flags please.\n", ll_get_fsname(sb, NULL, 0));
+				"flags please.\n", ll_s2sbi(sb)->ll_fsname);
 			return -EINVAL;
 		}
 
@@ -1155,8 +1155,7 @@ static ssize_t ll_root_squash_seq_write(struct file *file,
 	struct ll_sb_info *sbi = ll_s2sbi(sb);
 	struct root_squash_info *squash = &sbi->ll_squash;
 
-	return lprocfs_wr_root_squash(buffer, count, squash,
-				      ll_get_fsname(sb, NULL, 0));
+	return lprocfs_wr_root_squash(buffer, count, squash, sbi->ll_fsname);
 }
 
 LDEBUGFS_SEQ_FOPS(ll_root_squash);
@@ -1192,8 +1191,7 @@ static ssize_t ll_nosquash_nids_seq_write(struct file *file,
 	struct root_squash_info *squash = &sbi->ll_squash;
 	int rc;
 
-	rc = lprocfs_wr_nosquash_nids(buffer, count, squash,
-				      ll_get_fsname(sb, NULL, 0));
+	rc = lprocfs_wr_nosquash_nids(buffer, count, squash, sbi->ll_fsname);
 	if (rc < 0)
 		return rc;
 
