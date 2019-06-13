fda59160f1f55f59f42899198747bd6187fb2a6b
Ian Ziemba <iziemba@cray.com>
2019-06-13 14:14:13 -0500

    LUS-7478 kfilnd: Support multiple LNet networks
    Each LNet NI is assigned a KFI LND device. This reserves a unique set
    of kfabric resources (scalable endpoint, CQs, and TX/RX contexts) for
    each LNet NI. But, if the LNet NI is using the same NIC as other LNet
    NIs, some kfabric resources (fabrics and domains) can be shared.
    
    To support the sharing of kfabric fabrics and domains, a KFI LND
    domain structure is defined. During KFI LND device allocating, a KFI
    LND domain is acquired. During KFI LND domain acquisition, checks are
    performed to see if a previous KFI LND domain can be reused. KFI LND
    domain reuse is preferred over allocation of a new KFI LND domain.
    
    This should allow for the sharing of some resources between LNet NIs
    using the same NIC while still being about to allocate unique kfabric
    resources critical for data transfer on a per LNet NI basis.
    
    Signed-off-by: Ian Ziemba <iziemba@cray.com>
    Change-Id: I089187f12493af5d4e368320f20dba58420c9605
    Reviewed-on: https://es-gerrit.dev.cray.com/155312
    Reviewed-by: Frank Zago <fzago@cray.com>

---
+++ lnet/klnds/kfilnd/kfilnd_dom.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * kfilnd domain implementation.
+ * Copyright 2019 Cray Inc. All Rights Reserved.
+ */
+#ifndef _KFILND_DOM_
+#define _KFILND_DOM_
+
+#include "kfilnd.h"
+
+void kfilnd_dom_put(struct kfilnd_dom *dom);
+struct kfilnd_dom *kfilnd_dom_get(struct lnet_ni *ni,
+				  struct kfi_info **dev_info);
+
+#endif /* _KFILND_DOM_ */
