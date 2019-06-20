4714126b74b286920bba0c86afda26b44783f24f
Ian Ziemba <iziemba@cray.com>
2019-06-20 13:56:13 -0500

    LUS-7508 kfilnd: Refactor event handling
    When a KFI LND endpoint receives an event, the entire event is handed
    to the transaction system for processing.
    
    Signed-off-by: Ian Ziemba <iziemba@cray.com>
    Change-Id: I64b3a403a4ac6077a5a7c130ae1033f979d725c6
    Reviewed-on: https://es-gerrit.dev.cray.com/155345
    Reviewed-by: Frank Zago <fzago@cray.com>

--- lnet/klnds/kfilnd/kfilnd_tn.h
+++ lnet/klnds/kfilnd/kfilnd_tn.h
@@ -8,6 +8,8 @@
 
 #include "kfilnd.h"
 
+void kfilnd_tn_cq_error(struct kfilnd_ep *ep, struct kfi_cq_err_entry *error);
+void kfilnd_tn_cq_event(struct kfilnd_ep *ep, struct kfi_cq_data_entry *event);
 void kfilnd_tn_free(struct kfilnd_transaction *tn);
 struct kfilnd_transaction *kfilnd_tn_alloc(struct kfilnd_dev *dev, int cpt,
 					   bool alloc_msg);
