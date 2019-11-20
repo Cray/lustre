828e582419c70507c4bb65a72d0767a8a391785b
Ian Ziemba <iziemba@cray.com>
2019-11-20 13:08:01 -0600

    LUS-8108 kfilnd: Rework peer cache
    Since the previous peer cache implementation did not take a reference
    against the peer's KFI address when performing a transaction, it is
    possible that the KFI address could have been removed from the KFI
    address vector, but a transaction may still be using it.
    
    To prevent a use-after-free issue, each transaction takes a reference
    against the peer structure. Once the transaction is complete, a
    reference is returned.
    
    If a transaction completes with a non-zero return code, the peer
    structure is marked for removal. Once marked, the next thread to
    return a reference to the peer structure will also cause the peer
    structure to be removed from the peer cache. Once all references are
    zero, the peer structure will be freed. Future transaction to this
    peer will require the allocation of a new peer structure.
    
    In addition, the peer cache was changed to use the Linux provided hash
    table.
    
    Signed-off-by: Ian Ziemba <iziemba@cray.com>
    Change-Id: I070ba4a9145570bd895f781ce368c3c9a924d1c5
    Reviewed-on: https://es-gerrit.dev.cray.com/156269
    Reviewed-by: Frank Zago <fzago@cray.com>

--- lnet/klnds/kfilnd/kfilnd_dev.c
+++ lnet/klnds/kfilnd/kfilnd_dev.c
@@ -6,242 +6,7 @@
 #include "kfilnd_dev.h"
 #include "kfilnd_ep.h"
 #include "kfilnd_dom.h"
-
-static unsigned int kfilnd_nid_hash(struct cfs_hash *hs, const void *key,
-				    unsigned int mask)
-{
-	return cfs_hash_u64_hash(*(lnet_nid_t *)key, mask);
-}
-
-static void *kfilnd_nid_key(struct hlist_node *hnode)
-{
-	struct kfilnd_nid_entry *entry;
-
-	entry = hlist_entry(hnode, struct kfilnd_nid_entry, node);
-	return &entry->nid;
-}
-
-static int kfilnd_nid_keycmp(const void *key, struct hlist_node *hnode)
-{
-	return !memcmp(kfilnd_nid_key(hnode), key, sizeof(lnet_nid_t));
-}
-
-static void *kfilnd_nid_object(struct hlist_node *hnode)
-{
-	return hlist_entry(hnode, struct kfilnd_nid_entry, node);
-}
-
-static void kfilnd_nid_get(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-	struct kfilnd_nid_entry *entry;
-
-	entry = hlist_entry(hnode, struct kfilnd_nid_entry, node);
-	refcount_inc(&entry->cnt);
-}
-
-static void kfilnd_nid_free(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-	struct kfilnd_nid_entry *entry;
-
-	entry = hlist_entry(hnode, struct kfilnd_nid_entry, node);
-	kfi_av_remove(entry->dev->kfd_av, &entry->addr, 1, 0);
-	kfree(entry);
-}
-
-static void kfilnd_nid_put(struct cfs_hash *hs, struct hlist_node *hnode)
-{
-	struct kfilnd_nid_entry *entry;
-
-	entry = hlist_entry(hnode, struct kfilnd_nid_entry, node);
-
-	/* Refcount only reaches zero if the entry has been deleted from the NID
-	 * hash.
-	 */
-	if (refcount_dec_and_test(&entry->cnt))
-		kfilnd_nid_free(hs, hnode);
-}
-
-static struct cfs_hash_ops kfilnd_nid_hash_ops = {
-	.hs_hash = kfilnd_nid_hash,
-	.hs_key = kfilnd_nid_key,
-	.hs_keycmp = kfilnd_nid_keycmp,
-	.hs_object = kfilnd_nid_object,
-	.hs_get = kfilnd_nid_get,
-	.hs_put = kfilnd_nid_put,
-	.hs_put_locked = kfilnd_nid_put,
-	.hs_exit = kfilnd_nid_free,
-};
-
-/**
- * kfilnd_dev_remove_peer_address() - Remove a peer from the NID hash.
- * @dev: KFI LND device.
- * @nid: Peer LNet NID to be removed.
- */
-void kfilnd_dev_remove_peer_address(struct kfilnd_dev *dev, lnet_nid_t nid)
-{
-	/* cfs_hash_del_key() will call cfs_hash_put() on the entry being
-	 * deleted. This, or an outstanding cfs_hash_put(), will push the
-	 * refcount of this entry to zero causing it to be freed.
-	 *
-	 * cfs_hash_del_key() will also return a pointer to the entry that was
-	 * freed. But, since this entry may have been freed when cfs_hash_put()
-	 * was called, the output from cfs_hash_del_key() is ignored.
-	 */
-	cfs_hash_del_key(dev->nid_hash, &nid);
-}
-
-/**
- * kfilnd_dev_lookup_peer_address() - Lookup a peer's KFI address.
- * @dev: KFI LND device.
- * @nid: Peer LNet NID used to lookup the KFI address.
- * @addr: Peer KFI address to be set.
- *
- * Lookup involves searching the device's NID hash for a matching KFI address.
- * If an entry is not found, address resolution from LNet NID to KFI address is
- * done through the KFI address vector (AV). If address resolution is
- * successful, the device's NID hash is updated with the result for future
- * lookup requests.
- *
- * Return: On success, zero is returned and the addr argument is set to the
- * matching KFI address for the given LNet NID. On error, negative errno is
- * returned.
- */
-int kfilnd_dev_lookup_peer_address(struct kfilnd_dev *dev, lnet_nid_t nid,
-				   kfi_addr_t *addr)
-{
-	char *node;
-	char *service;
-	int rc;
-	u32 nid_addr = LNET_NIDADDR(nid);
-	u32 net_num = LNET_NETNUM(LNET_NIDNET(nid));
-	struct kfilnd_nid_entry *nid_entry;
-
-again:
-	nid_entry = cfs_hash_lookup(dev->nid_hash, &nid);
-	if (nid_entry) {
-		*addr = kfi_rx_addr(nid_entry->addr,
-				    atomic_read(&nid_entry->rx_context),
-				    KFILND_FAB_RX_CTX_BITS);
-
-		cfs_hash_put(dev->nid_hash, &nid_entry->node);
-		return 0;
-	}
-
-	nid_entry = kzalloc(sizeof(*nid_entry), GFP_KERNEL);
-	if (!nid_entry) {
-		rc = -ENOMEM;
-		goto err;
-	}
-
-	node = kasprintf(GFP_KERNEL, "%pI4h", &nid_addr);
-	if (!node) {
-		rc = -ENOMEM;
-		goto err_free_nid_entry;
-	}
-
-	service = kasprintf(GFP_KERNEL, "%u", net_num);
-	if (!service) {
-		rc = -ENOMEM;
-		goto err_free_node_str;
-	}
-
-	rc = kfi_av_insertsvc(dev->kfd_av, node, service, addr, 0, dev);
-
-	kfree(service);
-	kfree(node);
-
-	if (rc < 0)
-		goto err_free_nid_entry;
-
-	nid_entry->dev = dev;
-	nid_entry->nid = nid;
-	nid_entry->addr = *addr;
-	atomic_set(&nid_entry->rx_context, 0);
-	refcount_set(&nid_entry->cnt, 0);
-
-	/* We could be racing with another thread to add this entry to the NID
-	 * hash. If so, free this entry and remove the redundant KFI address.
-	 * Then, lookup the unique entry from the NID hash.
-	 */
-	rc = cfs_hash_add_unique(dev->nid_hash, &nid_entry->nid,
-				 &nid_entry->node);
-	if (rc) {
-		kfilnd_nid_free(dev->nid_hash, &nid_entry->node);
-		goto again;
-	}
-
-	return 0;
-
-err_free_node_str:
-	kfree(node);
-err_free_nid_entry:
-	kfree(nid_entry);
-err:
-	return rc;
-}
-
-/**
- * _kfilnd_dev_update_peer_address() - Update a peer's receive context.
- * @dev: KFI LND device.
- * @nid: Peer LNet NID to be updated.
- * @rx_context: New RX context for peer LNet NID.
- *
- * Return: On success, zero. Else, -EADDRNOTAVAIL if the NID is not found in the
- * NID hash.
- */
-static int _kfilnd_dev_update_peer_address(struct kfilnd_dev *dev,
-					   lnet_nid_t nid,
-					   unsigned int rx_context)
-{
-	struct kfilnd_nid_entry *nid_entry;
-
-	nid_entry = cfs_hash_lookup(dev->nid_hash, &nid);
-	if (nid_entry) {
-		atomic_set(&nid_entry->rx_context, rx_context);
-
-		cfs_hash_put(dev->nid_hash, &nid_entry->node);
-		return 0;
-	}
-
-	return -EADDRNOTAVAIL;
-}
-
-/**
- * kfilnd_dev_update_peer_address() - Update a peer's receive context.
- * @dev: KFI LND device.
- * @nid: Peer LNet NID to be updated.
- * @rx_context: New RX context for peer LNet NID.
- *
- * Peers send a preferred RX context which the local host should use when
- * initiating a transaction to that given peer. The RX context is a specific
- * receive endpoint at the peer.
- *
- * Constantly updating this value will only impact the RX context for a future
- * transaction to a peer. Any in-flight transactions will not be impacted.
- *
- * If the NID does not exist in the NID hash, an attempt to allocate the entry
- * is done. Then, an update is attempted again.
- *
- * Return: On success, zero. Else, negative errno.
- */
-int kfilnd_dev_update_peer_address(struct kfilnd_dev *dev, lnet_nid_t nid,
-				   unsigned int rx_context)
-{
-	kfi_addr_t addr;
-	int rc;
-
-	rc = _kfilnd_dev_update_peer_address(dev, nid, rx_context);
-	if (!rc)
-		return 0;
-
-	rc = kfilnd_dev_lookup_peer_address(dev, nid, &addr);
-	if (rc)
-		return rc;
-
-	rc = _kfilnd_dev_update_peer_address(dev, nid, rx_context);
-
-	return rc;
-}
+#include "kfilnd_peer.h"
 
 /**
  * kfilnd_dev_post_imm_buffers() - Post all immediate receive buffers on each
@@ -296,8 +61,7 @@ void kfilnd_dev_free(struct kfilnd_dev *dev)
 	for (i = 0; i < dev->kfd_ni->ni_ncpts; i++)
 		kfilnd_ep_free(dev->kfd_endpoints[i]);
 
-	/* Safe to free all resources. */
-	cfs_hash_putref(dev->nid_hash);
+	kfilnd_peer_destroy(dev);
 
 	lnet_ncpts = cfs_cpt_number(lnet_cpt_table());
 	LIBCFS_FREE(dev->cpt_to_endpoint,
@@ -339,7 +103,6 @@ struct kfilnd_dev *kfilnd_dev_alloc(struct lnet_ni *ni)
 	int rc;
 	struct kfi_av_attr av_attr = {};
 	struct kfi_info *dev_info;
-	char *hash_name;
 	int cpt;
 	int lnet_ncpts;
 	struct kfilnd_dev *dev;
@@ -434,27 +197,7 @@ struct kfilnd_dev *kfilnd_dev_alloc(struct lnet_ni *ni)
 		dev->cpt_to_endpoint[cpt] = dev->kfd_endpoints[i];
 	}
 
-	/* Hash for LNet NIDs to KFI addresses. */
-	hash_name = kasprintf(GFP_KERNEL, "KFILND_NID_HASH_%X",
-			      LNET_NIDADDR(ni->ni_nid));
-	if (!hash_name) {
-		rc = -ENOMEM;
-		goto err_free_endpoints;
-	}
-
-	dev->nid_hash = cfs_hash_create(hash_name, KFILND_CURRENT_HASH_BITS,
-					KFILND_MAX_HASH_BITS,
-					KFILND_CURRENT_HASH_BITS, 0,
-					CFS_HASH_MIN_THETA, CFS_HASH_MAX_THETA,
-					&kfilnd_nid_hash_ops,
-					CFS_HASH_DEFAULT | CFS_HASH_BIGNAME);
-
-	kfree(hash_name);
-
-	if (!dev->nid_hash) {
-		rc = -ENOMEM;
-		goto err_free_endpoints;
-	}
+	kfilnd_peer_init(dev);
 
 	/* Mark that the dev/NI has now been initialized */
 	dev->kfd_state = KFILND_STATE_INITIALIZED;
