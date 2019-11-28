Cray/lustre

commit 3a7149a1a3c993f9e69aa187a97cfc0958cdabc9

LU-11089 obdclass: make key_set_version an atomic_t
As a first step to simplifying the locking in lu_object.c,
change key_set_version to an atomic_t.  This will mean
we don't need to hold a spinlock when incrementing.

It is clear that keys_fill() (and so lu_context_refill())
cannot race with itself as it holds no locks for the main
part of the work.  So updates to lc_version, and testing of
that value cannot need locking either.
So remove the locking from keys_fill() and lu_context_refill()
as there is no longer anything to protect.  The locking around
lu_keys_initing_cnt is preserved for now.

Also, don't increment when deregistering or quiescing a key.
key_set_version is *only* use to avoid filling new key values
if there have been no changes to the set of key.
Deregistering a key does not mean that we need to try filling
any new value, so the increment is pointless.

Finally, remove the refill loop in keys_fill().  If a key
is registered or revived while keys_fill() is running it must be safe
to ignore it just as we would if it was registered immediately
after keys_fill() ran.  The important thing is that the
keys_set_version stored in ctx->lc_version must be sampled
*before* those unseen keys were added.
So sample keys_set_version early.

Linux-commit : 0fbfbc5ad0f892cf4c5e087a4e7e67102b2289af

Change-Id: Ic6907561d0bb864b10e1c53fb3e5469d0c60f888
Signed-off-by: NeilBrown <neilb@suse.com>
Signed-off-by: James Simmons <uja.ornl@yahoo.com>
Reviewed-on: https://review.whamcloud.com/32711
Reviewed-by: Gu Zheng <gzheng@ddn.com>
Tested-by: Jenkins
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-on: https://es-gerrit.dev.cray.com/156034
Reviewed-by: Alexander Zarochentsev <c17826@cray.com>
Reviewed-by: Artem Blagodarenko <c17828@cray.com>
Tested-by: Alexander Lezhoev <c17454@cray.com>

lustre/obdclass/lu_object.c
@@ -1355,7 +1355,7 @@ static atomic_t lu_key_initing_cnt = ATOMIC_INIT(0);
  * lu_context_refill(). No locking is provided, as initialization and shutdown
  * are supposed to be externally serialized.
  */
-static unsigned key_set_version = 0;
+static atomic_t key_set_version = ATOMIC_INIT(0);
 
 /**
  * Register new key.
@@ -1379,7 +1379,7 @@ int lu_context_key_register(struct lu_context_key *key)
                         lu_keys[i] = key;
                         lu_ref_init(&key->lct_reference);
                         result = 0;
-                        ++key_set_version;
+			atomic_inc(&key_set_version);
                         break;
                 }
         }
@@ -1422,7 +1422,6 @@ void lu_context_key_degister(struct lu_context_key *key)
 	lu_context_key_quiesce(key);
 
 	write_lock(&lu_keys_guard);
-	++key_set_version;
 	key_fini(&lu_shrink_env.le_ctx, key->lct_index);
 
 	/**
@@ -1583,7 +1582,6 @@ void lu_context_key_quiesce(struct lu_context_key *key)
 				    lc_remember)
 			key_fini(ctx, key->lct_index);
 
-		++key_set_version;
 		write_unlock(&lu_keys_guard);
 	}
 }
@@ -1592,7 +1590,7 @@ void lu_context_key_revive(struct lu_context_key *key)
 {
 	write_lock(&lu_keys_guard);
 	key->lct_tags &= ~LCT_QUIESCENT;
-	++key_set_version;
+	atomic_inc(&key_set_version);
 	write_unlock(&lu_keys_guard);
 }
 
@@ -1613,7 +1611,6 @@ static void keys_fini(struct lu_context *ctx)
 static int keys_fill(struct lu_context *ctx)
 {
 	unsigned int i;
-	unsigned pre_version;
 
 	/*
 	 * A serialisation with lu_context_key_quiesce() is needed, but some
@@ -1626,16 +1623,15 @@ static int keys_fill(struct lu_context *ctx)
 	 */
 	read_lock(&lu_keys_guard);
 	atomic_inc(&lu_key_initing_cnt);
-	pre_version = key_set_version;
 	read_unlock(&lu_keys_guard);
+	ctx->lc_version = atomic_read(&key_set_version);
 
-refill:
-	LINVRNT(ctx->lc_value != NULL);
+	LINVRNT(ctx->lc_value);
 	for (i = 0; i < ARRAY_SIZE(lu_keys); ++i) {
 		struct lu_context_key *key;
 
 		key = lu_keys[i];
-		if (ctx->lc_value[i] == NULL && key != NULL &&
+		if (!ctx->lc_value[i] && key &&
 		    (key->lct_tags & ctx->lc_tags) &&
 		    /*
 		     * Don't create values for a LCT_QUIESCENT key, as this
@@ -1673,17 +1669,7 @@ refill:
 		}
 	}
 
-	read_lock(&lu_keys_guard);
-	if (pre_version != key_set_version) {
-		pre_version = key_set_version;
-		read_unlock(&lu_keys_guard);
-		goto refill;
-	}
-
-	ctx->lc_version = key_set_version;
-
 	atomic_dec(&lu_key_initing_cnt);
-	read_unlock(&lu_keys_guard);
 	return 0;
 }
 
@@ -1792,13 +1778,9 @@ EXPORT_SYMBOL(lu_context_exit);
  */
 int lu_context_refill(struct lu_context *ctx)
 {
-	read_lock(&lu_keys_guard);
-	if (likely(ctx->lc_version == key_set_version)) {
-		read_unlock(&lu_keys_guard);
+	if (likely(ctx->lc_version == atomic_read(&key_set_version)))
 		return 0;
-	}
 
-	read_unlock(&lu_keys_guard);
 	return keys_fill(ctx);
 }
 
@@ -1809,14 +1791,14 @@ int lu_context_refill(struct lu_context *ctx)
  * predefined when the lu_device type are registered, during the module probe
  * phase.
  */
-__u32 lu_context_tags_default = LCT_CL_THREAD;
-__u32 lu_session_tags_default = LCT_SESSION;
+u32 lu_context_tags_default;
+u32 lu_session_tags_default;
 
 void lu_context_tags_update(__u32 tags)
 {
 	write_lock(&lu_keys_guard);
 	lu_context_tags_default |= tags;
-	key_set_version++;
+	atomic_inc(&key_set_version);
 	write_unlock(&lu_keys_guard);
 }
 EXPORT_SYMBOL(lu_context_tags_update);
@@ -1825,7 +1807,7 @@ void lu_context_tags_clear(__u32 tags)
 {
 	write_lock(&lu_keys_guard);
 	lu_context_tags_default &= ~tags;
-	key_set_version++;
+	atomic_inc(&key_set_version);
 	write_unlock(&lu_keys_guard);
 }
 EXPORT_SYMBOL(lu_context_tags_clear);
@@ -1834,7 +1816,7 @@ void lu_session_tags_update(__u32 tags)
 {
 	write_lock(&lu_keys_guard);
 	lu_session_tags_default |= tags;
-	key_set_version++;
+	atomic_inc(&key_set_version);
 	write_unlock(&lu_keys_guard);
 }
 EXPORT_SYMBOL(lu_session_tags_update);
@@ -1843,7 +1825,7 @@ void lu_session_tags_clear(__u32 tags)
 {
 	write_lock(&lu_keys_guard);
 	lu_session_tags_default &= ~tags;
-	key_set_version++;
+	atomic_inc(&key_set_version);
 	write_unlock(&lu_keys_guard);
 }
 EXPORT_SYMBOL(lu_session_tags_clear);

commit 5350f48afc4c840ae29f78ab1dfbc7d0839ba538

LUS-8128 obdclass: proper fix for a merge conflict
In cray-2.12-int, LUS-7552 was landed, then LU-11089.
However, in master they were applied in the reverse order.

There is a merge conflict between lu_*_tags_defaults
initialization which wasn't properly resolved when
porting LU-11089 on cray-2.12-int.

This patch corrects the initialization.

Change-Id: If1516f67bb3a618b99f88c22d72e144d93a6ebb4
Signed-off-by: Andrew Perepechko <c17827@cray.com>
Fixes: 3a7149a1a3 ("LU-11089 obdclass: make key_set_version")
Reviewed-on: https://es-gerrit.dev.cray.com/156176
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Tested-by: Alexander Lezhoev <c17454@cray.com>

lustre/obdclass/lu_object.c
@@ -1791,8 +1791,8 @@ int lu_context_refill(struct lu_context *ctx)
  * predefined when the lu_device type are registered, during the module probe
  * phase.
  */
-u32 lu_context_tags_default;
-u32 lu_session_tags_default;
+u32 lu_context_tags_default = LCT_CL_THREAD;
+u32 lu_session_tags_default = LCT_SESSION;
 
 void lu_context_tags_update(__u32 tags)
 {