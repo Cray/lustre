Cray/lustre

commit 2a5d2db69df56d6bba09aca70be3a7cc17a23b0e

LUS-8007 osd: use per-cpu counters for brw_stats
Based on perf reports, oh_lock is highly contended
while running IOR with nvme storage, so we need to
move to percpu counters.

Change-Id: I24536a0138067fb868aaf962d9321dea7566d13f
Signed-off-by: Andrew Perepechko <c17827@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/156016
Reviewed-by: Alexander Zarochentsev <c17826@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Tested-by: Alexander Lezhoev <c17454@cray.com>

lustre/obdclass/lprocfs_status.c
@@ -2203,6 +2203,102 @@ void lprocfs_oh_clear(struct obd_histogram *oh)
 }
 EXPORT_SYMBOL(lprocfs_oh_clear);
 
+void lprocfs_oh_tally_pcpu(struct obd_hist_pcpu **oh,
+			   unsigned int value)
+{
+	struct obd_hist_pcpu *ohd;
+
+	if (value >= OBD_HIST_MAX)
+		value = OBD_HIST_MAX - 1;
+
+	rcu_read_lock();
+	ohd = rcu_dereference(*oh);
+	ohd->oh_buckets[smp_processor_id()][value]++;
+	rcu_read_unlock();
+}
+EXPORT_SYMBOL(lprocfs_oh_tally_pcpu);
+
+void lprocfs_oh_tally_log2_pcpu(struct obd_hist_pcpu **oh,
+				unsigned int value)
+{
+	unsigned int val = 0;
+
+	if (likely(value != 0))
+		val = min(fls(value - 1), OBD_HIST_MAX);
+
+	lprocfs_oh_tally_pcpu(oh, val);
+}
+EXPORT_SYMBOL(lprocfs_oh_tally_log2_pcpu);
+
+unsigned long lprocfs_oh_counter_pcpu(struct obd_hist_pcpu **oh,
+				      unsigned int value)
+{
+	struct obd_hist_pcpu *ohd;
+	unsigned long ret = 0;
+	int i;
+
+	rcu_read_lock();
+	ohd = rcu_dereference(*oh);
+	for_each_possible_cpu(i)
+		ret +=  ohd->oh_buckets[i][value];
+	rcu_read_unlock();
+
+	return ret;
+}
+EXPORT_SYMBOL(lprocfs_oh_counter_pcpu);
+
+unsigned long lprocfs_oh_sum_pcpu(struct obd_hist_pcpu **oh)
+{
+	struct obd_hist_pcpu *ohd;
+	unsigned long ret = 0;
+	int i, j;
+
+	rcu_read_lock();
+	ohd = rcu_dereference(*oh);
+	for_each_possible_cpu(j)
+		for (i = 0; i < OBD_HIST_MAX; i++)
+			ret += ohd->oh_buckets[j][i];
+	rcu_read_unlock();
+
+	return ret;
+}
+EXPORT_SYMBOL(lprocfs_oh_sum_pcpu);
+
+static inline int obd_hist_pcpu_size(void)
+{
+	return sizeof(unsigned long[num_possible_cpus()][OBD_HIST_MAX]);
+}
+
+void lprocfs_oh_clear_pcpu(struct obd_hist_pcpu **oh)
+{
+	static DEFINE_SPINLOCK(clear_lock);
+	struct obd_hist_pcpu *oh_old, *oh_new;
+
+	OBD_ALLOC_LARGE(oh_new, obd_hist_pcpu_size());
+	if (!oh_new)
+		return;
+
+	spin_lock(&clear_lock);
+	oh_old = rcu_dereference(*oh);
+	rcu_assign_pointer(*oh, oh_new);
+	spin_unlock(&clear_lock);
+
+	if (!oh_old)
+		return;
+
+	/* not a perf critical part, so let's do it sync for simplicity */
+	synchronize_rcu();
+	OBD_FREE_LARGE(oh_old, obd_hist_pcpu_size());
+}
+EXPORT_SYMBOL(lprocfs_oh_clear_pcpu);
+
+void lprocfs_oh_release_pcpu(struct obd_hist_pcpu **oh)
+{
+	OBD_FREE_LARGE(*oh, obd_hist_pcpu_size());
+	*oh = NULL;
+}
+EXPORT_SYMBOL(lprocfs_oh_release_pcpu);
+
 ssize_t lustre_attr_show(struct kobject *kobj,
 			 struct attribute *attr, char *buf)
 {

lustre/include/lprocfs_status.h
@@ -89,6 +89,15 @@ struct obd_histogram {
 	unsigned long	oh_buckets[OBD_HIST_MAX];
 };
 
+struct obd_hist_pcpu {
+	/*
+	 * cpus go first so as to reduce cache line contention
+	 * for typical access with several threads updating
+	 * the same counter from different cpus
+	 */
+	unsigned long	oh_buckets[0][OBD_HIST_MAX];
+};
+
 enum {
         BRW_R_PAGES = 0,
         BRW_W_PAGES,
@@ -108,7 +117,7 @@ enum {
 };
 
 struct brw_stats {
-        struct obd_histogram hist[BRW_LAST];
+        struct obd_hist_pcpu *hist[BRW_LAST];
 };
 
 enum {
@@ -623,6 +632,14 @@ void lprocfs_oh_tally(struct obd_histogram *oh, unsigned int value);
 void lprocfs_oh_tally_log2(struct obd_histogram *oh, unsigned int value);
 void lprocfs_oh_clear(struct obd_histogram *oh);
 unsigned long lprocfs_oh_sum(struct obd_histogram *oh);
+
+void lprocfs_oh_tally_pcpu(struct obd_hist_pcpu **oh, unsigned int value);
+void lprocfs_oh_tally_log2_pcpu(struct obd_hist_pcpu **oh, unsigned int value);
+void lprocfs_oh_clear_pcpu(struct obd_hist_pcpu **oh);
+void lprocfs_oh_release_pcpu(struct obd_hist_pcpu **oh);
+unsigned long lprocfs_oh_sum_pcpu(struct obd_hist_pcpu **oh);
+unsigned long lprocfs_oh_counter_pcpu(struct obd_hist_pcpu **oh,
+				      unsigned int value);
 
 void lprocfs_stats_collect(struct lprocfs_stats *stats, int idx,
                            struct lprocfs_counter *cnt);

lustre/osd-ldiskfs/osd_io.c
@@ -147,11 +147,11 @@ void osd_fini_iobuf(struct osd_device *d, struct osd_iobuf *iobuf)
                 iobuf->dr_elapsed_valid = 0;
                 LASSERT(iobuf->dr_dev == d);
                 LASSERT(iobuf->dr_frags > 0);
-                lprocfs_oh_tally(&d->od_brw_stats.
-                                 hist[BRW_R_DIO_FRAGS+rw],
-                                 iobuf->dr_frags);
-		lprocfs_oh_tally_log2(&d->od_brw_stats.hist[BRW_R_IO_TIME+rw],
-				      ktime_to_ms(iobuf->dr_elapsed));
+		lprocfs_oh_tally_pcpu(&d->od_brw_stats.
+				      hist[BRW_R_DIO_FRAGS+rw],
+				      iobuf->dr_frags);
+		lprocfs_oh_tally_log2_pcpu(&d->od_brw_stats.hist[BRW_R_IO_TIME+rw],
+					   ktime_to_ms(iobuf->dr_elapsed));
         }
 }
 
@@ -250,22 +250,24 @@ static void dio_complete_routine(struct bio *bio, int error)
 
 static void record_start_io(struct osd_iobuf *iobuf, int size)
 {
-	struct osd_device    *osd = iobuf->dr_dev;
-	struct obd_histogram *h = osd->od_brw_stats.hist;
+	struct osd_device	*osd = iobuf->dr_dev;
+	struct brw_stats	*h = &osd->od_brw_stats;
 
 	iobuf->dr_frags++;
 	atomic_inc(&iobuf->dr_numreqs);
 
 	if (iobuf->dr_rw == 0) {
 		atomic_inc(&osd->od_r_in_flight);
-		lprocfs_oh_tally(&h[BRW_R_RPC_HIST],
-				 atomic_read(&osd->od_r_in_flight));
-		lprocfs_oh_tally_log2(&h[BRW_R_DISK_IOSIZE], size);
+		lprocfs_oh_tally_pcpu(&h->hist[BRW_R_RPC_HIST],
+				      atomic_read(&osd->od_r_in_flight));
+		lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_R_DISK_IOSIZE],
+					   size);
 	} else if (iobuf->dr_rw == 1) {
 		atomic_inc(&osd->od_w_in_flight);
-		lprocfs_oh_tally(&h[BRW_W_RPC_HIST],
+		lprocfs_oh_tally_pcpu(&h->hist[BRW_W_RPC_HIST],
 				 atomic_read(&osd->od_w_in_flight));
-		lprocfs_oh_tally_log2(&h[BRW_W_DISK_IOSIZE], size);
+		lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_W_DISK_IOSIZE],
+					   size);
 	} else {
 		LBUG();
 	}

lustre/osd-ldiskfs/osd_lproc.c
@@ -60,7 +60,7 @@ void osd_brw_stats_update(struct osd_device *osd, struct osd_iobuf *iobuf)
 
 	blocks_per_page = PAGE_SIZE >> osd_sb(osd)->s_blocksize_bits;
 
-        lprocfs_oh_tally_log2(&s->hist[BRW_R_PAGES+rw], nr_pages);
+        lprocfs_oh_tally_log2_pcpu(&s->hist[BRW_R_PAGES+rw], nr_pages);
 
         while (nr_pages-- > 0) {
                 if (last_page && (*pages)->index != (last_page->index + 1))
@@ -74,12 +74,12 @@ void osd_brw_stats_update(struct osd_device *osd, struct osd_iobuf *iobuf)
                 }
         }
 
-        lprocfs_oh_tally(&s->hist[BRW_R_DISCONT_PAGES+rw], discont_pages);
-        lprocfs_oh_tally(&s->hist[BRW_R_DISCONT_BLOCKS+rw], discont_blocks);
+        lprocfs_oh_tally_pcpu(&s->hist[BRW_R_DISCONT_PAGES+rw], discont_pages);
+        lprocfs_oh_tally_pcpu(&s->hist[BRW_R_DISCONT_BLOCKS+rw], discont_blocks);
 }
 
 static void display_brw_stats(struct seq_file *seq, char *name, char *units,
-        struct obd_histogram *read, struct obd_histogram *write, int scale)
+        struct obd_hist_pcpu **read, struct obd_hist_pcpu **write, int scale)
 {
         unsigned long read_tot, write_tot, r, w, read_cum = 0, write_cum = 0;
         int i;
@@ -88,11 +88,11 @@ static void display_brw_stats(struct seq_file *seq, char *name, char *units,
         seq_printf(seq, "%-22s %-5s %% cum %% |  %-11s %% cum %%\n",
                    name, units, units);
 
-        read_tot = lprocfs_oh_sum(read);
-        write_tot = lprocfs_oh_sum(write);
+        read_tot = lprocfs_oh_sum_pcpu(read);
+        write_tot = lprocfs_oh_sum_pcpu(write);
         for (i = 0; i < OBD_HIST_MAX; i++) {
-                r = read->oh_buckets[i];
-                w = write->oh_buckets[i];
+                r = lprocfs_oh_counter_pcpu(read, i);
+                w = lprocfs_oh_counter_pcpu(write, i);
                 read_cum += r;
                 write_cum += w;
                 if (read_cum == 0 && write_cum == 0)
@@ -174,7 +174,7 @@ static ssize_t osd_brw_stats_seq_write(struct file *file,
         int i;
 
         for (i = 0; i < BRW_LAST; i++)
-                lprocfs_oh_clear(&osd->od_brw_stats.hist[i]);
+                lprocfs_oh_clear_pcpu(&osd->od_brw_stats.hist[i]);
 
         return len;
 }
@@ -187,7 +187,7 @@ static int osd_stats_init(struct osd_device *osd)
         ENTRY;
 
         for (i = 0; i < BRW_LAST; i++)
-		spin_lock_init(&osd->od_brw_stats.hist[i].oh_lock);
+		lprocfs_oh_clear_pcpu(&osd->od_brw_stats.hist[i]);
 
         osd->od_stats = lprocfs_alloc_stats(LPROC_OSD_LAST, 0);
         if (osd->od_stats != NULL) {
@@ -805,6 +805,11 @@ out:
 
 int osd_procfs_fini(struct osd_device *osd)
 {
+	int i;
+
+	for (i = 0; i < BRW_LAST; i++)
+		lprocfs_oh_release_pcpu(&osd->od_brw_stats.hist[i]);
+
 	if (osd->od_stats)
 		lprocfs_free_stats(&osd->od_stats);
 

lustre/osd-zfs/osd_io.c
@@ -72,43 +72,48 @@ static void dbuf_set_pending_evict(dmu_buf_t *db)
 
 static void record_start_io(struct osd_device *osd, int rw, int discont_pages)
 {
-	struct obd_histogram *h = osd->od_brw_stats.hist;
+	struct brw_stats *h = &osd->od_brw_stats;
 
 	if (rw == READ) {
 		atomic_inc(&osd->od_r_in_flight);
-		lprocfs_oh_tally(&h[BRW_R_RPC_HIST],
-				 atomic_read(&osd->od_r_in_flight));
-		lprocfs_oh_tally(&h[BRW_R_DISCONT_PAGES], discont_pages);
+		lprocfs_oh_tally_pcpu(&h->hist[BRW_R_RPC_HIST],
+				      atomic_read(&osd->od_r_in_flight));
+		lprocfs_oh_tally_pcpu(&h->hist[BRW_R_DISCONT_PAGES],
+				      discont_pages);
 
 	} else {
 		atomic_inc(&osd->od_w_in_flight);
-		lprocfs_oh_tally(&h[BRW_W_RPC_HIST],
-				 atomic_read(&osd->od_w_in_flight));
-		lprocfs_oh_tally(&h[BRW_W_DISCONT_PAGES], discont_pages);
-
+		lprocfs_oh_tally_pcpu(&h->hist[BRW_W_RPC_HIST],
+				      atomic_read(&osd->od_w_in_flight));
+		lprocfs_oh_tally_pcpu(&h->hist[BRW_W_DISCONT_PAGES],
+				      discont_pages);
 	}
 }
 
 static void record_end_io(struct osd_device *osd, int rw,
 			  unsigned long elapsed, int disksize, int npages)
 {
-	struct obd_histogram *h = osd->od_brw_stats.hist;
+	struct brw_stats *h = &osd->od_brw_stats;
 
 	if (rw == READ) {
 		atomic_dec(&osd->od_r_in_flight);
-		lprocfs_oh_tally_log2(&h[BRW_R_PAGES], npages);
+		lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_R_PAGES], npages);
 		if (disksize > 0)
-			lprocfs_oh_tally_log2(&h[BRW_R_DISK_IOSIZE], disksize);
+			lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_R_DISK_IOSIZE],
+						   disksize);
 		if (elapsed)
-			lprocfs_oh_tally_log2(&h[BRW_R_IO_TIME], elapsed);
+			lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_R_IO_TIME],
+						   elapsed);
 
 	} else {
 		atomic_dec(&osd->od_w_in_flight);
-		lprocfs_oh_tally_log2(&h[BRW_W_PAGES], npages);
+		lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_W_PAGES], npages);
 		if (disksize > 0)
-			lprocfs_oh_tally_log2(&h[BRW_W_DISK_IOSIZE], disksize);
+			lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_W_DISK_IOSIZE],
+						   disksize);
 		if (elapsed)
-			lprocfs_oh_tally_log2(&h[BRW_W_IO_TIME], elapsed);
+			lprocfs_oh_tally_log2_pcpu(&h->hist[BRW_W_IO_TIME],
+						   elapsed);
 	}
 }
 

lustre/osd-zfs/osd_lproc.c
@@ -47,8 +47,9 @@
 #ifdef CONFIG_PROC_FS
 
 static void display_brw_stats(struct seq_file *seq, char *name, char *units,
-			      struct obd_histogram *read,
-			      struct obd_histogram *write, int scale)
+			      struct obd_hist_pcpu **read,
+			      struct obd_hist_pcpu **write,
+			      int scale)
 {
 	unsigned long read_tot, write_tot, r, w, read_cum = 0, write_cum = 0;
 	int i;
@@ -57,11 +58,11 @@ static void display_brw_stats(struct seq_file *seq, char *name, char *units,
 	seq_printf(seq, "%-22s %-5s %% cum %% |  %-11s %% cum %%\n",
 		   name, units, units);
 
-	read_tot = lprocfs_oh_sum(read);
-	write_tot = lprocfs_oh_sum(write);
+	read_tot = lprocfs_oh_sum_pcpu(read);
+	write_tot = lprocfs_oh_sum_pcpu(write);
 	for (i = 0; i < OBD_HIST_MAX; i++) {
-		r = read->oh_buckets[i];
-		w = write->oh_buckets[i];
+		r = lprocfs_oh_counter_pcpu(read, i);
+		w = lprocfs_oh_counter_pcpu(write, i);
 		read_cum += r;
 		write_cum += w;
 		if (read_cum == 0 && write_cum == 0)
@@ -140,7 +141,7 @@ static ssize_t osd_brw_stats_seq_write(struct file *file,
 	int i;
 
 	for (i = 0; i < BRW_LAST; i++)
-		lprocfs_oh_clear(&osd->od_brw_stats.hist[i]);
+		lprocfs_oh_clear_pcpu(&osd->od_brw_stats.hist[i]);
 
 	return len;
 }
@@ -153,7 +154,7 @@ static int osd_stats_init(struct osd_device *osd)
 	ENTRY;
 
 	for (i = 0; i < BRW_LAST; i++)
-		spin_lock_init(&osd->od_brw_stats.hist[i].oh_lock);
+		lprocfs_oh_clear_pcpu(&osd->od_brw_stats.hist[i]);
 
 	osd->od_stats = lprocfs_alloc_stats(LPROC_OSD_LAST, 0);
 	if (osd->od_stats != NULL) {
@@ -476,7 +477,12 @@ out:
 
 int osd_procfs_fini(struct osd_device *osd)
 {
+	int i;
+
 	ENTRY;
+
+	for (i = 0; i < BRW_LAST; i++)
+		lprocfs_oh_release_pcpu(&osd->od_brw_stats.hist[i]);
 
 	if (osd->od_stats)
 		lprocfs_free_stats(&osd->od_stats);

commit 3ab725354702dd4576e6546102fec5a1a387d839

LUS-8185 obdclass: properly handle incomplete init of osd stats
osd_procfs_fini() can be called even if osd_procfs_init() never
completed. Do not attempt to free any memory in this case.

Change-Id: I5483adfb6641eed70340db26000b1111fd4b421f
Signed-off-by: Andrew Perepechko <c17827@cray.com>
Fixes: 2a5d2db69d ("LUS-8007 osd: use per-cpu counters for brw_stats")
Test-Parameters: fstype=zfs
Reviewed-on: https://es-gerrit.dev.cray.com/156273
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Shaun Tancheff <stancheff@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/obdclass/lprocfs_status.c
@@ -2294,8 +2294,10 @@ EXPORT_SYMBOL(lprocfs_oh_clear_pcpu);
 
 void lprocfs_oh_release_pcpu(struct obd_hist_pcpu **oh)
 {
-	OBD_FREE_LARGE(*oh, obd_hist_pcpu_size());
-	*oh = NULL;
+	if (*oh) {
+		OBD_FREE_LARGE(*oh, obd_hist_pcpu_size());
+		*oh = NULL;
+	}
 }
 EXPORT_SYMBOL(lprocfs_oh_release_pcpu);
 