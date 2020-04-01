ad23695fa26bf97a7c638e09151fdfd9c1a93d9d
Shaun Tancheff <stancheff@cray.com>
2020-04-01 09:26:29 -0500

    LUS-8626 vvp: dirty pages with pagevec
    When doing i/o from multiple writers to a single file, the
    per-file page cache lock (the mapping lock) becomes a
    bottleneck.
    
    Most current uses are single page at a time.  This converts
    one prominent use, marking page as dirty, to use a pagevec.
    
    When many threads are writing to one file, this improves
    write performance by around 25%.
    
    This requires implementing our own version of the
    set_page_dirty-->__set_page_dirty_nobuffers functions.
    
    This was modeled on upstream tip of tree:
    v5.2-rc4-224-ge01e060fe0 (7/13/2019)
    
    The relevant code is unchanged since Linux 4.17, and has
    changed only minimally since before Linux 2.6.
    
    Trivial cherry-pick clean up of LU-9920
    
    Whamcloud-bug-id: LU-9920
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I01444f33eec77ea6b8b28c0a6dab3200499a38fc
    Reviewed-on: https://es-gerrit.dev.cray.com/157007
    Reviewed-by: Vitaly Fertman <c17818@cray.com>
    Tested-by: Vitaly Fertman <c17818@cray.com>

--- lustre/llite/llite_lib.c
+++ lustre/llite/llite_lib.c
@@ -2144,6 +2144,8 @@ void ll_delete_inode(struct inode *inode)
 	struct address_space *mapping = &inode->i_data;
 	unsigned long nrpages;
 	int rc;
+	unsigned long flags;
+
 	ENTRY;
 
 	if (S_ISREG(inode->i_mode) && lli->lli_clob != NULL) {
@@ -2170,9 +2172,9 @@ void ll_delete_inode(struct inode *inode)
 	 */
 	nrpages = mapping->nrpages;
 	if (nrpages) {
-		xa_lock_irq(&mapping->i_pages);
+		xa_lock_irqsave(&mapping->i_pages, flags);
 		nrpages = mapping->nrpages;
-		xa_unlock_irq(&mapping->i_pages);
+		xa_unlock_irqrestore(&mapping->i_pages, flags);
 	} /* Workaround end */
 
 	LASSERTF(nrpages == 0, "%s: inode="DFID"(%p) nrpages=%lu, "
