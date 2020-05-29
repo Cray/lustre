feaaf7bc8fe3002b9addb61f4d5051b4d287a510
Alexander Boyko <c17825@cray.com>
2020-05-29 04:37:17 -0400

    LUS-8924 llite: don't hold inode_lock for security notify
    With selinux enabled client has a dead lock which leads to
    client eviction from MDS.
    1 thread                    2 thread
    do file open                do stat
    inode_lock(parend dir)
                                got LDLM_PR(parent dir)
    enqueue LDLM_CW(parent dir) waits on inode_lock to notify security
    waits
    timeout on enqueue
    and client eviction because client didn't cancel a LDLM_PR lock
    
    security_inode_notifysecctx()->selinux_inode_notifysecctx()->
    selinux_inode_setsecurity()
    The call of selinux_inode_setsecurity doesn't need to hold
    inode_lock.
    
    Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
    Change-Id: I8b219b5581a6b14929d3dca2852eae6169b7fe3d
    Reviewed-by: Andrew Perepechko <c17827@cray.com>
    Reviewed-by: Andriy Skulysh <c17819@cray.com>
    Tested-by: Alexander Lezhoev <c17454@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/157364/

--- lustre/llite/namei.c
+++ lustre/llite/namei.c
@@ -673,10 +673,8 @@ static int ll_lookup_it_finish(struct ptlrpc_request *request,
 		}
 
 		if (secctx != NULL && secctxlen != 0) {
-			inode_lock(inode);
 			rc = security_inode_notifysecctx(inode, secctx,
 							 secctxlen);
-			inode_unlock(inode);
 			if (rc)
 				CWARN("cannot set security context for "
 				      DFID": rc = %d\n",
@@ -1219,12 +1217,10 @@ static int ll_create_it(struct inode *dir, struct dentry *dentry,
 
 	if ((ll_i2sbi(inode)->ll_flags & LL_SBI_FILE_SECCTX) &&
 	    secctx != NULL) {
-		inode_lock(inode);
 		/* must be done before d_instantiate, because it calls
 		 * security_d_instantiate, which means a getxattr if security
 		 * context is not set yet */
 		rc = security_inode_notifysecctx(inode, secctx, secctxlen);
-		inode_unlock(inode);
 		if (rc)
 			RETURN(rc);
 	}
@@ -1340,14 +1336,12 @@ again:
 		GOTO(err_exit, err);
 
 	if (sbi->ll_flags & LL_SBI_FILE_SECCTX) {
-		inode_lock(inode);
 		/* must be done before d_instantiate, because it calls
 		 * security_d_instantiate, which means a getxattr if security
 		 * context is not set yet */
 		err = security_inode_notifysecctx(inode,
 						  op_data->op_file_secctx,
 						  op_data->op_file_secctx_size);
-		inode_unlock(inode);
 		if (err)
 			GOTO(err_exit, err);
 	}
