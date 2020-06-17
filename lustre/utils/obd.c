d98504a1908b711e787308a7dc7c79f8f3262e44
Sergey Cheremencev <c17829@cray.com>
2020-06-17 14:01:13 +0300

    LUS-9008 utils: pool_add/remove error code fix
    jt_pool_cmd should always return error code, even
    if it failed to add/remove just one of OSTs from list.
    Before this patch it returned latest command result,
    ignoring previous failures.
    
    Change-Id: Ife6cefc006f061b47a1b00daf826d0d1d34fd66c
    Signed-off-by: Sergey Cheremencev <c17829@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/157459
    Reviewed-by: Vladimir Saveliev <c17830@cray.com>
    Reviewed-by: Artem Blagodarenko <c17828@cray.com>
    Tested-by: Elena Gryaznova <c17455@cray.com>

--- lustre/utils/obd.c
+++ lustre/utils/obd.c
@@ -4887,20 +4887,23 @@ int jt_pool_cmd(int argc, char **argv)
                         }
 
                         for (j = 0; j < array_sz; j++) {
-                                char ostname[MAX_OBD_NAME + 1];
+				char ostname[MAX_OBD_NAME + 1];
+				int rc2;
 
                                 snprintf(ostname, MAX_OBD_NAME, format,
                                          array[j]);
                                 ostname[MAX_OBD_NAME] = '\0';
 
-                                rc = check_and_complete_ostname(fsname,ostname);
-                                if (rc) {
-                                        free(array);
-                                        free(cmds);
-                                        if (ostnames_buf)
-                                                free(ostnames_buf);
-                                        goto out;
-                                }
+				rc2 = check_and_complete_ostname(fsname,
+								ostname);
+				if (rc2) {
+					free(array);
+					free(cmds);
+					if (ostnames_buf)
+						free(ostnames_buf);
+					rc = rc ? rc : rc2;
+					goto out;
+				}
                                 if (ostnames_buf != NULL) {
                                         cmds[j].ostname =
                                           &ostnames_buf[(MAX_OBD_NAME + 1) * j];
