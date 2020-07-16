1b7eeaef04e527d750953376a9e90244fd6369ec
Artem Blagodarenko <artem.blagodarenko@hpe.com>
2020-07-16 04:37:51 -0400

    LUS-2510 mgs: no local logs flag
    There is feature that start a target with local copy of
    config log in order to avoid a delay on communicating with
    an mgs and to load mgs log updates later on.
    However, that feature is not always useful
    
    When replace_nids adds records with new nids it does not
    append remote config logs but overwrite corresponding
    records in place. If a target starts using local config
    log - it gets confused by outdated nids.
    
    This patch adds tunefs.lustre --nolocallogs key that
    sets nolocallogs flag, which says ignore local configs copy.
    The flag is reset once new logs are uploaded from MGS.
    
    tunefs.lustre --nolocallogs is suggested to be executed on
    targets together with replace_nids on MGS.
    
    Change-Id: I949c19ac701d287e1c1199bc12445989476a707b
    Signed-off-by: Artem Blagodarenko <artem.blagodarenko@hpe.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/157574
    Reviewed-by: Vladimir Saveliev <c17830@cray.com>
    Reviewed-by: Nikitas Angelinas <nangelinas@cray.com>
    Tested-by: Alexander Lezhoev <c17454@cray.com>

--- lustre/include/lustre_disk.h
+++ lustre/include/lustre_disk.h
@@ -106,6 +106,7 @@ struct lustre_mount_data {
 #define LMD_FLG_UPDATE		0x2000	/* update parameters */
 #define LMD_FLG_HSM		0x4000	/* Start coordinator */
 #define LMD_FLG_DEV_RDONLY	0x8000	/* discard modification quitely */
+#define LMD_FLG_NO_LOCAL_LOGS	0x40000 /* Use config logs from MGS */
 
 #define lmd_is_client(x) ((x)->lmd_flags & LMD_FLG_CLIENT)
 
