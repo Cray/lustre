26e08092734c2439a19c809be04a3c3d54e7bed0
NeilBrown <neilb@suse.com>
2020-08-17 19:55:48 +0300

    LU-9679 osc: remove cl_io_cancel()
    cl_io_cancel() is never used, so remove it and various
    other things that it is the only user of.
    
    Signed-off-by: Mr NeilBrown <neilb@suse.com>
    Change-Id: I6cf9b53aa7fc3379e57fa0ac0ea236ccda4ff6b7
    Reviewed-on: https://review.whamcloud.com/37597
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Wang Shilong <wshilong@ddn.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/osc/osc_request.c
+++ lustre/osc/osc_request.c
@@ -1948,21 +1948,19 @@ static int osc_brw_redo_request(struct ptlrpc_request *request,
                 RETURN(rc);
 
 	list_for_each_entry(oap, &aa->aa_oaps, oap_rpc_item) {
-                if (oap->oap_request != NULL) {
-                        LASSERTF(request == oap->oap_request,
-                                 "request %p != oap_request %p\n",
-                                 request, oap->oap_request);
-                        if (oap->oap_interrupted) {
-                                ptlrpc_req_finished(new_req);
-                                RETURN(-EINTR);
-                        }
-                }
-        }
-        /* New request takes over pga and oaps from old request.
-         * Note that copying a list_head doesn't work, need to move it... */
-        aa->aa_resends++;
-        new_req->rq_interpret_reply = request->rq_interpret_reply;
-        new_req->rq_async_args = request->rq_async_args;
+		if (oap->oap_request != NULL) {
+			LASSERTF(request == oap->oap_request,
+				 "request %p != oap_request %p\n",
+				 request, oap->oap_request);
+		}
+	}
+	/*
+	 * New request takes over pga and oaps from old request.
+	 * Note that copying a list_head doesn't work, need to move it...
+	 */
+	aa->aa_resends++;
+	new_req->rq_interpret_reply = request->rq_interpret_reply;
+	new_req->rq_async_args = request->rq_async_args;
 	new_req->rq_commit_cb = request->rq_commit_cb;
 	/* cap resend delay to the current request timeout, this is similar to
 	 * what ptlrpc does (see after_reply()) */
@@ -2199,7 +2197,6 @@ int osc_build_rpc(const struct lu_env *env, struct client_obd *cli,
 	int				mem_tight = 0;
 	int				page_count = 0;
 	bool				soft_sync = false;
-	bool				interrupted = false;
 	bool				ndelay = false;
 	int				i;
 	int				grant = 0;
@@ -2256,8 +2253,6 @@ int osc_build_rpc(const struct lu_env *env, struct client_obd *cli,
 			else
 				LASSERT(oap->oap_page_off + oap->oap_count ==
 					PAGE_SIZE);
-			if (oap->oap_interrupted)
-				interrupted = true;
 		}
 		if (ext->oe_ndelay)
 			ndelay = true;
@@ -2296,8 +2291,6 @@ int osc_build_rpc(const struct lu_env *env, struct client_obd *cli,
 	req->rq_interpret_reply = brw_interpret;
 	req->rq_memalloc = mem_tight != 0;
 	oap->oap_request = ptlrpc_request_addref(req);
-	if (interrupted && !req->rq_intr)
-		ptlrpc_mark_interrupted(req);
 	if (ndelay) {
 		req->rq_no_resend = req->rq_no_delay = 1;
 		/* probably set a shorter timeout value.
