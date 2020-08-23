fc3e4844de45de4d8045187b976268ab549e8b65
Chris Horn <chris.horn@hpe.com>
2020-08-23 10:14:22 -0500

    LUS-9109 lnet: Age peer NI out of recovery
    No longer send recovery pings to a peer NI that has been in recovery
    for the recovery time limit. A peer NI will become eligible for
    recovery again once we receive a message from it.
    
    The existing lpni_last_alive field is utilized for this new purpose.
    Current uses are converted from ktime_get_seconds() to
    ktime_get_real_seconds().
    
    A check for NULL lpni is removed from
    lnet_handle_remote_failure_locked() because all callers of that
    function already ensure the lpni is non-NULL.
    
    Amongst other changes, lnet_peer_ni_add_to_recoveryq_locked() is
    modified to take a ref on the peer NI if it is added to the recovery
    queue. Previously, it was the responsibility of callers to take this
    ref.
    
    Test-Parameters: trivial
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: Ib4676540ac4bb040690a4fb047236c54eea0e752
    Reviewed-on: https://es-gerrit.dev.cray.com/157734
    Reviewed-by: Alexander Boyko <c17825@cray.com>
    Reviewed-by: Alexey Lyashkov <c17817@cray.com>
    Tested-by: Vitaly Fertman <c17818@cray.com>

--- lnet/include/lnet/lib-lnet.h
+++ lnet/include/lnet/lib-lnet.h
@@ -616,7 +616,9 @@ extern int libcfs_ioctl_getdata(struct libcfs_ioctl_hdr **hdr_pp,
 extern int lnet_get_peer_list(__u32 *countp, __u32 *sizep,
 			      struct lnet_process_id __user *ids);
 extern void lnet_peer_ni_set_healthv(lnet_nid_t nid, int value, bool all);
-extern void lnet_peer_ni_add_to_recoveryq_locked(struct lnet_peer_ni *lpni);
+extern void lnet_peer_ni_add_to_recoveryq_locked(struct lnet_peer_ni *lpni,
+						 struct list_head *queue,
+						 time64_t now);
 
 void lnet_router_debugfs_init(void);
 void lnet_router_debugfs_fini(void);
