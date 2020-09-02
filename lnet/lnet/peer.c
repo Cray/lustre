df9fd7739c28f311a1604802dd954ea72ce7575f
Chris Horn <chris.horn@hpe.com>
2020-09-02 15:07:25 -0500

    LUS-9293 lnet: Provide kernel API for adding peers
    Implement LNetAddPeer() API to allow other kernel modules to add
    peers to LNet.
    
    Peers created via this API are not marked as having been configured
    by DLC. As such, they can be overwritten by discovery.
    
    Test-Parameters: trivial
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: I9c942385d39a1bf7d1706a3c60886bd165dcaf0e
    Reviewed-by: Alexey Lyashkov <c17817@cray.com>
    Reviewed-by: Sergey Cheremencev <c17829@cray.com>
    Tested-by: Jenkins Build User <nssreleng@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/159083

--- lnet/lnet/peer.c
+++ lnet/lnet/peer.c
@@ -1489,6 +1489,11 @@ lnet_peer_add(lnet_nid_t nid, unsigned flags)
 			else if ((lp->lp_state ^ flags) & LNET_PEER_MULTI_RAIL)
 				rc = -EPERM;
 			goto out;
+		} else if (!(flags & LNET_PEER_CONFIGURED)) {
+			if (lp->lp_primary_nid == nid) {
+				rc = -EEXIST;
+				goto out;
+			}
 		}
 		/* Delete and recreate as a configured peer. */
 		lnet_peer_del(lp);
@@ -1729,17 +1734,19 @@ out:
  * being created/modified/deleted by a different thread.
  */
 int
-lnet_add_peer_ni(lnet_nid_t prim_nid, lnet_nid_t nid, bool mr)
+lnet_add_peer_ni(lnet_nid_t prim_nid, lnet_nid_t nid, bool mr, bool temp)
 {
 	struct lnet_peer *lp = NULL;
 	struct lnet_peer_ni *lpni;
-	unsigned flags;
+	unsigned int flags = 0;
 
 	/* The prim_nid must always be specified */
 	if (prim_nid == LNET_NID_ANY)
 		return -EINVAL;
 
-	flags = LNET_PEER_CONFIGURED;
+	if (!temp)
+		flags = LNET_PEER_CONFIGURED;
+
 	if (mr)
 		flags |= LNET_PEER_MULTI_RAIL;
 
@@ -1758,7 +1765,7 @@ lnet_add_peer_ni(lnet_nid_t prim_nid, lnet_nid_t nid, bool mr)
 	lp = lpni->lpni_peer_net->lpn_peer;
 
 	/* Peer must have been configured. */
-	if (!(lp->lp_state & LNET_PEER_CONFIGURED)) {
+	if (!temp && !(lp->lp_state & LNET_PEER_CONFIGURED)) {
 		CDEBUG(D_NET, "peer %s was not configured\n",
 		       libcfs_nid2str(prim_nid));
 		return -ENOENT;
