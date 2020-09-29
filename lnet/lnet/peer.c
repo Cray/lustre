522e23528c2344165b7359bf3f2d16f5b9d9666c
Chris Horn <chris.horn@hpe.com>
2020-09-29 13:53:51 -0500

    LUS-9187 lnet: Restore warning about zombie NIs
    The warning message in lnet_peer_ni_finalize_wait() is never printed.
    Restore the message since it provides some information about why LNet
    might be stalled during shutdown.
    
    Fixes: fa8b4e6357 ("LU-7734 lnet: peer/peer_ni handling adjustments")
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: Ia7d4d48e59f0a605bdfd0bf267268ab40ff35918
    Reviewed-on: https://es-gerrit.dev.cray.com/157921
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
    Tested-by: Jenkins Build User <nssreleng@cray.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>

--- lnet/lnet/peer.c
+++ lnet/lnet/peer.c
@@ -594,7 +594,7 @@ lnet_peer_ni_finalize_wait(struct lnet_peer_table *ptable)
 	while (ptable->pt_zombies) {
 		spin_unlock(&ptable->pt_zombie_lock);
 
-		if (is_power_of_2(i)) {
+		if (is_power_of_2(i++)) {
 			CDEBUG(D_WARNING,
 			       "Waiting for %d zombies on peer table\n",
 			       ptable->pt_zombies);
