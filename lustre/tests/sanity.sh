5e3a49ea5ad9b87e83765dc91e4a162804c1d043
Sebastien Buisson <sbuisson@ddn.com>
2020-12-09 18:53:12 +0100

    LU-14204 tests: make sure we have a single import
    In sanity, retrieve the exact name of the import being used on the
    client, in order to properly get information such as lock_count
    or lru_size.
    
    Change-Id: I065b7da7990c7171d5baa24f3400c5f8ffc12fc9
    Test-Parameters: trivial
    Signed-off-by: Sebastien Buisson <sbuisson@ddn.com>
    Reviewed-on: https://review.whamcloud.com/40998
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: James Nunez <jnunez@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -18705,6 +18705,9 @@ test_255b() {
 run_test 255b "check 'lfs ladvise -a dontneed'"
 
 test_255c() {
+	local ost1_imp=$(get_osc_import_name client ost1)
+	local imp_name=$($LCTL list_param osc.$ost1_imp | head -n1 |
+			 cut -d'.' -f2)
 	local count
 	local new_count
 	local difference
@@ -18755,7 +18758,7 @@ test_255c() {
 		cancel_lru_locks osc
 
 		count=$($LCTL get_param -n \
-		       ldlm.namespaces.$FSNAME-OST0000*osc-[-0-9a-f]*.lock_unused_count)
+		       ldlm.namespaces.$imp_name.lock_unused_count)
 
 		lockahead_test -d $DIR/$tdir -t $i -f $tfile
 		rc=$?
@@ -18764,7 +18767,7 @@ test_255c() {
 		fi
 
 		new_count=$($LCTL get_param -n \
-		       ldlm.namespaces.$FSNAME-OST0000*osc-[-0-9a-f]*.lock_unused_count)
+		       ldlm.namespaces.$imp_name.lock_unused_count)
 		difference="$((new_count - count))"
 
 		# Test 15 output is divided by 100 to map down to valid return
@@ -22912,6 +22915,10 @@ test_815()
 run_test 815 "zero byte tiny write doesn't hang (LU-12382)"
 
 test_816() {
+	local ost1_imp=$(get_osc_import_name client ost1)
+	local imp_name=$($LCTL list_param osc.$ost1_imp | head -n1 |
+			 cut -d'.' -f2)
+
 	[ "$SHARED_KEY" = true ] &&
 		skip "OSC connections never go IDLE with Shared-Keys enabled"
 
@@ -22925,12 +22932,12 @@ test_816() {
 	local before
 	local now
 	before=$($LCTL get_param -n \
-		 ldlm.namespaces.$FSNAME-OST0000-osc-[^M]*.lru_size)
+		 ldlm.namespaces.$imp_name.lru_size)
 
 	sleep 25
 	dd if=/dev/null of=$DIR/$tfile bs=1k count=1 conv=sync
 	now=$($LCTL get_param -n \
-	      ldlm.namespaces.$FSNAME-OST0000-osc-[^M]*.lru_size)
+	      ldlm.namespaces.$imp_name.lru_size)
 	[ $before == $now ] || error "lru_size changed $before != $now"
 }
 run_test 816 "do not reset lru_resize on idle reconnect"
