5af448966bb35d836c4555389c8d2da1096f67ba
Yang Sheng <ys@whamcloud.com>
2021-02-09 14:50:49 +0300

    LU-11814 obdcalss: ensure LCT_QUIESCENT take sync
    Add locking in lu_device_init ensure LCT_QUIESCENT
    operating can be seen on other thread in parallel
    mounting. Also add extra checking before unset the
    flag to make sure we don't do it after device has
    been started.
    
    (osd_handler.c:7730:osd_device_init0()) ASSERTION( info ) failed:
    (osd_handler.c:7730:osd_device_init0()) LBUG
    Pid: 28098, comm: mount.lustre 3.10.0-1062.9.1.el7_lustre.x86_64
    Call Trace:
     libcfs_call_trace+0x8c/0xc0 [libcfs]
     lbug_with_loc+0x4c/0xa0 [libcfs]
     osd_device_alloc+0x778/0x8f0 [osd_ldiskfs]
     obd_setup+0x129/0x2f0 [obdclass]
     class_setup+0x48f/0x7f0 [obdclass]
     class_process_config+0x190f/0x2830 [obdclass]
     do_lcfg+0x258/0x500 [obdclass]
     lustre_start_simple+0x88/0x210 [obdclass]
     server_fill_super+0xf55/0x1890 [obdclass]
     lustre_fill_super+0x498/0x990 [obdclass]
     mount_nodev+0x4f/0xb0
     lustre_mount+0x18/0x20 [obdclass]
     mount_fs+0x3e/0x1b0
     vfs_kern_mount+0x67/0x110
     do_mount+0x1ef/0xce0
     SyS_mount+0x83/0xd0
     system_call_fastpath+0x25/0x2a
     0xffffffffffffffff
     Kernel panic - not syncing: LBUG
    
    Change-Id: I001897b9d74b521e5012f13e958e7eb9a6bdc86b
    Cray-bug-id: LUS-9202
    Signed-off-by: Yang Sheng <ys@whamcloud.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/158426
    Reviewed-by: Alexander Boyko <c17825@cray.com>
    Tested-by: Elena Gryaznova <c17455@cray.com>
    Reviewed-by: Vitaly Fertman <c17818@cray.com>

--- lustre/include/lu_object.h
+++ lustre/include/lu_object.h
@@ -1159,8 +1159,9 @@ int   lu_context_key_register(struct lu_context_key *key);
 void  lu_context_key_degister(struct lu_context_key *key);
 void *lu_context_key_get     (const struct lu_context *ctx,
                                const struct lu_context_key *key);
-void  lu_context_key_quiesce (struct lu_context_key *key);
-void  lu_context_key_revive  (struct lu_context_key *key);
+void  lu_context_key_quiesce(struct lu_device_type *t,
+			     struct lu_context_key *key);
+void  lu_context_key_revive(struct lu_context_key *key);
 
 
 /*
@@ -1205,12 +1206,12 @@ void  lu_context_key_revive  (struct lu_context_key *key);
         }                                                       \
         struct __##mod##_dummy_type_start {;}
 
-#define LU_TYPE_STOP(mod, ...)                                  \
-        static void mod##_type_stop(struct lu_device_type *t)   \
-        {                                                       \
-                lu_context_key_quiesce_many(__VA_ARGS__, NULL); \
-        }                                                       \
-        struct __##mod##_dummy_type_stop {;}
+#define LU_TYPE_STOP(mod, ...)                                     \
+	static void mod##_type_stop(struct lu_device_type *t)      \
+	{                                                          \
+		lu_context_key_quiesce_many(t, __VA_ARGS__, NULL); \
+	}                                                          \
+	struct __##mod##_dummy_type_stop { }
 
 
 
@@ -1234,7 +1235,8 @@ int   lu_context_refill(struct lu_context *ctx);
 int  lu_context_key_register_many(struct lu_context_key *k, ...);
 void lu_context_key_degister_many(struct lu_context_key *k, ...);
 void lu_context_key_revive_many  (struct lu_context_key *k, ...);
-void lu_context_key_quiesce_many (struct lu_context_key *k, ...);
+void lu_context_key_quiesce_many(struct lu_device_type *t,
+				 struct lu_context_key *k, ...);
 
 /*
  * update/clear ctx/ses tags.
