Cray/lustre

commit 6ec6ed1afdd74bef769637fc6fe65ca0836e004c

LU-11762 ldlm: ensure the recovery timer is armed
During recovery, when the recovery timer is expired, the VBR phase
is initiated only the current recovery timeout is less than the hard
recovery timeout, or it will be stuck in the "wait_event_timeout()"
because there is no timer and it can't be waked up.

Change-Id: I32467afa45393e37f255e2b14f160c9da710461b
Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/35627
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ldlm/ldlm_lib.c
@@ -2106,7 +2106,8 @@ repeat:
 		/** evict exports which didn't finish recovery yet */
 		class_disconnect_stale_exports(obd, exp_finished);
 		return 1;
-	} else if (obd->obd_recovery_expired) {
+	} else if (obd->obd_recovery_expired &&
+		   obd->obd_recovery_timeout < obd->obd_recovery_time_hard) {
 		obd->obd_recovery_expired = 0;
 
 		/** If some clients died being recovered, evict them */

commit 002766082a1b6983fba89ce43931106bccf3c4a9

LUS-8299 ldlm: revert LU-11762
Commit 6ec6ed1afd introduced a problem for recovery.

When recovery timeout reaches hard recovery timeout
target_recovery_overseer() leaves obd_recovery_expired flag set. That
makes check_for_next_transno() to not wait until next replay request
arrives which leads to assertion:
LASSERT(atomic_read(&obd->obd_req_replay_clients) == 0);

Fixes: 6ec6ed1afd ("LU-11762 ldlm: ensure the recovery timer is armed")
Signed-off-by: Vladimir Saveliev <c17830@cray.com>
Change-Id: Ifd4aa8bcd164838eddf7dd3c4bfaf0cf764a92c6
Reviewed-on: https://es-gerrit.dev.cray.com/157758
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Reviewed-by: Sergey Cheremencev <c17829@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/ldlm/ldlm_lib.c
@@ -2123,8 +2123,7 @@ repeat:
 		/** evict exports which didn't finish recovery yet */
 		class_disconnect_stale_exports(obd, exp_finished);
 		return 1;
-	} else if (obd->obd_recovery_expired &&
-		   obd->obd_recovery_timeout < obd->obd_recovery_time_hard) {
+	} else if (obd->obd_recovery_expired) {
 		obd->obd_recovery_expired = 0;
 
 		/** If some clients died being recovered, evict them */