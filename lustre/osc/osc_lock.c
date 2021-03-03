c0fb1d97469db4e38419087d3dfccadb4f676aca
Patrick Farrell <paf@cray.com>
2021-03-03 09:50:04 +0300

    LUS-6432 osc: Batch gang_lookup cbs
    The osc_page_gang_lookup call backs can be trivially
    converted to operate in batches rather than one page at a
    time.  This improves cancellation time for locks protecting
    large numbers of pages by about 10% (after landing another optimization
    (LU-11290 ldlm: page discard speedup) it shows only 6% for canceling a
    lock for 30GB cached file ).
    
    Truncate to zero time (with one lock protecting many pages)
    was improved by about 5-10% as well.  Lock weighing
    performance should be improved slightly as well, but is
    tricky to benchmark.
    
    Change-Id: Ibb2c562f6977c1bfbef8854d90edb0bd15c7b223
    Signed-off-by: Patrick Farrell <paf@cray.com>
    Signed-off-by: Alexander Zarochentsev <c17826@cray.com>
    Reviewed-by: Andrew Perepechko <c17827@cray.com>
    Signed-off-by: Andriy Skulysh <c17819@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/158468
    Tested-by: Jenkins Build User <nssreleng@cray.com>

--- lustre/osc/osc_lock.c
+++ lustre/osc/osc_lock.c
@@ -654,15 +654,20 @@ out:
 EXPORT_SYMBOL(osc_ldlm_glimpse_ast);
 
 static int weigh_cb(const struct lu_env *env, struct cl_io *io,
-		    struct osc_page *ops, void *cbdata)
+		    void **pvec, int count, void *cbdata)
 {
-	struct cl_page *page = ops->ops_cl.cpl_page;
+	int i;
 
-	if (cl_page_is_vmlocked(env, page) || PageDirty(page->cp_vmpage) ||
-	    PageWriteback(page->cp_vmpage))
-		return CLP_GANG_ABORT;
+	for (i = 0; i < count; i++) {
+		struct osc_page *ops = pvec[i];
+		struct cl_page *page = ops->ops_cl.cpl_page;
 
-	*(pgoff_t *)cbdata = osc_index(ops) + 1;
+		if (cl_page_is_vmlocked(env, page) || PageDirty(page->cp_vmpage) ||
+				PageWriteback(page->cp_vmpage))
+			return CLP_GANG_ABORT;
+
+		*(pgoff_t *)cbdata = osc_index(ops) + 1;
+	}
 	return CLP_GANG_OKAY;
 }
 
