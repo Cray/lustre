cc4c4c7d53811b99081614daf6a7944940837d09
Li Dongyang <dongyangli@ddn.com>
2021-03-19 20:26:28 +1100

    LU-14536 o2iblnd: don't resend if there's no listener
    If there's no listener at remote peer, we will
    get IB_CM_REJ_INVALID_SERVICE_ID, currently we
    will try to resend which makes the discovery longer
    than necessary when connecting to a node which is
    not up.
    Use -EHOSTUNREACH instead of -ECONNREFUSED,
    so we don't end up queued for resend.
    
    Change-Id: Ifaf14bc3ada2e2469669285917e366af669817e2
    Test-Parameters: trivial
    Signed-off-by: Li Dongyang <dongyangli@ddn.com>
    Reviewed-on: https://review.whamcloud.com/42109
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
    Reviewed-by: Chris Horn <chris.horn@hpe.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lnet/klnds/o2iblnd/o2iblnd_cb.c
+++ lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -2902,6 +2902,7 @@ static void
 kiblnd_rejected(struct kib_conn *conn, int reason, void *priv, int priv_nob)
 {
 	struct kib_peer_ni *peer_ni = conn->ibc_peer;
+	int status = -ECONNREFUSED;
 
 	LASSERT (!in_interrupt());
 	LASSERT (conn->ibc_state == IBLND_CONN_ACTIVE_CONNECT);
@@ -2913,6 +2914,7 @@ kiblnd_rejected(struct kib_conn *conn, int reason, void *priv, int priv_nob)
 		break;
 
 	case IB_CM_REJ_INVALID_SERVICE_ID:
+		status = -EHOSTUNREACH;
 		peer_ni->ibp_retries++;
 		kiblnd_check_reconnect(conn, IBLND_MSG_VERSION, 0,
 				       IBLND_REJECT_INVALID_SRV_ID, NULL);
@@ -3023,7 +3025,7 @@ kiblnd_rejected(struct kib_conn *conn, int reason, void *priv, int priv_nob)
                 break;
         }
 
-        kiblnd_connreq_done(conn, -ECONNREFUSED);
+	kiblnd_connreq_done(conn, status);
 }
 
 static void
