7f2e6224916ac6350fd93893e6a9250ab3726f7a
Vitaly Fertman <c17818@cray.com>
2021-04-16 22:01:27 +0300

    LUS-9686 lfsck: SEL component end alignment fix
    a couple of fixes made during upstreaming
    - the end must be aligned by the stripe size only for non 0-lenght
      component;
    - make the same fix in lfs setstripe, lov_layout_verify;
    - lfsck should still skip DOM layout silently;
    
    Signed-off-by: Vitaly Fertman <c17818@cray.com>
    Change-Id: I1f02412e02cf5f4c845284195f9f95f0e7752578
    Reviewed-on: https://es-gerrit.dev.cray.com/158604
    Reviewed-by: Andriy Skulysh <c17819@cray.com>
    Tested-by: Alexander Lezhoev <c17454@cray.com>
    Reviewed-by: Alexander Boyko <c17825@cray.com>

--- lustre/lod/lod_lov.c
+++ lustre/lod/lod_lov.c
@@ -1981,7 +1981,7 @@ recheck:
 		if (rc)
 			RETURN(rc);
 
-		if (prev_end == LUSTRE_EOF)
+		if (prev_end == LUSTRE_EOF || ext->e_start == prev_end)
 			continue;
 
 		/* extent end must be aligned with the stripe_size */
