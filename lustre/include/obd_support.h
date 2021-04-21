88a63efeebe78be2382743f9b6a216f6acecd7c1
Alexander Boyko <c17825@cray.com>
2021-04-21 04:28:27 -0400

    LUS-9950 mdt: changelog purge deletes plain llog
    With a massive cancel records changelog could delete a plain
    llog file and skip one by one record cancelling.
    Also patch fixes the race between llog_destroy and llog_next_block.
    
    Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
    Change-Id: I50c268dc44e8dbc096fb6a96c18057356b315c2e
    Reviewed-on: https://es-gerrit.dev.cray.com/158608
    Reviewed-by: Andrew Perepechko <c17827@cray.com>
    Reviewed-by: Alexander Zarochentsev <c17826@cray.com>
    Tested-by: Elena Gryaznova <c17455@cray.com>

--- lustre/include/obd_support.h
+++ lustre/include/obd_support.h
@@ -257,6 +257,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_MDS_STAT_DELAY		 0x167
 #define OBD_FAIL_MDS_LINKEA_DELAY	 0x168
 #define OBD_FAIL_MDS_COMMITRW_DELAY	 0x169
+#define OBD_FAIL_MDS_CHANGELOG_DEL	 0x16c
 
 /* layout lock */
 #define OBD_FAIL_MDS_NO_LL_GETATTR	 0x170
