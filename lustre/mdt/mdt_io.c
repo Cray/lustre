Cray/lustre

commit d56dd4df208b067634f2a5daffea05cfee7bdcf8

LUS-9549 mdt: mdt_lvb2body crash fix
mdt_lvb2body may crash if res->lr_lvb_data is
not allocated, make it tolerant to not initialized
lvb_data pointer;
handling lbvo memory allcation failures.

Signed-off-by: Alexander Zarochentsev <c17826@cray.com>
Change-Id: Ie31cbba9187f9b04b3d1f8d2abc59e0612a44b41
Reviewed-on: https://es-gerrit.dev.cray.com/158105
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Reviewed-by: Alexey Lyashkov <c17817@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/mdt/mdt_io.c
@@ -1058,19 +1058,23 @@ static void mdt_lvb2reply(struct ldlm_resource *res, struct mdt_body *mb,
 
 	lock_res(res);
 	res_lvb = res->lr_lvb_data;
-	if (lvb)
-		*lvb = *res_lvb;
 
-	if (mb) {
-		mb->mbo_dom_size = res_lvb->lvb_size;
-		mb->mbo_dom_blocks = res_lvb->lvb_blocks;
-		mb->mbo_mtime = res_lvb->lvb_mtime;
-		mb->mbo_ctime = res_lvb->lvb_ctime;
-		mb->mbo_atime = res_lvb->lvb_atime;
-		mb->mbo_valid |= OBD_MD_FLATIME | OBD_MD_FLCTIME |
-				 OBD_MD_FLMTIME | OBD_MD_DOM_SIZE;
+	if (res_lvb) {
+		if (lvb)
+			*lvb = *res_lvb;
+
+		if (mb) {
+			mb->mbo_dom_size = res_lvb->lvb_size;
+			mb->mbo_dom_blocks = res_lvb->lvb_blocks;
+			mb->mbo_mtime = res_lvb->lvb_mtime;
+			mb->mbo_ctime = res_lvb->lvb_ctime;
+			mb->mbo_atime = res_lvb->lvb_atime;
+			mb->mbo_valid |= OBD_MD_FLATIME | OBD_MD_FLCTIME |
+				         OBD_MD_FLMTIME | OBD_MD_DOM_SIZE;
+		}
+
+		CDEBUG(D_DLMTRACE, "size %llu\n", res_lvb->lvb_size);
 	}
-	CDEBUG(D_DLMTRACE, "size %llu\n", res_lvb->lvb_size);
 	unlock_res(res);
 }
 
@@ -1099,7 +1103,7 @@ int mdt_dom_object_size(const struct lu_env *env, struct mdt_device *mdt,
 
 	/* Update lvbo data if DoM lock returned or if LVB is not yet valid. */
 	if (dom_lock || !mdt_dom_lvb_is_valid(res))
-		mdt_dom_lvbo_update(res, NULL, NULL, false);
+		rc = mdt_dom_lvbo_update(res, NULL, NULL, false);
 
 	mdt_lvb2reply(res, mb, NULL);
 	ldlm_resource_putref(res);
@@ -1205,10 +1209,14 @@ int mdt_glimpse_enqueue(struct mdt_thread_info *mti, struct ldlm_namespace *ns,
 	rc = ELDLM_LOCK_ABORTED;
 fill_mbo:
 	/* LVB can be without valid data in case of DOM */
-	if (!mdt_dom_lvb_is_valid(res))
-		mdt_dom_lvbo_update(res, lock, NULL, false);
-	mdt_lvb2reply(res, mbo, lvb);
+	if (!mdt_dom_lvb_is_valid(res)) {
+		int rc2;
 
+		rc2 = mdt_dom_lvbo_update(res, lock, NULL, false);
+		if (rc2)
+			RETURN(rc2);
+	}
+	mdt_lvb2reply(res, mbo, lvb);
 	RETURN(rc);
 }
 

lustre/mdt/mdt_lvb.c
@@ -155,13 +155,9 @@ int mdt_dom_lvbo_update(struct ldlm_resource *res, struct ldlm_lock *lock,
 	ENTRY;
 
 	/* Before going further let's check that OBD and export are healthy.
-	 */
-	if (exp != NULL &&
-	    (exp->exp_disconnected || exp->exp_failed ||
-	     exp->exp_obd->obd_stopping)) {
-		CDEBUG(D_INFO, "Skip LVB update, export is %s, obd is %s\n",
-		       exp->exp_failed ? "failed" : "disconnected",
-		       exp->exp_obd->obd_stopping ? "stopping" : "OK");
+	 * The condition matches one in ptlrpc_send_reply() */
+	if (exp && exp->exp_obd && exp->exp_obd->obd_fail) {
+		CDEBUG(D_INFO, "Skip LVB update, obd is failing over\n");
 		RETURN(0);
 	}
 

commit e66bdafd109558afb95edfda9a08f7b781e11220

LUS-9924 mdt: ldlm_lock_enqueue() assertion fails.
The ASSERTION(*flags & 0x0100000000000000ULL) fails
when mdt_dom_disk_lvbo_update() returns -ENOENT
in case of non-existent object.

mdc_attr_get() is able to detect that
lvbo data isn't filled. So the return value can be omited.

Fixes: d56dd4df20 ("LUS-9549 mdt: mdt_lvb2body crash fix")
Change-Id: Ia86ee7698e8561ebd93ce57985778df509532b09
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/158593
Reviewed-by: Alexander Zarochentsev <c17826@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/mdt/mdt_io.c
@@ -1209,13 +1209,8 @@ int mdt_glimpse_enqueue(struct mdt_thread_info *mti, struct ldlm_namespace *ns,
 	rc = ELDLM_LOCK_ABORTED;
 fill_mbo:
 	/* LVB can be without valid data in case of DOM */
-	if (!mdt_dom_lvb_is_valid(res)) {
-		int rc2;
-
-		rc2 = mdt_dom_lvbo_update(res, lock, NULL, false);
-		if (rc2)
-			RETURN(rc2);
-	}
+	if (!mdt_dom_lvb_is_valid(res))
+		mdt_dom_lvbo_update(res, lock, NULL, false);
 	mdt_lvb2reply(res, mbo, lvb);
 	RETURN(rc);
 }