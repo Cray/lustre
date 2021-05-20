Cray/lustre

commit 9a0b96ca8cdf754c08663ae1643a34036c29e120

LUS-9472 build: Support for CLE5
Supply smp_load_acquire() and smp_store_release() if the
kernel does not supply one.

Supply wrappers for idr_alloc() et. al. when the kernel
does not supply one.

Add missing header files to cray-obs/cray-lustre-gem_s.spec

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I989bb753da09cdb9f2a4145dea7e669eb85e460c
Reviewed-on: https://es-gerrit.dev.cray.com/158008
Tested-by: Jenkins Build User <nssreleng@cray.com>
Reviewed-by: Chris Horn <hornc@cray.com>
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>

lustre/obdclass/cl_page.c
@@ -45,6 +45,82 @@
 #include <cl_object.h>
 #include "cl_internal.h"
 
+#ifndef HAVE_SMP_STORE_LOAD
+
+#ifdef __x86_64__
+#if defined(CONFIG_X86_PPRO_FENCE)
+
+/*
+ * For either of these options x86 doesn't have a strong TSO memory
+ * model and we should fall back to full barriers.
+ */
+
+#define smp_store_release(p, v)						\
+do {									\
+	smp_mb();							\
+	ACCESS_ONCE(*p) = (v);						\
+} while (0)
+
+#define smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1 = ACCESS_ONCE(*p);				\
+	smp_mb();							\
+	___p1;								\
+})
+
+#else /* regular x86 TSO memory ordering */
+
+#define smp_store_release(p, v)						\
+do {									\
+	barrier();							\
+	ACCESS_ONCE(*p) = (v);						\
+} while (0)
+
+#define smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1 = ACCESS_ONCE(*p);				\
+	barrier();							\
+	___p1;								\
+})
+
+#endif /* x86 */
+#endif /* CONFIG_X86_PPRO_FENCE */
+
+#ifdef __aarch64__
+
+#define smp_store_release(p, v)						\
+do {									\
+	switch (sizeof(*p)) {						\
+	case 4:								\
+		asm volatile ("stlr %w1, %0"				\
+				: "=Q" (*p) : "r" (v) : "memory");	\
+		break;							\
+	case 8:								\
+		asm volatile ("stlr %1, %0"				\
+				: "=Q" (*p) : "r" (v) : "memory");	\
+		break;							\
+	}								\
+} while (0)
+
+#define smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1;						\
+	switch (sizeof(*p)) {						\
+	case 4:								\
+		asm volatile ("ldar %w0, %1"				\
+			: "=r" (___p1) : "Q" (*p) : "memory");		\
+		break;							\
+	case 8:								\
+		asm volatile ("ldar %0, %1"				\
+			: "=r" (___p1) : "Q" (*p) : "memory");		\
+		break;							\
+	}								\
+	___p1;								\
+})
+
+#endif /* __aarch64__ */
+#endif /* HAVE_SMP_STORE_LOAD */
+
 static void cl_page_delete0(const struct lu_env *env, struct cl_page *pg);
 static DEFINE_MUTEX(cl_page_kmem_mutex);
 

cray-obs/cray-lustre-gem_s.spec
@@ -198,8 +198,9 @@ done
 for header in bitmap.h curproc.h libcfs_cpu.h libcfs_debug.h \
 	      libcfs_fail.h  libcfs.h libcfs_hash.h libcfs_heap.h \
 	      libcfs_prim.h libcfs_private.h libcfs_string.h  \
-	      libcfs_workitem.h range_lock.h linux-cpu.h linux-crypto.h \
-	      linux-fs.h linux-mem.h linux-misc.h linux-time.h
+	      libcfs_workitem.h range_lock.h \
+	      linux-cpu.h linux-crypto.h linux-fs.h linux-hash.h linux-list.h \
+	      linux-mem.h linux-misc.h linux-time.h linux-wait.h
 do
     found=`find %{_builddir}/cray-lustre-%{_version}/libcfs/include/libcfs -name $header`
     if [ -n "${found}" ]; then

libcfs/autoconf/lustre-libcfs.m4
@@ -275,6 +275,47 @@ nsecs_to_jiffies64, [
 EXTRA_KCFLAGS="$tmp_flags"
 ]) # LIBCFS_MOD_DELAYED_WORK
 
+
+#
+# LIBCFS_HAVE_IDR_ALLOC
+# v3.9
+#
+AC_DEFUN([LIBCFS_HAVE_IDR_ALLOC], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if 'idr_alloc()' is present],
+nsecs_to_jiffies64, [
+	#include <linux/idr.h>
+],[
+	(void)idr_alloc(NULL, NULL, 0, 0, GFP_NOFS);
+],[
+	AC_DEFINE(HAVE_IDR_ALLOC, 1,
+		['idr_alloc()' is present])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LIBCFS_HAVE_IDR_ALLOC
+
+#
+# LIBCFS_HAVE_SMP_STORE_LOAD
+# v3.12
+#
+AC_DEFUN([LIBCFS_HAVE_SMP_STORE_LOAD], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if 'smp_load_acquire()' is present],
+nsecs_to_jiffies64, [
+	#include <linux/rwsem.h>
+],[
+	long a[1] = {0};
+	long v = smp_load_acquire(&a[0]);
+	(void)v;
+],[
+	AC_DEFINE(HAVE_SMP_STORE_LOAD, 1,
+		['smp_load_acquire()' is present])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LIBCFS_HAVE_SMP_STORE_LOAD
+
 #
 # Kernel version v3.8-rc4-82-g4f522a247bc2 exported d_hash_and_lookup()
 # It was added in v2.6.16-3821-g3e7e241f8c5c, so no worries about header.
@@ -1369,12 +1410,14 @@ LIBCFS_MOD_DELAYED_WORK
 LIBCFS_HAVE_CRC32
 LIBCFS_D_HASH_AND_LOOKUP
 LIBCFS_ENABLE_CRC32_ACCEL
+LIBCFS_HAVE_IDR_ALLOC
 # 3.10
 LIBCFS_ENABLE_CRC32C_ACCEL
 # 3.11
 LIBCFS_DENTRY_D_HASH_2ARGS
 LIBCFS_KTIME_GET_TS64
 # 3.12
+LIBCFS_HAVE_SMP_STORE_LOAD
 LIBCFS_PREPARE_TO_WAIT_EVENT
 LIBCFS_KERNEL_PARAM_OPS
 LIBCFS_KTIME_ADD

lustre/mdc/mdc_changelog.c
@@ -116,15 +116,53 @@ enum {
 static DEFINE_IDR(chlg_minor_idr);
 static DEFINE_SPINLOCK(chlg_minor_lock);
 
+#ifndef HAVE_IDR_ALLOC
+static int cfs_idr_alloc(struct idr *idr, void *ptr, int start, int end,
+			 gfp_t gfp_mask)
+{
+	int id = -1;
+	int err = idr_get_new(idr, ptr, &id);
+
+	if (err)
+		return err;
+	if (id > end) {
+		idr_remove(idr, id);
+		return -ENOSPC;
+	}
+
+	return id;
+}
+
+static int cfs_idr_preload(struct idr *idr, gfp_t gfp_mask)
+{
+	return idr_pre_get(idr, gfp_mask);
+}
+
+#define idr_preload_end()
+
+#else
+
+#define cfs_idr_alloc(idr, ptr, start, end, gfp_mask) \
+	idr_alloc((idr), (ptr), (start), (end), (gfp_mask))
+
+static int cfs_idr_preload(struct idr *idr, gfp_t gfp_mask)
+{
+	idr_preload(gfp_mask);
+	return 1; /* returns 1 on success */
+}
+#endif
+
 static int chlg_minor_alloc(int *pminor)
 {
 	void *minor_allocated = (void *)-1;
 	int minor;
 
-	idr_preload(GFP_KERNEL);
+	if (!cfs_idr_preload(&chlg_minor_idr, GFP_KERNEL))
+		return -ENOMEM;
+
 	spin_lock(&chlg_minor_lock);
-	minor = idr_alloc(&chlg_minor_idr, minor_allocated, 0,
-			  MDC_CHANGELOG_DEV_COUNT, GFP_NOWAIT);
+	minor = cfs_idr_alloc(&chlg_minor_idr, minor_allocated, 0,
+			      MDC_CHANGELOG_DEV_COUNT, GFP_NOWAIT);
 	spin_unlock(&chlg_minor_lock);
 	idr_preload_end();
 

commit cce4d5b961ce0ec670f4912f01e05261bdbd4a19

LUS-9979 libcfs: Move smp_store_release/_load_aquire
Usage of smp_store_release / smp_load_aquire has proliferated
Move it to libcfs for older kernels that require it.

Fixes: 9a0b96ca8c ("LUS-9472 build: Support for CLE5")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I057e483695317e028ee21a025c441c641e939e69
Reviewed-on: https://es-gerrit.dev.cray.com/158640
Tested-by: Jenkins Build User <nssreleng@cray.com>
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Chris Horn <hornc@cray.com>

libcfs/include/libcfs/libcfs_cpu.h
@@ -371,4 +371,80 @@ cfs_mem_cache_cpt_alloc(struct kmem_cache *cachep, struct cfs_cpt_table *cptab,
 int  cfs_cpu_init(void);
 void cfs_cpu_fini(void);
 
+#ifndef HAVE_SMP_STORE_LOAD
+
+#ifdef __x86_64__
+#if defined(CONFIG_X86_PPRO_FENCE)
+
+/*
+ * For either of these options x86 doesn't have a strong TSO memory
+ * model and we should fall back to full barriers.
+ */
+
+#define smp_store_release(p, v)						\
+do {									\
+	smp_mb();							\
+	ACCESS_ONCE(*p) = (v);						\
+} while (0)
+
+#define smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1 = ACCESS_ONCE(*p);				\
+	smp_mb();							\
+	___p1;								\
+})
+
+#else /* regular x86 TSO memory ordering */
+
+#define smp_store_release(p, v)						\
+do {									\
+	barrier();							\
+	ACCESS_ONCE(*p) = (v);						\
+} while (0)
+
+#define smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1 = ACCESS_ONCE(*p);				\
+	barrier();							\
+	___p1;								\
+})
+
+#endif /* x86 */
+#endif /* CONFIG_X86_PPRO_FENCE */
+
+#ifdef __aarch64__
+
+#define smp_store_release(p, v)						\
+do {									\
+	switch (sizeof(*p)) {						\
+	case 4:								\
+		asm volatile ("stlr %w1, %0"				\
+				: "=Q" (*p) : "r" (v) : "memory");	\
+		break;							\
+	case 8:								\
+		asm volatile ("stlr %1, %0"				\
+				: "=Q" (*p) : "r" (v) : "memory");	\
+		break;							\
+	}								\
+} while (0)
+
+#define smp_load_acquire(p)						\
+({									\
+	typeof(*p) ___p1;						\
+	switch (sizeof(*p)) {						\
+	case 4:								\
+		asm volatile ("ldar %w0, %1"				\
+			: "=r" (___p1) : "Q" (*p) : "memory");		\
+		break;							\
+	case 8:								\
+		asm volatile ("ldar %0, %1"				\
+			: "=r" (___p1) : "Q" (*p) : "memory");		\
+		break;							\
+	}								\
+	___p1;								\
+})
+
+#endif /* __aarch64__ */
+#endif /* HAVE_SMP_STORE_LOAD */
+
 #endif /* __LIBCFS_CPU_H__ */

lustre/obdclass/cl_page.c
@@ -45,82 +45,6 @@
 #include <cl_object.h>
 #include "cl_internal.h"
 
-#ifndef HAVE_SMP_STORE_LOAD
-
-#ifdef __x86_64__
-#if defined(CONFIG_X86_PPRO_FENCE)
-
-/*
- * For either of these options x86 doesn't have a strong TSO memory
- * model and we should fall back to full barriers.
- */
-
-#define smp_store_release(p, v)						\
-do {									\
-	smp_mb();							\
-	ACCESS_ONCE(*p) = (v);						\
-} while (0)
-
-#define smp_load_acquire(p)						\
-({									\
-	typeof(*p) ___p1 = ACCESS_ONCE(*p);				\
-	smp_mb();							\
-	___p1;								\
-})
-
-#else /* regular x86 TSO memory ordering */
-
-#define smp_store_release(p, v)						\
-do {									\
-	barrier();							\
-	ACCESS_ONCE(*p) = (v);						\
-} while (0)
-
-#define smp_load_acquire(p)						\
-({									\
-	typeof(*p) ___p1 = ACCESS_ONCE(*p);				\
-	barrier();							\
-	___p1;								\
-})
-
-#endif /* x86 */
-#endif /* CONFIG_X86_PPRO_FENCE */
-
-#ifdef __aarch64__
-
-#define smp_store_release(p, v)						\
-do {									\
-	switch (sizeof(*p)) {						\
-	case 4:								\
-		asm volatile ("stlr %w1, %0"				\
-				: "=Q" (*p) : "r" (v) : "memory");	\
-		break;							\
-	case 8:								\
-		asm volatile ("stlr %1, %0"				\
-				: "=Q" (*p) : "r" (v) : "memory");	\
-		break;							\
-	}								\
-} while (0)
-
-#define smp_load_acquire(p)						\
-({									\
-	typeof(*p) ___p1;						\
-	switch (sizeof(*p)) {						\
-	case 4:								\
-		asm volatile ("ldar %w0, %1"				\
-			: "=r" (___p1) : "Q" (*p) : "memory");		\
-		break;							\
-	case 8:								\
-		asm volatile ("ldar %0, %1"				\
-			: "=r" (___p1) : "Q" (*p) : "memory");		\
-		break;							\
-	}								\
-	___p1;								\
-})
-
-#endif /* __aarch64__ */
-#endif /* HAVE_SMP_STORE_LOAD */
-
 static void cl_page_delete0(const struct lu_env *env, struct cl_page *pg);
 static DEFINE_MUTEX(cl_page_kmem_mutex);
 