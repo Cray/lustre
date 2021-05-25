1e691ebdf7549b339319e410e0cb30ebd3483572
Sergey Cheremencev <c17829@cray.com>
2021-05-26 01:37:59 +0300

    LUS-10029 tests: check EDQUOT after OST failover
    Check that OST gets correct edquot value after failover.
    
    Test-Parameters: testlist="sanity-quota/76"
    Change-Id: I06018a7acdfbeecb9c5d0a00144cc7dfba04c06a
    Signed-off-by: Sergey Cheremencev <c17829@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/158779
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Reviewed-by: Alexander Boyko <c17825@cray.com>
    Tested-by: Jenkins Build User <nssreleng@cray.com>

--- lustre/quota/qmt_handler.c
+++ lustre/quota/qmt_handler.c
@@ -640,6 +640,9 @@ int qmt_dqacq0(const struct lu_env *env, struct qmt_device *qmt,
 	if (OBD_FAIL_CHECK(OBD_FAIL_QUOTA_RECOVERABLE_ERR))
 		RETURN(-cfs_fail_val);
 
+	if (OBD_FAIL_CHECK(OBD_FAIL_QUOTA_PREACQ) && req_is_preacq(qb_flags))
+		RETURN(-EAGAIN);
+
 	if (qti_lqes_restore_init(env))
 		RETURN(-ENOMEM);
 
