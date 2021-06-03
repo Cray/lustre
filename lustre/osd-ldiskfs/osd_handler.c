f1db5916b9956ed424cca39a7503fa3244bcc7ee
Vitaly Fertman <c17818@cray.com>
2021-06-03 17:57:10 +0300

    Revert "LUS-9062 out: don't return einprogress error"
    This reverts commit 58b9a909c496af1e6f6fe547aac307c1fe5d0363.
    
    Due to LUS-9844

--- lustre/osd-ldiskfs/osd_handler.c
+++ lustre/osd-ldiskfs/osd_handler.c
@@ -1253,10 +1253,8 @@ trigger:
 		if (scrub->os_partial_scan && !scrub->os_in_join)
 			goto join;
 
-		if (IS_ERR_OR_NULL(inode) || result) {
-			osd_oii_insert(dev, fid, id, result == -ENOENT);
+		if (IS_ERR_OR_NULL(inode) || result)
 			GOTO(out, result = -EINPROGRESS);
-		}
 
 		LASSERT(remote);
 		LASSERT(obj->oo_inode == inode);
@@ -1284,10 +1282,8 @@ join:
 	if (rc1 && rc1 != -EALREADY)
 		GOTO(out, result = -EREMCHG);
 
-	if (IS_ERR_OR_NULL(inode) || result) {
-		osd_oii_insert(dev, fid, id, result == -ENOENT);
+	if (IS_ERR_OR_NULL(inode) || result)
 		GOTO(out, result = -EINPROGRESS);
-	}
 
 	LASSERT(remote);
 	LASSERT(obj->oo_inode == inode);
