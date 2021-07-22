f5902036a13270fad6dc0cd701ce00c766ded25e
Shaun Tancheff <shaun.tancheff@hpe.com>
2021-07-22 03:58:44 -0500

    LUS-10319 libcfs: Complete testing for tcp_sock_set_*
    Linux commits:
      v5.7-rc6-2504-gddd061b8daed
      tcp: add tcp_sock_set_quickack
    
      v5.7-rc6-2508-gd41ecaac903c
      tcp: add tcp_sock_set_keepintvl
    
      v5.7-rc6-2509-g480aeb9639d6
      tcp: add tcp_sock_set_keepcnt
    
    Introduced a series of helper functions that may be
    back ported individually.
    
    This is a port of:
    Lustre-commit: bbd9646f91fd13ea84f730296f3ce6983030aea2
    Lustre-change: https://review.whamcloud.com/44374
    
    WC-bug-id: LU-14881
    Fixes: 99d9638d6c ("LU-13783 libcfs: support removal of kernel_setsockopt()")
    Fixes: b284c3e21d ("LUS-10121 libcfs: support removal of kernel_setsockopt()")
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I26e4c005c63c99b8b1dcb9496701bcecab566927
    Reviewed-by: Chris Horn <hornc@cray.com>
    Tested-by: Chris Horn <hornc@cray.com>
    Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>

--- libcfs/include/libcfs/linux/linux-net.h
+++ libcfs/include/libcfs/linux/linux-net.h
@@ -27,6 +27,7 @@
 
 #include <net/tcp.h>
 
+#if !defined(HAVE_TCP_SOCK_SET_QUICKACK)
 static inline void tcp_sock_set_quickack(struct sock *sk, int opt)
 {
 	struct socket *sock = sk->sk_socket;
@@ -34,6 +35,7 @@ static inline void tcp_sock_set_quickack(struct sock *sk, int opt)
 	kernel_setsockopt(sock, SOL_TCP, TCP_QUICKACK,
 			  (char *)&opt, sizeof(opt));
 }
+#endif /* HAVE_TCP_SOCK_SET_QUICKACK */
 
 static inline void tcp_sock_set_nodelay(struct sock *sk)
 {
@@ -52,6 +54,7 @@ static inline int tcp_sock_set_keepidle(struct sock *sk, int opt)
 				 (char *)&opt, sizeof(opt));
 }
 
+#if !defined(HAVE_TCP_SOCK_SET_KEEPINTVL)
 static inline int tcp_sock_set_keepintvl(struct sock *sk, int opt)
 {
 	struct socket *sock = sk->sk_socket;
@@ -59,7 +62,9 @@ static inline int tcp_sock_set_keepintvl(struct sock *sk, int opt)
 	return kernel_setsockopt(sock, SOL_TCP, TCP_KEEPINTVL,
 				 (char *)&opt, sizeof(opt));
 }
+#endif /* HAVE_TCP_SOCK_SET_KEEPINTVL */
 
+#if !defined(HAVE_TCP_SOCK_SET_KEEPCNT)
 static inline int tcp_sock_set_keepcnt(struct sock *sk, int opt)
 {
 	struct socket *sock = sk->sk_socket;
@@ -67,6 +72,7 @@ static inline int tcp_sock_set_keepcnt(struct sock *sk, int opt)
 	return kernel_setsockopt(sock, SOL_TCP, TCP_KEEPCNT,
 				 (char *)&opt, sizeof(opt));
 }
+#endif /* HAVE_TCP_SOCK_SET_KEEPCNT */
 #endif /* HAVE_KERNEL_SETSOCKOPT */
 
 #endif /* __LIBCFS_LINUX_NET_H__ */
