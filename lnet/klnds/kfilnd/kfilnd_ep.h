9da75c62f14a770e87d38eb6ab8fed1a50ce3fbe
Ian Ziemba <ian.ziemba@hpe.com>
2021-08-11 10:50:03 -0500

    LUS-6620 kfilnd: Handle kfabric -EAGAIN
    Kfabric (and the kCXI provider) may return -EAGAIN for operations
    involving the queueing of underlying hardware commands. The
    expectation is the kfabric client (kfilnd) will retry the operation.
    
    The following are the kfilnd impacted operations:
    - Data transfer operations (send, tagged send, read, write)
    - Post receive (tagged and untagged)
    - Cancel receive (tagged and untagged)
    
    Two kfilnd objects are impacted: transactions and immediate buffers.
    
    To handle -EAGAIN with transactions, the transaction state machine has
    been updated to only allow for one kfabric operation per transaction
    state. This has resulted in the creation of two new transaction
    states: TN_STATE_TAGGED_RECV_POSTED and TN_STATE_SEND_FAILED. In
    addition, each transaction state will only perform non-idempotent
    operations if the kfabric operation returns success. The end result is
    that -EAGAIN is now propagated up to kfilnd_tn_event_handler() which
    will then queue the transaction on an endpoint replay queue.
    
    To handle -EAGAIN with immediate buffers, when the final reference is
    returned for an immediate buffer and the posting of the buffer returns
    -EAGAIN, the reference is returned and the immediate buffer is queued
    on an endpoint replay queue for replay.
    
    The endpoint will attempt to replay all queued operations once a timer
    expires or an endpoint completion queue is drained.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>
    Change-Id: Ia961200b98e810e579d44fc725f984956470909f
    Reviewed-on: https://es-gerrit.dev.cray.com/159185
    Reviewed-by: Ian Ziemba <iziemba@cray.com>
    Tested-by: Ian Ziemba <iziemba@cray.com>
    Reviewed-by: Chris Horn <hornc@cray.com>

--- lnet/klnds/kfilnd/kfilnd_ep.h
+++ lnet/klnds/kfilnd/kfilnd_ep.h
@@ -9,6 +9,11 @@ struct kfilnd_ep_err_fail_loc_work {
 	struct kfi_cq_err_entry err;
 };
 
+static inline bool kfilnd_ep_replays_pending(struct kfilnd_ep *ep)
+{
+	return atomic_read(&ep->replay_count) > 0;
+};
+
 void kfilnd_ep_dereg_mr(struct kfilnd_ep *ep, struct kfilnd_transaction *tn);
 int kfilnd_ep_reg_mr(struct kfilnd_ep *ep, struct kfilnd_transaction *tn);
 int kfilnd_ep_post_tagged_send(struct kfilnd_ep *ep,
@@ -20,12 +25,15 @@ int kfilnd_ep_post_tagged_recv(struct kfilnd_ep *ep,
 int kfilnd_ep_post_send(struct kfilnd_ep *ep, struct kfilnd_transaction *tn);
 int kfilnd_ep_post_write(struct kfilnd_ep *ep, struct kfilnd_transaction *tn);
 int kfilnd_ep_post_read(struct kfilnd_ep *ep, struct kfilnd_transaction *tn);
-int kfilnd_ep_imm_buffer_put(struct kfilnd_immediate_buffer *buf);
+void kfilnd_ep_imm_buffer_put(struct kfilnd_immediate_buffer *buf);
 int kfilnd_ep_post_imm_buffers(struct kfilnd_ep *ep);
 void kfilnd_ep_cancel_imm_buffers(struct kfilnd_ep *ep);
 void kfilnd_ep_free(struct kfilnd_ep *ep);
 struct kfilnd_ep *kfilnd_ep_alloc(struct kfilnd_dev *dev,
 				  unsigned int context_id, unsigned int cpt,
 				  size_t nrx, size_t rx_size);
+void kfilnd_ep_flush_replay_queue(struct kfilnd_ep *ep);
+void kfilnd_ep_queue_tn_replay(struct kfilnd_ep *ep,
+			       struct kfilnd_transaction *tn);
 
 #endif /* _KFILND_EP_ */
