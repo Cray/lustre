lustre/lustre-release

LU-12071 osd-ldiskfs: bypass pagecache if requested
in few cases (non-rotational drive, by request, or file size)
osd-ldiskfs may want to skip caching. If so, bypass page cache
instead of later cache invalidation, as cache invalidation can
be quite expensive.

set the maximum cached read/write IO size use:
     lctl set_param osd-ldiskfs.*.readcache_max_io_mb=N
     lctl set_param osd-ldiskfs.*.writethrough_max_io_mb=N
The default maximum cached IO size is 8MiB.

ladvise() enforces IO to go in the cache and all subsquent
reads will consult with the cache.

Change-Id: I37403ced7ad9553128ba168fa36315d6aa1aaf2d
Signed-off-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/34422
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Wang Shilong <wshilong@ddn.com>
Tested-by: Maloo <maloo@whamcloud.com>

lustre/osd-ldiskfs/osd_lproc.c
@@ -579,6 +579,89 @@ ldiskfs_osd_readcache_seq_write(struct file *file, const char __user *buffer,
 
 LDEBUGFS_SEQ_FOPS(ldiskfs_osd_readcache);
 
+static int ldiskfs_osd_readcache_max_io_seq_show(struct seq_file *m, void *data)
+{
+	struct osd_device *osd = osd_dt_dev((struct dt_device *)m->private);
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	seq_printf(m, "%lu\n", osd->od_readcache_max_iosize >> 20);
+	return 0;
+}
+
+static ssize_t
+ldiskfs_osd_readcache_max_io_seq_write(struct file *file,
+				       const char __user *buffer,
+				       size_t count, loff_t *off)
+{
+	struct seq_file *m = file->private_data;
+	struct dt_device *dt = m->private;
+	struct osd_device *osd = osd_dt_dev(dt);
+	s64 val;
+	int rc;
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	rc = lprocfs_str_with_units_to_s64(buffer, count, &val, 'M');
+	if (rc)
+		return rc;
+	if (val < 0)
+		return -ERANGE;
+
+	if (val > PTLRPC_MAX_BRW_SIZE)
+		return -ERANGE;
+	osd->od_readcache_max_iosize = val;
+	return count;
+}
+
+LDEBUGFS_SEQ_FOPS(ldiskfs_osd_readcache_max_io);
+
+static int ldiskfs_osd_writethrough_max_io_seq_show(struct seq_file *m,
+						    void *data)
+{
+	struct osd_device *osd = osd_dt_dev((struct dt_device *)m->private);
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	seq_printf(m, "%lu\n", osd->od_writethrough_max_iosize >> 20);
+	return 0;
+}
+
+static ssize_t
+ldiskfs_osd_writethrough_max_io_seq_write(struct file *file,
+				       const char __user *buffer,
+				       size_t count, loff_t *off)
+{
+	struct seq_file *m = file->private_data;
+	struct dt_device *dt = m->private;
+	struct osd_device *osd = osd_dt_dev(dt);
+	s64 val;
+	int rc;
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	rc = lprocfs_str_with_units_to_s64(buffer, count, &val, 'M');
+	if (rc)
+		return rc;
+	if (val < 0)
+		return -ERANGE;
+
+	if (val > PTLRPC_MAX_BRW_SIZE)
+		return -ERANGE;
+	osd->od_writethrough_max_iosize = val;
+	return count;
+}
+
+LDEBUGFS_SEQ_FOPS(ldiskfs_osd_writethrough_max_io);
+
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 0, 52, 0)
 static ssize_t index_in_idif_show(struct kobject *kobj, struct attribute *attr,
 				  char *buf)
@@ -694,6 +777,10 @@ struct lprocfs_vars lprocfs_osd_obd_vars[] = {
 	  .fops	=	&ldiskfs_osd_oi_scrub_fops	},
 	{ .name	=	"readcache_max_filesize",
 	  .fops	=	&ldiskfs_osd_readcache_fops	},
+	{ .name	=	"readcache_max_io_mb",
+	  .fops	=	&ldiskfs_osd_readcache_max_io_fops	},
+	{ .name	=	"writethrough_max_io_mb",
+	  .fops	=	&ldiskfs_osd_writethrough_max_io_fops	},
 	{ NULL }
 };
 

Cray/lustre

lustre/osd-ldiskfs/osd_lproc.c
@@ -566,6 +566,89 @@ ldiskfs_osd_readcache_seq_write(struct file *file, const char __user *buffer,
 }
 LPROC_SEQ_FOPS(ldiskfs_osd_readcache);
 
+static int ldiskfs_osd_readcache_max_io_seq_show(struct seq_file *m, void *data)
+{
+	struct osd_device *osd = osd_dt_dev((struct dt_device *)m->private);
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	seq_printf(m, "%lu\n", osd->od_readcache_max_iosize >> 20);
+	return 0;
+}
+
+static ssize_t
+ldiskfs_osd_readcache_max_io_seq_write(struct file *file,
+				       const char __user *buffer,
+				       size_t count, loff_t *off)
+{
+	struct seq_file *m = file->private_data;
+	struct dt_device *dt = m->private;
+	struct osd_device *osd = osd_dt_dev(dt);
+	s64 val;
+	int rc;
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	rc = lprocfs_str_with_units_to_s64(buffer, count, &val, 'M');
+	if (rc)
+		return rc;
+	if (val < 0)
+		return -ERANGE;
+
+	if (val > PTLRPC_MAX_BRW_SIZE)
+		return -ERANGE;
+	osd->od_readcache_max_iosize = val;
+	return count;
+}
+
+LPROC_SEQ_FOPS(ldiskfs_osd_readcache_max_io);
+
+static int ldiskfs_osd_writethrough_max_io_seq_show(struct seq_file *m,
+						    void *data)
+{
+	struct osd_device *osd = osd_dt_dev((struct dt_device *)m->private);
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	seq_printf(m, "%lu\n", osd->od_writethrough_max_iosize >> 20);
+	return 0;
+}
+
+static ssize_t
+ldiskfs_osd_writethrough_max_io_seq_write(struct file *file,
+				       const char __user *buffer,
+				       size_t count, loff_t *off)
+{
+	struct seq_file *m = file->private_data;
+	struct dt_device *dt = m->private;
+	struct osd_device *osd = osd_dt_dev(dt);
+	s64 val;
+	int rc;
+
+	LASSERT(osd != NULL);
+	if (unlikely(osd->od_mnt == NULL))
+		return -EINPROGRESS;
+
+	rc = lprocfs_str_with_units_to_s64(buffer, count, &val, 'M');
+	if (rc)
+		return rc;
+	if (val < 0)
+		return -ERANGE;
+
+	if (val > PTLRPC_MAX_BRW_SIZE)
+		return -ERANGE;
+	osd->od_writethrough_max_iosize = val;
+	return count;
+}
+
+LPROC_SEQ_FOPS(ldiskfs_osd_writethrough_max_io);
+
 #if LUSTRE_VERSION_CODE < OBD_OCD_VERSION(3, 0, 52, 0)
 static int ldiskfs_osd_index_in_idif_seq_show(struct seq_file *m, void *data)
 {
@@ -726,6 +809,10 @@ struct lprocfs_vars lprocfs_osd_obd_vars[] = {
 	  .fops	=	&ldiskfs_osd_readcache_fops	},
 	{ .name	=	"index_backup",
 	  .fops	=	&ldiskfs_osd_index_backup_fops	},
+	{ .name	=	"readcache_max_io_mb",
+	  .fops	=	&ldiskfs_osd_readcache_max_io_fops	},
+	{ .name	=	"writethrough_max_io_mb",
+	  .fops	=	&ldiskfs_osd_writethrough_max_io_fops	},
 	{ NULL }
 };
 