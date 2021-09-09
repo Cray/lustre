5927bc7859babd785fd272c5ef26217d8797e657
Ian Ziemba <ian.ziemba@hpe.com>
2021-09-09 15:16:34 -0500

    LUS-10394 kfilnd: Move RX ctx xchg into hello msg
    Instead of having RX context information (i/e prefer rx) exchanged in
    the base kfilnd message header, since this is really peer
    configuration information and there is a hello handshake, move the
    exchange of RX context information into the hello message.
    
    Two new fields are added to the hello message.
    1. RX base: Defines the RX base the remote peer should use when
    sending messages to the local peer. RX base is the same as the prefer
    RX field which has been removed from the kfilnd msg header.
    2. RX count: Defines the number of RX context the remote peer can
    target when sending messages to the local peer. Currently, kfilnd is
    only setup to allow for 1 RX context for peer. The exchange of this
    information allows for extensibility to enable new features such as
    the ability of the remote peer to round-robin over multiple local RX
    contexts.
    
    Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>
    Change-Id: I20e10b64cb42deaded2ba522c347feb680bb1b1a
    Reviewed-on: https://es-gerrit.dev.cray.com/159374
    Reviewed-by: Chris Horn <hornc@cray.com>
    Reviewed-by: Ian Ziemba <iziemba@cray.com>
    Tested-by: Ian Ziemba <iziemba@cray.com>

--- lnet/klnds/kfilnd/kfilnd_peer.h
+++ lnet/klnds/kfilnd/kfilnd_peer.h
@@ -13,10 +13,13 @@
 void kfilnd_peer_down(struct kfilnd_peer *peer);
 void kfilnd_peer_put(struct kfilnd_peer *peer);
 struct kfilnd_peer *kfilnd_peer_get(struct kfilnd_dev *dev, lnet_nid_t nid);
-void kfilnd_peer_update(struct kfilnd_peer *peer, unsigned int rx_context);
+void kfilnd_peer_update_rx_contexts(struct kfilnd_peer *peer,
+				    unsigned int rx_base,
+				    unsigned int rx_count);
 void kfilnd_peer_alive(struct kfilnd_peer *peer);
 void kfilnd_peer_destroy(struct kfilnd_dev *dev);
 void kfilnd_peer_init(struct kfilnd_dev *dev);
 kfi_addr_t kfilnd_peer_get_kfi_addr(struct kfilnd_peer *peer);
+u16 kfilnd_peer_target_rx_base(struct kfilnd_peer *peer);
 
 #endif /* _KFILND_PEER_ */
