Xyratex/lustre-stable

MRP-3434 llite: Lustre I/O hung waiting for page
A deadlock during lock_page()

Thread 1:
sleep_on_page
__wait_on_bit_lock
__lock_page
vvp_page_make_ready
cl_page_make_ready
osc_extent_make_ready
osc_io_unplug0

Thread 2:
__wait_on_bit
wait_on_page_bit
shrink_inactive_list
shrink_list
shrink_zone
do_try_to_free_pages
try_to_free_mem_cgroup_pages
mem_cgroup_hierarchical_reclaim
__mem_cgroup_try_charge
mem_cgroup_prepare_migration
migrate_pages
compact_zone
__compact_pgdat
compact_node
sysfs_compact_node

Thread 2 tries to migrate 2nd page from extent
and waits for 1st to complete writeback.

Lock all extent pages and modify them after.

Change-Id: I42c53058051db0ee7b40d43fb0416cac2c0c0cb8
Signed-off-by: Andriy Skulysh <andriy.skulysh@seagate.com>
Reviewed-on: http://es-gerrit.xyus.xyratex.com:8080/10522
Reviewed-by: Andrew Perepechko <andrew.perepechko@seagate.com>
Tested-by: Jenkins
Reviewed-by: Vitaly Fertman <vitaly.fertman@seagate.com>
Tested-by: Elena V. Gryaznova <elena.gryaznova@seagate.com>

lustre/llite/vvp_page.c
@@ -315,27 +315,41 @@ static void vvp_page_completion_write(const struct lu_env *env,
 }
 
 /**
- * Implements cl_page_operations::cpo_make_ready() method.
+ * Implements cl_page_operations::cpo_make_ready_start() method.
  *
  * This is called to yank a page from the transfer cache and to send it out as
- * a part of transfer. This function try-locks the page. If try-lock failed,
- * page is owned by some concurrent IO, and should be skipped (this is bad,
- * but hopefully rare situation, as it usually results in transfer being
- * shorter than possible).
+ * a part of transfer. This function is used to lock all pages from the extent.
+ *
+ * \retval 0      success, page is locked
+ *
+ */
+static int vvp_page_make_ready_start(const struct lu_env *env,
+				     const struct cl_page_slice *slice)
+{
+	struct page *vmpage = cl2vm_page(slice);
+
+	lock_page(vmpage);
+
+	return 0;
+}
+
+/**
+ * Implements cl_page_operations::cpo_make_ready_end() method.
+ *
+ * This is called to yank a page from the transfer cache and to send it out as
+ * a part of transfer. The page is already locked by
+ * vvp_page_make_ready_start().
  *
  * \retval 0      success, page can be placed into transfer
  *
- * \retval -EAGAIN page is either used by concurrent IO has been
- * truncated. Skip it.
  */
-static int vvp_page_make_ready(const struct lu_env *env,
-			       const struct cl_page_slice *slice)
+static int vvp_page_make_ready_end(const struct lu_env *env,
+				   const struct cl_page_slice *slice)
 {
 	struct page *vmpage = cl2vm_page(slice);
 	struct cl_page *pg = slice->cpl_page;
 	int result = 0;
 
-	lock_page(vmpage);
 	if (clear_page_dirty_for_io(vmpage)) {
 		LASSERT(pg->cp_state == CPS_CACHED);
 		/* This actually clears the dirty bit in the radix
@@ -412,12 +426,14 @@ static const struct cl_page_operations vvp_page_ops = {
                 [CRT_READ] = {
                         .cpo_prep        = vvp_page_prep_read,
                         .cpo_completion  = vvp_page_completion_read,
-                        .cpo_make_ready  = ccc_fail,
+			.cpo_make_ready_start = ccc_fail,
+			.cpo_make_ready_end  = ccc_fail,
                 },
                 [CRT_WRITE] = {
                         .cpo_prep        = vvp_page_prep_write,
                         .cpo_completion  = vvp_page_completion_write,
-                        .cpo_make_ready  = vvp_page_make_ready,
+			.cpo_make_ready_start = vvp_page_make_ready_start,
+			.cpo_make_ready_end = vvp_page_make_ready_end,
                 }
         }
 };

lustre/osc/osc_cache.c
@@ -51,8 +51,9 @@ static int osc_extent_wait(const struct lu_env *env, struct osc_extent *ext,
 			   enum osc_extent_state state);
 static void osc_ap_completion(const struct lu_env *env, struct client_obd *cli,
 			      struct osc_async_page *oap, int sent, int rc);
-static int osc_make_ready(const struct lu_env *env, struct osc_async_page *oap,
-			  int cmd);
+static struct osc_async_page* osc_make_ready(const struct lu_env *env,
+					     struct osc_extent *ext,
+					     int cmd);
 static int osc_refresh_count(const struct lu_env *env,
 			     struct osc_async_page *oap, int cmd);
 static int osc_io_unplug_async(const struct lu_env *env,
@@ -1034,10 +1035,8 @@ static int osc_extent_make_ready(const struct lu_env *env,
 				 struct osc_extent *ext)
 {
 	struct osc_async_page *oap;
-	struct osc_async_page *last = NULL;
+	struct osc_async_page *last;
 	struct osc_object *obj = ext->oe_obj;
-	unsigned int page_count = 0;
-	int rc;
 	ENTRY;
 
 	/* we're going to grab page lock, so object lock must not be taken. */
@@ -1048,31 +1047,7 @@ static int osc_extent_make_ready(const struct lu_env *env,
 
 	OSC_EXTENT_DUMP(D_CACHE, ext, "make ready\n");
 
-	list_for_each_entry(oap, &ext->oe_pages, oap_pending_item) {
-		++page_count;
-		if (last == NULL || last->oap_obj_off < oap->oap_obj_off)
-			last = oap;
-
-		/* checking ASYNC_READY is race safe */
-		if ((oap->oap_async_flags & ASYNC_READY) != 0)
-			continue;
-
-		rc = osc_make_ready(env, oap, OBD_BRW_WRITE);
-		switch (rc) {
-		case 0:
-			spin_lock(&oap->oap_lock);
-			oap->oap_async_flags |= ASYNC_READY;
-			spin_unlock(&oap->oap_lock);
-			break;
-		case -EALREADY:
-			LASSERT((oap->oap_async_flags & ASYNC_READY) != 0);
-			break;
-		default:
-			LASSERTF(0, "unknown return code: %d\n", rc);
-		}
-	}
-
-	LASSERT(page_count == ext->oe_nr_pages);
+	last = osc_make_ready(env, ext, OBD_BRW_WRITE);
 	LASSERT(last != NULL);
 	/* the last page is the only one we need to refresh its count by
 	 * the size of file. */
@@ -1206,20 +1181,58 @@ static inline int osc_is_ready(struct osc_object *osc)
 	       list_empty_marker(&(OSC)->oo_reading_exts),		       \
 	       ##args)
 
-static int osc_make_ready(const struct lu_env *env, struct osc_async_page *oap,
-			  int cmd)
+static struct osc_async_page* osc_make_ready(const struct lu_env *env,
+					     struct osc_extent *ext,
+					     int cmd)
 {
-	struct osc_page *opg  = oap2osc_page(oap);
-	struct cl_page  *page = oap2cl_page(oap);
-	int result;
-
-	LASSERT(cmd == OBD_BRW_WRITE); /* no cached reads */
-
+	int rc;
+	struct osc_async_page *oap, *next;
+	struct osc_async_page *last = NULL;
+	unsigned int page_count = 0;
+	struct list_head ready_list = LIST_HEAD_INIT(ready_list);
 	ENTRY;
-	result = cl_page_make_ready(env, page, CRT_WRITE);
-	if (result == 0)
-		opg->ops_submit_time = cfs_time_current();
-	RETURN(result);
+
+	list_for_each_entry(oap, &ext->oe_pages, oap_pending_item) {
+		struct cl_page  *page = oap2cl_page(oap);
+
+		/* checking ASYNC_READY is race safe */
+		if ((oap->oap_async_flags & ASYNC_READY) != 0)
+			continue;
+
+		LASSERT(list_empty(&oap->oap_rpc_item));
+		list_add_tail(&oap->oap_rpc_item, &ready_list);
+		cl_page_make_ready_start(env, page, CRT_WRITE);
+	}
+
+	list_for_each_entry_safe(oap, next, &ready_list, oap_rpc_item) {
+		struct osc_page *opg  = oap2osc_page(oap);
+		struct cl_page  *page = oap2cl_page(oap);
+
+		++page_count;
+		if (last == NULL || last->oap_obj_off < oap->oap_obj_off)
+			last = oap;
+
+		rc = cl_page_make_ready_end(env, page, CRT_WRITE);
+		list_del_init(&oap->oap_rpc_item);
+		if (rc == 0)
+			opg->ops_submit_time = cfs_time_current();
+		switch (rc) {
+		case 0:
+			spin_lock(&oap->oap_lock);
+			oap->oap_async_flags |= ASYNC_READY;
+			spin_unlock(&oap->oap_lock);
+			break;
+		case -EALREADY:
+			LASSERT((oap->oap_async_flags & ASYNC_READY) != 0);
+			break;
+		default:
+			LASSERTF(0, "unknown return code: %d\n", rc);
+		}
+	}
+
+	LASSERT(page_count == ext->oe_nr_pages);
+
+	RETURN(last);
 }
 
 static int osc_refresh_count(const struct lu_env *env,

Cray/lustre

lustre/llite/vvp_page.c
@@ -323,27 +323,41 @@ static void vvp_page_completion_write(const struct lu_env *env,
 }
 
 /**
- * Implements cl_page_operations::cpo_make_ready() method.
+ * Implements cl_page_operations::cpo_make_ready_start() method.
  *
  * This is called to yank a page from the transfer cache and to send it out as
- * a part of transfer. This function try-locks the page. If try-lock failed,
- * page is owned by some concurrent IO, and should be skipped (this is bad,
- * but hopefully rare situation, as it usually results in transfer being
- * shorter than possible).
+ * a part of transfer. This function is used to lock all pages from the extent.
+ *
+ * \retval 0      success, page is locked
+ *
+ */
+static int vvp_page_make_ready_start(const struct lu_env *env,
+				     const struct cl_page_slice *slice)
+{
+	struct page *vmpage = cl2vm_page(slice);
+
+	lock_page(vmpage);
+
+	return 0;
+}
+
+/**
+ * Implements cl_page_operations::cpo_make_ready_end() method.
+ *
+ * This is called to yank a page from the transfer cache and to send it out as
+ * a part of transfer. The page is already locked by
+ * vvp_page_make_ready_start().
  *
  * \retval 0      success, page can be placed into transfer
  *
- * \retval -EAGAIN page is either used by concurrent IO has been
- * truncated. Skip it.
  */
-static int vvp_page_make_ready(const struct lu_env *env,
-			       const struct cl_page_slice *slice)
+static int vvp_page_make_ready_end(const struct lu_env *env,
+				   const struct cl_page_slice *slice)
 {
 	struct page *vmpage = cl2vm_page(slice);
 	struct cl_page *pg = slice->cpl_page;
 	int result = 0;
 
-	lock_page(vmpage);
 	if (clear_page_dirty_for_io(vmpage)) {
 		LASSERT(pg->cp_state == CPS_CACHED);
 		/* This actually clears the dirty bit in the radix
@@ -415,12 +429,14 @@ static const struct cl_page_operations vvp_page_ops = {
 		[CRT_READ] = {
 			.cpo_prep       = vvp_page_prep_read,
 			.cpo_completion = vvp_page_completion_read,
-			.cpo_make_ready = vvp_page_fail,
+			.cpo_make_ready_start = vvp_page_fail,
+			.cpo_make_ready_end  = vvp_page_fail,
 		},
 		[CRT_WRITE] = {
 			.cpo_prep       = vvp_page_prep_write,
 			.cpo_completion = vvp_page_completion_write,
-			.cpo_make_ready = vvp_page_make_ready,
+			.cpo_make_ready_start = vvp_page_make_ready_start,
+			.cpo_make_ready_end = vvp_page_make_ready_end,
 		},
 	},
 };

lustre/osc/osc_cache.c
@@ -48,8 +48,9 @@ static int osc_extent_wait(const struct lu_env *env, struct osc_extent *ext,
 			   enum osc_extent_state state);
 static void osc_ap_completion(const struct lu_env *env, struct client_obd *cli,
 			      struct osc_async_page *oap, int sent, int rc);
-static int osc_make_ready(const struct lu_env *env, struct osc_async_page *oap,
-			  int cmd);
+static struct osc_async_page* osc_make_ready(const struct lu_env *env,
+					     struct osc_extent *ext,
+					     int cmd);
 static int osc_refresh_count(const struct lu_env *env,
 			     struct osc_async_page *oap, int cmd);
 static int osc_io_unplug_async(const struct lu_env *env,
@@ -1069,10 +1070,8 @@ static int osc_extent_make_ready(const struct lu_env *env,
 				 struct osc_extent *ext)
 {
 	struct osc_async_page *oap;
-	struct osc_async_page *last = NULL;
+	struct osc_async_page *last;
 	struct osc_object *obj = ext->oe_obj;
-	unsigned int page_count = 0;
-	int rc;
 	ENTRY;
 
 	/* we're going to grab page lock, so object lock must not be taken. */
@@ -1083,31 +1082,7 @@ static int osc_extent_make_ready(const struct lu_env *env,
 
 	OSC_EXTENT_DUMP(D_CACHE, ext, "make ready\n");
 
-	list_for_each_entry(oap, &ext->oe_pages, oap_pending_item) {
-		++page_count;
-		if (last == NULL || last->oap_obj_off < oap->oap_obj_off)
-			last = oap;
-
-		/* checking ASYNC_READY is race safe */
-		if ((oap->oap_async_flags & ASYNC_READY) != 0)
-			continue;
-
-		rc = osc_make_ready(env, oap, OBD_BRW_WRITE);
-		switch (rc) {
-		case 0:
-			spin_lock(&oap->oap_lock);
-			oap->oap_async_flags |= ASYNC_READY;
-			spin_unlock(&oap->oap_lock);
-			break;
-		case -EALREADY:
-			LASSERT((oap->oap_async_flags & ASYNC_READY) != 0);
-			break;
-		default:
-			LASSERTF(0, "unknown return code: %d\n", rc);
-		}
-	}
-
-	LASSERT(page_count == ext->oe_nr_pages);
+	last = osc_make_ready(env, ext, OBD_BRW_WRITE);
 	LASSERT(last != NULL);
 	/* the last page is the only one we need to refresh its count by
 	 * the size of file. */
@@ -1250,20 +1225,58 @@ static inline int osc_is_ready(struct osc_object *osc)
 	       list_empty_marker(&(OSC)->oo_reading_exts),		       \
 	       ##args)
 
-static int osc_make_ready(const struct lu_env *env, struct osc_async_page *oap,
-			  int cmd)
+static struct osc_async_page* osc_make_ready(const struct lu_env *env,
+					     struct osc_extent *ext,
+					     int cmd)
 {
-	struct osc_page *opg  = oap2osc_page(oap);
-	struct cl_page  *page = oap2cl_page(oap);
-	int result;
-
-	LASSERT(cmd == OBD_BRW_WRITE); /* no cached reads */
-
+	int rc;
+	struct osc_async_page *oap, *next;
+	struct osc_async_page *last = NULL;
+	unsigned int page_count = 0;
+	struct list_head ready_list = LIST_HEAD_INIT(ready_list);
 	ENTRY;
-	result = cl_page_make_ready(env, page, CRT_WRITE);
-	if (result == 0)
-		opg->ops_submit_time = ktime_get();
-	RETURN(result);
+
+	list_for_each_entry(oap, &ext->oe_pages, oap_pending_item) {
+		struct cl_page  *page = oap2cl_page(oap);
+
+		/* checking ASYNC_READY is race safe */
+		if ((oap->oap_async_flags & ASYNC_READY) != 0)
+			continue;
+
+		LASSERT(list_empty(&oap->oap_rpc_item));
+		list_add_tail(&oap->oap_rpc_item, &ready_list);
+		cl_page_make_ready_start(env, page, CRT_WRITE);
+	}
+
+	list_for_each_entry_safe(oap, next, &ready_list, oap_rpc_item) {
+		struct osc_page *opg  = oap2osc_page(oap);
+		struct cl_page  *page = oap2cl_page(oap);
+
+		++page_count;
+		if (last == NULL || last->oap_obj_off < oap->oap_obj_off)
+			last = oap;
+
+		rc = cl_page_make_ready_end(env, page, CRT_WRITE);
+		list_del_init(&oap->oap_rpc_item);
+		if (rc == 0)
+			opg->ops_submit_time = ktime_get();
+		switch (rc) {
+		case 0:
+			spin_lock(&oap->oap_lock);
+			oap->oap_async_flags |= ASYNC_READY;
+			spin_unlock(&oap->oap_lock);
+			break;
+		case -EALREADY:
+			LASSERT((oap->oap_async_flags & ASYNC_READY) != 0);
+			break;
+		default:
+			LASSERTF(0, "unknown return code: %d\n", rc);
+		}
+	}
+
+	LASSERT(page_count == ext->oe_nr_pages);
+
+	RETURN(last);
 }
 
 static int osc_refresh_count(const struct lu_env *env,