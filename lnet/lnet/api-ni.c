ad9cb51be67fbacc6a41a8ac8ad0ee89186d6922
Chris Horn <chris.horn@hpe.com>
2022-03-07 11:03:50 -0600

    LUS-10681 lnet: ln_api_mutex deadlocks
    LNetNIFini() acquires the ln_api_mutex and holds onto it throughout
    various shutdown routines. Meanwhile, LND threads (via
    lnet_nid2peerni_locked()) or the discovery thread (via
    lnet_peer_data_present()) may need to acquire this mutex in order to
    progress.
    
    Address these potential deadlocks by setting the_lnet.ln_state to
    LNET_STATE_STOPPING earlier in LNetNIFini(), and release the mutex
    prior to any call into LND module or before any wait.
    
    LNetNIInit() is modified to return -ESHUTDOWN if it finds that there
    is a concurrent shutdown in progress.
    
    Test-Parameters: trivial
    HPE-bug-id: LUS-10681
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: Ia8b28cc95ff71e66a0f99aed4f2c22ec9d44ce1e
    Reviewed-on: https://es-gerrit.dev.cray.com/160308
    Tested-by: Jenkins Build User <nssreleng@cray.com>
    Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
    Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>

--- lnet/lnet/api-ni.c
+++ lnet/lnet/api-ni.c
@@ -1454,7 +1454,7 @@ lnet_prepare(lnet_pid_t requested_pid)
 }
 
 static int
-lnet_unprepare (void)
+lnet_unprepare(void)
 {
 	/* NB no LNET_LOCK since this is the last reference.  All LND instances
 	 * have shut down already, so it is safe to unlink and free all
@@ -2426,14 +2426,16 @@ lnet_clear_zombies_nis_locked(struct lnet_net *net)
 		islo = ni->ni_net->net_lnd->lnd_type == LOLND;
 
 		LASSERT(!in_interrupt());
-		/* Holding the mutex makes it safe for lnd_shutdown
+		/* Holding the LND mutex makes it safe for lnd_shutdown
 		 * to call module_put(). Module unload cannot finish
 		 * until lnet_unregister_lnd() completes, and that
-		 * requires the mutex.
+		 * requires the LND mutex.
 		 */
+		mutex_unlock(&the_lnet.ln_api_mutex);
 		mutex_lock(&the_lnet.ln_lnd_mutex);
 		(net->net_lnd->lnd_shutdown)(ni);
 		mutex_unlock(&the_lnet.ln_lnd_mutex);
+		mutex_lock(&the_lnet.ln_api_mutex);
 
 		if (!islo)
 			CDEBUG(D_LNI, "Removed LNI %s\n",
@@ -2504,7 +2506,8 @@ lnet_shutdown_lndnets(void)
 	/* NB called holding the global mutex */
 
 	/* All quiet on the API front */
-	LASSERT(the_lnet.ln_state == LNET_STATE_RUNNING);
+	LASSERT(the_lnet.ln_state == LNET_STATE_RUNNING ||
+		the_lnet.ln_state == LNET_STATE_STOPPING);
 	LASSERT(the_lnet.ln_refcount == 0);
 
 	lnet_net_lock(LNET_LOCK_EX);
@@ -3006,6 +3009,11 @@ LNetNIInit(lnet_pid_t requested_pid)
 
 	CDEBUG(D_OTHER, "refs %d\n", the_lnet.ln_refcount);
 
+	if (the_lnet.ln_state == LNET_STATE_STOPPING) {
+		mutex_unlock(&the_lnet.ln_api_mutex);
+		return -ESHUTDOWN;
+	}
+
 	if (the_lnet.ln_refcount > 0) {
 		rc = the_lnet.ln_refcount++;
 		mutex_unlock(&the_lnet.ln_api_mutex);
@@ -3158,6 +3166,10 @@ LNetNIFini(void)
 		the_lnet.ln_refcount--;
 	} else {
 		LASSERT(!the_lnet.ln_niinit_self);
+
+		lnet_net_lock(LNET_LOCK_EX);
+		the_lnet.ln_state = LNET_STATE_STOPPING;
+		lnet_net_unlock(LNET_LOCK_EX);
 
 		lnet_fault_fini();
 
@@ -3627,6 +3639,10 @@ static int lnet_handle_legacy_ip2nets(char *ip2nets,
 	lnet_set_tune_defaults(tun);
 
 	mutex_lock(&the_lnet.ln_api_mutex);
+	if (the_lnet.ln_state != LNET_STATE_RUNNING) {
+		rc = -ESHUTDOWN;
+		goto out;
+	}
 	while (!list_empty(&net_head)) {
 		net = list_entry(net_head.next, struct lnet_net, net_list);
 		list_del_init(&net->net_list);
@@ -3690,8 +3706,10 @@ int lnet_dyn_add_ni(struct lnet_ioctl_config_ni *conf)
 	lnet_set_tune_defaults(tun);
 
 	mutex_lock(&the_lnet.ln_api_mutex);
-
-	rc = lnet_add_net_common(net, tun);
+	if (the_lnet.ln_state != LNET_STATE_RUNNING)
+		rc = -ESHUTDOWN;
+	else
+		rc = lnet_add_net_common(net, tun);
 
 	mutex_unlock(&the_lnet.ln_api_mutex);
 
@@ -3714,6 +3732,10 @@ int lnet_dyn_del_ni(struct lnet_ioctl_config_ni *conf)
 		return -EINVAL;
 
 	mutex_lock(&the_lnet.ln_api_mutex);
+	if (the_lnet.ln_state != LNET_STATE_RUNNING) {
+		rc = -ESHUTDOWN;
+		goto unlock_api_mutex;
+	}
 
 	lnet_net_lock(0);
 
@@ -3807,6 +3829,10 @@ lnet_dyn_add_net(struct lnet_ioctl_config_data *conf)
 		return rc == 0 ? -EINVAL : rc;
 
 	mutex_lock(&the_lnet.ln_api_mutex);
+	if (the_lnet.ln_state != LNET_STATE_RUNNING) {
+		rc = -ESHUTDOWN;
+		goto out_unlock_clean;
+	}
 
 	if (rc > 1) {
 		rc = -EINVAL; /* only add one network per call */
@@ -3859,6 +3885,10 @@ lnet_dyn_del_net(__u32 net_id)
 		return -EINVAL;
 
 	mutex_lock(&the_lnet.ln_api_mutex);
+	if (the_lnet.ln_state != LNET_STATE_RUNNING) {
+		rc = -ESHUTDOWN;
+		goto out;
+	}
 
 	lnet_net_lock(0);
 
