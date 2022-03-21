lustre/lustre-release

commit 6bce536725efd166d2772f13fe954f271f9c53b8

LU-4198 clio: turn on lockless for some kind of IO
We can safely turn on lockless for Direct IO
and no lock.

Direct IO will still enqueue lock in the server side,
and we could not use lockless for in the following case:

1) If group lock is held before DIO, use lockless will
make us deadlock, so we use group lock instead and trust
this to protect consistecy.

2) Direct IO might fallback to Buffer IO in some cases,
and we will restart Direct IO with normal lock holding

The main motivation for this patch is to support AIO.

Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Change-Id: Ia004d6b39272df8159c9df3cc76662e198230b55
Reviewed-on: https://review.whamcloud.com/8201
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Patrick Farrell <farr0186@gmail.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/sanityn.sh
@@ -457,6 +457,46 @@ test_16c() {
 }
 run_test 16c "verify data consistency on ldiskfs with cache disabled (b=17397)"
 
+test_16d() {
+	local file1=$DIR1/$tfile
+	local file2=$DIR2/$tfile
+	local file3=$DIR1/file
+	local stripe_size=$(do_facet $SINGLEMDS \
+		"$LCTL get_param -n lod.$(facet_svc $SINGLEMDS)*.stripesize")
+
+	# to allocate grant because it may run out due to test_15.
+	$LFS setstripe -c -1 $file1
+	dd if=/dev/zero of=$file1 bs=$stripe_size count=$OSTCOUNT oflag=sync
+	dd if=/dev/zero of=$file2 bs=$stripe_size count=$OSTCOUNT oflag=sync
+	rm -f $file1
+
+	local tmpfile=`mktemp`
+	$LFS setstripe -c -1 $file1 # b=10919
+	$LCTL set_param ldlm.namespaces.*.lru_size=clear
+	
+	# direct write on one client and direct read from another
+	dd if=/dev/urandom of=$file1 bs=1M count=100 oflag=direct
+	dd if=$file2 of=$tmpfile iflag=direct bs=1M
+	diff $file1 $tmpfile || error "file different(1)"
+	rm -f $file1
+
+	# buffer write on one client, but direct read from another
+	dd if=$tmpfile of=$file1 bs=1M count=100
+	dd if=$file2 of=$file3 bs=1M iflag=direct count=100
+	diff $file3 $tmpfile || error "file different(2)"
+
+	rm -f $file3 $file2 $file1
+	# direct write on one client
+	dd if=$tmpfile of=$file1 bs=1M count=100 oflag=direct
+	# buffer read from another client
+	dd if=$file2 of=$file3 bs=1M count=100
+	diff $file3 $tmpfile || error "file different(3)"
+
+	rm -f $file1 $file2 $file3 $tmpfile
+
+}
+run_test 16d "Verify DIO and buffer IO with two clients"
+
 
 test_17() { # bug 3513, 3667
 	remote_ost_nodsh && skip "remote OST with nodsh" && return

lustre/include/cl_object.h
@@ -1915,6 +1915,10 @@ struct cl_io {
 	 */
 			     ci_async_readahead:1,
 	/**
+	 * Ignore lockless and do normal locking for this io.
+	 */
+			     ci_ignore_lockless:1,
+	/**
 	 * Set if we've tried all mirrors for this read IO, if it's not set,
 	 * the read IO will check to-be-read OSCs' status, and make fast-switch
 	 * another mirror if some of the OSTs are not healthy.

lustre/include/lustre_osc.h
@@ -938,7 +938,9 @@ struct osc_extent {
 	 * called by page WB daemon, or sync write or reading requests. */
 				oe_urgent:1,
 	/** Non-delay RPC should be used for this extent. */
-				oe_ndelay:1;
+				oe_ndelay:1,
+	/** direct IO pages */
+				oe_dio:1;
 	/** how many grants allocated for this extent.
 	 *  Grant allocated for this extent. There is no grant allocated
 	 *  for reading extents and sync write extents. */

lustre/llite/file.c
@@ -858,14 +858,17 @@ restart:
 		GOTO(out_och_free, rc);
 
 	mutex_unlock(&lli->lli_och_mutex);
-        fd = NULL;
 
-        /* Must do this outside lli_och_mutex lock to prevent deadlock where
-           different kind of OPEN lock for this same inode gets cancelled
-           by ldlm_cancel_lru */
-        if (!S_ISREG(inode->i_mode))
-                GOTO(out_och_free, rc);
+	/* lockless for direct IO so that it can do IO in parallel */
+	if (file->f_flags & O_DIRECT)
+		fd->fd_flags |= LL_FILE_LOCKLESS_IO;
+	fd = NULL;
 
+	/* Must do this outside lli_och_mutex lock to prevent deadlock where
+	   different kind of OPEN lock for this same inode gets cancelled
+	   by ldlm_cancel_lru */
+	if (!S_ISREG(inode->i_mode))
+		GOTO(out_och_free, rc);
 	cl_lov_delay_create_clear(&file->f_flags);
 	GOTO(out_och_free, rc);
 
@@ -1476,6 +1479,7 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 	int			rc = 0;
 	unsigned		retried = 0;
 	bool			restarted = false;
+	unsigned		ignore_lockless = 0;
 
 	ENTRY;
 
@@ -1486,6 +1490,7 @@ ll_file_io_generic(const struct lu_env *env, struct vvp_io_args *args,
 restart:
 	io = vvp_env_thread_io(env);
 	ll_io_init(io, file, iot, args);
+	io->ci_ignore_lockless = ignore_lockless;
 	io->ci_ndelay_tried = retried;
 
 	if (cl_io_rw_init(env, io, iot, *ppos, count) == 0) {
@@ -1558,7 +1563,8 @@ out:
 	       file->f_path.dentry->d_name.name,
 	       iot, rc, result, io->ci_need_restart);
 
-	if ((rc == 0 || rc == -ENODATA) && count > 0 && io->ci_need_restart) {
+	if ((rc == 0 || rc == -ENODATA || rc == -ENOLCK) &&
+	    count > 0 && io->ci_need_restart) {
 		CDEBUG(D_VFSTRACE,
 		       "%s: restart %s from %lld, count: %zu, ret: %zd, rc: %d\n",
 		       file_dentry(file)->d_name.name,
@@ -1566,6 +1572,7 @@ out:
 		       *ppos, count, result, rc);
 		/* preserve the tried count for FLR */
 		retried = io->ci_ndelay_tried;
+		ignore_lockless = io->ci_ignore_lockless;
 		restarted = true;
 		goto restart;
 	}

lustre/llite/rw.c
@@ -1569,6 +1569,20 @@ int ll_readpage(struct file *file, struct page *vmpage)
 		RETURN(result);
 	}
 
+	/**
+	 * Direct read can fall back to buffered read, but DIO is done
+	 * with lockless i/o, and buffered requires LDLM locking, so in
+	 * this case we must restart without lockless.
+	 */
+	if (file->f_flags & O_DIRECT &&
+	    lcc && lcc->lcc_type == LCC_RW &&
+	    !io->ci_ignore_lockless) {
+		unlock_page(vmpage);
+		io->ci_ignore_lockless = 1;
+		io->ci_need_restart = 1;
+		RETURN(-ENOLCK);
+	}
+
 	LASSERT(io->ci_state == CIS_IO_GOING);
 	page = cl_page_find(env, clob, vmpage->index, vmpage, CPT_CACHEABLE);
 	if (!IS_ERR(page)) {

lustre/llite/rw26.c
@@ -672,14 +672,25 @@ static int ll_write_begin(struct file *file, struct address_space *mapping,
 	env = lcc->lcc_env;
 	io  = lcc->lcc_io;
 
-	if (file->f_flags & O_DIRECT && io->ci_designated_mirror > 0) {
+	if (file->f_flags & O_DIRECT) {
 		/* direct IO failed because it couldn't clean up cached pages,
 		 * this causes a problem for mirror write because the cached
 		 * page may belong to another mirror, which will result in
 		 * problem submitting the I/O. */
-		GOTO(out, result = -EBUSY);
-	}
+		if (io->ci_designated_mirror > 0)
+			GOTO(out, result = -EBUSY);
 
+		/**
+		 * Direct read can fall back to buffered read, but DIO is done
+		 * with lockless i/o, and buffered requires LDLM locking, so
+		 * in this case we must restart without lockless.
+		 */
+		if (!io->ci_ignore_lockless) {
+			io->ci_ignore_lockless = 1;
+			io->ci_need_restart = 1;
+			GOTO(out, result = -ENOLCK);
+		}
+	}
 again:
 	/* To avoid deadlock, try to lock page first. */
 	vmpage = grab_cache_page_nowait(mapping, index);

lustre/llite/vvp_io.c
@@ -568,6 +568,16 @@ static int vvp_io_rw_lock(const struct lu_env *env, struct cl_io *io,
 		ast_flags |= CEF_NONBLOCK;
 	if (io->ci_lock_no_expand)
 		ast_flags |= CEF_LOCK_NO_EXPAND;
+	if (vio->vui_fd) {
+		/* Group lock held means no lockless any more */
+		if (vio->vui_fd->fd_flags & LL_FILE_GROUP_LOCKED)
+			io->ci_ignore_lockless = 1;
+
+		if (ll_file_nolock(vio->vui_fd->fd_file) ||
+		    (vio->vui_fd->fd_flags & LL_FILE_LOCKLESS_IO &&
+		     !io->ci_ignore_lockless))
+			ast_flags |= CEF_NEVER;
+	}
 
 	result = vvp_mmap_locks(env, vio, io);
 	if (result == 0)

lustre/llite/vvp_page.c
@@ -419,55 +419,11 @@ static const struct cl_page_operations vvp_page_ops = {
 	},
 };
 
-static int vvp_transient_page_prep(const struct lu_env *env,
-				   const struct cl_page_slice *slice,
-				   struct cl_io *unused)
-{
-	ENTRY;
-	/* transient page should always be sent. */
-	RETURN(0);
-}
-
-static void vvp_transient_page_verify(const struct cl_page *page)
-{
-}
-
-static int vvp_transient_page_own(const struct lu_env *env,
-                                  const struct cl_page_slice *slice,
-                                  struct cl_io *unused, int nonblock)
-{
-        vvp_transient_page_verify(slice->cpl_page);
-        return 0;
-}
-
-static void vvp_transient_page_assume(const struct lu_env *env,
-                                      const struct cl_page_slice *slice,
-                                      struct cl_io *unused)
-{
-        vvp_transient_page_verify(slice->cpl_page);
-}
-
-static void vvp_transient_page_unassume(const struct lu_env *env,
-                                        const struct cl_page_slice *slice,
-                                        struct cl_io *unused)
-{
-        vvp_transient_page_verify(slice->cpl_page);
-}
-
-static void vvp_transient_page_disown(const struct lu_env *env,
-                                      const struct cl_page_slice *slice,
-                                      struct cl_io *unused)
-{
-        vvp_transient_page_verify(slice->cpl_page);
-}
-
 static void vvp_transient_page_discard(const struct lu_env *env,
                                        const struct cl_page_slice *slice,
                                        struct cl_io *unused)
 {
         struct cl_page *page = slice->cpl_page;
-
-        vvp_transient_page_verify(slice->cpl_page);
 
         /*
          * For transient pages, remove it from the radix tree.
@@ -478,21 +434,7 @@ static void vvp_transient_page_discard(const struct lu_env *env,
 static int vvp_transient_page_is_vmlocked(const struct lu_env *env,
 					  const struct cl_page_slice *slice)
 {
-	struct inode    *inode = vvp_object_inode(slice->cpl_obj);
-	int	locked;
-
-	locked = !inode_trylock(inode);
-	if (!locked)
-		inode_unlock(inode);
-	return locked ? -EBUSY : -ENODATA;
-}
-
-static void
-vvp_transient_page_completion(const struct lu_env *env,
-                              const struct cl_page_slice *slice,
-                              int ioret)
-{
-        vvp_transient_page_verify(slice->cpl_page);
+	return -EBUSY;
 }
 
 static void vvp_transient_page_fini(const struct lu_env *env,
@@ -500,32 +442,17 @@ static void vvp_transient_page_fini(const struct lu_env *env,
 				    struct pagevec *pvec)
 {
 	struct vvp_page *vpg = cl2vvp_page(slice);
-	struct cl_page *clp = slice->cpl_page;
-	struct vvp_object *clobj = cl2vvp(clp->cp_obj);
+	struct vvp_object *clobj = cl2vvp(slice->cpl_obj);
 
 	vvp_page_fini_common(vpg, pvec);
 	atomic_dec(&clobj->vob_transient_pages);
 }
 
 static const struct cl_page_operations vvp_transient_page_ops = {
-	.cpo_own		= vvp_transient_page_own,
-	.cpo_assume		= vvp_transient_page_assume,
-	.cpo_unassume		= vvp_transient_page_unassume,
-	.cpo_disown		= vvp_transient_page_disown,
 	.cpo_discard		= vvp_transient_page_discard,
 	.cpo_fini		= vvp_transient_page_fini,
 	.cpo_is_vmlocked	= vvp_transient_page_is_vmlocked,
 	.cpo_print		= vvp_page_print,
-	.io = {
-		[CRT_READ] = {
-			.cpo_prep	= vvp_transient_page_prep,
-			.cpo_completion	= vvp_transient_page_completion,
-		},
-		[CRT_WRITE] = {
-			.cpo_prep	= vvp_transient_page_prep,
-			.cpo_completion	= vvp_transient_page_completion,
-		}
-	}
 };
 
 int vvp_page_init(const struct lu_env *env, struct cl_object *obj,

Cray/lustre

commit 63db5bc4f17765972fd7b0243f382a8593d41c02

LUS-10151 ofd: update lvb before dropping server lock
If this is a local lock on a server namespace and this was
the last reference, lock will be canceled directly instead
of staying at LRU, this might be problem that we will lose
O_DIRECT write size expanding without LVB updating.

This patch try to fix the problem by updating LVB just after
writing but before lock is dropped.

This is a port of:

Lustre-change: https://review.whamcloud.com/37790
Lustre-commit: 92d799217aeac4e1e859264d157c62e0f02b83a8

WC-bug-id: LU-13227
Fixes: 6bce536725 ("LU-4198 clio: turn on lockless for some kind of IO")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Idb57f49e148f81a40407d2186664a793df8183e5
Reviewed-on: https://es-gerrit.dev.cray.com/158935
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/ofd/ofd_io.c
@@ -1318,6 +1318,8 @@ int ofd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 	struct ofd_thread_info *info = ofd_info(env);
 	struct ofd_device *ofd = ofd_exp(exp);
 	const struct lu_fid *fid = &oa->o_oi.oi_fid;
+	struct ldlm_namespace *ns = ofd->ofd_namespace;
+	struct ldlm_resource *rs = NULL;
 	__u64 valid;
 	int rc = 0;
 
@@ -1372,6 +1374,23 @@ int ofd_commitrw(const struct lu_env *env, int cmd, struct obd_export *exp,
 
 			oa->o_valid |= OBD_MD_FLFLAGS;
 			oa->o_valid |= OBD_MD_FLALLQUOTA;
+		}
+
+		/**
+		 * Update LVB after writing finish for server lock, see
+		 * comments in ldlm_lock_decref_internal(), If this is a
+		 * local lock on a server namespace and this was the last
+		 * reference, lock will be destroyed directly thus there
+		 * is no chance for ldlm_request_cancel() to update lvb.
+		 */
+		if (rc == 0 && (rnb[0].rnb_flags & OBD_BRW_SRVLOCK)) {
+			ost_fid_build_resid(fid, &info->fti_resid);
+			rs = ldlm_resource_get(ns, NULL, &info->fti_resid,
+					       LDLM_EXTENT, 0);
+			if (!IS_ERR(rs)) {
+				ldlm_res_lvbo_update(rs, NULL, 1);
+				ldlm_resource_putref(rs);
+			}
 		}
 
 		/* Convert back to client IDs. LU-9671.

lustre/tests/sanityn.sh
@@ -513,6 +513,26 @@ test_16d() {
 }
 run_test 16d "Verify DIO and buffer IO with two clients"
 
+test_16e() { # LU-13227
+	local file1=$DIR1/$tfile
+	local file2=$DIR2/$tfile
+
+	# client1 write 10M data
+	dd if=/dev/zero of=$file1 bs=1M count=10
+	# drop locks
+	cancel_lru_locks osc > /dev/null
+	# use lockahead to generate one PW lock to keep LVB loaded.
+	$LFS ladvise -a lockahead --start 0 --length 1M \
+		--mode WRITE $file1
+	# direct write to extend file size on client2
+	dd if=/dev/zero of=$file2 bs=1M seek=20 count=1 \
+		oflag=direct conv=notrunc
+	local filesize=$(stat -c %s $file2)
+	[ "$filesize" -eq 22020096 ] ||
+		error "expected filesize 22020096 got $filesize"
+	rm -f $file1
+}
+run_test 16e "Verify size consistency for O_DIRECT write"
 
 test_17() { # bug 3513, 3667
 	remote_ost_nodsh && skip "remote OST with nodsh" && return