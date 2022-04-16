b9cdd7d80e882b61b370c2481e07ad42373bb835
Chris Horn <chris.horn@hpe.com>
2022-04-16 10:01:57 -0600

    LUS-10878 lnet: Adjust niov checks for large MD
    An LNet user can allocate a large contiguous MD. That MD can have >
    LNET_MAX_IOV pages which causes some LNDs to assert on either niov
    argument passed to lnd_recv() or the value stored in
    lnet_msg::msg_niov. This is true even in cases where the actual
    transfer size is <= LNET_MTU and will not exceed limits in the LNDs.
    
    Adjust ksocklnd_send()/ksocklnd_recv() to assert on the return value
    of lnet_extract_kiov().
    
    Remove the assert on msg_niov (payload_niov) from kiblnd_send().
    kiblnd_setup_rd_kiov() will already fail if we exceed ko2iblnd's
    available scatter gather entries.
    
    Adjust kfilnd_send() so that it no longer returns -EINVAL if msg_niov
    if it exceeds LNET_MAX_IOV. Instead, modify kfilnd_tn_set_kiov_buf()
    to return -EINVAL in the case where we would exceed the size of the
    kfilnd_transaction::tn_kiov array.
    
    Also fix a bug in kfilnd_tn_set_kiov_buf() where tn_num_iovec was
    being set to the wrong value.
    
    Test-Parameters: trivial
    Fixes: 857f11169f ("LU-13004 lnet: always put a page list into struct lnet_libmd")
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: Iaa851d90f735d04e5167bb9c07235625759245b2
    Reviewed-on: https://es-gerrit.dev.cray.com/160504
    Reviewed-by: Ian Ziemba <iziemba@cray.com>
    Reviewed-by: Shaun Tancheff <stancheff@cray.com>
    Tested-by: Vitaly Fertman <c17818@cray.com>

--- lnet/klnds/kfilnd/kfilnd_tn.h
+++ lnet/klnds/kfilnd/kfilnd_tn.h
@@ -19,7 +19,7 @@ void kfilnd_tn_event_handler(struct kfilnd_transaction *tn,
 			     enum tn_events event, int status);
 void kfilnd_tn_cleanup(void);
 int kfilnd_tn_init(void);
-void kfilnd_tn_set_kiov_buf(struct kfilnd_transaction *tn, struct bio_vec *kiov,
-		            size_t num_iov, size_t offset, size_t nob);
+int kfilnd_tn_set_kiov_buf(struct kfilnd_transaction *tn, struct bio_vec *kiov,
+		           size_t num_iov, size_t offset, size_t nob);
 
 #endif /* _KFILND_TN_ */
