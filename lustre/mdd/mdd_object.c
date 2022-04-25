05cb5683abb611ed7ba6d3edd21c5a4ae2b434aa
Mikhail Pershin <mpershin@whamcloud.com>
2022-04-25 09:13:53 +0300

    LU-12031 mdt: explicit data version of DoM files
    Use EA to store 'data_version' for DoM files explicitly.
    
    Unlike OST objects the 'inode_version' of DoM file is changed
    by metadata operations as well and that leads to problems
    during HSM operations, e.g. writing HSM EA with file data
    version inside causes DoM object version update making this
    HSM EA version obsoleted, also any metadata update on
    restored file makes it dirty and prevents second release.
    
    DoM files have now explicitly updated 'data_version' in
    addition to ordinary 'inode_version'. The 'data_version'
    is updated along with 'inode_version' upon write/truncate and
    fallocate operations and is stored as 'trusted.dataver' EA.
    Layout swap procedure is updated to move data version between
    files being swept along with HSM attributes.
    If DoM file is migrated to RAID0 file then 'dataver' EA is
    deleted.
    
    Corresponding test 1f is added to sanity-hsm.sh and
    270j to sanity.sh.
    
    Signed-off-by: Mikhail Pershin <mpershin@whamcloud.com>
    Change-Id: I4689c56394c7323d32cd6f7dd86f58beb6e53353
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/47139
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162436
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

--- lustre/mdd/mdd_object.c
+++ lustre/mdd/mdd_object.c
@@ -1800,9 +1800,6 @@ static int mdd_split_ea(struct lov_comp_md_v1 *comp_v1, __u16 mirror_id,
 	return 0;
 }
 
-static int mdd_dom_data_truncate(const struct lu_env *env,
-				 struct mdd_device *mdd, struct mdd_object *mo);
-
 static int mdd_xattr_split(const struct lu_env *env, struct md_object *md_obj,
 			   struct md_rejig_data *mrd, struct md_attr *ma)
 {
@@ -1968,7 +1965,7 @@ stop:
 
 	/* Truncate local DOM data if all went well */
 	if (!rc && dom_stripe)
-		mdd_dom_data_truncate(env, mdd, obj);
+		mdd_dom_fixup(env, mdd, obj, NULL);
 
 	lu_buf_free(buf_save);
 	lu_buf_free(buf);
@@ -2379,36 +2376,61 @@ static inline int mdd_set_lmm_gen(struct lov_mds_md *lmm, __u32 *gen)
 	return mdd_lmm_gen(lmm, gen, false);
 }
 
-static int mdd_dom_data_truncate(const struct lu_env *env,
-				 struct mdd_device *mdd, struct mdd_object *mo)
+int mdd_dom_fixup(const struct lu_env *env, struct mdd_device *mdd,
+		  struct mdd_object *mo, struct mdd_object *vo)
 {
+	struct dt_object *dom, *vlt;
+	dt_obj_version_t dv = 0;
 	struct thandle *th;
-	struct dt_object *dom;
 	int rc;
 
+	ENTRY;
+
+	if (vo) {
+		vlt = dt_object_locate(mdd_object_child(vo), mdd->mdd_bottom);
+		if (!vlt)
+			GOTO(out, rc = -ENOENT);
+		dv = dt_data_version_get(env, vlt);
+		if (!dv)
+			GOTO(out, rc = -ENODATA);
+	}
+
 	dom = dt_object_locate(mdd_object_child(mo), mdd->mdd_bottom);
-	if (!dom)
-		GOTO(out, rc = -ENODATA);
 
 	th = dt_trans_create(env, mdd->mdd_bottom);
 	if (IS_ERR(th))
 		GOTO(out, rc = PTR_ERR(th));
 
-	rc = dt_declare_punch(env, dom, 0, OBD_OBJECT_EOF, th);
-	if (rc)
-		GOTO(stop, rc);
+	if (vo) {
+		rc = dt_declare_data_version_set(env, dom, th);
+		if (rc)
+			GOTO(stop, rc);
+	} else {
+		rc = dt_declare_data_version_del(env, dom, th);
+		if (rc)
+			GOTO(stop, rc);
+		rc = dt_declare_punch(env, dom, 0, OBD_OBJECT_EOF, th);
+		if (rc)
+			GOTO(stop, rc);
+	}
 
 	rc = dt_trans_start_local(env, mdd->mdd_bottom, th);
 	if (rc != 0)
 		GOTO(stop, rc);
 
-	rc = dt_punch(env, dom, 0, OBD_OBJECT_EOF, th);
+	if (vo) {
+		dt_data_version_set(env, dom, dv, th);
+	} else {
+		dt_data_version_del(env, dom, th);
+		rc = dt_punch(env, dom, 0, OBD_OBJECT_EOF, th);
+	}
+
 stop:
 	dt_trans_stop(env, mdd->mdd_bottom, th);
 out:
 	/* Ignore failure but report the error */
 	if (rc)
-		CERROR("%s: can't truncate DOM inode "DFID" data: rc = %d\n",
+		CERROR("%s: can't manage DOM file "DFID" data: rc = %d\n",
 		       mdd_obj_dev_name(mo), PFID(mdd_object_fid(mo)), rc);
 	return rc;
 }
@@ -2451,7 +2473,7 @@ static int mdd_swap_layouts(const struct lu_env *env, struct md_object *obj1,
 	struct lu_buf dom_buf = LU_BUF_NULL;
 	struct ost_id *saved_oi = NULL;
 	struct thandle *handle;
-	struct mdd_object *dom_o = NULL;
+	struct mdd_object *dom_o = NULL, *vlt_o = NULL;
 	struct niobuf_local *lnb = NULL;
 	__u64 domsize_dom, domsize_vlt;
 	__u32 fst_gen, snd_gen, saved_gen;
@@ -2517,6 +2539,7 @@ static int mdd_swap_layouts(const struct lu_env *env, struct md_object *obj1,
 	if ((!(fst_o->mod_flags & (ORPHAN_OBJ | VOLATILE_OBJ)) &&
 	    !(snd_o->mod_flags & (ORPHAN_OBJ | VOLATILE_OBJ))) &&
 	    (domsize_vlt || domsize_dom)) {
+
 		/* 'lfs swap_layouts' case, neither file should have DoM */
 		rc = -EOPNOTSUPP;
 		CDEBUG(D_LAYOUT, "cannot swap layouts with DOM component, use migration instead: rc = %d\n",
@@ -2524,6 +2547,7 @@ static int mdd_swap_layouts(const struct lu_env *env, struct md_object *obj1,
 		GOTO(stop, rc);
 	}
 	dom_o = domsize_dom ? fst_o : NULL;
+	vlt_o = domsize_vlt ? snd_o : NULL;
 
 	/* Import file to directory with DOM striping */
 	if (domsize_vlt > 0 && domsize_dom == 0) {
@@ -2600,10 +2624,6 @@ static int mdd_swap_layouts(const struct lu_env *env, struct md_object *obj1,
 		       mdd_obj_dev_name(fst_o),
 		       PFID(mdd_object_fid(fst_o)), rc);
 		GOTO(stop, rc);
-	} else if (domsize_vlt == domsize_dom) {
-		/* Migration with the same DOM component size, no need to
-		 * truncate local data, it is still being used */
-		dom_o = NULL;
 	}
 
 	/* swapping 2 non existant layouts is a success */
@@ -2830,9 +2850,12 @@ out_restore:
 stop:
 	rc = mdd_trans_stop(env, mdd, rc, handle);
 
-	/* Truncate local DOM data if all went well */
+	/* Truncate local DOM data if all went well, except migration case
+	 * with the same DOM component size. In that case a local data is
+	 * still in use and shouldn't be deleted.
+	 */
 	if (!rc && dom_o)
-		mdd_dom_data_truncate(env, mdd, dom_o);
+		mdd_dom_fixup(env, mdd, dom_o, vlt_o);
 
 	mdd_write_unlock(env, snd_o);
 	mdd_write_unlock(env, fst_o);
--- lustre/mdt/mdt_io.c
+++ lustre/mdt/mdt_io.c
@@ -650,7 +650,7 @@ retry:
 			GOTO(out_stop, rc);
 	}
 
-	tgt_vbr_obj_set(env, dob);
+	tgt_vbr_obj_data_set(env, dob, true);
 	rc = dt_trans_start(env, dt, th);
 	if (rc)
 		GOTO(out_stop, rc);
@@ -916,7 +916,7 @@ int mdt_object_fallocate(const struct lu_env *env, struct dt_device *dt,
 	if (rc)
 		GOTO(stop, rc);
 
-	tgt_vbr_obj_set(env, dob);
+	tgt_vbr_obj_data_set(env, dob, true);
 	rc = dt_trans_start(env, dt, th);
 	if (rc)
 		GOTO(stop, rc);
@@ -1106,7 +1106,7 @@ int mdt_object_punch(const struct lu_env *env, struct dt_device *dt,
 	if (rc)
 		GOTO(stop, rc);
 
-	tgt_vbr_obj_set(env, dob);
+	tgt_vbr_obj_data_set(env, dob, true);
 	rc = dt_trans_start(env, dt, th);
 	if (rc)
 		GOTO(stop, rc);
@@ -1657,7 +1657,7 @@ int mdt_data_version_get(struct tgt_session_info *tsi)
 		GOTO(out, rc = -EBADF);
 
 	/* Get version first */
-	version = dt_version_get(tsi->tsi_env, mdt_obj2dt(mo));
+	version = dt_data_version_get(tsi->tsi_env, mdt_obj2dt(mo));
 	if (version && version != -EOPNOTSUPP) {
 		repbody->mbo_valid |= OBD_MD_FLDATAVERSION;
 		/* re-use mbo_ioepoch to transfer version */
