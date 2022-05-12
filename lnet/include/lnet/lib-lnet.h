lustre/lustre-release

commit 5549b1b9e032c6eae63c9bde7104769697b93a0f

LU-15220 lustre: use 'fallthrough' pseudo keyword for switch
'/* fallthrough */' hits implicit-fallthrough error with GCC 11.

This patch replaces the existing '/* fallthrough */' comments and
its variants with the 'fallthrough' pseudo keyword, which was added
by Linux kernel commit v5.4-rc2-141-g294f69e662d1.

Test-Parameters: trivial
Change-Id: Icace4c9953950f86d3c48068d8c6bba7dd1160a6
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/46269
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Peter Jones <pjones@whamcloud.com>

lustre/include/lustre_compat.h
@@ -599,6 +599,10 @@ static inline bool is_root_inode(struct inode *inode)
 #define register_shrinker(_s) (register_shrinker(_s), 0)
 #endif
 
+#ifndef fallthrough
+#define fallthrough do {} while (0)  /* fallthrough */
+#endif
+
 static inline void ll_security_release_secctx(char *secdata, u32 seclen)
 {
 #ifdef HAVE_SEC_RELEASE_SECCTX_1ARG

lustre/ldlm/ldlm_request.c
@@ -1665,7 +1665,7 @@ ldlm_cancel_no_wait_policy(struct ldlm_namespace *ns, struct ldlm_lock *lock,
 		case LDLM_IBITS:
 			if (ns->ns_cancel != NULL && ns->ns_cancel(lock) != 0)
 				break;
-			/* fallthrough */
+			fallthrough;
 		default:
 			result = LDLM_POLICY_SKIP_LOCK;
 			break;

lustre/lfsck/lfsck_layout.c
@@ -7018,7 +7018,7 @@ int lfsck_layout_setup(const struct lu_env *env, struct lfsck_instance *lfsck)
 	default:
 		CERROR("%s: unknown lfsck_layout status %d\n",
 		       lfsck_lfsck2name(lfsck), lo->ll_status);
-		/* fall through */
+		fallthrough;
 	case LS_SCANNING_PHASE1:
 	case LS_SCANNING_PHASE2:
 		/* No need to store the status to disk right now.
@@ -7027,7 +7027,7 @@ int lfsck_layout_setup(const struct lu_env *env, struct lfsck_instance *lfsck)
 		lo->ll_status = LS_CRASHED;
 		if (!lfsck->li_master)
 			lo->ll_flags |= LF_INCOMPLETE;
-		/* fall through */
+		fallthrough;
 	case LS_PAUSED:
 	case LS_CRASHED:
 	case LS_CO_FAILED:

lustre/lfsck/lfsck_namespace.c
@@ -3778,7 +3778,7 @@ static int lfsck_namespace_double_scan_one(const struct lu_env *env,
 
 			repaired = true;
 
-			/* fall through */
+			/* fallthrough */
 		}
 
 		parent = lfsck_object_find_bottom(env, lfsck, pfid);
@@ -7152,14 +7152,14 @@ int lfsck_namespace_setup(const struct lu_env *env,
 	default:
 		CERROR("%s: unknown lfsck_namespace status %d\n",
 		       lfsck_lfsck2name(lfsck), ns->ln_status);
-		/* fall through */
+		fallthrough;
 	case LS_SCANNING_PHASE1:
 	case LS_SCANNING_PHASE2:
 		/* No need to store the status to disk right now.
 		 * If the system crashed before the status stored,
 		 * it will be loaded back when next time. */
 		ns->ln_status = LS_CRASHED;
-		/* fall through */
+		fallthrough;
 	case LS_PAUSED:
 	case LS_CRASHED:
 		spin_lock(&lfsck->li_lock);

lustre/llite/dir.c
@@ -2004,9 +2004,7 @@ out_quotactl:
 		RETURN(rc);
         }
 	case OBD_IOC_GETNAME_OLD:
-		/* fall through */
 	case OBD_IOC_GETDTNAME:
-		/* fall through */
 	case OBD_IOC_GETMDNAME:
                 RETURN(ll_get_obd_name(inode, cmd, arg));
         case LL_IOC_FLUSHCTX:

lustre/llite/file.c
@@ -172,7 +172,7 @@ static int ll_close_inode_openhandle(struct inode *inode,
 		op_data->op_attr_blocks += ((struct inode *)data)->i_blocks;
 		op_data->op_attr.ia_valid |= ATTR_SIZE;
 		op_data->op_xvalid |= OP_XVALID_BLOCKS;
-		/* fallthrough */
+		fallthrough;
 	case MDS_CLOSE_LAYOUT_SPLIT:
 	case MDS_CLOSE_LAYOUT_SWAP: {
 		struct split_param *sp = data;
@@ -3348,7 +3348,7 @@ static int ll_ladvise_sanity(struct inode *inode,
 			       ladvise_names[advice], rc);
 			GOTO(out, rc);
 		}
-		/* fallthrough */
+		fallthrough;
 	case LU_LADVISE_WILLREAD:
 	case LU_LADVISE_DONTNEED:
 	default:
@@ -4039,9 +4039,7 @@ out:
                 RETURN(0);
         }
 	case OBD_IOC_GETNAME_OLD:
-		/* fall through */
 	case OBD_IOC_GETDTNAME:
-		/* fall through */
 	case OBD_IOC_GETMDNAME:
 		RETURN(ll_get_obd_name(inode, cmd, arg));
 	case LL_IOC_HSM_STATE_GET: {

lustre/llite/llite_lib.c
@@ -1054,7 +1054,7 @@ static int ll_options(char *options, struct super_block *sb)
 
 		case LL_SBI_CHECKSUM:
 			sbi->ll_checksum_set = 1;
-			/* fall through */
+			fallthrough;
 		case LL_SBI_USER_XATTR:
 		case LL_SBI_USER_FID2PATH:
 		case LL_SBI_LRU_RESIZE:
@@ -1122,7 +1122,7 @@ static int ll_options(char *options, struct super_block *sb)
 				LCONSOLE_ERROR_MSG(0x152,
 						   "invalid %s option\n", s1);
 			}
-		/* fall through */
+		fallthrough;
 		default:
 			break;
 		}

lustre/llite/namei.c
@@ -423,13 +423,13 @@ int ll_md_need_convert(struct ldlm_lock *lock)
 	switch (lock->l_req_mode) {
 	case LCK_PR:
 		mode = LCK_PR;
-		/* fallthrough */
+		fallthrough;
 	case LCK_PW:
 		mode |= LCK_CR;
 		break;
 	case LCK_CW:
 		mode = LCK_CW;
-		/* fallthrough */
+		fallthrough;
 	case LCK_CR:
 		mode |= LCK_CR;
 		break;
@@ -1795,7 +1795,7 @@ static int ll_mknod(struct user_namespace *mnt_userns, struct inode *dir,
 	switch (mode & S_IFMT) {
 	case 0:
 		mode |= S_IFREG;
-		/* fallthrough */
+		fallthrough;
 	case S_IFREG:
 	case S_IFCHR:
 	case S_IFBLK:

Cray/lustre

commit 6ffd83b6d0f533fae77d5136f61f9f801c0e70b0

LU-15492 build: fallthrough macro for pre/post gcc-7
gcc-7.5 on openSUSE 15:
   error: this statement may fall through [-Werror=implicit-fallthrough=]

Use the __attribute__((fallthrough)) for gcc-7 and later.
and use a no op statement for earlier gcc where the fallthrough
attribute is not available.

Test-Parameters: trivial
Fixes: 5549b1b9e0 ("LU-15220 lustre: use 'fallthrough' pseudo keyword for switch")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ib72f5996149c738805f15e354e1e1606d981ce29

lnet/include/lnet/lib-lnet.h
@@ -110,7 +110,11 @@ extern struct lnet the_lnet;			/* THE network */
 #endif
 
 #ifndef fallthrough
-#define fallthrough do {} while (0)  /* fallthrough */
+# if defined(__GNUC__) && __GNUC__ >= 7
+#  define fallthrough  __attribute__((fallthrough)) /* fallthrough */
+# else
+#  define fallthrough do {} while (0)  /* fallthrough */
+# endif
 #endif
 
 int choose_ipv4_src(__u32 *ret,

lustre/include/lustre/lustreapi.h
@@ -62,7 +62,11 @@ extern "C" {
 #endif
 
 #ifndef fallthrough
-#define fallthrough do {} while (0)  /* fallthrough */
+# if defined(__GNUC__) && __GNUC__ >= 7
+#  define fallthrough  __attribute__((fallthrough)) /* fallthrough */
+# else
+#  define fallthrough do {} while (0)  /* fallthrough */
+# endif
 #endif
 
 typedef struct statx lstatx_t;

lustre/include/lustre_compat.h
@@ -600,7 +600,11 @@ static inline bool is_root_inode(struct inode *inode)
 #endif
 
 #ifndef fallthrough
-#define fallthrough do {} while (0)  /* fallthrough */
+# if defined(__GNUC__) && __GNUC__ >= 7
+#  define fallthrough  __attribute__((fallthrough)) /* fallthrough */
+# else
+#  define fallthrough do {} while (0)  /* fallthrough */
+# endif
 #endif
 
 static inline void ll_security_release_secctx(char *secdata, u32 seclen)

lustre/utils/gss/gss_util.h
@@ -35,7 +35,11 @@
 #include "write_bytes.h"
 
 #ifndef fallthrough
-#define fallthrough do {} while (0)  /* fallthrough */
+# if defined(__GNUC__) && __GNUC__ >= 7
+#  define fallthrough  __attribute__((fallthrough)) /* fallthrough */
+# else
+#  define fallthrough do {} while (0)  /* fallthrough */
+# endif
 #endif
 
 extern char *this_realm;