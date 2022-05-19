Cray/lustre

commit f47b0206348fb430b6e95046afbcbe33550409a8

LUS-10198 kfilnd: Support kfabric auth keys
Authorization keys are used to isolate RDMA traffic between other
applications and services. To isolate kfilnd traffic from application
and other service traffic, kfilnd needs the ability to set the
authorization key.

Signed-off-by: Ian Ziemba <ian.ziemba@hpe.com>
Change-Id: Idd3d03ff7641820553e37dcd3353fff2c5ed20d8
Reviewed-on: https://es-gerrit.dev.cray.com/159353
Reviewed-by: Chris Horn <hornc@cray.com>
Reviewed-by: Ian Ziemba <iziemba@cray.com>
Tested-by: Ian Ziemba <iziemba@cray.com>

lnet/utils/lnetconfig/liblnetconfig_lnd.c
@@ -95,6 +95,9 @@ lustre_kfilnd_show_tun(struct cYAML *lndparams,
 
 	if (cYAML_create_number(lndparams, "prov_minor_version",
 				lnd_cfg->lnd_prov_minor_version) == NULL)
+
+	if (cYAML_create_number(lndparams, "auth_key",
+				lnd_cfg->lnd_auth_key) == NULL)
 		return LUSTRE_CFG_RC_OUT_OF_MEM;
 
 	return LUSTRE_CFG_RC_NO_ERR;
@@ -200,6 +203,7 @@ yaml_extract_kfi_tun(struct cYAML *tree,
 {
 	struct cYAML *prov_major_version = NULL;
 	struct cYAML *prov_minor_version = NULL;
+	struct cYAML *auth_key = NULL;
 	struct cYAML *lndparams = NULL;
 
 	lndparams = cYAML_get_object_item(tree, "lnd tunables");
@@ -215,6 +219,10 @@ yaml_extract_kfi_tun(struct cYAML *tree,
 		cYAML_get_object_item(lndparams, "prov_minor_version");
 	lnd_cfg->lnd_prov_minor_version =
 		(prov_minor_version) ? prov_minor_version->cy_valueint : 0;
+
+	auth_key = cYAML_get_object_item(lndparams, "auth_key");
+	lnd_cfg->lnd_auth_key =
+		(auth_key) ? auth_key->cy_valueint : 0;
 }
 
 static void

lnet/include/uapi/linux/lnet/lnet-dlc.h
@@ -85,6 +85,7 @@ struct lnet_ioctl_config_kfilnd_tunables {
 	__u32 lnd_version;
 	__u32 lnd_prov_major_version;
 	__u32 lnd_prov_minor_version;
+	__u32 lnd_auth_key;
 };
 
 struct lnet_ioctl_config_socklnd_tunables {

lnet/klnds/kfilnd/kfilnd_dom.c
@@ -319,6 +319,10 @@ struct kfilnd_dom *kfilnd_dom_get(struct lnet_ni *ni, const char *node,
 	hints->tx_attr->op_flags = KFI_COMPLETION;
 	hints->tx_attr->iov_limit = 256; /* 1 MiB LNet message */
 	hints->tx_attr->rma_iov_limit = 256; /* 1 MiB LNet message */
+	hints->ep_attr->auth_key =
+		(void *)&ni->ni_lnd_tunables.lnd_tun_u.lnd_kfi.lnd_auth_key;
+	hints->ep_attr->auth_key_size =
+		sizeof(ni->ni_lnd_tunables.lnd_tun_u.lnd_kfi.lnd_auth_key);
 
 	/* Check to see if any KFI LND fabrics/domains can be reused. */
 	fab = kfilnd_fab_reuse(node, service, hints);
@@ -333,6 +337,14 @@ struct kfilnd_dom *kfilnd_dom_get(struct lnet_ni *ni, const char *node,
 	 * device allocation.
 	 */
 	rc = kfi_getinfo(0, node, service, KFI_SOURCE, hints, &info);
+
+	/* Authorization key information is now stored in the returned kfi_info
+	 * structure. Since kfi_freeinfo() will try to free the auth_key pointer
+	 * and this memory is owned as part of the LNet NI, need to zero this
+	 * information in the hints to prevent LNet NI corruption.
+	 */
+	hints->ep_attr->auth_key = NULL;
+	hints->ep_attr->auth_key_size = 0;
 
 	kfi_freeinfo(hints);
 	kfree(service);

lnet/klnds/kfilnd/kfilnd_modparams.c
@@ -69,6 +69,10 @@ module_param(prov_minor_version, int, 0444);
 MODULE_PARM_DESC(prov_minor_version,
 		 "Default kfabric provider minor version kfilnd should use");
 
+static unsigned int auth_key = 255;
+module_param(auth_key, uint, 0444);
+MODULE_PARM_DESC(auth_key, "Default authorization key to be used for LNet NIs");
+
 int kfilnd_tunables_setup(struct lnet_ni *ni)
 {
 	struct lnet_ioctl_config_lnd_cmn_tunables *net_tunables;
@@ -93,6 +97,11 @@ int kfilnd_tunables_setup(struct lnet_ni *ni)
 	if (!ni->ni_lnd_tunables_set) {
 		kfilnd_tunables->lnd_prov_major_version = prov_major_version;
 		kfilnd_tunables->lnd_prov_minor_version = prov_minor_version;
+
+		/* Treat zero as uninitialized. */
+		if (ni->ni_lnd_tunables.lnd_tun_u.lnd_kfi.lnd_auth_key == 0)
+			ni->ni_lnd_tunables.lnd_tun_u.lnd_kfi.lnd_auth_key =
+				auth_key;
 	}
 
 	return 0;
@@ -117,6 +126,11 @@ int kfilnd_tunables_init(void)
 
 	if (immediate_rx_buf_count < 2) {
 		CERROR("Immediate multi-receive buffer count less than 2");
+		return -EINVAL;
+	}
+
+	if (auth_key < 1) {
+		CERROR("Authorization key cannot be less than 1");
 		return -EINVAL;
 	}
 

lnet/utils/lnetctl.c
@@ -164,7 +164,8 @@ command_t net_cmds[] = {
 	 "\t--credits: Network Interface credits\n"
 	 "\t--cpt: CPU Partitions configured net uses (e.g. [0,1]\n"
 	 "\t--conns-per-peer: number of connections per peer\n"
-	 "\t--skip-mr-route-setup: do not add linux route for the ni\n"},
+	 "\t--skip-mr-route-setup: do not add linux route for the ni\n"
+	 "\t--auth-key: Network authorization key (kfilnd only)\n"},
 	{"del", jt_del_ni, 0, "delete a network\n"
 	 "\t--net: net name (e.g. tcp0)\n"
 	 "\t--if: physical interface (e.g. eth0)\n"},
@@ -1049,7 +1050,7 @@ static int jt_add_route(int argc, char **argv)
 static int jt_add_ni(int argc, char **argv)
 {
 	char *ip2net = NULL;
-	long int pto = -1, pc = -1, pbc = -1, cre = -1, cpp = -1;
+	long int pto = -1, pc = -1, pbc = -1, cre = -1, cpp = -1, auth_key = -1;
 	struct cYAML *err_rc = NULL;
 	int rc, opt, cpt_rc = -1;
 	struct lnet_dlc_network_descr nw_descr;
@@ -1061,8 +1062,9 @@ static int jt_add_ni(int argc, char **argv)
 	memset(&tunables, 0, sizeof(tunables));
 	lustre_lnet_init_nw_descr(&nw_descr);
 
-	const char *const short_options = "b:c:i:k:m:n:p:r:s:t:";
+	const char *const short_options = "a:b:c:i:k:m:n:p:r:s:t:";
 	static const struct option long_options[] = {
+	{ .name = "auth-key",	  .has_arg = required_argument, .val = 'a' },
 	{ .name = "peer-buffer-credits",
 				  .has_arg = required_argument, .val = 'b' },
 	{ .name = "peer-credits", .has_arg = required_argument, .val = 'c' },
@@ -1085,6 +1087,14 @@ static int jt_add_ni(int argc, char **argv)
 	while ((opt = getopt_long(argc, argv, short_options,
 				   long_options, NULL)) != -1) {
 		switch (opt) {
+		case 'a':
+			rc = parse_long(optarg, &auth_key);
+			if (rc != 0) {
+				/* ignore option */
+				auth_key = -1;
+				continue;
+			}
+			break;
 		case 'b':
 			rc = parse_long(optarg, &pbc);
 			if (rc != 0) {
@@ -1154,6 +1164,11 @@ static int jt_add_ni(int argc, char **argv)
 		default:
 			return 0;
 		}
+	}
+
+	if (auth_key > 0 && LNET_NETTYP(nw_descr.nw_id) == KFILND) {
+		tunables.lt_tun.lnd_tun_u.lnd_kfi.lnd_auth_key = auth_key;
+		found = true;
 	}
 
 	if (pto > 0 || pc > 0 || pbc > 0 || cre > 0 || cpp > -1) {

commit 10b6624b569bee7ffdf7f501ac8a6f5a4a49cead

LUS-10960 lnet: Fix display of kfilnd tunables
lustre_kfilnd_show_tun() is missing a return so not all tunables
are displayed.

Fixes: f47b020634 ("LUS-10198 kfilnd: Support kfabric auth keys")
Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Iaf675f979ad85ff05201b2879ad5adcc1505e2d9
Reviewed-on: https://es-gerrit.dev.cray.com/160655
Reviewed-by: Ron Gredvig <rgredvig@cray.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Tested-by: Jenkins Build User <nssreleng@cray.com>

lnet/utils/lnetconfig/liblnetconfig_lnd.c
@@ -95,6 +95,7 @@ lustre_kfilnd_show_tun(struct cYAML *lndparams,
 
 	if (cYAML_create_number(lndparams, "prov_minor_version",
 				lnd_cfg->lnd_prov_minor_version) == NULL)
+		return LUSTRE_CFG_RC_OUT_OF_MEM;
 
 	if (cYAML_create_number(lndparams, "auth_key",
 				lnd_cfg->lnd_auth_key) == NULL)