24654731c9715d85751d60d9aea0d7033790feff
Vladimir Saveliev <vlaidimir.saveliev@hpe.com>
2022-06-06 19:36:39 +0300

    LUS-10989 tests: delay of file_remove_privs causes eviction
    This test delays mds enqueue rpc executed by
    file_remove_privs->..->ll_xattr_get_common..->mdc_intent_lock while
    two clients write a file in parallel. Reasonably long delay may cause
    a client eviction.
    
    Test-Parameters: testlist=replay-dual/33
    Change-Id: Ied5f422ae8c7e9be464bc858bd4aed7caf9b3de5
    Signed-off-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/160753
    Reviewed-by: Elena Gryaznova <elena.gryaznova@hpe.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>
    Reviewed-by: Alexander Boyko <c17825@cray.com>

--- lustre/tests/replay-dual.sh
+++ lustre/tests/replay-dual.sh
@@ -1177,6 +1177,32 @@ test_32() {
 }
 run_test 32 "gap in update llog shouldn't break recovery"
 
+test_33()
+{
+	# disable AT so that blocking timeout gets set to obd_timeout/2
+	local amo=$(at_max_get ost1)
+	at_max_set 0 ost1
+	stack_trap "at_max_set $amo ost1"
+	local timeout=$(do_facet ost1 $LCTL get_param -n timeout)
+
+	$LFS setstripe -i 0 -c 1 $DIR/$tfile
+
+	#define OBD_FAIL_LLITE_XATTR_PAUSE                 0x1420
+	$LCTL set_param fail_loc=0x80001420
+	$MULTIOP $DIR/$tfile Osw4096c &
+	local multiop1pid=$!
+	sleep 0.5
+	$MULTIOP $MOUNT2/$tfile oO_WRONLY:_w4096c &
+	local multiop2pid=$!
+	sleep 0.5
+	fail mds1 $((timeout / 2 + 5)) &
+	sleep 0.5
+	kill -USR1 $multiop2pid
+
+	wait $multiop1pid && wait $multiop2pid|| error "multiop failed"
+}
+run_test 33 "eviction on file_remove_privs's enqueue delay"
+
 complete $SECONDS
 SLEEP=$((SECONDS - $NOW))
 [ $SLEEP -lt $TIMEOUT ] && sleep $SLEEP
