76221899cac4949d99e7b2198c4b07dfaeb8230e
Chris Horn <chris.horn@hpe.com>
2022-06-06 13:09:03 -0500

    LU-15914 lnet: Fix null md deref for finalized message
    When a message is finalized the lnet_msg:msg_md field may be cleared
    (see lnet_finalize() -> lnet_msg_detach_md()).
    When an LNet router is forwarding such message, or if an ACK has been
    requested for such a message, then the NULL msg_md may be deref'd in
    lnet_get_best_ni(). Check for this in lnet_get_best_ni() before
    dereferencing the MD.
    
    It may also be dereferenced in kiblnd_send(), so check for this
    situation there, too. Some style cleanup is included in
    kiblnd_send().
    
    Test-Parameters: trivial
    Fixes: 959304eac7 ("LU-15189 lnet: fix memory mapping.")
    HPE-bug-id: LUS-10997
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: I3cfdc8d342bd3b49a61d1ce6c31a245848accf8f
    Reviewed-on: https://review.whamcloud.com/47546
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
    Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: James Simmons <jsimmons@infradead.org>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lnet/lnet/lib-move.c
+++ lnet/lnet/lib-move.c
@@ -1637,7 +1637,7 @@ lnet_get_best_ni(struct lnet_net *local_net, struct lnet_ni *best_ni,
 	unsigned int best_dev_prio;
 	int best_ni_fatal;
 	unsigned int dev_idx = UINT_MAX;
-	bool gpu = md->md_flags & LNET_MD_FLAG_GPU;
+	bool gpu = md ? (md->md_flags & LNET_MD_FLAG_GPU) : false;
 
 	if (gpu) {
 		struct page *page = lnet_get_first_page(md, offset);
