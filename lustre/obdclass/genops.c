Cray/lustre

commit ea7249b03c2c5756dfe5553af90a90b82d4628b8

LUS-9344 ptlrpc: Close request can't be sent
There is a slot for close request but close
thread can't be woken up due to exclusive wait.

Make close wait as non-exclusive, so it will be
woken up with each call.

Change-Id: I032d379e6488793d0b6a489f45d52d9e62b99fe0
Fixes: 7fa0fd4157 ("LU-11441 obdclass: make mod rpc slot wait queue FIFO")
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/157856
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Vladimir Saveliev <c17830@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/obdclass/genops.c
@@ -2309,9 +2309,13 @@ __u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc)
 		       "opc %u, max %hu\n",
 		       cli->cl_import->imp_obd->obd_name, opc, max);
 
-		wait_event_idle_exclusive(cli->cl_mod_rpcs_waitq,
-					  obd_mod_rpc_slot_avail(cli,
-								 close_req));
+		if (close_req) {
+			wait_event_idle(cli->cl_mod_rpcs_waitq,
+				     obd_mod_rpc_slot_avail(cli, close_req));
+		} else {
+			wait_event_idle_exclusive(cli->cl_mod_rpcs_waitq,
+					obd_mod_rpc_slot_avail(cli, close_req));
+		}
 	} while (true);
 }
 EXPORT_SYMBOL(obd_get_mod_rpc_slot);

commit d62a3fce115896e2bdb316ef7f8fe31b97bb5245

LUS-10895 ptlrpc: Revert wake_up_all on close_req completion
obd_get_mod_rpc_slot was changed in
  "LUS-9344 ptlrpc: Close request can't be sent"
which splits close_req to be added as non-exclusive waiters.

This places close_req waiters on the front of the wait queue,
and everything else is added to the end of the wait queue.

obd_put_mod_rpc_slot was changed in
  "LU-14741 obdclass: Wake up entire queue of requests on
   close completion"
which changes the wake_up side to 'wake up all' for the
close_req case.

These two changes seem to have conflicting approaches toward
the same goal.

It seems reasonable to proceed with removing the wake_up_all()
and trust the ordering of the wait_queue to do the right thing.

Fixes: ea7249b03c ("LUS-9344 ptlrpc: Close request can't be sent")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I5d5b38354dd2f62e3f22ee33c697f6a2caf6dd26
Reviewed-on: https://es-gerrit.dev.cray.com/160587
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>
Reviewed-by: Vladimir Saveliev <c17830@cray.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>

lustre/obdclass/genops.c
@@ -2180,15 +2180,13 @@ int obd_set_max_mod_rpcs_in_flight(struct client_obd *cli, __u16 max)
 	}
 
 	spin_lock(&cli->cl_mod_rpcs_lock);
-
 	prev = cli->cl_max_mod_rpcs_in_flight;
 	cli->cl_max_mod_rpcs_in_flight = max;
-
-	/* wakeup waiters if limit has been increased */
-	if (cli->cl_max_mod_rpcs_in_flight > prev)
-		wake_up(&cli->cl_mod_rpcs_waitq);
-
 	spin_unlock(&cli->cl_mod_rpcs_lock);
+
+	/* wake_up_all waiters if limit has been increased */
+	if (max > prev)
+		wake_up_all(&cli->cl_mod_rpcs_waitq);
 
 	return 0;
 }
@@ -2275,11 +2273,8 @@ static inline bool obd_mod_rpc_slot_avail(struct client_obd *cli,
  */
 __u16 obd_get_mod_rpc_slot(struct client_obd *cli, __u32 opc)
 {
-	bool			close_req = false;
-	__u16			i, max;
-
-	if (opc == MDS_CLOSE)
-		close_req = true;
+	bool close_req = (opc == MDS_CLOSE);
+	__u16 i, max;
 
 	do {
 		spin_lock(&cli->cl_mod_rpcs_lock);
@@ -2328,13 +2323,10 @@ EXPORT_SYMBOL(obd_get_mod_rpc_slot);
  */
 void obd_put_mod_rpc_slot(struct client_obd *cli, __u32 opc, __u16 tag)
 {
-	bool			close_req = false;
+	bool close_req = (opc == MDS_CLOSE);
 
 	if (tag == 0)
 		return;
-
-	if (opc == MDS_CLOSE)
-		close_req = true;
 
 	spin_lock(&cli->cl_mod_rpcs_lock);
 	cli->cl_mod_rpcs_in_flight--;
@@ -2344,11 +2336,7 @@ void obd_put_mod_rpc_slot(struct client_obd *cli, __u32 opc, __u16 tag)
 	LASSERT(tag - 1 < OBD_MAX_RIF_MAX);
 	LASSERT(test_and_clear_bit(tag - 1, cli->cl_mod_tag_bitmap) != 0);
 	spin_unlock(&cli->cl_mod_rpcs_lock);
-	/* LU-14741 - to prevent close RPCs stuck behind normal ones */
-	if (close_req)
-		wake_up_all(&cli->cl_mod_rpcs_waitq);
-	else
-		wake_up(&cli->cl_mod_rpcs_waitq);
+	wake_up(&cli->cl_mod_rpcs_waitq);
 }
 EXPORT_SYMBOL(obd_put_mod_rpc_slot);
 