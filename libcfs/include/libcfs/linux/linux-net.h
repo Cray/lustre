Cray/lustre

commit a48d4bd8d5ea71c7bf04539a7212e9ac0ba36efe

LUS-10353 libcfs: sk_reuseport not avail use kernel_setsockopt
Linux v3.8-rc3-650-g055dc21a1d1d added initial support for
so_reuseport.

For prior kernels use SO_REUSEADDR with kernel_setsockopt()
instead of setting the sk_reuseport directly.

Test-Parameters: trivial
Fixes: b284c3e21d ("LUS-10121 libcfs: support removal of kernel_setsockopt()")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I4de8c3ce81dcad8706228523707647dbe1d87ba4
Reviewed-on: https://es-gerrit.dev.cray.com/159293
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Tested-by: Vitaly Fertman <c17818@cray.com>

libcfs/include/libcfs/linux/linux-net.h
@@ -102,6 +102,17 @@ lnet_genl_dumpit_info(struct netlink_callback *cb)
 #ifdef HAVE_KERNEL_SETSOCKOPT
 
 #include <net/tcp.h>
+#include <net/sock.h>
+
+static inline void libcfs_set_so_reuseport(struct socket *sock, int option)
+{
+#ifndef HAVE_SOCKET_SK_SK_REUSEPORT
+	(void)kernel_setsockopt(sock, SOL_SOCKET, SO_REUSEADDR,
+				(char *)&option, sizeof(option));
+#else
+	sock->sk->sk_reuseport = option;
+#endif
+}
 
 #if !defined(HAVE_TCP_SOCK_SET_QUICKACK)
 static inline void tcp_sock_set_quickack(struct sock *sk, int opt)

libcfs/autoconf/lustre-libcfs.m4
@@ -70,6 +70,29 @@ EXTRA_KCFLAGS="$tmp_flags"
 ]) # LIBCFS_HAVE_PERCPU_REFCOUNT
 
 #
+# LIBCFS_HAVE_SOCKET_SK_SK_REUSEPORT
+#
+# v3.8-rc3-650-g055dc21a1d1d
+# soreuseport: infrastructure
+#
+AC_DEFUN([LIBCFS_HAVE_SOCKET_SK_SK_REUSEPORT], [
+tmp_flags="$EXTRA_KCFLAGS"
+EXTRA_KCFLAGS="-Werror"
+LB_CHECK_COMPILE([if 'struct socket' has 'sk->sk_reuseport' member],
+nsecs_to_jiffies64, [
+	#include <net/sock.h>
+],[
+	struct socket *sock = NULL;
+
+	sock->sk->sk_reuseport = 1;
+],[
+	AC_DEFINE(HAVE_SOCKET_SK_SK_REUSEPORT, 1,
+		['struct socket' member 'sk->sk_reuseport' is available])
+])
+EXTRA_KCFLAGS="$tmp_flags"
+]) # LIBCFS_HAVE_SOCKET_SK_SK_REUSEPORT
+
+#
 # LIBCFS_HAVE_IDR_ALLOC
 # v3.9
 #
@@ -1960,6 +1983,7 @@ LIBCFS_CONFIG_PANIC_DUMPLOG
 
 # 3.8
 LIBCFS_D_HASH_AND_LOOKUP
+LIBCFS_HAVE_SOCKET_SK_SK_REUSEPORT
 LIBCFS_HAVE_IDR_ALLOC
 # 3.11
 LIBCFS_HAVE_PERCPU_REFCOUNT

lnet/lnet/lib-socket.c
@@ -209,7 +209,7 @@ retry:
 		return ERR_PTR(rc);
 	}
 
-	sock->sk->sk_reuseport = 1;
+	libcfs_set_so_reuseport(sock, 1);
 
 	if (interface >= 0 || local_port != 0) {
 		struct sockaddr_storage locaddr = {};

commit e94c8c1adb5146b4f0760735df91647654eb720e

LUS-10924 libcfs: Ensure libcfs_set_so_reuseport is available
Move libcfs_set_so_reuseport out of libcfs_set_so_reuseport

Fixes: a48d4bd8d5 (LUS-10353 libcfs: sk_reuseport not avail use kernel_setsockopt")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Iebbb1b5eb6d6d60c44e82b068d911171d79a2848
Reviewed-on: https://es-gerrit.dev.cray.com/160571
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

libcfs/include/libcfs/linux/linux-net.h
@@ -99,11 +99,6 @@ lnet_genl_dumpit_info(struct netlink_callback *cb)
 }
 #endif /* HAVE_GENL_DUMPIT_INFO */
 
-#ifdef HAVE_KERNEL_SETSOCKOPT
-
-#include <net/tcp.h>
-#include <net/sock.h>
-
 static inline void libcfs_set_so_reuseport(struct socket *sock, int option)
 {
 #ifndef HAVE_SOCKET_SK_SK_REUSEPORT
@@ -113,6 +108,11 @@ static inline void libcfs_set_so_reuseport(struct socket *sock, int option)
 	sock->sk->sk_reuseport = option;
 #endif
 }
+
+#ifdef HAVE_KERNEL_SETSOCKOPT
+
+#include <net/tcp.h>
+#include <net/sock.h>
 
 #if !defined(HAVE_TCP_SOCK_SET_QUICKACK)
 static inline void tcp_sock_set_quickack(struct sock *sk, int opt)