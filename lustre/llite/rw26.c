lustre/lustre-release

commit 45c46c6effd8273dc4ca52f32447f9f7103057b1

LU-14043 llite: allow DIO with unaligned IO count
DIO only require user buffer page aligned and
IO offset page aligned, it is ok that io count is
not page aligned, remove this unnecessary limit
so that we could use DIO with file not aligned
with PAGE SIZE.

Change-Id: Iaf1e224b3f180f91d1b54016c6e6b7adaf674bf8
Signed-off-by: Wang Shilong <wshilong@ddn.com>
Reviewed-on: https://review.whamcloud.com/40392
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: John L. Hammond <jhammond@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alex Zhuravlev <bzzz@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/llite/rw26.c
@@ -250,12 +250,12 @@ static ssize_t ll_get_user_pages(int rw, struct iov_iter *iter,
 
 /* iov_iter_alignment() is introduced in 3.16 similar to HAVE_DIO_ITER */
 #if defined(HAVE_DIO_ITER)
-static unsigned long ll_iov_iter_alignment(const struct iov_iter *i)
+static unsigned long iov_iter_alignment_vfs(const struct iov_iter *i)
 {
 	return iov_iter_alignment(i);
 }
 #else /* copied from alignment_iovec() */
-static unsigned long ll_iov_iter_alignment(const struct iov_iter *i)
+static unsigned long iov_iter_alignment_vfs(const struct iov_iter *i)
 {
 	const struct iovec *iov = i->iov;
 	unsigned long res;
@@ -281,6 +281,35 @@ static unsigned long ll_iov_iter_alignment(const struct iov_iter *i)
 	return res;
 }
 #endif
+
+/*
+ * Lustre could relax a bit for alignment, io count is not
+ * necessary page alignment.
+ */
+static unsigned long ll_iov_iter_alignment(struct iov_iter *i)
+{
+	size_t orig_size = i->count;
+	size_t count = orig_size & ~PAGE_MASK;
+	unsigned long res;
+
+	if (!count)
+		return iov_iter_alignment_vfs(i);
+
+	if (orig_size > PAGE_SIZE) {
+		iov_iter_truncate(i, orig_size - count);
+		res = iov_iter_alignment_vfs(i);
+		iov_iter_reexpand(i, orig_size);
+
+		return res;
+	}
+
+	res = iov_iter_alignment_vfs(i);
+	/* start address is page aligned */
+	if ((res & ~PAGE_MASK) == orig_size)
+		return PAGE_SIZE;
+
+	return res;
+}
 
 /** direct IO pages */
 struct ll_dio_pages {
@@ -433,8 +462,8 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 		return 0;
 
 	/* FIXME: io smaller than PAGE_SIZE is broken on ia64 ??? */
-	if ((file_offset & ~PAGE_MASK) || (count & ~PAGE_MASK))
-		return -EINVAL;
+	if (file_offset & ~PAGE_MASK)
+		RETURN(-EINVAL);
 
 	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID"(%p), size=%zd (max %lu), "
 	       "offset=%lld=%llx, pages %zd (max %lu)\n",
@@ -444,7 +473,7 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 
 	/* Check that all user buffers are aligned as well */
 	if (ll_iov_iter_alignment(iter) & ~PAGE_MASK)
-		return -EINVAL;
+		RETURN(-EINVAL);
 
 	lcc = ll_cl_find(file);
 	if (lcc == NULL)

Cray/lustre

commit 87d42f332be0f26cd32b6256869df9cb0aca76cc

LUS-10874 llite: restrict DIO to a multiple of pages
During scale testing variations on the following:
   ASSERTION( ... (ergo(i == 0, poff + pg->count == PAGE_SIZE)
   ... failed: i: 0/2 pg: 00000000295a3943 off: 0, count: 3928
was observed.

This reverts a change to allow page aligned DIO that is
not page sized.

lfs migrate will temporarily drop O_DIRECT for I/O
that is not a multiple of pages.

Fixes: 45c46c6eff ("LU-14043 llite: allow DIO with unaligned IO count")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I49998284745351b3b8babbf0b7d35ed44927c136
Reviewed-on: https://es-gerrit.dev.cray.com/160715
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>
Reviewed-by: Chris Horn <hornc@cray.com>
Reviewed-by: Petros Koutoupis <pkoutoupis@cray.com>

lustre/llite/rw26.c
@@ -418,7 +418,7 @@ ll_direct_IO_impl(struct kiocb *iocb, struct iov_iter *iter, int rw)
 		return 0;
 
 	/* FIXME: io smaller than PAGE_SIZE is broken on ia64 ??? */
-	if (file_offset & ~PAGE_MASK)
+	if ((file_offset & ~PAGE_MASK) || (count & ~PAGE_MASK))
 		RETURN(-EINVAL);
 
 	CDEBUG(D_VFSTRACE, "VFS Op:inode="DFID"(%p), size=%zd (max %lu), "

lustre/utils/lfs.c
@@ -799,6 +799,7 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int))
 	off_t data_end = 0;
 	size_t page_size = sysconf(_SC_PAGESIZE);
 	bool sparse;
+	int fopt;
 	int rc;
 
 	layout = llapi_layout_get_by_fd(fd_src, 0);
@@ -825,6 +826,10 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int))
 			return rc;
 		}
 	}
+
+	fopt = fcntl(fd_dst, F_GETFL);
+	if (fopt < 0)
+		fopt = 0;
 
 	while (1) {
 		off_t data_off;
@@ -873,7 +878,11 @@ static int migrate_copy_data(int fd_src, int fd_dst, int (*check_file)(int))
 		while (to_write > 0) {
 			ssize_t written;
 
+			if ((to_write & (page_size - 1)) && (fopt & O_DIRECT))
+				fcntl(fd_dst, F_SETFL, fopt & ~O_DIRECT);
 			written = pwrite(fd_dst, buf, to_write, pos);
+			if ((to_write & (page_size - 1)) && (fopt & O_DIRECT))
+				fcntl(fd_dst, F_SETFL, fopt);
 			if (written < 0) {
 				rc = -errno;
 				goto out;