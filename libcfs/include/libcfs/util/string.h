3adef8d091bc586798569df6ace3a2e2052bc09f
James Simmons <jsimmons@infradead.org>
2022-07-31 10:06:08 -0400

    LU-10973 lutf: Fix order of linking for python modules
    LUTF normally works but in some test cases at startup we got:
    
    ImportError: lustre/test/lutrf/src/_lnetconfig.so:
    undefined symbol: lustre_lnet_del_ni
    
    If you check the symbol is there. The issue is the linking order.
    We need to put the generated module name before all its
    dependencies.
    
    Also, remove cfs_expr_list_match from string.h, move the definition
    to nidstrings.c, and make it static.
    
    Test-Parameters: @lnet
    Change-Id: Ia57fbd9d5795d845ea14bc1416f968383afcba2b
    Signed-off-by: James Simmons <jsimmons@infradead.org>
    Signed-off-by: Timothy Day <timday@amazon.com>
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/46478
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
    Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
    Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- libcfs/include/libcfs/util/string.h
+++ libcfs/include/libcfs/util/string.h
@@ -123,7 +123,6 @@ int cfs_gettok(struct cfs_lstr *next, char delim, struct cfs_lstr *res);
 int cfs_str2num_check(char *str, int nob, unsigned *num,
 		      unsigned min, unsigned max);
 int cfs_expr2str(struct list_head *list, char *str, size_t size);
-int cfs_expr_list_match(__u32 value, struct cfs_expr_list *expr_list);
 int cfs_expr_list_print(char *buffer, int count,
 			struct cfs_expr_list *expr_list);
 int cfs_expr_list_parse(char *str, int len, unsigned min, unsigned max,
