9233dd721d8f67a0853efb7a40db88127031bcb4
Vitaly Fertman <c17818@cray.com>
2022-09-01 23:41:05 +0300

    LUS-11090 test: mkdir space balance fixes
    tests for some minor problems:
    - getdirstripe keeps showing the root default for underlying children
      instead of 0 when max_inherit_rr is exhausted;
    - it may happen that the depth is not set on a dir, do not consider
      depth == 0 as a real depth while checking if the root default is
      applicable;
    - setdirstripe util implicitely sets max_inherit to 3 for non-striped
      dir when -i option is given but -c is not; at the same time 3 is the
      default for striped dirs only;
    - getdirstripe shows inherited default layouts with max_inherit==0,
      whereas it has no sense anymore;
    
    Signed-off-by: Vitaly Fertman <c17818@cray.com>
    Change-Id: I4a4c549e6768ae9969671f9c50996de937d5f797
    Reviewed-on: https://es-gerrit.dev.cray.com/161131
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -26017,6 +26017,67 @@ test_413f() {
 }
 run_test 413f "lfs getdirstripe -D list ROOT default LMV if it's not set on dir"
 
+test_413g() {
+	[ $MDSCOUNT -lt 2 ] && skip_env "needs >= 2 MDTs"
+
+	(( MDS1_VERSION >= $(version_code 2.14.55) )) ||
+		skip "Need server version at least 2.14.55"
+
+	getfattr -d -m trusted.dmv --absolute-names $DIR > $TMP/dmv.ea ||
+		error "dump $DIR default LMV failed"
+	stack_trap "setfattr --restore=$TMP/dmv.ea"
+
+	local testdir=$DIR/$tdir
+	local def_max_rr=1
+	local def_max=3
+	local count
+
+	$LFS setdirstripe -D -i-1 -c1 --max-inherit=$def_max \
+		--max-inherit-rr=$def_max_rr $DIR ||
+		error "set $DIR default LMV failed"
+
+	for i in $(seq 2 3); do
+		def_max=$((def_max - 1))
+		(( def_max_rr == 0 )) || def_max_rr=$((def_max_rr - 1))
+
+		mkdir $testdir
+		# RR is decremented and keeps zeroed once exhausted
+		count=$($LFS getdirstripe -D --max-inherit-rr $testdir)
+		(( count == def_max_rr )) ||
+			error_noexit "$testdir: max-inherit-rr $count != $def_max_rr"
+
+		# max-inherit is decremented
+		count=$($LFS getdirstripe -D --max-inherit $testdir)
+		(( count == def_max )) ||
+			error_noexit "$testdir: max-inherit $count != $def_max"
+
+		testdir=$testdir/d$i
+	done
+
+	# d3 is the last inherited from ROOT, no inheritance anymore
+	mkdir -p $testdir/d4/d5
+	count=$($LFS getdirstripe -D --max-inherit $testdir)
+	(( count == -1 )) ||
+		error_noexit "$testdir: max-inherit $count != -1"
+
+	local p_count=$($LFS getdirstripe -i $testdir)
+
+	for i in $(seq 4 5); do
+		testdir=$testdir/d$i
+
+		# the root default layout is not applied once exhausted
+		count=$($LFS getdirstripe -i $testdir)
+		(( count == p_count )) ||
+			error_noexit "$testdir: stripe-offset $count != parent offset $p_count"
+	done
+
+	$LFS setdirstripe -i 0 $DIR/d2
+	count=$($LFS getdirstripe -D --max-inherit $DIR/d2)
+	(( count == -1 )) ||
+		error_noexit "$DIR/d2: max-inherit non-striped default $count != -1"
+}
+run_test 413g "check default layout inheritance"
+
 test_413z() {
 	local pids=""
 	local subdir
