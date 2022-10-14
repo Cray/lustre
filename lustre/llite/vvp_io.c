0a7a502dca5346d8c48563e89c0ecc295dd3781a
Vitaly Fertman <c17818@cray.com>
2022-10-14 21:22:48 +0300

    Revert "LUS-11146 llite: SUSE 15 SP4 account_page_dirtied fallback"
    This reverts commit ea411ec7d1d6ce68e5dea785ba9929558c0f0af8.
    to be replaced by LU-15959

--- lustre/llite/vvp_io.c
+++ lustre/llite/vvp_io.c
@@ -1032,16 +1032,6 @@ void vvp_set_pagevec_dirty(struct pagevec *pvec)
 	for (i = 0; i < count; i++)
 		__set_page_dirty_nobuffers(pvec->pages[i]);
 #else
-	/*
-	 * In kernel 5.14.21, kallsyms_lookup_name is defined but
-	 * account_page_dirtied is not exported.
-	 */
-	if (!vvp_account_page_dirtied) {
-		for (i = 0; i < count; i++)
-			__set_page_dirty_nobuffers(pvec->pages[i]);
-		goto end;
-	}
-
 	for (i = 0; i < count; i++) {
 		page = pvec->pages[i];
 
@@ -1091,7 +1081,6 @@ void vvp_set_pagevec_dirty(struct pagevec *pvec)
 		/* !PageAnon && !swapper_space */
 		__mark_inode_dirty(mapping->host, I_DIRTY_PAGES);
 	}
-end:
 #endif
 	EXIT;
 }
