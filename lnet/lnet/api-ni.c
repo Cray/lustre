cd8d13ddafe2abaa08cc6bf9390ead4b19fc745f
Chris Horn <chris.horn@hpe.com>
2022-11-01 14:33:18 -0600

    LUS-11317 lnet: Signal completion on ping send failure
    Call complete() on the ping_data::completion if we get
    LNET_EVENT_SEND with non-zero status. Otherwise the thread which
    issued the ping is stuck waiting for the full ping timeout.
    
    A pd_unlinked member is added to struct ping_data to indicate whether
    the associated MD has been unlinked. This is checked by lnet_ping() to
    determine whether it needs to explicitly called LNetMDUnlink().
    
    Lastly, in cases where we do not receive a reply, we now return the
    value of pd.rc, if it is non-zero, rather than -EIO. This can provide
    more information about the underlying ping failure.
    
    Test-Parameters: trivial testlist=sanity-lnet/252
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: I1bc573cf7397e319993fa8aabb31c5f3b59768e7
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/161430
    Tested-by: Jenkins Build User <nssreleng@cray.com>
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Reviewed-by: Andriy Skulysh <c17819@cray.com>
    Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>

--- lnet/lnet/api-ni.c
+++ lnet/lnet/api-ni.c
@@ -4757,6 +4757,7 @@ EXPORT_SYMBOL(LNetGetId);
 struct ping_data {
 	int rc;
 	int replied;
+	int pd_unlinked;
 	struct lnet_handle_md mdh;
 	struct completion completion;
 };
@@ -4777,7 +4778,12 @@ lnet_ping_event_handler(struct lnet_event *event)
 		pd->replied = 1;
 		pd->rc = event->mlength;
 	}
+
 	if (event->unlinked)
+		pd->pd_unlinked = 1;
+
+	if (event->unlinked ||
+	    (event->type == LNET_EVENT_SEND && event->status))
 		complete(&pd->completion);
 }
 
@@ -4841,13 +4847,14 @@ static int lnet_ping(struct lnet_process_id id, struct lnet_nid *src_nid,
 		/* NB must wait for the UNLINK event below... */
 	}
 
-	if (wait_for_completion_timeout(&pd.completion, timeout) == 0) {
-		/* Ensure completion in finite time... */
+	/* Ensure completion in finite time... */
+	wait_for_completion_timeout(&pd.completion, timeout);
+	if (!pd.pd_unlinked) {
 		LNetMDUnlink(pd.mdh);
 		wait_for_completion(&pd.completion);
 	}
 	if (!pd.replied) {
-		rc = -EIO;
+		rc = pd.rc ?: -EIO;
 		goto fail_ping_buffer_decref;
 	}
 
