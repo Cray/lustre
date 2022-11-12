b3a9bb8c44b48a93010bf0d03fe6590f7355db5b
Shaun Tancheff <shaun.tancheff@hpe.com>
2022-11-12 03:29:42 -0600

    LU-16118 build: Use pde_data() when available
    Linux commit v5.16-11573-g6dfbbae14a7b
       introduce pde_data() and
    Linux commit v5.16-11574-g359745d78351
       remove PDE_DATA()
    
    Use PDE_DATA() when pde_data is not available.
    
    Test-Parameters: trivial
    HPE-bug-id: LUS-11193
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: Ida570462acd466a251adc81a14bc1fbf35d96b00
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48363
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Jian Yu <yujian@whamcloud.com>

--- lustre/include/lprocfs_status.h
+++ lustre/include/lprocfs_status.h
@@ -783,7 +783,7 @@ static int name##_single_open(struct inode *inode, struct file *file)	\
 {									\
 	return single_open(file, name##_seq_show,			\
 			   inode->i_private ? inode->i_private :	\
-					      PDE_DATA(inode));		\
+					      pde_data(inode));		\
 }									\
 static const struct proc_ops name##_fops = {				\
 	PROC_OWNER(THIS_MODULE)						\
@@ -830,7 +830,7 @@ static const struct proc_ops name##_fops = {				\
 	{								\
 		return single_open(file, NULL,				\
 				   inode->i_private ? inode->i_private : \
-				   PDE_DATA(inode));			\
+				   pde_data(inode));			\
 	}								\
 	static const struct proc_ops name##_##type##_fops = {		\
 		.proc_open	= name##_##type##_open,			\
