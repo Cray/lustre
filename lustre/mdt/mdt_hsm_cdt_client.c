9c589ae2fa1d8d3e5d737e8e45e3278c1ac53c9a
Etienne AUJAMES <etienne.aujames@cea.fr>
2022-11-22 19:47:36 +0300

    LUS-11055 tests: check for 2nd restore request
    This test checks if an HSM restore handle exists before taking the
    lock.
    
    WC-bug-id: LU-15132
    Change-Id: I7c0e1e4d7b2f6580e569fb6facf02fd2a1d582d2
    Test-Parameters: testlist=sanity-hsm/12s
    Signed-off-by: Sergey Cheremencev <c17829@cray.com>
    Reviewed-on: https://es-gerrit.dev.cray.com/161588
    Reviewed-by: Nikitas Angelinas <nangelinas@cray.com>
    Reviewed-by: Vitaly Fertman <c17818@cray.com>
    Tested-by: Vitaly Fertman <c17818@cray.com>

--- lustre/mdt/mdt_hsm_cdt_client.c
+++ lustre/mdt/mdt_hsm_cdt_client.c
@@ -372,6 +372,9 @@ static int mdt_hsm_register_hal(struct mdt_thread_info *mti,
 			if (hai->hai_extent.offset != 0)
 				GOTO(out, rc = -EPROTO);
 
+			/* LU-15132 */
+			OBD_RACE(OBD_FAIL_MDS_HSM_RESTORE_RACE);
+
 			rc = cdt_restore_handle_add(mti, cdt, &hai->hai_fid,
 						    &hai->hai_extent);
 			if (rc < 0)
