lustre/lustre-release

commit 23773b32bfe15fccc9c029d722dc1bd5b0144779

LU-11444 ptlrpc: resend may corrupt the data
Late resend if arrives much later than another modification RPC
which has been already handled on this slot, may be still applied
and therefore overrides the last one

Send RPCs from client in increasing order for each tag
and check it on server to check late resend.

A slot can be reused by a client after kill while
the server continue to rely on it.

Add flag for such obsolete requests, here we trust the
client and perform xid check for all in progress requests.

Cray-bug-id: LUS-6272, LUS-7277, LUS-7339
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Change-Id: I97806577cf979f49a75379ffc55947cc3dcd02b1
Reviewed-on: https://review.whamcloud.com/35114
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Alexandr Boyko <c17825@cray.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ptlrpc/service.c
@@ -41,6 +41,7 @@
 #include <lu_object.h>
 #include <uapi/linux/lnet/lnet-types.h>
 #include "ptlrpc_internal.h"
+#include <linux/delay.h>
 
 /* The following are visible and mutable through /sys/module/ptlrpc */
 int test_req_buffer_pressure = 0;
@@ -1024,6 +1025,30 @@ void ptlrpc_server_drop_request(struct ptlrpc_request *req)
 	}
 }
 
+static void ptlrpc_add_exp_list_nolock(struct ptlrpc_request *req,
+				       struct obd_export *export, bool hp)
+{
+	__u16 tag = lustre_msg_get_tag(req->rq_reqmsg);
+
+	if (hp)
+		list_add(&req->rq_exp_list, &export->exp_hp_rpcs);
+	else
+		list_add(&req->rq_exp_list, &export->exp_reg_rpcs);
+	if (tag && export->exp_used_slots)
+		set_bit(tag - 1, export->exp_used_slots);
+}
+
+static void ptlrpc_del_exp_list(struct ptlrpc_request *req)
+{
+	__u16 tag = lustre_msg_get_tag(req->rq_reqmsg);
+
+	spin_lock(&req->rq_export->exp_rpc_lock);
+	list_del_init(&req->rq_exp_list);
+	if (tag && !req->rq_obsolete && req->rq_export->exp_used_slots)
+		clear_bit(tag - 1, req->rq_export->exp_used_slots);
+	spin_unlock(&req->rq_export->exp_rpc_lock);
+}
+
 /** Change request export and move hp request from old export to new */
 void ptlrpc_request_change_export(struct ptlrpc_request *req,
 				  struct obd_export *export)
@@ -1031,19 +1056,13 @@ void ptlrpc_request_change_export(struct ptlrpc_request *req,
 	if (req->rq_export != NULL) {
 		LASSERT(!list_empty(&req->rq_exp_list));
 		/* remove rq_exp_list from last export */
-		spin_lock(&req->rq_export->exp_rpc_lock);
-		list_del_init(&req->rq_exp_list);
-		spin_unlock(&req->rq_export->exp_rpc_lock);
-		/*
-		 * export has one reference already, so it`s safe to
+		ptlrpc_del_exp_list(req);
+		/* export has one reference already, so it's safe to
 		 * add req to export queue here and get another
 		 * reference for request later
 		 */
 		spin_lock(&export->exp_rpc_lock);
-		if (req->rq_ops != NULL) /* hp request */
-			list_add(&req->rq_exp_list, &export->exp_hp_rpcs);
-		else
-			list_add(&req->rq_exp_list, &export->exp_reg_rpcs);
+		ptlrpc_add_exp_list_nolock(req, export, req->rq_ops != NULL);
 		spin_unlock(&export->exp_rpc_lock);
 
 		class_export_rpc_dec(req->rq_export);
@@ -1664,6 +1683,47 @@ found:
 	return tmp;
 }
 
+#ifdef HAVE_SERVER_SUPPORT
+static void ptlrpc_server_mark_obsolete(struct ptlrpc_request *req)
+{
+	req->rq_obsolete = 1;
+}
+
+static void
+ptlrpc_server_mark_in_progress_obsolete(struct ptlrpc_request *req)
+{
+	struct ptlrpc_request	*tmp = NULL;
+	__u16			tag;
+
+	if (!tgt_is_increasing_xid_client(req->rq_export) ||
+	    req->rq_export->exp_used_slots == NULL)
+		return;
+
+	tag = lustre_msg_get_tag(req->rq_reqmsg);
+	if (tag == 0)
+		return;
+
+	if (!test_bit(tag - 1, req->rq_export->exp_used_slots))
+		return;
+
+	/* This list should not be longer than max_requests in
+	 * flights on the client, so it is not all that long.
+	 * Also we only hit this codepath in case of a resent
+	 * request which makes it even more rarely hit */
+	list_for_each_entry(tmp, &req->rq_export->exp_reg_rpcs, rq_exp_list) {
+		if (tag == lustre_msg_get_tag(tmp->rq_reqmsg) &&
+		    req->rq_xid > tmp->rq_xid)
+			ptlrpc_server_mark_obsolete(tmp);
+
+	}
+	list_for_each_entry(tmp, &req->rq_export->exp_hp_rpcs, rq_exp_list) {
+		if (tag == lustre_msg_get_tag(tmp->rq_reqmsg) &&
+		    req->rq_xid > tmp->rq_xid)
+			ptlrpc_server_mark_obsolete(tmp);
+	}
+}
+#endif
+
 /**
  * Check if a request should be assigned with a high priority.
  *
@@ -1721,9 +1781,7 @@ static void ptlrpc_server_hpreq_fini(struct ptlrpc_request *req)
 		if (req->rq_ops && req->rq_ops->hpreq_fini)
 			req->rq_ops->hpreq_fini(req);
 
-		spin_lock(&req->rq_export->exp_rpc_lock);
-		list_del_init(&req->rq_exp_list);
-		spin_unlock(&req->rq_export->exp_rpc_lock);
+		ptlrpc_del_exp_list(req);
 	}
 	EXIT;
 }
@@ -1770,7 +1828,7 @@ static int ptlrpc_server_request_add(struct ptlrpc_service_part *svcpt,
 	hp = rc > 0;
 	ptlrpc_nrs_req_initialize(svcpt, req, hp);
 
-	if (req->rq_export != NULL) {
+	while (req->rq_export != NULL) {
 		struct obd_export *exp = req->rq_export;
 
 		/*
@@ -1778,7 +1836,18 @@ static int ptlrpc_server_request_add(struct ptlrpc_service_part *svcpt,
 		 * atomically
 		 */
 		spin_lock_bh(&exp->exp_rpc_lock);
+#ifdef HAVE_SERVER_SUPPORT
+		ptlrpc_server_mark_in_progress_obsolete(req);
+#endif
 		orig = ptlrpc_server_check_resend_in_progress(req);
+		if (orig && OBD_FAIL_PRECHECK(OBD_FAIL_PTLRPC_RESEND_RACE)) {
+			spin_unlock_bh(&exp->exp_rpc_lock);
+
+			OBD_RACE(OBD_FAIL_PTLRPC_RESEND_RACE);
+			msleep(4 * MSEC_PER_SEC);
+			continue;
+		}
+
 		if (orig && likely(atomic_inc_not_zero(&orig->rq_refcount))) {
 			bool linked;
 
@@ -1801,14 +1870,17 @@ static int ptlrpc_server_request_add(struct ptlrpc_service_part *svcpt,
 				ptlrpc_at_add_timed(orig);
 			ptlrpc_server_drop_request(orig);
 			ptlrpc_nrs_req_finalize(req);
+
+			/* don't mark slot unused for resend in progress */
+			req->rq_obsolete = 1;
+
 			RETURN(-EBUSY);
 		}
 
-		if (hp || req->rq_ops != NULL)
-			list_add(&req->rq_exp_list, &exp->exp_hp_rpcs);
-		else
-			list_add(&req->rq_exp_list, &exp->exp_reg_rpcs);
+		ptlrpc_add_exp_list_nolock(req, exp, hp || req->rq_ops != NULL);
+
 		spin_unlock_bh(&exp->exp_rpc_lock);
+		break;
 	}
 
 	/*

lustre/include/lu_target.h
@@ -455,7 +455,7 @@ void tgt_register_lfsck_query(int (*query)(const struct lu_env *,
 					   struct lfsck_request *,
 					   struct lfsck_reply *,
 					   struct lfsck_query *));
-bool req_can_reconstruct(struct ptlrpc_request *req, struct tg_reply_data *trd);
+int req_can_reconstruct(struct ptlrpc_request *req, struct tg_reply_data *trd);
 
 extern struct tgt_handler tgt_sec_ctx_handlers[];
 extern struct tgt_handler tgt_lfsck_handlers[];
@@ -493,9 +493,10 @@ int tgt_client_new(const struct lu_env *env, struct obd_export *exp);
 int tgt_server_data_update(const struct lu_env *env, struct lu_target *tg,
 			   int sync);
 int tgt_reply_data_init(const struct lu_env *env, struct lu_target *tgt);
-bool tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd);
+int tgt_lookup_reply(struct ptlrpc_request *req, struct tg_reply_data *trd);
 int tgt_add_reply_data(const struct lu_env *env, struct lu_target *tgt,
 		       struct tg_export_data *ted, struct tg_reply_data *trd,
+		       struct ptlrpc_request *req,
 		       struct thandle *th, bool update_lrd_file);
 struct tg_reply_data *tgt_lookup_reply_by_xid(struct tg_export_data *ted,
 					       __u64 xid);

lustre/include/lustre_export.h
@@ -307,7 +307,8 @@ struct obd_export {
 	/** highest XID received by export client that has no
 	 * unreceived lower-numbered XID
 	 */
-	__u64			  exp_last_xid;
+	__u64			exp_last_xid;
+	long			*exp_used_slots;
 };
 
 #define exp_target_data u.eu_target_data

lustre/include/lustre_mdc.h
@@ -73,6 +73,7 @@ static inline void mdc_get_mod_rpc_slot(struct ptlrpc_request *req,
 	opc = lustre_msg_get_opc(req->rq_reqmsg);
 	tag = obd_get_mod_rpc_slot(cli, opc, it);
 	lustre_msg_set_tag(req->rq_reqmsg, tag);
+	ptlrpc_reassign_next_xid(req);
 }
 
 static inline void mdc_put_mod_rpc_slot(struct ptlrpc_request *req,

lustre/include/lustre_net.h
@@ -999,7 +999,8 @@ struct ptlrpc_request {
 	unsigned int
 		rq_hp:1,		/**< high priority RPC */
 		rq_at_linked:1,		/**< link into service's srv_at_array */
-		rq_packed_final:1;	/**< packed final reply */
+		rq_packed_final:1,	/**< packed final reply */
+		rq_obsolete:1;		/* aborted by a signal on a client */
 	/** @} */
 
 	/** one of RQ_PHASE_* */
@@ -2169,6 +2170,7 @@ void ptlrpc_retain_replayable_request(struct ptlrpc_request *req,
 __u64 ptlrpc_next_xid(void);
 __u64 ptlrpc_sample_next_xid(void);
 __u64 ptlrpc_req_xid(struct ptlrpc_request *request);
+void ptlrpc_reassign_next_xid(struct ptlrpc_request *req);
 
 /* Set of routines to run a function in ptlrpcd context */
 void *ptlrpcd_alloc_work(struct obd_import *imp,

lustre/include/obd_support.h
@@ -450,6 +450,7 @@ extern char obd_jobid_var[];
 #define OBD_FAIL_PTLRPC_LONG_BOTH_UNLINK 0x51c
 #define OBD_FAIL_PTLRPC_CLIENT_BULK_CB3  0x520
 #define OBD_FAIL_PTLRPC_BULK_ATTACH      0x521
+#define OBD_FAIL_PTLRPC_RESEND_RACE	 0x525
 #define OBD_FAIL_PTLRPC_ROUND_XID	 0x530
 #define OBD_FAIL_PTLRPC_CONNECT_RACE	 0x531
 

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -896,6 +896,7 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_FLR |\
 				OBD_CONNECT2_LOCK_CONVERT | \
 				OBD_CONNECT2_ARCHIVE_ID_ARRAY | \
+				OBD_CONNECT2_INC_XID | \
 				OBD_CONNECT2_SELINUX_POLICY | \
 				OBD_CONNECT2_LSOM | \
 				OBD_CONNECT2_ASYNC_DISCARD | \
@@ -920,7 +921,7 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT_GRANT_PARAM | \
 				OBD_CONNECT_SHORTIO | OBD_CONNECT_FLAGS2)
 
-#define OST_CONNECT_SUPPORTED2 OBD_CONNECT2_LOCKAHEAD
+#define OST_CONNECT_SUPPORTED2 (OBD_CONNECT2_LOCKAHEAD | OBD_CONNECT2_INC_XID)
 
 #define ECHO_CONNECT_SUPPORTED (OBD_CONNECT_FID)
 #define ECHO_CONNECT_SUPPORTED2 0

lustre/llite/llite_lib.c
@@ -255,6 +255,7 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 				   OBD_CONNECT2_FLR |
 				   OBD_CONNECT2_LOCK_CONVERT |
 				   OBD_CONNECT2_ARCHIVE_ID_ARRAY |
+				   OBD_CONNECT2_INC_XID |
 				   OBD_CONNECT2_LSOM |
 				   OBD_CONNECT2_ASYNC_DISCARD |
 				   OBD_CONNECT2_PCC;
@@ -464,7 +465,8 @@ static int client_common_fill_super(struct super_block *sb, char *md, char *dt,
 	data->ocd_connect_flags |= OBD_CONNECT_LOCKAHEAD_OLD;
 #endif
 
-	data->ocd_connect_flags2 = OBD_CONNECT2_LOCKAHEAD;
+	data->ocd_connect_flags2 = OBD_CONNECT2_LOCKAHEAD |
+				   OBD_CONNECT2_INC_XID;
 
 	if (!OBD_FAIL_CHECK(OBD_FAIL_OSC_CONNECT_GRANT_PARAM))
 		data->ocd_connect_flags |= OBD_CONNECT_GRANT_PARAM;

Cray/lustre

commit afb24dac2e51529fe1e9e5a89f7860ba53b2cd12

LUS-11368 ptlrpc: racy rq_obsolete bit modification
Racy bit modification causes assertion failure in
ptlrpc_at_remove_timed():
ASSERTION( !list_empty(&req->rq_srv.sr_timed_list) )

rq_obsolete should be modified under rq_lock

Change-Id: I8396bb08f6ae7f2028f09a2b95f8e679acc42baa
Fixes:  23773b32bf ("LU-11444 ptlrpc: resend may corrupt the data")
Signed-off-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/161597
Tested-by: Jenkins Build User <nssreleng@cray.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>

lustre/ptlrpc/service.c
@@ -1653,7 +1653,9 @@ found:
 #ifdef HAVE_SERVER_SUPPORT
 static void ptlrpc_server_mark_obsolete(struct ptlrpc_request *req)
 {
+	spin_lock(&req->rq_lock);
 	req->rq_obsolete = 1;
+	spin_unlock(&req->rq_lock);
 }
 
 static void
@@ -1840,7 +1842,9 @@ static int ptlrpc_server_request_add(struct ptlrpc_service_part *svcpt,
 			ptlrpc_nrs_req_finalize(req);
 
 			/* don't mark slot unused for resend in progress */
+			spin_lock(&req->rq_lock);
 			req->rq_obsolete = 1;
+			spin_unlock(&req->rq_lock);
 
 			RETURN(-EBUSY);
 		}