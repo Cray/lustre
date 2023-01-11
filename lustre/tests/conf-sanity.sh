b550b381cbfe078034ec037eedef218c7e1aabed
Andreas Dilger <adilger@whamcloud.com>
2023-01-11 12:02:04 -0700

    LU-16456 tests: skip conf-sanity test_129/132 in interop
    test_129 was added in commit v2_14_56-40-gcefabee52
    test_132 was added in commit v2_14_56-96-ge26d7cc39
    They should be skipped for older MDS versions.
    
    Lustre-change: https://review.whamcloud.com/49601
    Lustre-commit: 7e566c6a1f9d5324718ebc7149153f3272363b9c
    
    Test-Parameters: trivial testlist=conf-sanity env=ONLY=122-133 serverversion=2.14.0
    Fixes: cefabee52 ("LU-15112 mgc: do not ignore target registration failure")
    Fixes: e26d7cc399 ("LU-14399 hsm: process hsm_actions in coordinator")
    Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
    Change-Id: If1e276c816ecf2f30dc970f9b5afe85d722540e5
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/49602
    Reviewed-by: Sarah Liu <sarah@whamcloud.com>
    Reviewed-by: Arshad Hussain <arshad.hussain@aeoncomputing.com>
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>

--- lustre/tests/conf-sanity.sh
+++ lustre/tests/conf-sanity.sh
@@ -9816,6 +9816,8 @@ run_test 128 "Force using remote logs with --nolocallogs"
 
 test_129()
 {
+	(( MDS1_VERSION >= $(version_code 2.14.57) )) ||
+		skip "Need MDS version at least 2.14.57"
 	stopall
 	start_mgsmds || error "MGS/MDS start failed"
 	format_ost 1
@@ -9894,6 +9896,8 @@ test_132() {
 	local err_cnt
 	local err_cnt2
 
+	(( MDS1_VERSION >= $(version_code 2.14.57) )) ||
+		skip "Need MDS version at least 2.14.57"
 	reformat
 	combined_mgs_mds || start_mgs || error "unable to start MGS"
 	start_mdt 1 || error "unable to start mdt1"
