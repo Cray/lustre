0762fa4692d8b4af359865e70bb6835285ae0a3e
Hongchao Zhang <hongchao@whamcloud.com>
2023-01-15 21:21:09 -0500

    LU-15880 quota: fix insane grant quota
    Fix the insane grant value in quota master/slave index,
    the logs often contain the content similar to the following,
    
    LustreError: 39815:0:(qmt_handler.c:527:qmt_dqacq0())
    $$$ Release too much! uuid:work-MDT0000-lwp-MDT0002_UUID
    release:18446744070274413724 granted:18446744070291193856,
    total:4118877744 qmt:work-QMT0000 pool:0-dt id:40212 enforced:1
    hard:128849018880 soft:12884901888 granted:4118877744 time:0
    qunit: 16777216 edquot:0 may_rel:0 revoke:0 default:no
    
    It could be caused by chgrp, which reserves quota before changing
    GID for some file at MDT, then release the reserved quota after
    the file GID has been changed on the corresponding OST, (this issue
    is tracked at LU-5152 and LU-11303)
    
    In some case, some quota could be released even the quota was not
    reserved correctly, which cause the grant quota to be some negative
    value, which is regarded as some insane big value because the type
    of grant is "__u64", then the normal grant release will fail and
    the grant field of some quota ID in the quota file (both at QMT and
    QSD) contain insane value, but can't be reset correctly.
    
    This patch resets the affected quota by clear the quota limits and
    grant, and the grant will be reported by each QSD when the quota ID
    is enforced again, then rebuild the grant at QMT.
    
    Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
    Change-Id: I083afa3b6648db5a1ccca0235667da022ff27e65
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48981
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162556
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

--- lustre/mdt/mdt_handler.c
+++ lustre/mdt/mdt_handler.c
@@ -3237,6 +3237,7 @@ static int mdt_quotactl(struct tgt_session_info *tsi)
 	case LUSTRE_Q_SETINFOPOOL:
 	case LUSTRE_Q_SETDEFAULT_POOL:
 	case LUSTRE_Q_DELETEQID:
+	case LUSTRE_Q_RESETQID:
 		if (!nodemap_can_setquota(nodemap, oqctl->qc_type,
 					  oqctl->qc_id))
 			GOTO(out_nodemap, rc = -EPERM);
@@ -3308,6 +3309,7 @@ static int mdt_quotactl(struct tgt_session_info *tsi)
 	case LUSTRE_Q_SETDEFAULT_POOL:
 	case LUSTRE_Q_GETDEFAULT_POOL:
 	case LUSTRE_Q_DELETEQID:
+	case LUSTRE_Q_RESETQID:
 		/* forward quotactl request to QMT */
 		rc = qmt_hdls.qmth_quotactl(tsi->tsi_env, qmt, oqctl);
 		break;
