ef6e254f2cd6d57a61f4b500d3ecb2e308d06bc7
Chris Horn <chris.horn@hpe.com>
2023-02-21 10:37:56 -0600

    LU-16575 lnet: memory leak in copy_ioc_udsp_descr
    copy_ioc_udsp_descr() doesn't correctly handle the case where a
    net number was not specified. In this case, there isn't any net
    number range that needs to be copied into the udsp descriptor.
    
    Test-Parameters: trivial testlist=sanity-lnet env=ONLY=400
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: Ica900e2b0ec816237a8303d1d9e07cc1f6c5a652
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/50081
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
    Reviewed-by: Frank Sehr <fsehr@whamcloud.com>
    Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/tests/sanity-lnet.sh
+++ lustre/tests/sanity-lnet.sh
@@ -2554,6 +2554,17 @@ test_302() {
 }
 run_test 302 "TAG_RX_OK is possible after TX_FAIL"
 
+test_400() {
+	reinit_dlc || return $?
+
+	do_lnetctl udsp add --src tcp --priority 0 ||
+		error "Failed to add udsp rule"
+	do_lnetctl udsp del --idx 0 ||
+		error "Failed to del udsp rule"
+	unload_modules
+}
+run_test 400 "Check for udsp add/delete net rule without net num"
+
 complete $SECONDS
 
 cleanup_testsuite
