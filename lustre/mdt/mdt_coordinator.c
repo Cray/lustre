f04fbbc3384582e91b48cefc508f539ee53ec7c2
Nikitas Angelinas <nikitas.angelinas@hpe.com>
2023-04-11 13:01:52 -0700

    LUS-11569 hsm: yield cdt_lock while updating actions llog records
    The CDT thread holds cdt_lock in write mode while scheduling requests or
    updating records in the HSM actions llog; if there is a large number of
    requests, other MDT threads that also need to take the lock in order to
    e.g. mark an action as complete, add a new action, or print the contents
    of the llog can be starved and get blocked. Yield cdt_lock whenever a
    certain number of requests have been handled to avoid this. The value
    for the number of requests is based on field reports of the issue and
    some experimentation; it may not be optimal but should hopefully help
    address the issue.
    
    Change-Id: Id607e1ac9720a954ecda1fb38b4b22ef491c4549
    Signed-off-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162356
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

--- lustre/mdt/mdt_coordinator.c
+++ lustre/mdt/mdt_coordinator.c
@@ -146,6 +146,7 @@ struct hsm_scan_data {
 	int			 hsd_action_count;
 	int			 hsd_request_len; /* array alloc len */
 	int			 hsd_request_count; /* array used count */
+	int			 hsd_requests_handled;
 	struct hsm_scan_request	*hsd_request;
 };
 
@@ -430,6 +431,16 @@ static int mdt_coordinator_cb(const struct lu_env *env,
 	struct coordinator *cdt = &mdt->mdt_coordinator;
 	ENTRY;
 
+	/* Allow other threads that need cdt_lock to make
+	 * progress, when there is a large number of updates
+	 */
+	if (++hsd->hsd_requests_handled % CDT_BATCH_SIZE == 0 &&
+	     rwsem_is_contended(&cdt->cdt_lock) && need_resched()) {
+		up_write(&cdt->cdt_lock);
+		cond_resched();
+		down_write(&cdt->cdt_lock);
+	}
+
 	larr = (struct llog_agent_req_rec *)hdr;
 	dump_llog_agent_req_rec("mdt_coordinator_cb(): ", larr);
 	switch (larr->arr_status) {
@@ -682,6 +693,7 @@ static int mdt_coordinator(void *data)
 
 		hsd.hsd_action_count = 0;
 		hsd.hsd_request_count = 0;
+		hsd.hsd_requests_handled = 0;
 		hsd.hsd_one_restore = false;
 
 		rc = cdt_llog_process(mti->mti_env, mdt, mdt_coordinator_cb,
