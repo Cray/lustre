4f96c388b71ed98ae114f4dd205d37cced086b04
Chris Horn <chris.horn@hpe.com>
2023-04-13 09:36:32 -0600

    LUS-11572 tests: Check TAG_RX_OK in TN_STATE_FAIL
    Verify that getting TAG_RX_OK while transaciton is in TN_STATE_FAIL
    does not trigger LBUG.
    
    Also updates test_301 to correct a typo and cleanup the test file and
    directory used by the test.
    
    Test-Parameters: trivial testlist=sanity-lnet/301
    Test-Parameters: testlist=sanity-lnet/302
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: I4ae5e7388f034887477714bb181faae7768652cb
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162357
    Reviewed-by: Ian Ziemba <ian.ziemba@hpe.com>
    Reviewed-by: Ron Gredvig <rgredvig@cray.com>
    Tested-by: Jenkins Build User <nssreleng@cray.com>

--- lnet/klnds/kfilnd/kfilnd.h
+++ lnet/klnds/kfilnd/kfilnd.h
@@ -100,6 +100,7 @@
 #define CFS_KFI_FAIL_RECV 0xF112
 #define CFS_KFI_FAIL_MSG_UNPACK 0xF113
 #define CFS_KFI_FAIL_MSG_TYPE 0xF114
+#define CFS_KFI_FAIL_WAIT_SEND_COMP3 0xF117
 
 /* Maximum number of transaction keys supported. */
 #define KFILND_EP_KEY_BITS 16U
