Cray/lustre

commit ec4e1a6d403db625b1186bd205fa4c95da8cfd01

LUS-10683 osd-ldiskfs: lets have ea inode check global
EA inode is ldiskfs internal object and this should be
excluded at any osd access.
Panic fixed by this change because iterator will return
an error when tries to access to the ea inode.

osd_ea_fid_get()) Process leaving (rc=18446744073709551614 : -2
osd_it_ea_rec()) Process leaving (rc=18446744073709551614 : -2)
osd_it_ea_next()) Process entered

Change-Id: I768be5fac9ea420e72bb697e9411d3cfb7a0ccfc
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-on: https://es-gerrit.dev.cray.com/159977
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -517,11 +517,17 @@ struct inode *osd_iget(struct osd_thread_info *info, struct osd_device *dev,
 		iput(inode);
 		inode = ERR_PTR(-ESTALE);
 	} else if (is_bad_inode(inode)) {
-		rc = -ENOENT;
 		CWARN("%s: bad inode: ino = %u: rc = %d\n",
-		      osd_dev2name(dev), id->oii_ino, rc);
+		      osd_dev2name(dev), id->oii_ino, -ENOENT);
 		iput(inode);
-		inode = ERR_PTR(rc);
+		inode = ERR_PTR(-ENOENT);
+	} else  if (osd_is_ea_inode(inode)) {
+		/**
+		 EA inode is internal ldiskfs object, should don't visible on osd
+		 */
+		CDEBUG(D_INODE, "EA inode: ino = %u\n", id->oii_ino);
+		iput(inode);
+		inode = ERR_PTR(-ENOENT);
 	} else if ((rc = osd_attach_jinode(inode))) {
 		iput(inode);
 		inode = ERR_PTR(rc);
@@ -634,7 +640,7 @@ static struct inode *osd_iget_check(struct osd_thread_info *info,
 	 */
 
 again:
-	inode = osd_ldiskfs_iget(osd_sb(dev), id->oii_ino);
+	inode = osd_iget(info, dev, id);
 	if (IS_ERR(inode)) {
 		rc = PTR_ERR(inode);
 		if (!trusted && (rc == -ENOENT || rc == -ESTALE))
@@ -644,40 +650,6 @@ again:
 		       PFID(fid), id->oii_ino, rc);
 		GOTO(put, rc);
 	}
-
-	if (is_bad_inode(inode)) {
-		rc = -ENOENT;
-		if (!trusted)
-			goto check_oi;
-
-		CDEBUG(D_INODE, "bad inode for FID: "DFID", ino = %u\n",
-		       PFID(fid), id->oii_ino);
-		GOTO(put, rc);
-	}
-
-	if (id->oii_gen != OSD_OII_NOGEN &&
-	    inode->i_generation != id->oii_gen) {
-		rc = -ESTALE;
-		if (!trusted)
-			goto check_oi;
-
-		CDEBUG(D_INODE, "unmatched inode for FID: "DFID", ino = %u, "
-		       "oii_gen = %u, i_generation = %u\n", PFID(fid),
-		       id->oii_ino, id->oii_gen, inode->i_generation);
-		GOTO(put, rc);
-	}
-
-	if (inode->i_nlink == 0) {
-		rc = -ENOENT;
-		if (!trusted)
-			goto check_oi;
-
-		CDEBUG(D_INODE, "stale inode for FID: "DFID", ino = %u\n",
-		       PFID(fid), id->oii_ino);
-		GOTO(put, rc);
-	}
-
-	ldiskfs_clear_inode_state(inode, LDISKFS_STATE_LUSTRE_DESTROY);
 
 check_oi:
 	if (rc != 0) {
@@ -752,19 +724,6 @@ check_oi:
 			rc = -ENOENT;
 		else
 			rc = -EREMCHG;
-	} else {
-		if (id->oii_gen == OSD_OII_NOGEN)
-			osd_id_gen(id, inode->i_ino, inode->i_generation);
-
-		/*
-		 * Do not update file c/mtime in ldiskfs.
-		 * NB: we don't have any lock to protect this because we don't
-		 * have reference on osd_object now, but contention with
-		 * another lookup + attr_set can't happen in the tiny window
-		 * between if (...) and set S_NOCMTIME.
-		 */
-		if (!(inode->i_flags & S_NOCMTIME))
-			inode->i_flags |= S_NOCMTIME;
 	}
 
 	GOTO(put, rc);

lustre/osd-ldiskfs/osd_compat.c
@@ -726,15 +726,6 @@ static int osd_obj_update_entry(struct osd_thread_info *info,
 		GOTO(out, rc);
 	}
 
-	/*
-	 * The EA inode should NOT be in OI, old OI scrub may added
-	 * such OI mapping by wrong, replace it.
-	 */
-	if (unlikely(osd_is_ea_inode(inode))) {
-		iput(inode);
-		goto update;
-	}
-
 	rc = osd_get_lma(info, inode, dentry, &info->oti_ost_attrs);
 	if (rc == -ENODATA) {
 		rc = osd_get_idif(info, inode, dentry, oi_fid);

lustre/osd-ldiskfs/osd_oi.c
@@ -742,14 +742,6 @@ int osd_oi_insert(struct osd_thread_info *info, struct osd_device *osd,
 			return rc;
 		}
 
-		/* The EA inode should NOT be in OI, old OI scrub may added
-		 * such OI mapping by wrong, replace it.
-		 */
-		if (unlikely(osd_is_ea_inode(inode))) {
-			iput(inode);
-			goto update;
-		}
-
 		rc = osd_get_lma(info, inode, &info->oti_obj_dentry,
 				 &info->oti_ost_attrs);
 		iput(inode);

lustre/osd-ldiskfs/osd_scrub.c
@@ -290,10 +290,6 @@ osd_scrub_check_update(struct osd_thread_info *info, struct osd_device *dev,
 			GOTO(out, rc);
 		}
 
-		/* The inode has been reused as EA inode, ignore it. */
-		if (unlikely(osd_is_ea_inode(inode)))
-			GOTO(out, rc = 0);
-
 		sf->sf_flags |= SF_UPGRADE;
 		sf->sf_internal_flags &= ~SIF_NO_HANDLE_OLD_FID;
 		dev->od_check_ff = 1;
@@ -333,10 +329,6 @@ iget:
 					rc = 0;
 				GOTO(out, rc);
 			}
-
-			/* The inode has been reused as EA inode, ignore it. */
-			if (unlikely(osd_is_ea_inode(inode)))
-				GOTO(out, rc = 0);
 		}
 
 		switch (val) {
@@ -627,10 +619,6 @@ static int osd_iit_iget(struct osd_thread_info *info, struct osd_device *dev,
 
 	if (dev->od_is_ost && S_ISREG(inode->i_mode) && inode->i_nlink > 1)
 		dev->od_scrub.os_scrub.os_has_ml_file = 1;
-
-	/* It is an EA inode, no OI mapping for it, skip it. */
-	if (osd_is_ea_inode(inode))
-		GOTO(put, rc = SCRUB_NEXT_CONTINUE);
 
 	if (scrub &&
 	    ldiskfs_test_inode_state(inode, LDISKFS_STATE_LUSTRE_NOSCRUB)) {

commit f535c990f4ec8125b0850e24eb28a76029f39316

LUS-11540 osd: checking OI with errors
osd_iget_check() function have no ability to check
OI when osd_iget() returns error, because inode is
lost during error. Let's return old logic.

Fixes: ec4e1a6d40 ("LUS-10683 osd-ldiskfs: lets have ea inode check global")
Signed-off-by: Alexander Boyko <alexander.boyko@hpe.com>
Change-Id: I82bce8e2e20b794c5e72c9466a272a3603699130
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162338
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Jenkins Build User <nssreleng@cray.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -484,10 +484,11 @@ int osd_get_lma(struct osd_thread_info *info, struct inode *inode,
 /*
  * retrieve object from backend ext fs.
  **/
-struct inode *osd_iget(struct osd_thread_info *info, struct osd_device *dev,
-		       struct osd_inode_id *id)
+static struct inode *osd_iget2(struct osd_thread_info *info,
+			       struct osd_device *dev, struct osd_inode_id *id,
+			       int *err)
 {
-	int rc;
+	int rc = 0;
 	struct inode *inode = NULL;
 
 	/*
@@ -496,6 +497,7 @@ struct inode *osd_iget(struct osd_thread_info *info, struct osd_device *dev,
 	 * osd_dirent_check_repair() breaks this
 	 */
 	 /* LASSERT(current->journal_info == NULL); */
+	LASSERT(err != NULL);
 
 	inode = osd_ldiskfs_iget(osd_sb(dev), id->oii_ino);
 	if (IS_ERR(inode)) {
@@ -506,31 +508,26 @@ struct inode *osd_iget(struct osd_thread_info *info, struct osd_device *dev,
 		CDEBUG(D_INODE, "unmatched inode: ino = %u, oii_gen = %u, "
 		       "i_generation = %u\n",
 		       id->oii_ino, id->oii_gen, inode->i_generation);
-		iput(inode);
-		inode = ERR_PTR(-ESTALE);
+		rc = -ESTALE;
 	} else if (inode->i_nlink == 0) {
 		/*
 		 * due to parallel readdir and unlink,
 		 * we can have dead inode here.
 		 */
 		CDEBUG(D_INODE, "stale inode: ino = %u\n", id->oii_ino);
-		iput(inode);
-		inode = ERR_PTR(-ESTALE);
+		rc = -ESTALE;
 	} else if (is_bad_inode(inode)) {
 		CWARN("%s: bad inode: ino = %u: rc = %d\n",
 		      osd_dev2name(dev), id->oii_ino, -ENOENT);
-		iput(inode);
-		inode = ERR_PTR(-ENOENT);
+		rc = -ENOENT;
 	} else  if (osd_is_ea_inode(inode)) {
 		/**
-		 EA inode is internal ldiskfs object, should don't visible on osd
+		 * EA is internal ldiskfs object, shouldn't be visible on osd
 		 */
 		CDEBUG(D_INODE, "EA inode: ino = %u\n", id->oii_ino);
-		iput(inode);
-		inode = ERR_PTR(-ENOENT);
+		rc = -ENOENT;
 	} else if ((rc = osd_attach_jinode(inode))) {
-		iput(inode);
-		inode = ERR_PTR(rc);
+		CDEBUG(D_INODE, "jbd: ino = %u rc = %d\n", id->oii_ino, rc);
 	} else {
 		ldiskfs_clear_inode_state(inode, LDISKFS_STATE_LUSTRE_DESTROY);
 		if (id->oii_gen == OSD_OII_NOGEN)
@@ -546,6 +543,25 @@ struct inode *osd_iget(struct osd_thread_info *info, struct osd_device *dev,
 		if (!(inode->i_flags & S_NOCMTIME))
 			inode->i_flags |= S_NOCMTIME;
 	}
+
+	*err = rc;
+
+	return inode;
+}
+
+struct inode *osd_iget(struct osd_thread_info *info, struct osd_device *dev,
+		       struct osd_inode_id *id)
+{
+	struct inode *inode;
+	int rc = 0;
+
+	inode = osd_iget2(info, dev, id, &rc);
+
+	if (rc) {
+		iput (inode);
+		inode = ERR_PTR(rc);
+	}
+
 	return inode;
 }
 
@@ -640,9 +656,8 @@ static struct inode *osd_iget_check(struct osd_thread_info *info,
 	 */
 
 again:
-	inode = osd_iget(info, dev, id);
-	if (IS_ERR(inode)) {
-		rc = PTR_ERR(inode);
+	inode = osd_iget2(info, dev, id, &rc);
+	if (rc) {
 		if (!trusted && (rc == -ENOENT || rc == -ESTALE))
 			goto check_oi;
 