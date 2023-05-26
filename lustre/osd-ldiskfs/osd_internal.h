c9164adda8433151e1396ded5dce5431fe7085bd
Alexey Lyashkov <alexey.lyashkov@hpe.com>
2023-05-26 13:15:36 +0300

    LU-16847 ldiskfs: ->fiemap replaced with ldiskfs_map_blocks.
    lets avoid hacks with data copy from fiemap buffer to kernel
    space by using an ldiskfs_map_blocks directly.
    It might code lots clear.
    
    Fixes: 5cd5a49c7213 ("LU-16321 osd: Allow fiemap on kernel buffers")
    HPe-bug-id: LUS-11645
    Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
    Change-Id: I1c527ce653b8943801e5bf56fd172a5f05e22dfc
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/51146
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>

--- lustre/osd-ldiskfs/osd_internal.h
+++ lustre/osd-ldiskfs/osd_internal.h
@@ -611,6 +611,8 @@ struct osd_iobuf {
 };
 
 #define osd_dirty_inode(inode, flag)  (inode)->i_sb->s_op->dirty_inode((inode), flag)
+#define osd_i_blocks(inode, size) ((size) >> (inode)->i_blkbits)
+
 
 #ifndef HAVE_ALLOC_FILE_PSEUDO
 
