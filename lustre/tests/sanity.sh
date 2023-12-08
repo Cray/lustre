ba1773e6c48c371ea284550865bcde840012e11d
Andrew Perepechko <andrew.perepechko@hpe.com>
2023-12-08 20:42:19 +0300

    LUS-11937 tests: tolerate fresh page cache pages after truncate
    Added a patch to verify that write vs truncate race is
    properly handled.
    
    Change-Id: I1e449ad24dad3a485d7ce1d2e4cc6922854c8f06
    Signed-off-by: Andrew Perepechko <andrew.perepechko@hpe.com>
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162563/
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Reviewed-by: Elena Gryaznova <elena.gryaznova@hpe.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

--- lustre/tests/sanity.sh
+++ lustre/tests/sanity.sh
@@ -27443,6 +27443,24 @@ test_434() {
 }
 run_test 434 "Client should not send RPCs for security.selinux with SElinux disabled"
 
+test_435() {
+	local pid1
+	local pid2
+	mkdir -p $DIR/$tdir
+	multiop $DIR/$tdir/$tfile.1 O_w1 & pid1=$!
+	multiop $DIR/$tdir/$tfile.1 O_w1 & pid2=$!
+	sleep 1
+	touch $DIR/$tdir/$tfile.2
+	$LFS swap_layouts -n $DIR/$tdir/$tfile.1 $DIR/$tdir/$tfile.2
+	$LCTL set_param fail_loc=0x1430
+	kill -USR1 $pid1
+	sleep 1
+	cat /proc/$pid/stack
+	kill -USR1 $pid2
+	wait
+}
+run_test 435 "truncate vs read/write should not panic"
+
 test_440() {
 	local file=$DIR/$tdir/$tfile
 
