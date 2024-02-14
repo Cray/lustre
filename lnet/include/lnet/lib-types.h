lustre/lustre-release

commit f792297212387c2ff1b1dd6ead1e2d451a375ade

LU-16211 o2iblnd: Avoid NULL md deref
struct lnet_msg::msg_md is NULL when a router is forwarding a
REPLY. ko2iblnd attempts to access this pointer on the receive path.
This causes a panic.

Test-Parameters: trivial
Fixes: 959304eac7 ("LU-15189 lnet: fix memory mapping.")
HPE-bug-id: LUS-11269
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I0c1dbb1e0bcd3c17b278f358755d465f7bbbb2b0
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/48777
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1840,7 +1840,7 @@ kiblnd_reply(struct lnet_ni *ni, struct kib_rx *rx, struct lnet_msg *lntmsg)
 	struct bio_vec *kiov = lntmsg->msg_kiov;
 	unsigned int offset = lntmsg->msg_offset;
 	unsigned int nob = lntmsg->msg_len;
-	struct lnet_libmd *payload_md = lntmsg->msg_md;
+	struct lnet_libmd *msg_md = lntmsg->msg_md;
 	struct kib_tx *tx;
 	int rc;
 
@@ -1851,7 +1851,9 @@ kiblnd_reply(struct lnet_ni *ni, struct kib_rx *rx, struct lnet_msg *lntmsg)
 		goto failed_0;
 	}
 
-	tx->tx_gpu = !!(payload_md->md_flags & LNET_MD_FLAG_GPU);
+
+	tx->tx_gpu = msg_md ? (msg_md->md_flags & LNET_MD_FLAG_GPU) : 0;
+
 	if (nob == 0)
 		rc = 0;
 	else
@@ -1883,8 +1885,8 @@ kiblnd_reply(struct lnet_ni *ni, struct kib_rx *rx, struct lnet_msg *lntmsg)
 		tx->tx_lntmsg[0] = lntmsg;
 	}
 
-        kiblnd_queue_tx(tx, rx->rx_conn);
-        return;
+	kiblnd_queue_tx(tx, rx->rx_conn);
+	return;
 
 
 failed_1:
@@ -1949,7 +1951,7 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 	case IBLND_MSG_PUT_REQ: {
 		struct kib_msg	*txmsg;
 		struct kib_rdma_desc *rd;
-		struct lnet_libmd *payload_md = lntmsg->msg_md;
+		struct lnet_libmd *msg_md = lntmsg->msg_md;
 
 		ibprm_cookie = rxmsg->ibm_u.putreq.ibprm_cookie;
 		if (mlen == 0) {
@@ -1968,7 +1970,8 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 			break;
 		}
 
-		tx->tx_gpu = !!(payload_md->md_flags & LNET_MD_FLAG_GPU);
+		tx->tx_gpu = msg_md ? (msg_md->md_flags & LNET_MD_FLAG_GPU) : 0;
+
 		txmsg = tx->tx_msg;
 		rd = &txmsg->ibm_u.putack.ibpam_rd;
 		rc = kiblnd_setup_rd_kiov(ni, tx, rd,

Cray/lustre

commit ccdee003e4cea42783f5ecb50bd012a1899d24cc

LU-17189 o2ib: assign tx_gpu properly
tx_gpu is not assigned or initialized properly.

Test-Parameters: softib

HPe-bug-id: LUS-12007
Fixes: f792297212 ("LU-16211 o2iblnd: Avoid NULL md deref")
Change-Id: I8fc7dc60817c6985aa19915bedb29d23bbe8ef02
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162633
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lnet/include/lnet/lib-types.h
@@ -247,6 +247,11 @@ struct lnet_libmd {
 #define LNET_MD_FLAG_HANDLING	 BIT(3)
 #define LNET_MD_FLAG_GPU	 BIT(5) /**< Special mapping needs */
 
+static inline bool lnet_md_is_gpu(struct lnet_libmd *md)
+{
+    return (md != NULL) && !!(md->md_flags & LNET_MD_FLAG_GPU);
+}
+
 struct lnet_test_peer {
 	/* info about peers we are trying to fail */
 	struct list_head	tp_list;	/* ln_test_peers */

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -136,6 +136,7 @@ kiblnd_get_idle_tx(struct lnet_ni *ni, lnet_nid_t target)
         LASSERT (tx->tx_lntmsg[1] == NULL);
         LASSERT (tx->tx_nfrags == 0);
 
+	tx->tx_gpu = 0;
 	tx->tx_gaps = false;
 	tx->tx_hstatus = LNET_MSG_STATUS_OK;
 
@@ -762,7 +763,8 @@ static int kiblnd_setup_rd_kiov(struct lnet_ni *ni, struct kib_tx *tx,
 	int max_nkiov;
 	int sg_count = 0;
 
-	CDEBUG(D_NET, "niov %d offset %d nob %d\n", nkiov, offset, nob);
+	CDEBUG(D_NET, "niov %d offset %d nob %d gpu %d\n",
+	       nkiov, offset, nob, tx->tx_gpu);
 
 	LASSERT(nob > 0);
 	LASSERT(nkiov > 0);
@@ -1692,7 +1694,7 @@ kiblnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg)
 		return -ENOMEM;
 	}
 	ibmsg = tx->tx_msg;
-	gpu = msg_md ? (msg_md->md_flags & LNET_MD_FLAG_GPU) : false;
+	gpu = lnet_md_is_gpu(msg_md);
 
 	switch (type) {
 	default:
@@ -1822,8 +1824,7 @@ kiblnd_reply(struct lnet_ni *ni, struct kib_rx *rx, struct lnet_msg *lntmsg)
 		goto failed_0;
 	}
 
-
-	tx->tx_gpu = msg_md ? (msg_md->md_flags & LNET_MD_FLAG_GPU) : 0;
+	tx->tx_gpu = lnet_md_is_gpu(msg_md);
 
 	if (nob == 0)
 		rc = 0;
@@ -1941,7 +1942,7 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 			break;
 		}
 
-		tx->tx_gpu = msg_md ? (msg_md->md_flags & LNET_MD_FLAG_GPU) : 0;
+		tx->tx_gpu = lnet_md_is_gpu(msg_md);
 
 		txmsg = tx->tx_msg;
 		rd = &txmsg->ibm_u.putack.ibpam_rd;

lnet/lnet/lib-move.c
@@ -1646,7 +1646,7 @@ lnet_get_best_ni(struct lnet_net *local_net, struct lnet_ni *best_ni,
 	unsigned int best_dev_prio;
 	int best_ni_fatal;
 	unsigned int dev_idx = UINT_MAX;
-	bool gpu = md ? (md->md_flags & LNET_MD_FLAG_GPU) : false;
+	bool gpu = lnet_md_is_gpu(md);
 
 	if (gpu) {
 		struct page *page = lnet_get_first_page(md, offset);

lnet/lnet/lnet_rdma.c
@@ -208,23 +208,16 @@ EXPORT_SYMBOL(lnet_rdma_unmap_sg);
 bool
 lnet_is_rdma_only_page(struct page *page)
 {
-	bool found = false;
+	bool is_gpu_page = false;
 	struct nvfs_dma_rw_ops *nvfs_ops;
 
-	if (!page)
-		return found;
+	LASSERT(page != NULL);
 
 	nvfs_ops = nvfs_get_ops();
-	if (!nvfs_ops)
-		return found;
-
-	if (!nvfs_ops->nvfs_is_gpu_page(page))
-		goto out;
-
-	found = true;
-
-out:
-	nvfs_put_ops();
-	return found;
+	if (nvfs_ops != NULL) {
+		is_gpu_page = nvfs_ops->nvfs_is_gpu_page(page);
+		nvfs_put_ops();
+	}
+	return is_gpu_page;
 }
 EXPORT_SYMBOL(lnet_is_rdma_only_page);