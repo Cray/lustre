Cray/lustre

commit f035b28e0bb790f7b74a6e06f9af8512c9727f59

LUS-11933 class: link export to timed list in imp_reverse_init
Only exports with reverse import are to be on obd's obd_exports_timed
list.

Signed-off-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>
Change-Id: Iae7c4c97b4434c036fa249405333d4c1055a1f03
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162605
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>

lustre/ldlm/ldlm_lib.c
@@ -1028,6 +1028,10 @@ int rev_import_init(struct obd_export *export)
 	spin_unlock(&export->exp_lock);
 	class_import_put(revimp);
 
+	spin_lock(&obd->obd_dev_lock);
+	list_add_tail(&export->exp_obd_chain_timed,
+		      &obd->obd_exports_timed);
+	spin_unlock(&obd->obd_dev_lock);
 	return 0;
 }
 EXPORT_SYMBOL(rev_import_init);

lustre/obdclass/genops.c
@@ -1080,14 +1080,12 @@ struct obd_export *__class_new_export(struct obd_device *obd,
                 }
         }
 
+	INIT_LIST_HEAD(&export->exp_obd_chain_timed);
 	if (!is_self) {
 		class_incref(obd, "export", export);
-		list_add_tail(&export->exp_obd_chain_timed,
-			      &obd->obd_exports_timed);
 		list_add(&export->exp_obd_chain, &obd->obd_exports);
 		obd->obd_num_exports++;
 	} else {
-		INIT_LIST_HEAD(&export->exp_obd_chain_timed);
 		INIT_LIST_HEAD(&export->exp_obd_chain);
 	}
 	spin_unlock(&obd->obd_dev_lock);

commit 6bdd9fdc0501284576e22768d9bdc484337b17d5

LUS-11933 class: drop exports from the timed list properly
due to a later adding to the timed list after the 1st LUS-11933 patch,
those which are to be removed from the list were left there.

Fixes: f035b28e0b (LUS-11933 class: link export to timed list in imp_reverse_init)
Signed-off-by: Vitaly Fertman <c17818@cray.com>
Change-Id: I9b045ff8a6bc0e3c2737238c093b253341460281
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162838
Tested-by: Jenkins Build User <nssreleng@cray.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Vladimir Saveliev <vladimir.saveliev@hpe.com>

lustre/include/lustre_export.h
@@ -280,7 +280,8 @@ struct obd_export {
 				exp_replay_done:1,
 				/* local client with recovery disabled */
 				exp_no_recovery:1,
-				exp_hashed:1;
+				exp_hashed:1,
+				exp_not_timed:1;
 	/* also protected by exp_lock */
 	enum lustre_sec_part	exp_sp_peer;
 	struct sptlrpc_flavor	exp_flvr;		/* current */

lustre/ldlm/ldlm_lib.c
@@ -882,8 +882,8 @@ static int target_handle_reconnect(struct lustre_handle *conn,
 	int rc = 0;
 
 	ENTRY;
-	hdl = &exp->exp_imp_reverse->imp_remote_handle;
-	if (!exp->exp_connection || !lustre_handle_is_used(hdl)) {
+	if (!exp->exp_connection ||
+	    !lustre_handle_is_used(&exp->exp_imp_reverse->imp_remote_handle)) {
 		conn->cookie = exp->exp_handle.h_cookie;
 		CDEBUG(D_HA,
 		       "connect export for UUID '%s' at %p, cookie %#llx\n",
@@ -892,6 +892,7 @@ static int target_handle_reconnect(struct lustre_handle *conn,
 	}
 
 	target = exp->exp_obd;
+	hdl = &exp->exp_imp_reverse->imp_remote_handle;
 
 	/* Might be a re-connect after a partition. */
 	if (memcmp(&conn->cookie, &hdl->cookie, sizeof(conn->cookie))) {
@@ -1028,10 +1029,12 @@ int rev_import_init(struct obd_export *export)
 	spin_unlock(&export->exp_lock);
 	class_import_put(revimp);
 
-	spin_lock(&obd->obd_dev_lock);
-	list_add_tail(&export->exp_obd_chain_timed,
-		      &obd->obd_exports_timed);
-	spin_unlock(&obd->obd_dev_lock);
+	if (!export->exp_not_timed) {
+		spin_lock(&obd->obd_dev_lock);
+		list_add_tail(&export->exp_obd_chain_timed,
+			      &obd->obd_exports_timed);
+		spin_unlock(&obd->obd_dev_lock);
+	}
 	return 0;
 }
 EXPORT_SYMBOL(rev_import_init);
@@ -1461,8 +1464,13 @@ dont_check_exports:
 		}
 		rc = obd_reconnect(req->rq_svc_thread->t_env,
 				   export, target, &cluuid, data, client_nid);
-		if (rc == 0)
+		if (rc == 0) {
 			reconnected = true;
+			/* In case of recovery, tgt_clients_data_init() created
+			 * the export, exp_imp_reverse is still needed. */
+			if (export->exp_imp_reverse == NULL)
+				rc = rev_import_init(export);
+		}
 	}
 	if (rc)
 		GOTO(out, rc);

lustre/mdt/mdt_handler.c
@@ -6727,9 +6727,9 @@ static int mdt_connect_internal(const struct lu_env *env,
 
 	if (OCD_HAS_FLAG(data, PINGLESS)) {
 		if (ptlrpc_pinger_suppress_pings()) {
-			spin_lock(&exp->exp_obd->obd_dev_lock);
-			list_del_init(&exp->exp_obd_chain_timed);
-			spin_unlock(&exp->exp_obd->obd_dev_lock);
+			spin_lock(&exp->exp_lock);
+			exp->exp_not_timed = 1;
+			spin_unlock(&exp->exp_lock);
 		} else {
 			data->ocd_connect_flags &= ~OBD_CONNECT_PINGLESS;
 		}
@@ -6994,9 +6994,9 @@ out:
 		/* Because we do not want this export to be evicted by pinger,
 		 * let's not add this export to the timed chain list. */
 		if (data->ocd_connect_flags & OBD_CONNECT_MDS_MDS) {
-			spin_lock(&lexp->exp_obd->obd_dev_lock);
-			list_del_init(&lexp->exp_obd_chain_timed);
-			spin_unlock(&lexp->exp_obd->obd_dev_lock);
+			spin_lock(&lexp->exp_lock);
+			lexp->exp_not_timed = 1;
+			spin_unlock(&lexp->exp_lock);
 		}
 	}
 
@@ -7026,6 +7026,12 @@ static int mdt_obd_reconnect(const struct lu_env *env,
 		mdt_export_stats_init(obd, exp, localdata);
 	else
 		nodemap_del_member(exp);
+
+	if (data->ocd_connect_flags & OBD_CONNECT_MDS_MDS) {
+		spin_lock(&exp->exp_lock);
+		exp->exp_not_timed = 1;
+		spin_unlock(&exp->exp_lock);
+	}
 
 	RETURN(rc);
 }

lustre/obdclass/genops.c
@@ -1057,6 +1057,7 @@ struct obd_export *__class_new_export(struct obd_device *obd,
 	spin_lock_init(&export->exp_bl_list_lock);
 	INIT_LIST_HEAD(&export->exp_bl_list);
 	INIT_LIST_HEAD(&export->exp_stale_list);
+	INIT_LIST_HEAD(&export->exp_obd_chain_timed);
 	INIT_WORK(&export->exp_zombie_work, obd_zombie_exp_cull);
 
 	export->exp_sp_peer = LUSTRE_SP_ANY;
@@ -1080,7 +1081,6 @@ struct obd_export *__class_new_export(struct obd_device *obd,
                 }
         }
 
-	INIT_LIST_HEAD(&export->exp_obd_chain_timed);
 	if (!is_self) {
 		class_incref(obd, "export", export);
 		list_add(&export->exp_obd_chain, &obd->obd_exports);

lustre/obdclass/obd_config.c
@@ -633,7 +633,7 @@ int class_attach(struct lustre_cfg *lcfg)
 	}
 
 	obd->obd_self_export = exp;
-	list_del_init(&exp->exp_obd_chain_timed);
+	exp->exp_not_timed = 1;
 	class_export_put(exp);
 
 	rc = class_register_device(obd);

lustre/obdecho/echo_client.c
@@ -3025,9 +3025,9 @@ static int echo_client_setup(const struct lu_env *env,
 	rc = obd_connect(env, &ec->ec_exp, tgt, &echo_uuid, ocd, NULL);
 	if (rc == 0) {
 		/* Turn off pinger because it connects to tgt obd directly. */
-		spin_lock(&tgt->obd_dev_lock);
-		list_del_init(&ec->ec_exp->exp_obd_chain_timed);
-		spin_unlock(&tgt->obd_dev_lock);
+		spin_lock(&ec->ec_exp->exp_lock);
+		ec->ec_exp->exp_not_timed = 1;
+		spin_unlock(&ec->ec_exp->exp_lock);
 	}
 
 	OBD_FREE(ocd, sizeof(*ocd));

lustre/ofd/ofd_obd.c
@@ -264,9 +264,9 @@ static int ofd_parse_connect_data(const struct lu_env *env,
 
 	if (OCD_HAS_FLAG(data, PINGLESS)) {
 		if (ptlrpc_pinger_suppress_pings()) {
-			spin_lock(&exp->exp_obd->obd_dev_lock);
-			list_del_init(&exp->exp_obd_chain_timed);
-			spin_unlock(&exp->exp_obd->obd_dev_lock);
+			spin_lock(&exp->exp_lock);
+			exp->exp_not_timed = 1;
+			spin_unlock(&exp->exp_lock);
 		} else {
 			data->ocd_connect_flags &= ~OBD_CONNECT_PINGLESS;
 		}

lustre/target/tgt_lastrcvd.c
@@ -1733,12 +1733,6 @@ static int tgt_clients_data_init(const struct lu_env *env,
 
 		class_export_put(exp);
 
-		rc = rev_import_init(exp);
-		if (rc != 0) {
-			class_unlink_export(exp);
-			GOTO(err_out, rc);
-		}
-
 		/* Need to check last_rcvd even for duplicated exports. */
 		CDEBUG(D_OTHER, "client at idx %d has last_transno = %llu\n",
 		       cl_idx, last_transno);