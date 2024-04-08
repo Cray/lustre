lustre/lustre-release

commit b07a57027ee5cc1afa82cc4c82be73a2c4894502

LU-15402 ldlm: speedup RD flock enqueue
Scanning of lr_granted can be done until
covering granted RD lock is reached.

Change-Id: I907cff002d9765c5f8496d377eddd5e62795d89c
HPE-bug-id: LUS-10623
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Reviewed-on: https://review.whamcloud.com/45957
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/ldlm/ldlm_flock.c
@@ -355,6 +355,15 @@ reprocess:
 				continue;
 			}
 
+			if (req->l_req_mode == LCK_PR &&
+			    lock->l_granted_mode == LCK_PR &&
+			    lock->l_policy_data.l_flock.start <=
+				req->l_policy_data.l_flock.start &&
+			    lock->l_policy_data.l_flock.end >=
+				req->l_policy_data.l_flock.end) {
+				/* there can't be granted WR lock */
+				break;
+			}
 			/* locks are compatible, overlap doesn't matter */
 			if (lockmode_compat(lock->l_granted_mode, mode))
 				continue;

Cray/lustre

commit bfd795b2f9dacad4c30ce852198df57e74337284

LUS-12243 ldlm: FLOCK ownlocks may be not set
Conflict checking loop should continue until ownlocks is set.
Ownlocks variable is essential for lock merges.

Fixes: b07a57027e (LU-15402 ldlm: speedup RD flock enqueue)
Change-Id: I6fbb27ca4831a752630952d099c562f47869cba4
Signed-off-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162841
Reviewed-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

lustre/ldlm/ldlm_flock.c
@@ -390,6 +390,7 @@ reprocess:
 #ifdef HAVE_SERVER_SUPPORT
 	else {
 		int reprocess_failed = 0;
+		int pr_matched = 0;
 		lockmode_verify(mode);
 
 		/* This loop determines if there are existing locks
@@ -400,8 +401,11 @@ reprocess:
 					  l_res_link);
 
 			if (ldlm_same_flock_owner(lock, req)) {
-				if (!ownlocks)
+				if (!ownlocks) {
 					ownlocks = tmp;
+					if (pr_matched)
+						break;
+				}
 				continue;
 			}
 
@@ -412,7 +416,10 @@ reprocess:
 			    lock->l_policy_data.l_flock.end >=
 				req->l_policy_data.l_flock.end) {
 				/* there can't be granted WR lock */
-				break;
+				if (ownlocks)
+					break;
+				pr_matched = 1;
+				continue;
 			}
 			/* locks are compatible, overlap doesn't matter */
 			if (lockmode_compat(lock->l_granted_mode, mode))