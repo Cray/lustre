Cray/lustre

commit 09c5478b84de46a641faab6c7b3a909a8deac8d7

LUS-12245 lnet: update kfi and o2ib to handle NULL lnet_msg
Handle the handle NULL lnet_msg cases in the lnd_recf() handlers
of kfi and o2ib lnds.

Test-Parameters: softib

Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I793fc3293d68a149054135c6cdcc28d51823a070
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162843
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

lnet/klnds/kfilnd/kfilnd.c
@@ -268,9 +268,12 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 	case KFILND_MSG_IMMEDIATE:
 		nob = offsetof(struct kfilnd_msg, proto.immed.payload[rlen]);
 		if (nob > tn->tn_rx_msg.length) {
+			char *nid = "unknown"
+
+			if (msg)
+				nid = libcfs_nidstr(&msg->msg_hdr.src_nid)
 			CERROR("Immediate message from %s too big: %d(%lu)\n",
-			       libcfs_nidstr(&msg->msg_hdr.src_nid),
-			       nob, tn->tn_rx_msg.length);
+				nid, nob, tn->tn_rx_msg.length);
 			return -EPROTO;
 		}
 		tn->tn_nob = nob;

lnet/klnds/o2iblnd/o2iblnd_cb.c
@@ -1905,9 +1905,12 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 	case IBLND_MSG_IMMEDIATE:
 		nob = offsetof(struct kib_msg, ibm_u.immediate.ibim_payload[rlen]);
 		if (nob > rx->rx_nob) {
+			char *nid = "unknown";
+
+			if (lntmsg)
+				nid = libcfs_nidstr(&lntmsg->msg_hdr.src_nid);
 			CERROR("Immediate message from %s too big: %d(%d)\n",
-			       libcfs_nidstr(&lntmsg->msg_hdr.src_nid),
-			       nob, rx->rx_nob);
+			       nid, nob, rx->rx_nob);
 			rc = -EPROTO;
 			break;
 		}
@@ -1923,7 +1926,10 @@ kiblnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *lntmsg,
 	case IBLND_MSG_PUT_REQ: {
 		struct kib_msg	*txmsg;
 		struct kib_rdma_desc *rd;
-		struct lnet_libmd *msg_md = lntmsg->msg_md;
+		struct lnet_libmd *msg_md = NULL;
+
+		if (lntmsg)
+			msg_md = lntmsg->msg_md;
 
 		ibprm_cookie = rxmsg->ibm_u.putreq.ibprm_cookie;
 		if (mlen == 0) {

commit a5d7961a7c9666d0e64ee70790641cf2df8a18b8

LUS-12329 build: kfilnd build error
Missing semicolon

Fixes: 09c5478b84 ("LUS-12245 lnet: update kfi and o2ib to handle NULL lnet_msg")

Signed-off-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>
Change-Id: I732950e39cb3c086e5551cd334c608674da26568
Test-Parameters: forbuildonly
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162894
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Cory Spitz <cory.spitz@hpe.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>

lnet/klnds/kfilnd/kfilnd.c
@@ -268,10 +268,10 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 	case KFILND_MSG_IMMEDIATE:
 		nob = offsetof(struct kfilnd_msg, proto.immed.payload[rlen]);
 		if (nob > tn->tn_rx_msg.length) {
-			char *nid = "unknown"
+			char *nid = "unknown";
 
 			if (msg)
-				nid = libcfs_nidstr(&msg->msg_hdr.src_nid)
+				nid = libcfs_nidstr(&msg->msg_hdr.src_nid);
 			CERROR("Immediate message from %s too big: %d(%lu)\n",
 				nid, nob, tn->tn_rx_msg.length);
 			return -EPROTO;