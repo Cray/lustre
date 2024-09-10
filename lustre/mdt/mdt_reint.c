lustre/lustre-release

commit d76cc65d5d68ed3e04bfbd9b7527f64ab0ee0ca7

LU-12125 mds: allow parallel regular file rename
Allow rename of non-directory files in the same directory to be done
in parallel, by only taking the DLM lock on the parent FID, without
also locking the global LUSTRE_BFL_FID (Big Filesystem Lock).

Older clients may not send the renamed file mode in mds_rec_rename.
In this case, the LUSTRE_BFL_FID lock will still be taken, and is not
worse than before parallel rename was allowed.

Similarly, if (for whatever reason) there is a mix of MDS versions
running in the same filesystem, at worst older MDSes will continue to
unnecessarily lock LUSTRE_BFL_FID before doing the file rename.

If MDT0000 is on an older MDS, but newer MDSes are doing renames of
non-directories, the newer MDSes will *not* lock LUSTRE_BFL_FID first,
but there will still be proper serialization from the parent directory
FID lock for other renames affecting the parent and the source/target
entries.  That MDT0000 is unaware of the rename is the whole point.

In case of a race, where the file mode sent by the client is stale,
this is also not a concern, because the file mode is rechecked later
under lock and the rename fails if the source and target mode differ.

Test-Parameters: testlist=racer env=DURATION=3600
Signed-off-by: Andreas Dilger <adilger@whamcloud.com>
Change-Id: If330b53eb6db46e40f50fd7834a83e80db3ebbe5
Reviewed-on: https://review.whamcloud.com/41186
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Lai Siyao <lai.siyao@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mike Pershin <mpershin@whamcloud.com>
Reviewed-by: Yingjin Qian <qian@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/mdt/mdt_reint.c
@@ -2604,11 +2604,22 @@ static int mdt_reint_rename(struct mdt_thread_info *info,
 		    mdt_object_remote(msrcdir))
 			GOTO(out_put_tgtdir, rc = -EXDEV);
 
-		rc = mdt_rename_lock(info, &rename_lh);
-		if (rc != 0) {
-			CERROR("%s: can't lock FS for rename: rc = %d\n",
-			       mdt_obd_name(mdt), rc);
-			GOTO(out_put_tgtdir, rc);
+		/* This may be further relaxed in the future for different
+		 * source and target parents. Start with only same-directory
+		 * renames of non-directory files for simplicity and because
+		 * this is by far the most common use case.
+		 */
+		if (msrcdir != mtgtdir || S_ISDIR(ma->ma_attr.la_mode)) {
+			rc = mdt_rename_lock(info, &rename_lh);
+			if (rc != 0) {
+				CERROR("%s: cannot lock for rename: rc = %d\n",
+				       mdt_obd_name(mdt), rc);
+				GOTO(out_put_tgtdir, rc);
+			}
+		} else {
+			CDEBUG(D_INFO, "%s: samedir rename "DFID"/"DNAME"\n",
+			       mdt_obd_name(mdt), PFID(rr->rr_fid1),
+			       PNAME(&rr->rr_name));
 		}
 	}
 

Cray/lustre

commit 6c02440d970ec95d0488479e7f7a207309745810

LUS-12446 mdt: rename lock inversion deadlock
MDT rewrite got rid of lock ordering by fid and replaced it
with parent/child ordering, but child to child lock inversion is
still possible with hardlinks as follows:

thread 1: mv dir1/10 dir2/10
thread 2: mv dir2/11 dir2/2
where dir1/10 is hardlink to dir4/2 and dir2/10 is hardlink to dir2/11

To solve this we enforce child ordering by fid in case of local rename

This should not create problems aynwhere else but rename since
the next closest candidate - link() does not delete the target
if it exists so it's not locked in that case.

Change-Id: I8aa55c1369194db192a5c4fe7faa7c7bf376c293
WC-bug-id: LU-15491
Fixes: d76cc65d5d68 ("LU-12125 mds: allow parallel regular file rename")
Signed-off-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Hongchao Zhang <hongchao@whamcloud.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163002
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

lustre/mdt/mdt_reint.c
@@ -2812,6 +2812,8 @@ relock:
 	rc = mdt_lookup_version_check(info, mtgtdir, &rr->rr_tgt_name, new_fid,
 				      3);
 	if (rc == 0) {
+		bool child_reverse_lock = false;
+
 		/* the new_fid should have been filled at this moment */
 		if (lu_fid_eq(old_fid, new_fid))
 			GOTO(out_put_old, rc);
@@ -2855,13 +2857,30 @@ relock:
 			GOTO(out_put_new, rc = -EISDIR);
 
 		lh_oldp = &info->mti_lh[MDT_LH_OLD];
+		lh_newp = &info->mti_lh[MDT_LH_NEW];
+
+		/* We will lock in child fid order here to avoid a
+		 * deadlock related to hardlinks thats only possible with
+		 * regular files. LU-15491
+		 */
+		if (!S_ISDIR(lu_object_attr(&mold->mot_obj)) &&
+		    lu_fid_cmp(old_fid, new_fid) > 0) {
+			child_reverse_lock = true;
+			rc = mdt_object_check_lock(info, mtgtdir, mnew, lh_newp,
+						   MDS_INODELOCK_LOOKUP |
+						   MDS_INODELOCK_UPDATE,
+						   LCK_EX, cos_incompat);
+			if (rc < 0)
+				GOTO(out_unlock_new, rc);
+		}
+
 		lh_lookup = &info->mti_lh[MDT_LH_LOOKUP];
 		rc = mdt_rename_source_lock(info, msrcdir, mold, lh_oldp,
 					    lh_lookup,
 					    MDS_INODELOCK_LOOKUP |
 					    MDS_INODELOCK_XATTR, cos_incompat);
 		if (rc < 0)
-			GOTO(out_put_new, rc);
+			GOTO(out_unlock_new, rc);
 
 		/* save version after locking */
 		mdt_version_get_save(info, mold, 2);
@@ -2875,7 +2894,7 @@ relock:
 			if (rc) {
 				if (rc == 1)
 					rc = -EINVAL;
-				GOTO(out_unlock_old, rc);
+				GOTO(out_unlock_new, rc);
 			}
 		}
 
@@ -2885,13 +2904,14 @@ relock:
 		 * lock. See LU-4002.
 		 */
 
-		lh_newp = &info->mti_lh[MDT_LH_NEW];
-		rc = mdt_object_check_lock(info, mtgtdir, mnew, lh_newp,
-					   MDS_INODELOCK_LOOKUP |
-					   MDS_INODELOCK_UPDATE, LCK_EX,
-					   cos_incompat);
-		if (rc != 0)
-			GOTO(out_unlock_new, rc);
+		if (!child_reverse_lock) {
+			rc = mdt_object_check_lock(info, mtgtdir, mnew, lh_newp,
+						   MDS_INODELOCK_LOOKUP |
+						   MDS_INODELOCK_UPDATE,
+						   LCK_EX, cos_incompat);
+			if (rc != 0)
+				GOTO(out_unlock_new, rc);
+		}
 
 		/* get and save version after locking */
 		mdt_version_get_save(info, mnew, 3);
@@ -2942,8 +2962,9 @@ relock:
 	EXIT;
 out_unlock_new:
 	if (mnew != NULL)
-		mdt_object_unlock(info, mnew, lh_newp, rc);
-out_unlock_old:
+		/* mnew is gone, no need to keep lock */
+		mdt_object_unlock(info, mnew, lh_newp, 1);
+
 	mdt_object_unlock(info, NULL, lh_lookup, rc);
 	mdt_object_unlock(info, mold, lh_oldp, rc);
 out_put_new: