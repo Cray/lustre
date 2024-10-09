lustre/lustre-release

LU-17703 lod: check the inherited pool for conflicts
In addition to LU-15658, the start index could be inherited from
parent and the pool from root: drop the pool in case of conflict
as well.

Another case of a problem inheritance is saving the inherited LOVEA
to subdir, when all the parameters are inherited but the ost list.

HPE-bug-id: LUS-11330, LUS-11631
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: Ief1dbd8c1ee0433bb625cbff1834b248d4fb2992
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/161800
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/54661
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/lod/lod_internal.h
@@ -761,7 +761,7 @@ int lod_fill_mirrors(struct lod_object *lo);
 int lod_init_comp_foreign(struct lod_layout_component *lod_comp, void *lmm);
 
 /* lod_pool.c */
-struct lod_pool_desc *lod_find_pool(struct lod_device *lod, char *poolname);
+struct lod_pool_desc *lod_find_pool(struct lod_device *lod, const char *poolname);
 void lod_pool_putref(struct lod_pool_desc *pool);
 int lod_pool_del(struct obd_device *obd, char *poolname);
 int lod_check_index_in_pool(__u32 idx, struct lod_pool_desc *pool);
@@ -807,6 +807,7 @@ int lod_use_defined_striping(const struct lu_env *, struct lod_object *,
 			     const struct lu_buf *);
 int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 			 const struct lu_buf *buf);
+void lod_qos_set_pool(struct lod_object *lo, int pos, const char *pool_name);
 int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 			struct lu_attr *attr, struct thandle *th,
 			int comp_idx, __u64 reserve);
@@ -915,7 +916,7 @@ void lod_check_and_spill_pool(const struct lu_env *env, struct lod_device *lod,
 			      char **poolname);
 void lod_spill_target_refresh(const struct lu_env *env, struct lod_device *lod,
 			      struct lod_pool_desc *pool);
-struct lod_pool_desc *lod_pool_find(struct lod_device *lod, char *poolname);
+struct lod_pool_desc *lod_pool_find(struct lod_device *lod, const char *poolname);
 int lod_tgt_weights_seq_show(struct seq_file *m, struct lod_device *lod,
 			     struct lu_tgt_pool *tgts, bool mdt);
 int lod_tgt_weights_seq_write(struct seq_file *m, const char __user *buf,

lustre/lod/lod_pool.c
@@ -360,7 +360,7 @@ bool lod_pool_exists(struct lod_device *lod, char *poolname)
 	return pool != NULL;
 }
 
-struct lod_pool_desc *lod_pool_find(struct lod_device *lod, char *poolname)
+struct lod_pool_desc *lod_pool_find(struct lod_device *lod, const char *poolname)
 {
 	struct lod_pool_desc *pool;
 
@@ -712,7 +712,7 @@ int lod_check_index_in_pool(__u32 idx, struct lod_pool_desc *pool)
  * \retval	pointer to pool descriptor on success
  * \retval	NULL if \a poolname could not be found or poolname is empty
  */
-struct lod_pool_desc *lod_find_pool(struct lod_device *lod, char *poolname)
+struct lod_pool_desc *lod_find_pool(struct lod_device *lod, const char *poolname)
 {
 	struct lod_pool_desc *pool;
 

Cray/lustre

lustre/lod/lod_internal.h
@@ -689,7 +689,7 @@ int lod_alloc_comp_entries(struct lod_object *lo, int mirror_cnt, int comp_cnt);
 int lod_fill_mirrors(struct lod_object *lo);
 
 /* lod_pool.c */
-struct pool_desc *lod_find_pool(struct lod_device *lod, char *poolname);
+struct pool_desc *lod_find_pool(struct lod_device *lod, const char *poolname);
 void lod_pool_putref(struct pool_desc *pool);
 int lod_pool_del(struct obd_device *obd, char *poolname);
 int lod_check_index_in_pool(__u32 idx, struct pool_desc *pool);
@@ -735,6 +735,7 @@ int lod_use_defined_striping(const struct lu_env *, struct lod_object *,
 			     const struct lu_buf *);
 int lod_qos_parse_config(const struct lu_env *env, struct lod_object *lo,
 			 const struct lu_buf *buf);
+void lod_qos_set_pool(struct lod_object *lo, int pos, const char *pool_name);
 int lod_qos_prep_create(const struct lu_env *env, struct lod_object *lo,
 			struct lu_attr *attr, struct thandle *th,
 			int comp_idx, __u64 reserve);

lustre/lod/lod_pool.c
@@ -353,7 +353,7 @@ bool lod_pool_exists(struct lod_device *lod, char *poolname)
 	return pool != NULL;
 }
 
-static struct pool_desc *lod_pool_find(struct lod_device *lod, char *poolname)
+static struct pool_desc *lod_pool_find(struct lod_device *lod, const char *poolname)
 {
 	struct pool_desc *pool;
 
@@ -672,7 +672,7 @@ int lod_check_index_in_pool(__u32 idx, struct pool_desc *pool)
  * \retval	pointer to pool descriptor on success
  * \retval	NULL if \a poolname could not be found or poolname is empty
  */
-struct pool_desc *lod_find_pool(struct lod_device *lod, char *poolname)
+struct pool_desc *lod_find_pool(struct lod_device *lod, const char *poolname)
 {
 	struct pool_desc *pool;
 