74f9a31a53e27a841c5e461c6e3851ec05fcfe46
Chris Horn <chris.horn@hpe.com>
2024-10-29 13:01:39 -0600

    LUS-12555 lnet: Track successful and failed resends
    Add a count of successful and failed resends to the LNet health
    stats. This will allow us to more easily determine the effectiveness
    of this feature.
    
    In the current code, lnet_incr_hstats() only uses cpt 0. Callers can
    now pass a cpt argument (they must hold net lock for that cpt), and
    lnet_incr_hstats() will update the corresponding per-cpt counters. In
    lnet_health_check() we will now lock either the msg_rx_cpt or
    msg_tx_cpt in the same manner as lnet_finalize(), and
    lnet_incr_hstats() will update the successful resend count.
    
    On the message resend path, the msg_tx_cpt was already being taken so
    we use this to update the failed resends counter. Failed resends is
    updated there rather than in lnet_incr_hstats() to avoid duplicating
    the check for whether the message retry count has exceeded the max
    retries.
    
    Test-Parameters: trivial testlist=sanity-lnet
    Signed-off-by: Chris Horn <chris.horn@hpe.com>
    Change-Id: I46430e51332303631d42268b0729273de9eefd2c
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163133
    Reviewed-by: Caleb Carlson <caleb.carlson@hpe.com>
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

--- lnet/include/uapi/linux/lnet/lnet-types.h
+++ lnet/include/uapi/linux/lnet/lnet-types.h
@@ -186,6 +186,8 @@ struct lnet_counters_health {
 	__u32	lch_remote_error_count;
 	__u32	lch_remote_timeout_count;
 	__u32	lch_network_timeout_count;
+	__u32	lch_failed_resends;
+	__u32	lch_successful_resends;
 };
 
 struct lnet_counters {
