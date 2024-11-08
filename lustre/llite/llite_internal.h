8eb6f09384a5490029a37e242f095dfb65a51bd3
Shaun Tancheff <shaun.tancheff@hpe.com>
2024-11-08 16:28:14 +0700

    LU-18432 llite: hybrid i/o statistics
    Include separate hybrid i/o read and write bytes stats:
      hybrid_read_bytes
      hybrid_write_bytes
    
    HPE-bug-id: LUS-12594
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I6678dd52f998cc9d24c75686c8f97eb8b6b56ebf
    Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56933
    Tested-by: jenkins <devops@whamcloud.com>
    Tested-by: Maloo <maloo@whamcloud.com>
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Reviewed-by: Patrick Farrell <pfarrell@whamcloud.com>
    Reviewed-by: Oleg Drokin <green@whamcloud.com>
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163206/
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

--- lustre/llite/llite_internal.h
+++ lustre/llite/llite_internal.h
@@ -1269,6 +1269,8 @@ void ll_free_rw_stats_info(struct ll_sb_info *sbi);
 enum {
 	LPROC_LL_READ_BYTES,
 	LPROC_LL_WRITE_BYTES,
+	LPROC_LL_HIO_READ,
+	LPROC_LL_HIO_WRITE,
 	LPROC_LL_READ,
 	LPROC_LL_WRITE,
 	LPROC_LL_IOCTL,
