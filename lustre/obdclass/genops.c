210dd0e5c50f000b6678ae4ec64b2f5ddf2db6fe
Shaun Tancheff <shaun.tancheff@hpe.com>
2024-12-06 12:38:42 +0700

    LUS-12621 obdclass: jobstat scaling
    When there is a large number of jobstats (>100k) list walking in
    seq_write does not scale and can trigger cpus soft lock-ups.
    
    Prefer red black trees for easier lookup, removal and simplified
    tree walk during seq_write()
    
    Add an rbtree with a generated sequential id as jobs are added
    to simplify iteration with restarts. The larger the amount of
    data being written via seq_write() the more likely and frequent
    a restart is needed to seek to new position which can be done
    quickly with an rbtree.
    
    Add an lru list to so periodic trim of jobs does not need to
    walk the entire tree of jobs.
    
    Unpatched:
      jobs  50000    100000   200000  350000  500000
      time  2.096s   3.672s   7.504s  11.875s 16.721s
    
    Patched:
      jobs  50000    100000   200000  350000  500000
      time  1.366s   3.296s   6.763s  8.895s  11.965s
    
    Lustre-change: https://review.whamcloud.com/56607
    Lustre-commit: [tbd: 537a26fb840bba529e6ec6532461592f69136d0b]
    
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: I77ec71f37654164efec386a761be6cb5acb37711
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163137
    Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

--- lustre/obdclass/genops.c
+++ lustre/obdclass/genops.c
@@ -1142,47 +1142,6 @@ static inline int ptlrpc_exp_deadline_cmp(const void *key,
 		*time > ned->ned_deadline ?  1 : 0);
 }
 
-#ifndef HAVE_RB_ADD
-static __always_inline void
-rb_add(struct rb_node *node, struct rb_root *tree,
-       bool (*less)(struct rb_node *, const struct rb_node *))
-{
-        struct rb_node **link = &tree->rb_node;
-        struct rb_node *parent = NULL;
-
-        while (*link) {
-                parent = *link;
-                if (less(node, parent))
-                        link = &parent->rb_left;
-                else
-                        link = &parent->rb_right;
-        }
-
-        rb_link_node(node, parent, link);
-        rb_insert_color(node, tree);
-}
-
-static __always_inline struct rb_node *
-rb_find(const void *key, const struct rb_root *tree,
-        int (*cmp)(const void *key, const struct rb_node *))
-{
-        struct rb_node *node = tree->rb_node;
-
-        while (node) {
-                int c = cmp(key, node);
-
-                if (c < 0)
-                        node = node->rb_left;
-                else if (c > 0)
-                        node = node->rb_right;
-                else
-                        return node;
-        }
-
-        return NULL;
-}
-#endif
-
 int obd_export_timed_init(struct obd_export *exp, void **data)
 
 {
