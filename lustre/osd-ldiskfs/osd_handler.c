Xyratex/lustre-stable

LUS-2492 tests: restore sanity_407
Transaction errors are checked better after LU-8411/MRP-3609,
so sanity_407 was disabled because it leaves filesystem in
broken state.

Make sanity_407 to work same as before LU-8411/MRP-3609 i.e
check only remote transaction errors.

Test-Parameters: testlist=sanity/407
Change-Id: I4c0b5efefa967d3ff20c27ed2e6a7b4ec172e1f2
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/18206
Reviewed-by: Artem Blagodarenko <c17828@cray.com>
Reviewed-by: Andrew Perepechko <c17827@cray.com>
Tested-by: Elena Gryaznova <c17455@cray.com>

lustre/osd-ldiskfs/osd_handler.c
@@ -1941,7 +1941,7 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 		if (rc2 != 0)
 			CERROR("%s: failed to stop transaction: rc = %d\n",
 			       osd_name(osd), rc2);
-		if (!rc)
+		if (!rc || OBD_FAIL_CHECK(OBD_FAIL_DT_TXN_STOP))
 			rc = rc2;
 	} else {
 		osd_trans_stop_cb(oh, th->th_result);

Cray/lustre

lustre/osd-ldiskfs/osd_handler.c
@@ -2211,7 +2211,7 @@ static int osd_trans_stop(const struct lu_env *env, struct dt_device *dt,
 		if (rc2 != 0)
 			CERROR("%s: failed to stop transaction: rc = %d\n",
 			       osd_name(osd), rc2);
-		if (!rc)
+		if (!rc || CFS_FAIL_CHECK(OBD_FAIL_DT_TXN_STOP))
 			rc = rc2;
 
 		/* We preserve the origin behavior of ignoring any