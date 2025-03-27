a1968bfa70327015c71fa6f5023bb0a60588130d
Shaun Tancheff <shaun.tancheff@hpe.com>
2025-03-27 08:42:38 +0700

    LUS-12797 build: Compatability updates for kernel v6.14
    Linux commit v6.13-rc1-1-g6fba89813ccf
      lsm: ensure the correct LSM context releaser
    
    struct lsm_context is now upstream, provide an lsmcontext
    mapping for Ubuntu
    
    Linux v6.13-rc1-7-g5be1fa8abd7b
      Pass parent directory inode and expected name to ->d_revalidate()
    
    Adjust d_revalidate() to handle the extra arguments.
    
    Use FMODE_NONOTIFY now that __FMODE_NONOTIFY macro is dropped.
    
    Lustre-change: https://review.whamcloud.com/58551
    Lustre-commit: 76fc6f9f571d59654e9e9501bd6ba4e4fa995ea8
    
    Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
    Change-Id: Ieee14c586f7e3aa37b6041af3381dd1ae7654215
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163379
    Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
    Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
    Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

--- lustre/include/lustre_compat.h
+++ lustre/include/lustre_compat.h
@@ -766,7 +766,7 @@ ll_shrinker_create(struct ll_shrinker_ops *ops, unsigned int flags,
 #ifdef HAVE_SEC_RELEASE_SECCTX_1ARG
 #ifndef HAVE_LSMCONTEXT_INIT
 /* Ubuntu 5.19 */
-static inline void lsmcontext_init(struct lsmcontext *cp, char *context,
+static inline void lsmcontext_init(struct lsm_context *cp, char *context,
 				   u32 size, int slot)
 {
 #ifdef HAVE_LSMCONTEXT_HAS_ID
@@ -784,7 +784,7 @@ static inline void ll_security_release_secctx(char *secdata, u32 seclen,
 					      int slot)
 {
 #ifdef HAVE_SEC_RELEASE_SECCTX_1ARG
-	struct lsmcontext context = { };
+	struct lsm_context context = { };
 
 	lsmcontext_init(&context, secdata, seclen, slot);
 	return security_release_secctx(&context);
