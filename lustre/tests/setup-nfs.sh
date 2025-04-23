lustre/lustre-release

LU-17152 tests: unmount NFS clients with zconf_umount_clients
This patch fixes cleanup_nfs() to unmount NFS clients by running
zconf_umount_clients(), which can find and kill active processes
that are accessing the NFS mount point so as to avoid the
"device is busy" failure.

The patch also adds racer_on_nfs test into always_except list for
parallel-scale-nfsv4 due to LU-17154.

Lustre-change: https://review.whamcloud.com/52533
Lustre-commit: 563deecae0ac2690b6d8d5571bf7af09408943cd

Test-Parameters: trivial testlist=parallel-scale-nfsv4

Change-Id: I37a38502362399540c28e78d1343e768b490ce8b
Signed-off-by: Jian Yu <yujian@whamcloud.com>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56886
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Alex Deiter <adeiter@ddn.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/tests/setup-nfs.sh
@@ -38,7 +38,7 @@ cleanup_nfs() {
 	local EXPORTS_FILE=$DEFAULT_EXPORTS_FILE
 
 	echo "Unmounting NFS clients"
-	do_nodes "$NFS_CLIENTS" "umount -v -f $NFS_MOUNT_POINT" || return 1
+	zconf_umount_clients "$NFS_CLIENTS" $NFS_MOUNT_POINT -f || return 1
 	do_nodes "$NFS_CLIENTS" "systemctl stop nfs-idmapd" || return 1
 
 	echo "Unexporting Lustre filesystem"

Cray/lustre

lustre/tests/setup-nfs.sh
@@ -70,7 +70,7 @@ cleanup_nfs() {
 	local NFS_CLIENTS=${3}
 
 	echo -e "\nUnmounting NFS clients..."
-	do_nodes $NFS_CLIENTS "umount -f $MNTPNT" || true
+	zconf_umount_clients "$NFS_CLIENTS" $MNTPNT -f || return 1
 
 	echo -e "\nUnexporting Lustre filesystem..."
 	do_nodes $NFS_CLIENTS "chkconfig --list rpcidmapd 2>/dev/null |