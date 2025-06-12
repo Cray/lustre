Cray/lustre

commit b1414e4f19bd8e7614bef93e94fdcbda764ff416

LUSQE-2112 build: sign kernel modules
Sign server kernel modules for the release

UEFI requires kernel modules signing, this CR adds
on-fly signing by injecting signHPE rpm macros
into the mock chroot

Test-Parameters: forbuildonly

Signed-off-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>
Change-Id: Ib3c4feb560c3826cd9619511adc47c64fc17d567
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163171
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>

contrib/jenkins/build_phase2.sh
@@ -54,6 +54,18 @@ BUILD_NUMBER=${BUILD_NUMBER:-3}  #never used
 # ---------------------------------
 RPMBUILD_DIR=$WORKSPACE
 
+if [ "$JP_BUILD_MODE" == "full" ]
+then
+    #LUSQE-2112, enable module code signing
+    if [ -f /build/bin/code_signing_tools/signmount ]; then
+        . /build/bin/code_signing_tools/signmount
+        SIGNOPTS=$(bindMount)
+        KMOD_SIGN_VALUE=/signtools/signko\ %{buildroot}/lib/modules
+    else
+        KMOD_SIGN_VALUE='{nil}'
+    fi
+fi
+
 build_iem() {
     PACKAGE=lustre-iem
     VERSION=${JP_VERSION}
@@ -375,9 +387,10 @@ fi
 KERNEL=$(rpm -r $ROOT -q --qf '%{VERSION}-%{RELEASE}'  kernel${BUILD_TYPE}-devel)
 K_ARCH=$JP_TARGET_ARCH
 release=$(echo "${KERNEL}" | tr "-" "_")
-${MOCK_CMD}  --no-clean --rebuild ${srpm} --define "myrelease ${release}${GITHASH}" --define "configure_args ${o_opt} ${JP_BUILD_OPTIONS}" \
+${MOCK_CMD}  ${SIGNOPTS} --no-clean --rebuild ${srpm} --define "myrelease ${release}${GITHASH}" --define "configure_args ${o_opt} ${JP_BUILD_OPTIONS}" \
     --define "kver ${KERNEL}${DOTDEBUG}" --define "kversion ${KERNEL}.${K_ARCH}${DOTDEBUG}" --define "kdir /usr/src/kernels/${KERNEL}.${K_ARCH}${DOTDEBUG}" \
     --define "lustre_name ${JP_PACKAGE_NAME}"  \
+    --define "__brp_kmod_sign $KMOD_SIGN_VALUE" \
     --define "mpi_name mpich" \
     --define "$WITH_GSS_KEYRING 1" \
     --define "rpm_rel ${RPM_RELEASE}" \

contrib/jenkins/Jenkinsfile
@@ -14,6 +14,7 @@ pipeline {
         BRANCH_NAME = "release"
         TARGET_OS = "hpc-lus-filesystem"
         TARGET_ARCH = "noarch"
+        CSCREDS = credentials('cscreds')
     }
 
     options {

commit 0919d3e2c02a18e325077c74c312a550186397fa

LUS-12906 build: kfilnd build fails in the absence of the signing tool
Missing '%' in the rpm macro

Fixes: b1414e4f19b ("LUSQE-2112 build: sign kernel modules")

Change-Id: Ia1996a08f176feee282700afa5ed9fe9a9ac33b9
Test-Parameters: forbuildonly
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163328
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>

contrib/jenkins/build_phase2.sh
@@ -54,6 +54,7 @@ BUILD_NUMBER=${BUILD_NUMBER:-3}  #never used
 # ---------------------------------
 RPMBUILD_DIR=$WORKSPACE
 
+KMOD_SIGN_VALUE='%{nil}'
 if [ "$JP_BUILD_MODE" == "full" ]
 then
     #LUSQE-2112, enable module code signing
@@ -61,8 +62,6 @@ then
         . /build/bin/code_signing_tools/signmount
         SIGNOPTS=$(bindMount)
         KMOD_SIGN_VALUE=/signtools/signko\ %{buildroot}/lib/modules
-    else
-        KMOD_SIGN_VALUE='{nil}'
     fi
 fi
 