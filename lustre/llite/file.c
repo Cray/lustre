lustre/lustre-release

commit 4a14a51a8e000e05e6a3c0dae7bf64b8d43f8254

LU-18448 llite: read dir on open
Let's read some pages at directory start,
a clients needs it probably.

walk over ~100k directories with 150 files on last leaf.

readdir on open enabled.

    real    0m39.977s
    user    0m0.121s
    sys     0m7.161s

readdir on open disabled

    real    1m18.106s
    user    0m0.151s
    sys     0m15.666s

HPE-bug-id: LUS-7695
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Change-Id: Iaa674ce0d2e5723b380d7ca09407b27a90bc37f5
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/57069
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>

lustre/llite/file.c
@@ -640,6 +640,67 @@ out_io:
 
 	EXIT;
 }
+void ll_dir_finish_open(struct inode *inode, struct ptlrpc_request *req)
+{
+	struct obd_export *exp = ll_i2mdexp(inode);
+	void *data;
+	struct page *page;
+	struct lu_dirpage *dp;
+	int is_hash64;
+	int rc;
+	unsigned long	offset;
+	__u64		hash;
+	unsigned int i;
+	unsigned int npages;
+
+	ENTRY;
+
+	if (!exp_connect_open_readdir(exp))
+		RETURN_EXIT;
+
+	if (!req_capsule_field_present(&req->rq_pill, &RMF_NIOBUF_INLINE,
+				       RCL_SERVER))
+		RETURN_EXIT;
+
+	data = req_capsule_server_get(&req->rq_pill, &RMF_NIOBUF_INLINE);
+	if (data == NULL)
+		RETURN_EXIT;
+
+	npages = req_capsule_get_size(&req->rq_pill, &RMF_NIOBUF_INLINE,
+				      RCL_SERVER);
+	if (npages < sizeof(*dp))
+		RETURN_EXIT;
+
+	/* div rou*/
+	npages = DIV_ROUND_UP(npages, PAGE_SIZE);
+	is_hash64 = test_bit(LL_SBI_64BIT_HASH, ll_i2sbi(inode)->ll_flags);
+
+	for (i = 0; i < npages; i++) {
+		page = page_cache_alloc(inode->i_mapping);
+		if (!page)
+			continue;
+
+		lock_page(page);
+		SetPageUptodate(page);
+
+		dp = kmap_atomic(page);
+		memcpy(dp, data, PAGE_SIZE);
+		hash = le64_to_cpu(dp->ldp_hash_start);
+		kunmap_atomic(dp);
+
+		offset = hash_x_index(hash, is_hash64);
+
+		prefetchw(&page->flags);
+		rc = add_to_page_cache_lru(page, inode->i_mapping, offset,
+				   GFP_KERNEL);
+		if (rc == 0)
+			unlock_page(page);
+
+		put_page(page);
+	}
+	EXIT;
+}
+
 
 static int ll_intent_file_open(struct dentry *de, void *lmm, ssize_t lmmsize,
 				struct lookup_intent *itp)
@@ -693,6 +754,9 @@ retry:
 	op_data->op_data = lmm;
 	op_data->op_data_size = lmmsize;
 
+	if (!sbi->ll_dir_open_read && S_ISDIR(de->d_inode->i_mode))
+		op_data->op_cli_flags &= ~CLI_READ_ON_OPEN;
+
 	CFS_FAIL_TIMEOUT(OBD_FAIL_LLITE_OPEN_DELAY, cfs_fail_val);
 
 	rc = ll_intent_lock(sbi->ll_md_exp, op_data, itp, &req,
@@ -730,6 +794,8 @@ retry:
 		ll_set_lock_data(sbi->ll_md_exp, de->d_inode, itp, &bits);
 		if (bits & MDS_INODELOCK_DOM && bits & MDS_INODELOCK_LAYOUT)
 			ll_dom_finish_open(de->d_inode, req);
+		if (bits & MDS_INODELOCK_UPDATE && S_ISDIR(de->d_inode->i_mode))
+			ll_dir_finish_open(de->d_inode, req);
 	}
 	/* open may not fetch LOOKUP lock, update dir depth and default LMV
 	 * anyway.

lustre/include/lu_object.h
@@ -814,8 +814,15 @@ struct lu_rdpg {
 	/** requested attr */
 	__u32                   rp_attrs;
 	/** pointers to pages */
-	struct page           **rp_pages;
+	union {
+		struct page	**rp_pages;
+		void		*rp_data;
+	};
 };
+
+/* for dt_index_walk / mdd_readpage */
+void *rdpg_page_get(const struct lu_rdpg *rdpg, unsigned int index);
+void rdpg_page_put(const struct lu_rdpg *rdpg, unsigned int index);
 
 enum lu_xattr_flags {
 	LU_XATTR_REPLACE = BIT(0),

lustre/include/lustre_dlm.h
@@ -1172,6 +1172,12 @@ static inline bool ldlm_has_dom(struct ldlm_lock *lock)
 		!!(lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_DOM);
 }
 
+static inline bool ldlm_has_update(struct ldlm_lock *lock)
+{
+	return lock->l_resource->lr_type == LDLM_IBITS &&
+		lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_UPDATE;
+}
+
 static inline char *
 ldlm_ns_name(struct ldlm_namespace *ns)
 {

lustre/include/lustre_export.h
@@ -519,6 +519,11 @@ static inline bool exp_connect_batch_rpc(struct obd_export *exp)
 	return (exp_connect_flags2(exp) & OBD_CONNECT2_BATCH_RPC);
 }
 
+static inline int exp_connect_open_readdir(struct obd_export *exp)
+{
+	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_READDIR_OPEN);
+}
+
 enum {
 	/* archive_ids in array format */
 	KKUC_CT_DATA_ARRAY_MAGIC	= 0x092013cea,

lustre/include/lustre_mdc.h
@@ -86,6 +86,15 @@ static inline void cl_lov_delay_create_clear(unsigned int *flags)
 		*flags &= ~O_LOV_DELAY_CREATE;
 }
 
+static inline unsigned long hash_x_index(__u64 hash, int hash64)
+{
+	if (BITS_PER_LONG == 32 && hash64)
+		hash >>= 32;
+	/* save hash 0 with hash 1 */
+	return ~0UL - (hash + !hash);
+}
+
+
 /** @} mdc */
 
 #endif

lustre/include/lustre_osc.h
@@ -377,7 +377,7 @@ enum osc_lock_state {
  *
  * - When reply is received from the server (osc_enqueue_interpret())
  *      - ldlm_cli_enqueue_fini()
- *          - LDLM_LOCK_PUT(): releases caller reference acquired by
+ *          - ldlm_lock_put(): releases caller reference acquired by
  *            ldlm_lock_new().
  *          - if (rc != 0)
  *                ldlm_lock_decref(): error case: matches ldlm_cli_enqueue().
@@ -385,7 +385,7 @@ enum osc_lock_state {
  *
  * - When lock is being cancelled (ldlm_lock_cancel())
  *      - ldlm_lock_destroy()
- *          - LDLM_LOCK_PUT(): releases hash-table reference acquired by
+ *          - ldlm_lock_put(): releases hash-table reference acquired by
  *            ldlm_lock_new().
  *
  * osc_lock is detached from ldlm_lock by osc_lock_detach() that is called

lustre/include/obd.h
@@ -915,6 +915,8 @@ enum md_cli_flags {
 	CLI_MIGRATE	= BIT(4),
 	CLI_DIRTY_DATA	= BIT(5),
 	CLI_NO_SLOT     = BIT(6),
+	/**< read on open (used for directory for now) */
+	CLI_READ_ON_OPEN = BIT(7),
 };
 
 enum md_op_code {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -848,6 +848,8 @@ struct ptlrpc_body_v2 {
 #define OBD_CONNECT2_SPARSE            0x1000000000ULL /* sparse LNet read */
 #define OBD_CONNECT2_MIRROR_ID_FIX     0x2000000000ULL /* rr_mirror_id move */
 #define OBD_CONNECT2_UPDATE_LAYOUT     0x4000000000ULL /* update compressibility */
+#define OBD_CONNECT2_READDIR_OPEN	0x8000000000ULL /* read first dir page on open */
+
 /* XXX README XXX README XXX README XXX README XXX README XXX README XXX
  * Please DO NOT add OBD_CONNECT flags before first ensuring that this value
  * is not in use by some other branch/patch.  Email adilger@whamcloud.com

Cray/lustre

commit 50752e1d55b9bd75ce58bc300504182e803820e7

LUS-12935 build: page_cache_alloc removed, missing return
Fix gcc -Wimplicit-function-declaration
   file.c:678:24: error: implicit declaration of function \
   page_cache_alloc [-Werror=implicit-function-declaration]

Fixes: 4a14a51a8e0 ("LU-18448 llite: read dir on open")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: If063b9cbbfc112ce09dd4a904407b9f7c81d77b1
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163362
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-by: Petros Koutoupis <petros.koutoupis@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/llite/file.c
@@ -685,7 +685,7 @@ void ll_dir_finish_open(struct inode *inode, struct ptlrpc_request *req)
 	is_hash64 = test_bit(LL_SBI_64BIT_HASH, ll_i2sbi(inode)->ll_flags);
 
 	for (i = 0; i < npages; i++) {
-		page = page_cache_alloc(inode->i_mapping);
+		page = __page_cache_alloc(mapping_gfp_mask(inode->i_mapping));
 		if (!page)
 			continue;
 