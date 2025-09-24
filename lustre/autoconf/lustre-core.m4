Cray/lustre

commit d9c5501caf6f120a5b42a14ea7ddedd399d8a1bd

LUS-12626 llite: reclaim dentries/inodes in smaller amounts
do_shrink_slab() may try to release a large amount of dentries
and inodes, especially with lower reclaim priority values.
Since reclaim is performed under shrinker_rwsem, other threads
working with it may timeout and panic.

Change-Id: I2cd887196e873660f622d25294bb2d9f100afb25
Signed-off-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163141
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/llite/llite_lib.c
@@ -1368,6 +1368,22 @@ static int super_setup_bdi_name(struct super_block *sb, char *fmt, ...)
 }
 #endif /* !HAVE_SUPER_SETUP_BDI_NAME */
 
+static unsigned long ll_count_objects(struct shrinker *shrink,
+				      struct shrink_control *sc)
+{
+	struct super_block *sb = container_of(shrink, struct super_block, s_shrink);
+	struct ll_sb_info *sbi = ll_s2sbi(sb);
+	unsigned int max = sbi->ll_max_shrink;
+	long total_objects;
+
+	total_objects = sbi->ll_count_objects(shrink, sc);
+	if (total_objects != SHRINK_EMPTY &&
+	    max && total_objects > max)
+		total_objects = max;
+
+	return total_objects;
+}
+
 int ll_fill_super(struct super_block *sb)
 {
 	struct	lustre_profile *lprof = NULL;
@@ -1511,6 +1527,10 @@ int ll_fill_super(struct super_block *sb)
 	err = client_common_fill_super(sb, md, dt);
 	if (err < 0)
 		GOTO(out_free_md, err);
+
+	sbi->ll_count_objects = sb->s_shrink.count_objects;
+	sb->s_shrink.count_objects = ll_count_objects;
+	sbi->ll_max_shrink = 0;
 
 	sbi->ll_client_common_fill_super_succeeded = 1;
 

lustre/llite/llite_internal.h
@@ -1042,6 +1042,10 @@ struct ll_sb_info {
 
 	/* LU-14535: the list of "lfs quota -a" */
 	struct list_head	 ll_all_quota_list;
+
+	unsigned long (*ll_count_objects)(struct shrinker *shrink,
+					  struct shrink_control *sc);
+	unsigned int		 ll_max_shrink;
 };
 
 #define SBI_DEFAULT_HEAT_DECAY_WEIGHT	((80 * 256 + 50) / 100)

lustre/llite/lproc_llite.c
@@ -2171,6 +2171,36 @@ static ssize_t dir_read_on_open_store(struct kobject *kobj,
 
 LUSTRE_RW_ATTR(dir_read_on_open);
 
+static ssize_t max_shrink_show(struct kobject *kobj,
+				     struct attribute *attr,
+				     char *buf)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+
+	return snprintf(buf, PAGE_SIZE, "%u\n", sbi->ll_max_shrink);
+}
+
+static ssize_t max_shrink_store(struct kobject *kobj,
+				      struct attribute *attr,
+				      const char *buffer,
+				      size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 10, &val);
+	if (rc)
+		return rc;
+
+	sbi->ll_max_shrink = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(max_shrink);
+
 static int ll_unstable_stats_seq_show(struct seq_file *m, void *v)
 {
 	struct super_block	*sb    = m->private;
@@ -2520,6 +2550,7 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_pcc_mode.attr,
 	&lustre_attr_pcc_async_affinity.attr,
 	&lustre_attr_dir_read_on_open.attr,
+	&lustre_attr_max_shrink.attr,
 	NULL,
 };
 

commit 9b5d3c8cd9f21c231a47c2022203152b05edaf12

LUS-12934 build: s_shrink may be a pointer
Fix gcc -Wimplicit-function-declaration
   llite_lib.c:1532:21: error: 'sb->s_shrink' is a pointer;

With the introduction of
  Linux commit v6.6-rc4-53-gc42d50aefd17
    mm: shrinker: add infrastructure for dynamically allocating...

Most kernels have change s_shrink to a pointer however RHEL 9.6
in particular has not, breaking the presumption that if
alloc_shrinker() is present s_shrink would be a pointer.

Fixes: d9c5501caf6 ("LUS-12626 llite: reclaim dentries/inodes in smaller amounts")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I12815ed77e08230c0b9b9e859a7146d71cac6a1d
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163363
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/autoconf/lustre-core.m4
@@ -4761,6 +4761,32 @@ AC_DEFUN([LC_HAVE_SHRINKER_ALLOC], [
 ]) # LC_HAVE_SHRINKER_ALLOC
 
 #
+# LC_HAVE_S_SHRINK_AS_A_POINTER
+#
+# Linux commit v6.6-rc4-53-gc42d50aefd17
+#   mm: shrinker: add infrastructure for dynamically allocating shrinker
+#
+# While s_shrink is now a pointer in most cases, el9.6 backport of this
+# keeps s_shrink as contained in struct superblock for their kABI support
+#
+AC_DEFUN([LC_SRC_HAVE_S_SHRINK_AS_A_POINTER], [
+	LB2_LINUX_TEST_SRC([s_shrink_as_a_pointer], [
+		#include <linux/shrinker.h>
+	],[
+		struct super_block *sb __attribute__ ((unused)) = NULL;
+
+		sb->s_shrink->count_objects = 0;
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_S_SHRINK_AS_A_POINTER], [
+	LB2_MSG_LINUX_TEST_RESULT([if s_shrink member of super_block is a pointer],
+	[s_shrink_as_a_pointer], [
+		AC_DEFINE(HAVE_S_SHRINK_AS_A_POINTER, 1,
+			[s_shrink member of super_block is a pointer])
+	])
+]) # LC_HAVE_S_SHRINK_AS_A_POINTER
+
+#
 # LC_HAVE_DENTRY_D_CHILDREN
 #
 # Linux commit v6.7-rc1-3-gda549bdd15c2
@@ -5344,6 +5370,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_HAVE_NSPROXY_COUNT_AS_REFCOUNT
 	LC_SRC_HAVE_INODE_GET_MTIME_SEC
 	LC_SRC_HAVE_SHRINKER_ALLOC
+	LC_SRC_HAVE_S_SHRINK_AS_A_POINTER
 
 	# 6.8
 	LC_SRC_HAVE_DENTRY_D_CHILDREN
@@ -5682,6 +5709,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_HAVE_NSPROXY_COUNT_AS_REFCOUNT
 	LC_HAVE_INODE_GET_MTIME_SEC
 	LC_HAVE_SHRINKER_ALLOC
+	LC_HAVE_S_SHRINK_AS_A_POINTER
 
 	# 6.8
 	LC_HAVE_DENTRY_D_CHILDREN

lustre/llite/llite_lib.c
@@ -1371,7 +1371,11 @@ static int super_setup_bdi_name(struct super_block *sb, char *fmt, ...)
 static unsigned long ll_count_objects(struct shrinker *shrink,
 				      struct shrink_control *sc)
 {
+#ifdef HAVE_S_SHRINK_AS_A_POINTER
+	struct super_block *sb = shrink->private_data;
+#else
 	struct super_block *sb = container_of(shrink, struct super_block, s_shrink);
+#endif
 	struct ll_sb_info *sbi = ll_s2sbi(sb);
 	unsigned int max = sbi->ll_max_shrink;
 	long total_objects;
@@ -1528,8 +1532,13 @@ int ll_fill_super(struct super_block *sb)
 	if (err < 0)
 		GOTO(out_free_md, err);
 
+#ifdef HAVE_S_SHRINK_AS_A_POINTER
+	sbi->ll_count_objects = sb->s_shrink->count_objects;
+	sb->s_shrink->count_objects = ll_count_objects;
+#else
 	sbi->ll_count_objects = sb->s_shrink.count_objects;
 	sb->s_shrink.count_objects = ll_count_objects;
+#endif
 	sbi->ll_max_shrink = 0;
 
 	sbi->ll_client_common_fill_super_succeeded = 1;