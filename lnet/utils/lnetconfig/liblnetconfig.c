lustre/lustre-release

commit fdc07e4ab73bfa9f318ca3feae10db11dc10485e

LU-19160 lnet: Fix CPT parsing in jt_import
jt_import() simply passes through the CPT yaml sequence to netlink,
however the kernel code expects user-space to have expanded any
specified ranges into a list of cores. i.e. the kernel understands
CPT: "[0, 1, 2, 3]"
but not
CPT: "[0-3]"

Modify jt_import() to use handle_cpt_sequence() when it encounters
a CPT sequence while processing a net sequence. handle_cpt_sequence()
is slightly modified to avoid code duplication now that it has an
additional caller.

A range like '[a-b]' is only valid if a <= b. Have
cfs_range_expr_parse() check this condition so that it can detect
invalid ranges.

While testing I found that yaml_lnet_config_ni() can segfault if
cfs_expr_list_values() is called with an empty list because it assumes
the cpt_array it passed in must be freed, but this is only the case
when the expression list is not empty. The code for emitting the CPT
sequence is moved into a new helper, lnet_yaml_emit_cpt_sequence(),
and the potential segfault is fixed by free'ing cpt_array only when
count > 0..

Tests are added to verify that invalid CPT sequences are rejected and
valid ones are parsed correctly.

Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I874e24e6d69aea9c56d72d07494998c9a869be55
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60031
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Frederick Dilger <fdilger@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lnet/utils/lnetctl.c
@@ -2016,6 +2016,57 @@ error:
 	return rc;
 }
 
+static int
+lnet_yaml_emit_cpt_sequence(yaml_emitter_t *emitter, struct cfs_expr_list *cpts)
+{
+	yaml_event_t event;
+	__u32 *cpt_array;
+	int count, i;
+	int rc = 0;
+
+	yaml_scalar_event_initialize(&event, NULL,
+				     (yaml_char_t *)YAML_STR_TAG,
+				     (yaml_char_t *)"CPT",
+				     strlen("CPT"), 1, 0,
+				     YAML_PLAIN_SCALAR_STYLE);
+	if (!yaml_emitter_emit(emitter, &event))
+		return 0;
+
+	yaml_sequence_start_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_SEQ_TAG,
+					     1,
+					     YAML_FLOW_SEQUENCE_STYLE);
+	if (!yaml_emitter_emit(emitter, &event))
+		return 0;
+
+	count = cfs_expr_list_values(cpts, LNET_MAX_SHOW_NUM_CPT, &cpt_array);
+	for (i = 0; i < count; i++) {
+		char core[INT_STRING_LEN];
+
+		snprintf(core, sizeof(core), "%u", cpt_array[i]);
+		yaml_scalar_event_initialize(&event, NULL,
+					     (yaml_char_t *)YAML_STR_TAG,
+					     (yaml_char_t *)core,
+					     strlen(core), 1, 0,
+					     YAML_PLAIN_SCALAR_STYLE);
+		if (!yaml_emitter_emit(emitter, &event))
+			goto out_free_expr;
+	}
+
+	yaml_sequence_end_event_initialize(&event);
+	if (!yaml_emitter_emit(emitter, &event))
+		goto out_free_expr;
+
+	rc = 1;
+
+out_free_expr:
+	cfs_expr_list_free(cpts);
+	if (count > 0)
+		free(cpt_array);
+
+	return rc;
+}
+
 static int yaml_lnet_config_ni(char *net_id, char *ip2net,
 			       struct lnet_dlc_network_descr *nw_descr,
 			       struct lnet_ioctl_config_lnd_tunables *tunables,
@@ -2289,50 +2340,9 @@ static int yaml_lnet_config_ni(char *net_id, char *ip2net,
 		}
 
 		if (global_cpts) {
-			__u32 *cpt_array;
-			int count, i;
-
-			yaml_scalar_event_initialize(&event, NULL,
-						     (yaml_char_t *)YAML_STR_TAG,
-						     (yaml_char_t *)"CPT",
-						     strlen("CPT"), 1, 0,
-						     YAML_PLAIN_SCALAR_STYLE);
-			rc = yaml_emitter_emit(&output, &event);
+			rc = lnet_yaml_emit_cpt_sequence(&output, global_cpts);
 			if (rc == 0)
 				goto emitter_error;
-
-			yaml_sequence_start_event_initialize(&event, NULL,
-							     (yaml_char_t *)YAML_SEQ_TAG,
-							     1,
-							     YAML_FLOW_SEQUENCE_STYLE);
-			rc = yaml_emitter_emit(&output, &event);
-			if (rc == 0)
-				goto emitter_error;
-
-			count = cfs_expr_list_values(global_cpts,
-						     LNET_MAX_SHOW_NUM_CPT,
-						     &cpt_array);
-			for (i = 0; i < count; i++) {
-				char core[INT_STRING_LEN];
-
-				snprintf(core, sizeof(core), "%u", cpt_array[i]);
-				yaml_scalar_event_initialize(&event, NULL,
-							     (yaml_char_t *)YAML_STR_TAG,
-							     (yaml_char_t *)core,
-							     strlen(core), 1, 0,
-							     YAML_PLAIN_SCALAR_STYLE);
-				rc = yaml_emitter_emit(&output, &event);
-				if (rc == 0)
-					goto emitter_error;
-			}
-
-			yaml_sequence_end_event_initialize(&event);
-			rc = yaml_emitter_emit(&output, &event);
-			if (rc == 0)
-				goto emitter_error;
-
-			cfs_expr_list_free(global_cpts);
-			free(cpt_array);
 		}
 
 		yaml_mapping_end_event_initialize(&event);
@@ -4729,6 +4739,25 @@ handle_cpt_sequence(yaml_parser_t *setup, yaml_event_t *event,
 {
 	int rc;
 	char errmsg[LNET_MAX_STR_LEN];
+	char *value;
+
+	yaml_event_delete(event);
+
+	rc = yaml_parser_parse(setup, event);
+	if (!rc)
+		goto yaml_parser_error;
+
+	if (event->type == YAML_SCALAR_EVENT) {
+		value = (char *)event->data.scalar.value;
+		rc = cfs_expr_list_parse(value, strlen(value), 0, UINT_MAX,
+					 global_cpts);
+		if (rc) {
+			snprintf(errmsg, LNET_MAX_STR_LEN,
+				 "Unable to parse CPT '%s'", value);
+			goto print_error;
+		}
+		return 1;
+	}
 
 	if (event->type != YAML_SEQUENCE_START_EVENT) {
 		snprintf(errmsg, sizeof(errmsg),
@@ -4798,8 +4827,9 @@ handle_cpt_sequence(yaml_parser_t *setup, yaml_event_t *event,
 			goto yaml_parser_error;
 	}
 
-	/* Note: Caller will delete the sequence end event */
 print_error:
+	yaml_event_delete(event);
+
 	if (rc < 0) {
 		errno = rc;
 		yaml_lnet_print_error(flags, "import", errmsg);
@@ -4807,10 +4837,9 @@ print_error:
 
 yaml_parser_error:
 	if (rc == 0) {
-		yaml_parser_log_error(setup, stderr, "ip2nets: ");
+		yaml_parser_log_error(setup, stderr, "import: ");
 		rc = -EINVAL;
 	}
-
 	return rc;
 }
 
@@ -5000,29 +5029,15 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 			}
 			lnd_tunables_set = true;
 		} else if (!strcmp(value, "CPT")) {
-			yaml_event_delete(&event);
+			rc = handle_cpt_sequence(setup, &event,
+						 &global_cpts, flags);
+			if (rc < 0 || !global_cpts)
+				goto out;
 
-			rc = yaml_parser_parse(setup, &event);
-			if (!rc)
-				goto yaml_parser_error;
-
-			if (event.type == YAML_SCALAR_EVENT) {
-				value = (char *)event.data.scalar.value;
-				rc = cfs_expr_list_parse(value, strlen(value),
-							 0, UINT_MAX,
-							 &global_cpts);
-				if (rc) {
-					snprintf(errmsg, LNET_MAX_STR_LEN,
-						 "Unable to parse CPT '%s'",
-						 value);
-					goto print_error;
-				}
-			} else {
-				rc = handle_cpt_sequence(setup, &event,
-							 &global_cpts, flags);
-				if (rc < 0)
-					goto out;
-			}
+			/* handle_cpt_sequence() deletes the last event that
+			 * it parsed
+			 */
+			continue;
 		}
 		yaml_event_delete(&event);
 	}
@@ -5072,6 +5087,7 @@ static int jt_import(int argc, char **argv)
 	const char *msg = NULL;
 	yaml_emitter_t output;
 	yaml_event_t event;
+	struct cfs_expr_list *global_cpts = NULL;
 
 	while ((opt = getopt_long(argc, argv, short_options,
 				   long_options, NULL)) != -1) {
@@ -5165,7 +5181,8 @@ static int jt_import(int argc, char **argv)
 		const char *scalar_value;
 		const struct command_mapping *mapping;
 
-		if (!yaml_parser_parse(&setup, &event)) {
+		rc = yaml_parser_parse(&setup, &event);
+		if (rc == 0) {
 			yaml_parser_log_error(&setup, stderr, "import: ");
 			break;
 		}
@@ -5197,6 +5214,20 @@ static int jt_import(int argc, char **argv)
 				goto free_reply;
 			else if (rc == 0)
 				goto emitter_error;
+		} else if (op == LNET_CMD_NETS &&
+			   !strcmp(scalar_value, "CPT")) {
+			rc = handle_cpt_sequence(&setup, &event, &global_cpts,
+						 flags);
+
+			if (rc < 0 || !global_cpts)
+				goto free_reply;
+
+			rc = lnet_yaml_emit_cpt_sequence(&output, global_cpts);
+			if (rc == 0)
+				goto emitter_error;
+
+			global_cpts = NULL;
+			continue;
 		} else if (!strcmp(scalar_value, "global")) {
 			if (op != LNET_CMD_UNSPEC) {
 				rc = yaml_netlink_complete_emitter(&output);

lustre/tests/sanity-lnet.sh
@@ -4572,6 +4572,95 @@ test_260() {
 }
 run_test 260 "test that linux sysctl parameter are set correctly"
 
+test_265() {
+	reinit_dlc || return $?
+
+	local cpt
+
+	for cpt in "[1-0]" "[]"; do
+		cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+      -     interfaces:
+                  0: ${INTERFACES[0]}
+            CPT: "${cpt}"
+EOF
+
+		! do_lnetctl import $TMP/sanity-lnet-$testnum.yaml ||
+			error "Import should have failed"
+
+		cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+      -     interfaces:
+                  0: ${INTERFACES[0]}
+            CPT: ${cpt}
+EOF
+
+		! do_lnetctl import $TMP/sanity-lnet-$testnum.yaml ||
+			error "Import should have failed"
+	done
+}
+run_test 265 "Import of invalid CPT should fail"
+
+test_266() {
+	reinit_dlc || return $?
+
+	add_net "${NETTYPE}" "${INTERFACES[0]}" || return $?
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
+
+	local cpt=$($LNETCTL net show -v --net ${NETTYPE} |
+		    awk '/CPT/{print $NF}')
+	local low=$(awk -F, '{print $1}'<<<"$cpt" | tr -d '[]"')
+	local high=$(awk -F, '{print $NF}'<<<"$cpt" | tr -d '[]"')
+
+	local tyaml=$TMP/sanity-lnet-$testnum.yaml
+
+	cat <<EOF > $tyaml
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+      -     interfaces:
+                  0: ${INTERFACES[0]}
+EOF
+	$LNETCTL net show -v -n ${NETTYPE} | awk '/^\s+tunables:$/,/^\s+CPT:/' |
+		grep -v -e 'dev cpt' -e 'CPT' >> $tyaml
+
+	echo "            CPT: \"[$low-$high]\"" >> $tyaml
+
+	reinit_dlc || return $?
+
+	do_lnetctl import $tyaml || error "Import failed rc = $?"
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-actual.yaml
+
+	compare_yaml_files || return $?
+
+	cat <<EOF > $tyaml
+net:
+-     net type: ${NETTYPE}
+      local NI(s):
+      -     interfaces:
+                  0: ${INTERFACES[0]}
+EOF
+	$LNETCTL net show -v -n ${NETTYPE} | awk '/^\s+tunables:$/,/^\s+CPT:/' |
+		grep -v -e 'dev cpt' -e 'CPT' >> $tyaml
+
+	echo "            CPT: [$low-$high]" >> $tyaml
+
+	reinit_dlc || return $?
+
+	do_lnetctl import $tyaml || error "Import failed rc = $?"
+
+	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-actual.yaml
+
+	compare_yaml_files || return $?
+}
+run_test 266 "Validate CPT parsing in network sequence"
+
 test_270() {
 	[[ "$NETTYPE" =~ tcp ]] || skip "Need tcp NETTYPE"
 

libcfs/libcfs/util/string.c
@@ -177,6 +177,9 @@ cfs_range_expr_parse(struct cfs_lstr *src, unsigned min, unsigned max,
 	}
 
  out:
+	if (re->re_lo > re->re_hi)
+		goto failed;
+
 	*expr = re;
 	return 0;
 

lnet/lnet/nidstrings.c
@@ -241,6 +241,9 @@ cfs_range_expr_parse(char *src, unsigned int min, unsigned int max,
 	re->re_stride = num;
 
 out:
+	if (re->re_lo > re->re_hi)
+		goto failed;
+
 	*expr = re;
 	return 0;
 

Cray/lustre

commit 6ba1619fdc36f95816aa263ef3f4eb243238d781

LUS-13075 lnet: use ip2nets handler for net sequence
Modify handle_ip2nets_sequence() (renamed to
handle_net_config_sequence()) to handle both the "ip2nets" and "net"
yaml sequences. This allows LNet to understand a net sequence where
more than one interface is specified.
i.e.

net:
-     net type: <net>
      local NI(s):
      -     interfaces:
	          0: <intf1>
	          1: <intf2>
                  ...
                  N: <intfN>

- Add validation to ensure ip2nets-specific fields like "ip-range"
  are only used with ip2nets sequences
- Add validation to ensure net-specific fields like "local NI(s)"
  are only used with net sequences
- Fix memory leak by properly freeing global_cpts in cleanup paths
- Remove cfs_expr_list_free(cpts) call from
  lnet_yaml_emit_cpt_sequence(). Callers are responsible for freeing
  this memory. This prevents some double-free and use-after-free bugs.
- Change "Unrecognizable key" in handle_lnd/cmn_tunable() to a
  warning. This aligns with how unknown keys are ignored by the
  kernel.
- Logic was missing from yaml_lnet_config_ni() to check whether
  struct lnet_dlc_intf_descr::cpt_expr was set, and to use it in place
  of the global CPT configuration. This is now done.

Fixes: fdc07e4ab7 ("LU-19160 lnet: Fix CPT parsing in jt_import")
Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I9f0cafe34afc732e19ae7d2aa7f80888c2f090ed
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163450
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Jenkins Build User <nssreleng@cray.com>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -1703,7 +1703,8 @@ failed:
 static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 					struct list_head *intf_list,
 					struct list_head *ip_ranges,
-					char *err_str, size_t str_len)
+					char *err_str, size_t str_len,
+					__u32 net_id)
 {
 	int rc;
 	__u32 ip;
@@ -1810,6 +1811,23 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 		}
 
 		if (ifaddr == NULL) {
+			/*
+			 * kfilnd interfaces like cxi0 may refer to hardware
+			 * devices not visible as standard Linux network
+			 * interfaces. Accept them without validation only if:
+			 * 1. This is a kfilnd network
+			 * 2. No ip-ranges were specified (validation required)
+			 * 3. Interface matches cxi[0-9]+ pattern
+			 */
+			if (LNET_NETTYP(net_id) == KFILND &&
+			    list_empty(ip_ranges)) {
+				unsigned int idx;
+
+				if (sscanf(intf_descr->intf_name, "cxi%u",
+					   &idx) == 1)
+					continue;
+			}
+
 			snprintf(err_str, str_len, "No interface matching '%s'",
 				 intf_descr->intf_name);
 			list_del(&intf_descr->intf_on_network);
@@ -1869,7 +1887,8 @@ int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
 	rc = lustre_lnet_match_ip_to_intf(ifa,
 					  &ip2nets->ip2nets_net.nw_intflist,
 					  &ip2nets->ip2nets_ip_ranges, err_str,
-					  str_len);
+					  str_len,
+					  ip2nets->ip2nets_net.nw_id);
 	if (rc != LUSTRE_CFG_RC_MATCH) {
 		freeifaddrs(ifa);
 		return rc;

lnet/utils/lnetctl.c
@@ -2042,7 +2042,6 @@ lnet_yaml_emit_cpt_sequence(yaml_emitter_t *emitter, struct cfs_expr_list *cpts)
 	rc = 1;
 
 out_free_expr:
-	cfs_expr_list_free(cpts);
 	if (count > 0)
 		free(cpt_array);
 
@@ -2201,6 +2200,8 @@ static int yaml_lnet_config_ni(char *net_id, char *ip2net,
 
 	list_for_each_entry(intf, &nw_descr->nw_intflist,
 			    intf_on_network) {
+		struct cfs_expr_list *cpt_expr = NULL;
+
 		yaml_mapping_start_event_initialize(&event, NULL,
 						    (yaml_char_t *)YAML_MAP_TAG,
 						    1, YAML_ANY_MAPPING_STYLE);
@@ -2321,8 +2322,13 @@ static int yaml_lnet_config_ni(char *net_id, char *ip2net,
 				goto emitter_error;
 		}
 
-		if (global_cpts) {
-			rc = lnet_yaml_emit_cpt_sequence(&output, global_cpts);
+		if (intf->cpt_expr)
+			cpt_expr = intf->cpt_expr;
+		else if (global_cpts)
+			cpt_expr = global_cpts;
+
+		if (cpt_expr) {
+			rc = lnet_yaml_emit_cpt_sequence(&output, cpt_expr);
 			if (rc == 0)
 				goto emitter_error;
 		}
@@ -4277,7 +4283,6 @@ struct command_mapping {
 };
 
 static const struct command_mapping cmd_mappings[] = {
-	{"net", LNET_CMD_NETS, 0, false},
 	{"peer", LNET_CMD_PEERS, 0, false},
 	{"route", LNET_CMD_ROUTES, 0, false},
 	{"discover", LNET_CMD_PING, 0, true},
@@ -4518,7 +4523,6 @@ handle_cmn_tunable(__u32 net_id,
 	struct lnet_ioctl_config_lnd_cmn_tunables *cmn_tun = &tunables->lt_cmn;
 	char *key;
 	long value;
-	char errmsg[LNET_MAX_STR_LEN];
 	int rc;
 
 	key = strdup((char *)event->data.scalar.value);
@@ -4544,16 +4548,14 @@ handle_cmn_tunable(__u32 net_id,
 	} else if (!strcmp(key, "credits")) {
 		cmn_tun->lct_max_tx_credits = value;
 	} else {
-		snprintf(errmsg, LNET_MAX_STR_LEN, "invalid key '%s'", key);
-		errno = rc = -EINVAL;
+		fprintf(stderr,
+			"Ignoring unrecognized key '%s' for 'tunables'\n",
+			key);
 	}
 
 	yaml_event_delete(event);
 
-	if (rc)
-		yaml_lnet_print_error(flags, "import", errmsg);
-	else
-		rc = 1;
+	rc = 1;
 
 out_free_key:
 	free(key);
@@ -4621,14 +4623,13 @@ handle_lnd_tunable(__u32 net_id,
 		else if (LNET_NETTYP(net_id) == O2IBLND)
 			lnd_tun->lnd_tun_u.lnd_o2ib.lnd_tos = value;
 	} else {
-		snprintf(errmsg, LNET_MAX_STR_LEN,
-			 "Unrecognized key '%s' for 'lnd tunables'", key);
-		errno = rc = -EINVAL;
-		yaml_lnet_print_error(flags, "import", errmsg);
-		goto out_free_key;
+		fprintf(stderr,
+			"Ignoring unrecognized key '%s' for 'lnd tunables'\n",
+			key);
 	}
 
 	yaml_event_delete(event);
+
 	rc = 1;
 
 out_free_key:
@@ -4801,9 +4802,29 @@ yaml_parser_error:
  *          <lnd_param1>: <val>
  *          <lnd_param2>: <val>
  *          <...>
+ *    CPT: "[<expr]"
+ *
+ * or
+ *
+ * net:
+ *  - net type: <net>[NUM]
+ *    interfaces:
+ *        0: <intf name>['['<expr>']']
+ *        1: <intf name>['['<expr>']']
+ *    tunables:
+ *          peer_timeout: <NUM>
+ *          peer_credits: <NUM>
+ *          peer_buffer_credits: <NUM>
+ *          credits: <NUM>
+ *    lnd tunables:
+ *          <lnd_param1>: <val>
+ *          <lnd_param2>: <val>
+ *          <...>
+ *    CPT: "[<expr]"
  */
 static int
-handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
+handle_net_config_sequence(yaml_parser_t *setup, int flags,
+			   bool is_ip2nets_sequence)
 {
 	struct lustre_lnet_ip2nets ip2nets;
 	struct lnet_dlc_network_descr *nw_descr = &ip2nets.ip2nets_net;
@@ -4817,6 +4838,8 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 	/* Track configured networks to ensure uniqueness for ip2nets */
 	__u32 *configured_nets = NULL;
 	size_t nets_cnt = 0;
+	unsigned int seq_depth = 0;
+	unsigned int map_depth = 0;
 
 	init_ip2nets_tunables(&ip2nets, &tunables);
 
@@ -4827,10 +4850,27 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 		if (!rc)
 			goto yaml_parser_error;
 
-		/* Reached the end of the ip2nets sequence */
-		if (event.type == YAML_SEQUENCE_END_EVENT) {
+		switch (event.type) {
+		case YAML_SEQUENCE_START_EVENT:
+			seq_depth++;
+			break;
+		case YAML_SEQUENCE_END_EVENT:
+			seq_depth--;
+			break;
+		case YAML_MAPPING_START_EVENT:
+			map_depth++;
+			break;
+		case YAML_MAPPING_END_EVENT:
+			map_depth--;
+			break;
+		default:
+			break;
+		}
+
+		/* Reached the end of the ip2nets/net sequence */
+		if (event.type == YAML_SEQUENCE_END_EVENT && seq_depth == 0) {
 			yaml_event_delete(&event);
-			if (nets_cnt == 0) {
+			if (is_ip2nets_sequence && nets_cnt == 0) {
 				/* If none of the rules applied then return an
 				 * error
 				 */
@@ -4844,13 +4884,13 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 		}
 
 		/* Reached the end of a network specification block */
-		if (event.type == YAML_MAPPING_END_EVENT) {
+		if (event.type == YAML_MAPPING_END_EVENT && map_depth == 0) {
 			struct lnet_ioctl_config_lnd_tunables *tun = NULL;
 			char *net;
 			lnet_nid_t *nids = NULL;
 			__u32 nnids = 0;
 			bool duplicate_net = false;
-			int i;
+			size_t i;
 			__u32 *tmp;
 
 			rc = lustre_lnet_resolve_ip2nets_rule(&ip2nets, &nids,
@@ -4863,7 +4903,8 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 			 * an error
 			 */
 			if (rc != LUSTRE_CFG_RC_NO_ERR &&
-			    rc != LUSTRE_CFG_RC_NO_MATCH)
+			    !(is_ip2nets_sequence &&
+			      rc == LUSTRE_CFG_RC_NO_MATCH))
 				goto print_error;
 
 			/* Skip configuration if resolution failed */
@@ -4879,8 +4920,8 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 				}
 			}
 
-			/* Skip configuration if duplicate */
-			if (duplicate_net)
+			/* Skip configuration for ip2nets if duplicate */
+			if (duplicate_net && is_ip2nets_sequence)
 				goto cleanup_block;
 
 			/* Configure the network interface */
@@ -4916,6 +4957,8 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 cleanup_block:
 			free_ip2nets_lists(&ip2nets);
 			init_ip2nets_tunables(&ip2nets, &tunables);
+			if (global_cpts)
+				cfs_expr_list_free(global_cpts);
 			global_cpts = NULL;
 			tunables_set = lnd_tunables_set = false;
 		}
@@ -4928,17 +4971,27 @@ cleanup_block:
 
 		value = (char *)event.data.scalar.value;
 
-		/* "net-spec" must be the first scalar */
-		if (ip2nets.ip2nets_net.nw_id == LNET_NET_ANY &&
-		    strcmp(value, "net-spec")) {
-			snprintf(errmsg, LNET_MAX_STR_LEN,
-				 "Malformed input. Expect 'net-spec' found '%s'",
-				 value);
-			rc = -EINVAL;
-			goto print_error;
+		/* "net-spec" must be the first scalar for an ip2nets sequence,
+		 * and "net type" must be the first in a net sequence
+		 */
+		if (ip2nets.ip2nets_net.nw_id == LNET_NET_ANY) {
+			if (is_ip2nets_sequence && strcmp(value, "net-spec")) {
+				snprintf(errmsg, LNET_MAX_STR_LEN,
+					 "Malformed input. Expect 'net-spec' found '%s'",
+					 value);
+				rc = -EINVAL;
+				goto print_error;
+			} else if (!is_ip2nets_sequence &&
+				 strcmp(value, "net type")) {
+				snprintf(errmsg, LNET_MAX_STR_LEN,
+					 "Malformed input. Expect 'net type' found '%s'",
+					 value);
+				rc = -EINVAL;
+				goto print_error;
+			}
 		}
 
-		if (!strcmp(value, "net-spec")) {
+		if (!strcmp(value, "net-spec") || !strcmp(value, "net type")) {
 			yaml_event_delete(&event);
 
 			rc = yaml_parser_parse(setup, &event);
@@ -4969,7 +5022,21 @@ cleanup_block:
 			} else if (!rc) {
 				goto yaml_parser_error;
 			}
+		} else if (!strcmp(value, "local NI(s)")) {
+			if (is_ip2nets_sequence) {
+				snprintf(errmsg, LNET_MAX_STR_LEN,
+					 "local NI(s)' not valid for ip2nets");
+				rc = -EINVAL;
+				goto print_error;
+			}
 		} else if (!strcmp(value, "ip-range")) {
+			if (!is_ip2nets_sequence) {
+				snprintf(errmsg, LNET_MAX_STR_LEN,
+					 "'ip-range' only valid for ip2nets");
+				rc = -EINVAL;
+				goto print_error;
+			}
+
 			yaml_event_delete(&event);
 
 			rc = parse_yaml_list(setup, &ip2nets.ip2nets_ip_ranges,
@@ -5041,6 +5108,8 @@ out:
 	free_ip2nets_lists(&ip2nets);
 	if (configured_nets)
 		free(configured_nets);
+	if (global_cpts)
+		cfs_expr_list_free(global_cpts);
 
 	return (rc < 0) ? rc : 1;
 }
@@ -5189,14 +5258,23 @@ static int jt_import(int argc, char **argv)
 			if (rc == 0)
 				goto emitter_error;
 			unspec = false;
-		} else if (!strcmp(scalar_value, "ip2nets")) {
+		} else if (!strcmp(scalar_value, "ip2nets") ||
+			   !strcmp(scalar_value, "net")) {
+			bool is_ip2nets = strcmp(scalar_value, "ip2nets") == 0;
+
+			/* The "route" yaml block can contain a "net" key */
+			if (!is_ip2nets && op == LNET_CMD_ROUTES)
+				goto skip_test;
+
 			if (op != LNET_CMD_UNSPEC) {
 				rc = yaml_netlink_complete_emitter(&output);
 				if (rc == 0)
 					goto emitter_error;
 			}
 			op = LNET_CMD_UNSPEC;
-			rc = handle_ip2nets_sequence(&setup, flags);
+
+			rc = handle_net_config_sequence(&setup, flags,
+							is_ip2nets);
 			if (rc < 0)
 				goto free_reply;
 			else if (rc == 0)
@@ -5213,6 +5291,7 @@ static int jt_import(int argc, char **argv)
 			if (rc == 0)
 				goto emitter_error;
 
+			cfs_expr_list_free(global_cpts);
 			global_cpts = NULL;
 			continue;
 		} else if (!strcmp(scalar_value, "global")) {

lustre/tests/sanity-lnet.sh
@@ -5264,14 +5264,14 @@ test_450() {
 
 	setup_fakeif || return $?
 
-	add_net "tcp" "${INTERFACES[0]}" || return $?
-	add_net "tcp" "$FAKE_IF" || return $?
+	add_net "$NETTYPE" "${INTERFACES[0]}" || return $?
+	add_net "$NETTYPE" "$FAKE_IF" || return $?
 
 	$LNETCTL export --backup > $TMP/sanity-lnet-$testnum-expected.yaml
 
 	cat <<EOF > $TMP/sanity-lnet-$testnum.yaml
 net:
-    - net type: tcp
+    - net type: $NETTYPE
       local NI(s):
         - interfaces:
               0: ${INTERFACES[0]}