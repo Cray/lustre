lustre/lustre-release

commit aa2fa25325edfb6ee03ee343a46cfc2acfd68b42

LU-10391 lnet: support setting LND timeouts
The patch that added support for NI setup with Netlink was
developed before individual LND timeout settings support was
merged. Add this missing settings. For ksocklnd we already
supported conns_per_peer so rearrange the code into a switch
statement.

Test-Parameters: trivial testlist=sanity-lnet
Fixes: 8f8f6e2f36e ("LU-10003 lnet: use Netlink to support old and new NI APIs.")
Change-Id: Iba955da7f5fa78b8a624bab6af66b577c75917e0
Signed-off-by: James Simmons <jsimmons@infradead.org>
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/53013
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Chris Horn <chris.horn@hpe.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lnet/klnds/gnilnd/gnilnd.c
@@ -27,7 +27,6 @@
 static int
 kgnilnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data)
 {
-	struct lnet_ioctl_config_gnilnd_tunables *tuns;
 	struct lnet_ni *ni = data;
 
 	if (!ni || !msg)
@@ -36,9 +35,28 @@ kgnilnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data)
 	if (cmd != LNET_CMD_NETS || type != LNET_NET_LOCAL_NI_ATTR_LND_TUNABLES)
 		return -EOPNOTSUPP;
 
-	tuns = &ni->ni_lnd_tunables.lnd_tun_u.lnd_gni;
 	nla_put_u32(msg, LNET_NET_GNILND_TUNABLES_ATTR_LND_TIMEOUT,
 		    kgnilnd_timeout());
+	return 0;
+}
+
+static int
+kgnilnd_nl_set(int cmd, struct nlattr *attr, int type, void *data)
+{
+	struct lnet_ni *ni = data;
+
+	if (cmd != LNET_CMD_NETS)
+		return -EOPNOTSUPP;
+
+	if (nla_type(attr) != LN_SCALAR_ATTR_INT_VALUE)
+		return -EINVAL;
+
+	if (type == LNET_NET_GNILND_TUNABLES_ATTR_LND_TIMEOUT) {
+		s64 timeout = nla_get_s64(attr);
+
+		ni->ni_lnd_tunables.lnd_tun_u.lnd_gni.lnd_timeout = timeout;
+	}
+
 	return 0;
 }
 
@@ -52,6 +70,7 @@ const struct lnet_lnd the_kgnilnd = {
 	.lnd_recv       = kgnilnd_recv,
 	.lnd_eager_recv = kgnilnd_eager_recv,
 	.lnd_nl_get	= kgnilnd_nl_get,
+	.lnd_nl_set	= kgnilnd_nl_set,
 };
 
 kgn_data_t      kgnilnd_data;

lnet/klnds/o2iblnd/o2iblnd.c
@@ -1328,6 +1328,9 @@ kiblnd_nl_set(int cmd, struct nlattr *attr, int type, void *data)
 	case LNET_NET_O2IBLND_TUNABLES_ATTR_NTX:
 		tunables->lnd_tun_u.lnd_o2ib.lnd_ntx = nla_get_s64(attr);
 		break;
+	case LNET_NET_O2IBLND_TUNABLES_ATTR_LND_TIMEOUT:
+		tunables->lnd_tun_u.lnd_o2ib.lnd_timeout = nla_get_s64(attr);
+		break;
 	case LNET_NET_O2IBLND_TUNABLES_ATTR_CONNS_PER_PEER:
 		num = nla_get_s64(attr);
 		clamp_t(s64, num, 1, 127);

lnet/klnds/socklnd/socklnd.c
@@ -845,13 +845,23 @@ ksocknal_nl_set(int cmd, struct nlattr *attr, int type, void *data)
 	if (cmd != LNET_CMD_NETS)
 		return -EOPNOTSUPP;
 
-	if (type != LNET_NET_SOCKLND_TUNABLES_ATTR_CONNS_PER_PEER ||
-	    nla_type(attr) != LN_SCALAR_ATTR_INT_VALUE)
+	if (nla_type(attr) != LN_SCALAR_ATTR_INT_VALUE)
 		return -EINVAL;
 
-	num = nla_get_s64(attr);
-	clamp_t(s64, num, 1, 127);
-	tunables->lnd_tun_u.lnd_sock.lnd_conns_per_peer = num;
+	switch (type) {
+	case LNET_NET_SOCKLND_TUNABLES_ATTR_CONNS_PER_PEER:
+		/* value values are 1 to 127. Zero mean calculate the value */
+		num = nla_get_s64(attr);
+		clamp_t(s64, num, 0, 127);
+		tunables->lnd_tun_u.lnd_sock.lnd_conns_per_peer = num;
+		break;
+	case LNET_NET_SOCKLND_TUNABLES_ATTR_LND_TIMEOUT:
+		num = nla_get_s64(attr);
+		tunables->lnd_tun_u.lnd_sock.lnd_timeout = num;
+		fallthrough;
+	default:
+		break;
+	}
 
 	return 0;
 }

Cray/lustre

commit f6fb62e99b4ef12f654d127200cf4ce31c06a422

LUS-13095 build: unused 'ni'
lnet/klnds/gnilnd/gnilnd.c:37:18: error: \
   unused variable 'ni' [-Werror=unused-variable]

Fixes: aa2fa25325 ("LU-10391 lnet: support setting LND timeouts")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: Ic16ce7a6af1bf95578137f0ce43bd3d3639f1b83
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163467
Tested-by: Jenkins Build User <nssreleng@cray.com>
Reviewed-by: Cory Spitz <cory.spitz@hpe.com>
Reviewed-by: Nikitas Angelinas <nikitas.angelinas@hpe.com>
Reviewed-by: Christopher Horn <chris.horn@hpe.com>

lnet/klnds/gnilnd/gnilnd.c
@@ -34,8 +34,6 @@ kgnilnd_nl_get(int cmd, struct sk_buff *msg, int type, void *data,
 static int
 kgnilnd_nl_set(int cmd, struct nlattr *attr, int type, void *data)
 {
-	struct lnet_ni *ni = data;
-
 	if (cmd != LNET_CMD_NETS)
 		return -EOPNOTSUPP;
 