lustre/lustre-release

commit 144da866d6598ef552b6d306aa2a5632b399200f

LU-19159 lnet: Handle ip2nets in jt_import
jt_import() is updated to understand ip2nets yaml sequence.

Note that not all tunables and lnd tunables are supported because
yaml_lnet_config_ni does not understand all of them. This will be
fixed in a separate patch.

Test-Parameters: trivial testlist=sanity-lnet
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I2bf41e5f97714c66e43172e758a36f91311ec88c
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/60035
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Serguei Smirnov <ssmirnov@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -49,25 +49,6 @@
 const char *gmsg_stat_names[] = {"sent_stats", "received_stats",
 				 "dropped_stats"};
 
-/*
- * lustre_lnet_ip_range_descr
- *	Describes an IP range.
- *	Each octect is an expression
- */
-struct lustre_lnet_ip_range_descr {
-	struct list_head ipr_entry;
-	struct list_head ipr_expr;
-};
-
-/*
- * lustre_lnet_ip2nets
- *	Describes an ip2nets rule. This can be on a list of rules.
- */
-struct lustre_lnet_ip2nets {
-	struct lnet_dlc_network_descr ip2nets_net;
-	struct list_head ip2nets_ip_ranges;
-};
-
 static int open_sysfs_file(const char *path, const char *attr, const int mode)
 {
 	int fd;
@@ -128,7 +109,7 @@ close_fd:
  * free_intf_descr
  *	frees the memory allocated for an intf descriptor.
  */
-static void free_intf_descr(struct lnet_dlc_intf_descr *intf_descr)
+void free_intf_descr(struct lnet_dlc_intf_descr *intf_descr)
 {
 	if (!intf_descr)
 		return;
@@ -150,7 +131,7 @@ static void free_intf_descr(struct lnet_dlc_intf_descr *intf_descr)
  *		As a result of lnetctl command
  *		When building a NID or P2P selection rules
  */
-static int lustre_lnet_add_ip_range(struct list_head *list, char *str_ip_range)
+int lustre_lnet_add_ip_range(struct list_head *list, char *str_ip_range)
 {
 	struct lustre_lnet_ip_range_descr *ip_range;
 	int rc;
@@ -172,7 +153,7 @@ static int lustre_lnet_add_ip_range(struct list_head *list, char *str_ip_range)
 	return LUSTRE_CFG_RC_NO_ERR;
 }
 
-static int lustre_lnet_add_intf_descr(struct list_head *list, char *intf,
+int lustre_lnet_add_intf_descr(struct list_head *list, char *intf,
 				      int len)
 {
 	char *open_sq_bracket = NULL, *close_sq_bracket = NULL,
@@ -1929,7 +1910,7 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 	return LUSTRE_CFG_RC_MATCH;
 }
 
-static int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
+int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
 					    lnet_nid_t **nids, __u32 *nnids,
 					    char *err_str, size_t str_len)
 {
@@ -2342,7 +2323,7 @@ lustre_lnet_ioctl_config_ni(struct list_head *intf_list,
 	return LUSTRE_CFG_RC_NO_ERR;
 }
 
-static int
+int
 lustre_lnet_config_ip2nets(struct lustre_lnet_ip2nets *ip2nets,
 			   struct lnet_ioctl_config_lnd_tunables *tunables,
 			   struct cfs_expr_list *global_cpts,
@@ -5345,25 +5326,20 @@ yaml_extract_cpt(struct cYAML *tree,
 
 /*
  * net:
- *    - net type: <net>[<NUM>]
-  *      local NI(s):
- *        - nid: <ip>@<net>[<NUM>]
- *          status: up
- *          interfaces:
- *               0: <intf_name>['['<expr>']']
- *               1: <intf_name>['['<expr>']']
- *        tunables:
- *               peer_timeout: <NUM>
- *               peer_credits: <NUM>
- *               peer_buffer_credits: <NUM>
- *               credits: <NUM>
-*         lnd tunables:
- *               peercredits_hiw: <NUM>
- *               map_on_demand: <NUM>
- *               concurrent_sends: <NUM>
- *               fmr_pool_size: <NUM>
- *               fmr_flush_trigger: <NUM>
- *               fmr_cache: <NUM>
+ * -     net type: <net>[<NUM>]
+ *       local NI(s):
+ *       -     interfaces:
+ *                   0: <intf_name>['['<expr>']']
+ *                   1: <intf_name>['['<expr>']']
+ *             tunables:
+ *                   peer_timeout: <NUM>
+ *                   peer_credits: <NUM>
+ *                   peer_buffer_credits: <NUM>
+ *                   credits: <NUM>
+ *             lnd tunables:
+ *                   <lnd_param1>: <val>
+ *                   <lnd_param2>: <val>
+ *                   <...>
  *
  * At least one interface is required. If no interfaces are provided the
  * network interface can not be configured.
@@ -5442,13 +5418,22 @@ static int handle_yaml_config_ni(struct cYAML *tree, struct cYAML **show_rc,
 
 /*
  * ip2nets:
- *  - net-spec: <tcp|o2ib|gni>[NUM]
+ *  - net-spec: <net>[NUM]
  *    interfaces:
  *        0: <intf name>['['<expr>']']
  *        1: <intf name>['['<expr>']']
  *    ip-range:
  *        0: <expr.expr.expr.expr>
  *        1: <expr.expr.expr.expr>
+ *    tunables:
+ *          peer_timeout: <NUM>
+ *          peer_credits: <NUM>
+ *          peer_buffer_credits: <NUM>
+ *          credits: <NUM>
+ *    lnd tunables:
+ *          <lnd_param1>: <val>
+ *          <lnd_param2>: <val>
+ *          <...>
  */
 static int handle_yaml_config_ip2nets(struct cYAML *tree,
 				      struct cYAML **show_rc,

Cray/lustre

commit e04254b08ee851a02643bbda984c3d1fabba0144

LUS-13074 lnet: Correct ip2nets yaml parsing
The ip2nets parsing in handle_ip2nets_sequence() had several issues
that prevented it from adhering to documented behavior:

- Rules that didn't match local interfaces were incorrectly treated
  as errors instead of being silently skipped as documented
- Duplicate networks could be configured multiple times in a sequence
- Error messages were generic and didn't help diagnose configuration
  issues

This patch fixes the parsing logic to:
- Properly handle LUSTRE_CFG_RC_NO_MATCH as a valid state for
  ip2nets rules that don't apply to the current node
- To accomodate this change, lustre_lnet_kfi_intf2nid() was changed
  to return RC_BAD_PARAM rather than RC_NO_MATCH on error.
- Track configured networks to prevent duplicates within sequences
- Provide specific error messages for different failure scenarios
  (interface not found, interface down, IP pattern mismatch, etc.)

The changes ensure that ip2nets behaves as documented: each node
identifies locally available networks based on IP address pattern
matching, with non-matching rules being silently ignored rather
than causing configuration failures.

Test-Parameters: trivial
Fixes: 144da866d6 ("LU-19159 lnet: Handle ip2nets in jt_import")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I6e1dac32423fc4f2056d30f0b10fa2dd847cd490
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163448
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

lnet/utils/lnetconfig/liblnetconfig.c
@@ -1541,7 +1541,7 @@ static int lustre_lnet_kfi_intf2nid(struct lnet_dlc_intf_descr *intf,
 
 	rc = sscanf(intf->intf_name, "cxi%u", &nic_index);
 	if (rc != 1)
-		return LUSTRE_CFG_RC_NO_MATCH;
+		return LUSTRE_CFG_RC_BAD_PARAM;
 
 	size = snprintf(NULL, 0, cxi_nic_addr_path, nic_index) + 1;
 	nic_addr_path = malloc(size);
@@ -1552,11 +1552,11 @@ static int lustre_lnet_kfi_intf2nid(struct lnet_dlc_intf_descr *intf,
 	rc = read_sysfs_file(nic_addr_path, "nic_addr", val, 1, sizeof(val));
 	free(nic_addr_path);
 	if (rc)
-		return LUSTRE_CFG_RC_NO_MATCH;
+		return LUSTRE_CFG_RC_BAD_PARAM;
 
 	addr = strtol(val, NULL, 16);
 	if (addr == LONG_MIN || addr == LONG_MAX)
-		return LUSTRE_CFG_RC_NO_MATCH;
+		return LUSTRE_CFG_RC_BAD_PARAM;
 
 	*nid_addr = addr;
 
@@ -1612,6 +1612,7 @@ static int lustre_lnet_intf2nids(struct lnet_dlc_network_descr *nw,
 		if (rc) {
 			snprintf(err_str, str_len,
 				 "\"cannot read gni nid\"");
+			rc = LUSTRE_CFG_RC_BAD_PARAM;
 			goto failed;
 		}
 		gni_num = atoi(val);
@@ -1667,7 +1668,7 @@ static int lustre_lnet_intf2nids(struct lnet_dlc_network_descr *nw,
 out:
 	*nnids = count;
 
-	return 0;
+	return LUSTRE_CFG_RC_NO_ERR;
 
 failed:
 	free(*nids);
@@ -1701,7 +1702,8 @@ failed:
  */
 static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 					struct list_head *intf_list,
-					struct list_head *ip_ranges)
+					struct list_head *ip_ranges,
+					char *err_str, size_t str_len)
 {
 	int rc;
 	__u32 ip;
@@ -1735,6 +1737,8 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 				return LUSTRE_CFG_RC_MATCH;
 			}
 		}
+
+		snprintf(err_str, str_len, "No UP interfaces were found");
 		return LUSTRE_CFG_RC_NO_MATCH;
 	}
 
@@ -1779,6 +1783,9 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 		if (!list_empty(intf_list))
 			return LUSTRE_CFG_RC_MATCH;
 
+		snprintf(err_str, str_len,
+			 "IP pattern(s) do not match available interfaces");
+
 		return LUSTRE_CFG_RC_NO_MATCH;
 	}
 
@@ -1803,15 +1810,20 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 		}
 
 		if (ifaddr == NULL) {
+			snprintf(err_str, str_len, "No interface matching '%s'",
+				 intf_descr->intf_name);
 			list_del(&intf_descr->intf_on_network);
 			free_intf_descr(intf_descr);
-			continue;
+			return LUSTRE_CFG_RC_NO_MATCH;
 		}
 
 		if ((ifaddr->ifa_flags & IFF_UP) == 0) {
+			snprintf(err_str, str_len,
+				 "Matched interface '%s' is not UP",
+				 intf_descr->intf_name);
 			list_del(&intf_descr->intf_on_network);
 			free_intf_descr(intf_descr);
-			continue;
+			return LUSTRE_CFG_RC_NO_MATCH;
 		}
 
 		ip = ((struct sockaddr_in *)ifaddr->ifa_addr)->sin_addr.s_addr;
@@ -1824,9 +1836,16 @@ static int lustre_lnet_match_ip_to_intf(struct ifaddrs *ifa,
 		}
 
 		if (!rc) {
-			/* no match for this interface */
+			/* This interface was specified by user but does not
+			 * match any IP-range that was specified. Therefore,
+			 * this ip2nets rule doesn't apply to this node
+			 */
+			snprintf(err_str, str_len,
+				 "Interface '%s' doesn't match an IP pattern",
+				 intf_descr->intf_name);
 			list_del(&intf_descr->intf_on_network);
 			free_intf_descr(intf_descr);
+			return LUSTRE_CFG_RC_NO_MATCH;
 		}
 	}
 
@@ -1849,10 +1868,9 @@ int lustre_lnet_resolve_ip2nets_rule(struct lustre_lnet_ip2nets *ip2nets,
 
 	rc = lustre_lnet_match_ip_to_intf(ifa,
 					  &ip2nets->ip2nets_net.nw_intflist,
-					  &ip2nets->ip2nets_ip_ranges);
+					  &ip2nets->ip2nets_ip_ranges, err_str,
+					  str_len);
 	if (rc != LUSTRE_CFG_RC_MATCH) {
-		snprintf(err_str, str_len,
-			 "\"couldn't match ip to existing interfaces\"");
 		freeifaddrs(ifa);
 		return rc;
 	}

lnet/utils/lnetctl.c
@@ -4814,6 +4814,9 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 	char errmsg[LNET_MAX_STR_LEN];
 	bool tunables_set = false;
 	bool lnd_tunables_set = false;
+	/* Track configured networks to ensure uniqueness for ip2nets */
+	__u32 *configured_nets = NULL;
+	size_t nets_cnt = 0;
 
 	init_ip2nets_tunables(&ip2nets, &tunables);
 
@@ -4827,6 +4830,16 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 		/* Reached the end of the ip2nets sequence */
 		if (event.type == YAML_SEQUENCE_END_EVENT) {
 			yaml_event_delete(&event);
+			if (nets_cnt == 0) {
+				/* If none of the rules applied then return an
+				 * error
+				 */
+				rc = -EINVAL;
+				snprintf(errmsg, LNET_MAX_STR_LEN,
+					 "No ip2nets rules applied");
+				goto print_error;
+			}
+
 			goto out;
 		}
 
@@ -4836,25 +4849,41 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 			char *net;
 			lnet_nid_t *nids = NULL;
 			__u32 nnids = 0;
-			char err_str[LNET_MAX_STR_LEN] = "\"success\"";
+			bool duplicate_net = false;
+			int i;
+			__u32 *tmp;
 
 			rc = lustre_lnet_resolve_ip2nets_rule(&ip2nets, &nids,
-							      &nnids, err_str,
-							      sizeof(err_str));
+							      &nnids, errmsg,
+							      sizeof(errmsg));
 			if (nids)
 				free(nids);
 
+			/* NO_MATCH is okay for ip2nets, but anything else is
+			 * an error
+			 */
 			if (rc != LUSTRE_CFG_RC_NO_ERR &&
-			    rc != LUSTRE_CFG_RC_MATCH)
+			    rc != LUSTRE_CFG_RC_NO_MATCH)
 				goto print_error;
 
-			if (list_empty(&ip2nets.ip2nets_net.nw_intflist)) {
-				snprintf(err_str, sizeof(err_str),
-					 "No interfaces match ip2nets rules");
-				rc = -ENOENT;
-				goto print_error;
+			/* Skip configuration if resolution failed */
+			if (rc != LUSTRE_CFG_RC_NO_ERR)
+				goto cleanup_block;
+
+			/* Check for duplicate networks in ip2nets sequences */
+			for (i = 0; i < nets_cnt; i++) {
+				if (configured_nets[i] ==
+				    ip2nets.ip2nets_net.nw_id) {
+					duplicate_net = true;
+					break;
+				}
 			}
 
+			/* Skip configuration if duplicate */
+			if (duplicate_net)
+				goto cleanup_block;
+
+			/* Configure the network interface */
 			net = libcfs_net2str(ip2nets.ip2nets_net.nw_id);
 			if (tunables_set || lnd_tunables_set)
 				tun = &tunables;
@@ -4862,14 +4891,29 @@ handle_ip2nets_sequence(yaml_parser_t *setup, int flags)
 			rc = yaml_lnet_config_ni(net, NULL,
 						 &ip2nets.ip2nets_net, tun, -1,
 						 global_cpts, LNET_GENL_VERSION,
-						 NLM_F_CREATE, stdout);
+						 flags, stdout);
 			if (rc < 0) {
-				snprintf(err_str, sizeof(err_str),
+				snprintf(errmsg, sizeof(errmsg),
 					 "Failed to configure NI on net '%s' rc = %d",
 					 net, rc);
 				goto print_error;
 			}
 
+			/* Record successfully configured network for ip2nets */
+			tmp = realloc(configured_nets, (nets_cnt + 1) *
+				      sizeof(*configured_nets));
+
+			if (!tmp) {
+				rc = -ENOMEM;
+				snprintf(errmsg, sizeof(errmsg),
+					 "Out of memory tracking configured nets");
+				goto print_error;
+			}
+			configured_nets = tmp;
+			configured_nets[nets_cnt] = ip2nets.ip2nets_net.nw_id;
+			nets_cnt++;
+
+cleanup_block:
 			free_ip2nets_lists(&ip2nets);
 			init_ip2nets_tunables(&ip2nets, &tunables);
 			global_cpts = NULL;
@@ -4995,6 +5039,9 @@ yaml_parser_error:
 	}
 out:
 	free_ip2nets_lists(&ip2nets);
+	if (configured_nets)
+		free(configured_nets);
+
 	return (rc < 0) ? rc : 1;
 }
 