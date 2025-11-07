Cray/lustre

commit cbfc6be1bcd0dc29c694b28b139125bbe486fd0a

LUS-9298 llite: Unreplayed open leads to version mismatch
Uncommitted close removes open from replay list.
So unlink from another client destroys the file
and all other requests the file fail with version checking.

Keep open in replay list until close is committed.

Change-Id: I696d4eab8770d4ec62879e8f62fb0a798aa730f4
Signed-off-by: Andriy Skulysh <c17819@cray.com>
Reviewed-on: https://es-gerrit.dev.cray.com/157819
Reviewed-by: Alexander Boyko <c17825@cray.com>
Reviewed-by: Vitaly Fertman <c17818@cray.com>
Tested-by: Vitaly Fertman <c17818@cray.com>

lustre/mdc/mdc_request.c
@@ -854,38 +854,68 @@ static void mdc_free_open(struct md_open_data *mod)
 		  mod->mod_open_req->rq_replay);
 
 	ptlrpc_request_committed(mod->mod_open_req, committed);
-	if (mod->mod_close_req)
-		ptlrpc_request_committed(mod->mod_close_req, committed);
 }
 
-static int mdc_clear_open_replay_data(struct obd_export *exp,
-				      struct obd_client_handle *och)
+static void mdc_clear_open_replay_data_locked(struct md_open_data *mod)
 {
-	struct md_open_data *mod = och->och_mod;
-	ENTRY;
-
-	/**
-	 * It is possible to not have \var mod in a case of eviction between
-	 * lookup and ll_file_open().
-	 **/
-	if (mod == NULL)
-		RETURN(0);
+        ENTRY;
 
 	LASSERT(mod != LP_POISON);
 	LASSERT(mod->mod_open_req != NULL);
 
 	spin_lock(&mod->mod_open_req->rq_lock);
+	mod->mod_open_req->rq_replay = 0;
 	if (mod->mod_och)
 		mod->mod_och->och_open_handle.cookie = 0;
 	mod->mod_open_req->rq_early_free_repbuf = 0;
 	spin_unlock(&mod->mod_open_req->rq_lock);
 	mdc_free_open(mod);
 
-	mod->mod_och = NULL;
-	och->och_mod = NULL;
-	obd_mod_put(mod);
+	ptlrpc_req_finished_with_imp_lock(mod->mod_open_req);
+
+        mod->mod_och->och_mod = NULL;
+        mod->mod_och = NULL;
+        obd_mod_put(mod);
+}
+
+int mdc_clear_open_replay_data(struct obd_export *exp,
+                               struct obd_client_handle *och)
+{
+	struct obd_import *imp;
+	struct md_open_data *mod = och->och_mod;
+
+        /**
+         * It is possible to not have \var mod in a case of eviction between
+         * lookup and ll_file_open().
+         **/
+        if (mod == NULL)
+		RETURN(0);
+
+	imp = mod->mod_open_req->rq_import;
+	spin_lock(&imp->imp_lock);
+	mdc_clear_open_replay_data_locked(mod);
+	spin_unlock(&imp->imp_lock);
 
 	RETURN(0);
+}
+
+static void mdc_commit_close(struct ptlrpc_request *req)
+{
+	struct obd_client_handle *och;
+	struct md_open_data *mod = req->rq_cb_data;
+
+        if (mod == NULL)
+		return;
+
+	req->rq_cb_data = NULL;
+	och = mod->mod_och;
+
+	if (!mod->mod_open_req->rq_committed)
+		mdc_commit_open(mod->mod_open_req);
+
+	mdc_clear_open_replay_data_locked(mod);
+	och->och_open_handle.cookie = DEAD_HANDLE_MAGIC;
+	OBD_FREE_PTR(och);
 }
 
 static int mdc_close(struct obd_export *exp, struct md_op_data *op_data,
@@ -942,11 +972,11 @@ static int mdc_close(struct obd_export *exp, struct md_op_data *op_data,
 		mod->mod_close_req = req;
 
 		DEBUG_REQ(D_RPCTRACE, mod->mod_open_req, "matched open");
-		/* We no longer want to preserve this open for replay even
-		 * though the open was committed. b=3632, b=3633 */
-		spin_lock(&mod->mod_open_req->rq_lock);
-		mod->mod_open_req->rq_replay = 0;
-		spin_unlock(&mod->mod_open_req->rq_lock);
+
+		if (req) {
+			req->rq_cb_data = mod;
+			req->rq_commit_cb = mdc_commit_close;
+		}
 	} else {
 		CDEBUG(D_HA, "couldn't find open req; expecting close error\n");
 	}

lustre/include/obd.h
@@ -1247,8 +1247,6 @@ static inline struct md_open_data *obd_mod_alloc(void)
 #define obd_mod_put(mod)                                          \
 ({                                                                \
 	if (atomic_dec_and_test(&(mod)->mod_refcount)) {      	  \
-		if ((mod)->mod_open_req)                          \
-			ptlrpc_req_finished((mod)->mod_open_req); \
 		OBD_FREE_PTR(mod);                                \
 	}                                                         \
 })

lustre/llite/file.c
@@ -151,6 +151,7 @@ static int ll_close_inode_openhandle(struct inode *inode,
 	struct md_op_data *op_data;
 	struct ptlrpc_request *req = NULL;
 	int rc;
+	int rc2 = 0;
 	ENTRY;
 
 	if (class_exp2obd(md_exp) == NULL) {
@@ -247,7 +248,7 @@ static int ll_close_inode_openhandle(struct inode *inode,
 
 		body = req_capsule_server_get(&req->rq_pill, &RMF_MDT_BODY);
 		if (!(body->mbo_valid & OBD_MD_CLOSE_INTENT_EXECED))
-			rc = -EBUSY;
+			rc2 = -EBUSY;
 
 		if (bias & MDS_PCC_ATTACH) {
 			struct pcc_param *param = data;
@@ -259,13 +260,13 @@ static int ll_close_inode_openhandle(struct inode *inode,
 	ll_finish_md_op_data(op_data);
 	EXIT;
 out:
-
-	md_clear_open_replay_data(md_exp, och);
-	och->och_open_handle.cookie = DEAD_HANDLE_MAGIC;
-	OBD_FREE_PTR(och);
-
+	if (rc) {
+		md_clear_open_replay_data(md_exp, och);
+		och->och_open_handle.cookie = DEAD_HANDLE_MAGIC;
+		OBD_FREE_PTR(och);
+	}
 	ptlrpc_req_finished(req);	/* This is close request */
-	return rc;
+	return rc2 ? rc2 : rc;
 }
 
 int ll_md_real_close(struct inode *inode, fmode_t fmode)

lustre/ptlrpc/client.c
@@ -1580,9 +1580,7 @@ static int after_reply(struct ptlrpc_request *req)
 			 * rq_replay_list, ptlrpc_free_committed() will call
 			 * it later, see LU-3618 for details
 			 */
-			spin_unlock(&imp->imp_lock);
 			req->rq_commit_cb(req);
-			spin_lock(&imp->imp_lock);
 		}
 
 		/*
@@ -2670,6 +2668,7 @@ void ptlrpc_req_finished_with_imp_lock(struct ptlrpc_request *request)
 	assert_spin_locked(&request->rq_import->imp_lock);
 	(void)__ptlrpc_req_finished(request, 1);
 }
+EXPORT_SYMBOL(ptlrpc_req_finished_with_imp_lock);
 
 /**
  * Helper function
@@ -2847,9 +2846,8 @@ void ptlrpc_request_committed(struct ptlrpc_request *req, int force)
 {
 	struct obd_import *imp = req->rq_import;
 
-	spin_lock(&imp->imp_lock);
+	assert_spin_locked(&imp->imp_lock);
 	if (list_empty(&req->rq_replay_list)) {
-		spin_unlock(&imp->imp_lock);
 		return;
 	}
 
@@ -2858,8 +2856,6 @@ void ptlrpc_request_committed(struct ptlrpc_request *req, int force)
 			imp->imp_replay_cursor = req->rq_replay_list.next;
 		ptlrpc_free_request(req);
 	}
-
-	spin_unlock(&imp->imp_lock);
 }
 EXPORT_SYMBOL(ptlrpc_request_committed);
 

commit dc1b6d61430074b60a63d4e6d43785f3760aaf6c

LUS-13038 llite: don't keep failed open in repaly queue
Open request don't have allocated och during ll_prep_inode(),
so in case of an error close request is sent but it doesn't
free corresponding open request.

Fixes: cbfc6be1bc ("LUS-9298 llite: Unreplayed open leads to version mismatch")

Change-Id: Ia02b5cbdbb6fee85ba4e8e620164c1bf11c2073e
Signed-off-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163437
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/llite/llite_internal.h
@@ -1557,7 +1557,7 @@ int ll_get_obd_name(struct inode *inode, unsigned int cmd, void __user *arg);
 void ll_compute_rootsquash_state(struct ll_sb_info *sbi);
 ssize_t ll_copy_user_md(const struct lov_user_md __user *md,
 			struct lov_user_md **kbuf);
-void ll_open_cleanup(struct super_block *sb, struct req_capsule *pill);
+void ll_open_cleanup(struct super_block *sb, struct lookup_intent *it);
 
 void ll_dom_finish_open(struct inode *inode, struct ptlrpc_request *req);
 void ll_dir_finish_open(struct inode *inode, struct ptlrpc_request *req);

lustre/llite/llite_lib.c
@@ -3663,16 +3663,22 @@ int ll_remount_fs(struct super_block *sb, int *flags, char *data)
  * \param[in] sb	super block for this file-system
  * \param[in] open_req	pointer to the original open request
  */
-void ll_open_cleanup(struct super_block *sb, struct req_capsule *pill)
+void ll_open_cleanup(struct super_block *sb, struct lookup_intent *it)
 {
 	struct mdt_body			*body;
 	struct md_op_data		*op_data;
+	struct ptlrpc_request		*open_req = it->it_request;
 	struct ptlrpc_request		*close_req = NULL;
 	struct obd_export		*exp	   = ll_s2sbi(sb)->ll_md_exp;
 
 	ENTRY;
 
-	body = req_capsule_server_get(pill, &RMF_MDT_BODY);
+	DEBUG_REQ(D_HA, open_req, "open :");
+	ll_intent_drop_lock(it);
+	spin_lock(&open_req->rq_lock);
+	open_req->rq_replay = 0;
+	spin_unlock(&open_req->rq_lock);
+	body = req_capsule_server_get(&open_req->rq_pill, &RMF_MDT_BODY);
 	OBD_ALLOC_PTR(op_data);
 	if (op_data == NULL) {
 		CWARN("%s: cannot allocate op_data to release open handle for "
@@ -3840,8 +3846,7 @@ out:
 	md_put_lustre_md(sbi->ll_md_exp, &md);
 
 	if (rc != 0 && it != NULL && it->it_op & IT_OPEN) {
-		ll_intent_drop_lock(it);
-		ll_open_cleanup(sb != NULL ? sb : (*inode)->i_sb, pill);
+		ll_open_cleanup(sb != NULL ? sb : (*inode)->i_sb, it);
 	}
 
 	return rc;

lustre/llite/namei.c
@@ -821,8 +821,7 @@ static int ll_lookup_it_finish(struct ptlrpc_request *request,
 
 out:
 	if (rc != 0 && it->it_op & IT_OPEN) {
-		ll_intent_drop_lock(it);
-		ll_open_cleanup((*de)->d_sb, &request->rq_pill);
+		ll_open_cleanup((*de)->d_sb, it);
 	}
 
 	return rc;

lustre/mdc/mdc_request.c
@@ -709,6 +709,8 @@ void mdc_replay_open(struct ptlrpc_request *req)
 void mdc_commit_open(struct ptlrpc_request *req)
 {
         struct md_open_data *mod = req->rq_cb_data;
+
+	LASSERT(mod || req->rq_committed == 1);
         if (mod == NULL)
                 return;
 