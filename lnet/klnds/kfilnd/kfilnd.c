Cray/lustre

commit 86775f2d6f6c8c86dc54a5704464729fcb5d5f67

LUS-10076 kfilnd: GDS support for kfilnd
Add GDS support to kfilnd.

Add config check to see if kfabric supports the KFI_SGL IOV type.

When kfabric supports KFI_SGL:
 - struct kfilnd_transaction will contain a struct sg_table and
   associated fields to leverage the LNet or Linux dma APIs:
   - lnet_rdma_map_sg_attrs()/lnet_rdma_unmap_sg(), for GPU pages
   - dma_map_sgtable()/dma_unmap_sgtable(), for CPU pages
 - kfilnd_tn_set_sgl_buf() maps the struct bio_vec provided by LNet
   to the scatterlist and then calls the appropriate "map" API.
 - The appropriate "unmap" API is called by kfilnd_tn_finalize() when
   the transaction has completed.
For older kfabric that does not support KFI_SGL we continue to use
the KFI_BVEC IOV.

Replace calls to kfilnd_tn_set_kiov_buf() with kfilnd_tn_set_buf().
kfilnd_tn_set_buf() will call either kfilnd_tn_set_sgl_buf() or
kfilnd_tn_set_kiov_buf() depending on whether kfabric has KFI_SGL
support.

For immediate messages, kfilnd_tn_set_kiov_buf() is essentially a
noop. It's only functional purpose was setting the tn_num_iovec and
tn_nob fields of the struct kfilnd_transaction. This was so they
could be used later in kfilnd_tn_pack_immed_msg() as arguments to
lnet_copy_kiov2flat(). lnet_copy_kiov2flat() is now called by
kfilnd_send() so that kfilnd_tn_set_sgl_buf() needn't bother setting
those fields in struct kfilnd_transaction.

Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I4b852a60dc5c97bd2bb2b4b5b9ac109d0749cf1e
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163286
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Ron Gredvig <ronald.gredvig@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lnet/klnds/kfilnd/kfilnd.c
@@ -89,6 +89,7 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 	int rc;
 	bool tn_key = false;
 	lnet_nid_t tgt_nid4;
+	bool gpu = lnet_md_is_gpu(msg->msg_md);
 
 	switch (type) {
 	default:
@@ -108,7 +109,7 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 
 		nob = offsetof(struct kfilnd_msg,
 			       proto.immed.payload[msg->msg_md->md_length]);
-		if (nob <= KFILND_IMMEDIATE_MSG_SIZE) {
+		if (nob <= KFILND_IMMEDIATE_MSG_SIZE && !gpu) {
 			lnd_msg_type = KFILND_MSG_IMMEDIATE;
 			break;
 		}
@@ -121,7 +122,7 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 	case LNET_MSG_PUT:
 		nob = offsetof(struct kfilnd_msg,
 			       proto.immed.payload[msg->msg_len]);
-		if (nob <= KFILND_IMMEDIATE_MSG_SIZE) {
+		if (nob <= KFILND_IMMEDIATE_MSG_SIZE && !gpu) {
 			lnd_msg_type = KFILND_MSG_IMMEDIATE;
 			break;
 		}
@@ -152,23 +153,30 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 		}
 	}
 
+	tn->tn_gpu = gpu;
+
 	switch (lnd_msg_type) {
 	case KFILND_MSG_IMMEDIATE:
-		rc = kfilnd_tn_set_kiov_buf(tn, msg->msg_kiov, msg->msg_niov,
-					    msg->msg_offset, msg->msg_len);
-		if (rc) {
-			CERROR("Failed to setup immediate buffer rc %d\n", rc);
-			kfilnd_tn_free(tn);
-			return rc;
-		}
+		CDEBUG(D_NET,
+		       "tn %p msg_kiov %p msg_niov %u msg_offset %u msg_len %u\n",
+		       tn, msg->msg_kiov, msg->msg_niov,
+		       msg->msg_offset, msg->msg_len);
 
+		lnet_copy_kiov2flat(KFILND_IMMEDIATE_MSG_SIZE,
+				    tn->tn_tx_msg.msg,
+				    offsetof(struct kfilnd_msg,
+					     proto.immed.payload),
+				    msg->msg_niov, msg->msg_kiov,
+				    msg->msg_offset, msg->msg_len);
+
+		tn->tn_nob = msg->msg_len;
 		event = TN_EVENT_INIT_IMMEDIATE;
 		break;
 
 	case KFILND_MSG_BULK_PUT_REQ:
 		tn->sink_buffer = false;
-		rc = kfilnd_tn_set_kiov_buf(tn, msg->msg_kiov, msg->msg_niov,
-					    msg->msg_offset, msg->msg_len);
+		rc = kfilnd_tn_set_buf(ni, tn, msg->msg_kiov, msg->msg_niov,
+				       msg->msg_offset, msg->msg_len);
 		if (rc) {
 			CERROR("Failed to setup PUT source buffer rc %d\n", rc);
 			kfilnd_tn_free(tn);
@@ -191,10 +199,10 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 		}
 
 		tn->sink_buffer = true;
-		rc = kfilnd_tn_set_kiov_buf(tn, msg->msg_md->md_kiov,
-					    msg->msg_md->md_niov,
-					    msg->msg_md->md_offset,
-					    msg->msg_md->md_length);
+		rc = kfilnd_tn_set_buf(ni, tn, msg->msg_md->md_kiov,
+				       msg->msg_md->md_niov,
+				       msg->msg_md->md_offset,
+				       msg->msg_md->md_length);
 		if (rc) {
 			CERROR("Failed to setup GET sink buffer rc %d\n", rc);
 			kfilnd_tn_free(tn);
@@ -272,10 +280,17 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 		if (mlen == 0) {
 			event = TN_EVENT_SKIP_TAG_RMA;
 		} else {
+			struct lnet_libmd *msg_md = NULL;
+
+			if (msg)
+				msg_md = msg->msg_md;
+
+			tn->tn_gpu = lnet_md_is_gpu(msg_md);
+
 			/* Post the buffer given us as a sink  */
 			tn->sink_buffer = true;
-			rc = kfilnd_tn_set_kiov_buf(tn, kiov, niov, offset,
-						    mlen);
+			rc = kfilnd_tn_set_buf(ni, tn, kiov, niov, offset,
+					       mlen);
 			if (rc) {
 				CERROR("Failed to setup PUT sink buffer rc %d\n", rc);
 				kfilnd_tn_free(tn);
@@ -290,12 +305,18 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 			event = TN_EVENT_SKIP_TAG_RMA;
 			status = -ENODATA;
 		} else {
+			struct lnet_libmd *msg_md = NULL;
+
+			if (msg)
+				msg_md = msg->msg_md;
+
+			tn->tn_gpu = lnet_md_is_gpu(msg_md);
+
 			/* Post the buffer given to us as a source  */
 			tn->sink_buffer = false;
-			rc = kfilnd_tn_set_kiov_buf(tn, msg->msg_kiov,
-						    msg->msg_niov,
-						    msg->msg_offset,
-						    msg->msg_len);
+			rc = kfilnd_tn_set_buf(ni, tn, msg->msg_kiov,
+					       msg->msg_niov, msg->msg_offset,
+					       msg->msg_len);
 			if (rc) {
 				CERROR("Failed to setup GET source buffer rc %d\n", rc);
 				kfilnd_tn_free(tn);
@@ -429,6 +450,18 @@ kfilnd_nl_set(int cmd, struct nlattr *attr, int type, void *data)
 	return rc;
 }
 
+static unsigned int
+kfilnd_get_dev_prio(struct lnet_ni *ni, unsigned int dev_idx)
+{
+	struct kfilnd_dev *dev = ni->ni_data;
+	struct device *device = NULL;
+
+	if (dev)
+		device = dev->device;
+
+	return lnet_get_dev_prio(device, dev_idx);
+}
+
 static int kfilnd_startup(struct lnet_ni *ni);
 
 static const struct lnet_lnd the_kfilnd = {
@@ -441,6 +474,7 @@ static const struct lnet_lnd the_kfilnd = {
 	.lnd_nl_set		= kfilnd_nl_set,
 	.lnd_keys		= &kfilnd_tunables_keys,
 	.lnd_get_timeout	= kfilnd_timeout,
+	.lnd_get_dev_prio	= kfilnd_get_dev_prio,
 };
 
 static int kfilnd_startup(struct lnet_ni *ni)

lnet/klnds/kfilnd/kfilnd.h
@@ -53,6 +53,7 @@
 #include <libcfs/libcfs.h>
 #include <libcfs/linux/linux-net.h>
 #include <lnet/lib-lnet.h>
+#include <lnet/lnet_rdma.h>
 #include "kfi_endpoint.h"
 #include "kfi_errno.h"
 #include "kfi_rma.h"
@@ -767,8 +768,18 @@ struct kfilnd_transaction {
 	/* LNet buffer used to register a memory region or perform a RMA
 	 * operation.
 	 */
+#ifdef HAVE_KFI_SGL
+	/* Set to true if @tn_sgt is mapped */
+	bool			tn_sgt_mapped;
+	struct sg_table		tn_sgt;
+	enum dma_data_direction	tn_dmadir;
+#else
 	struct bio_vec		tn_kiov[LNET_MAX_IOV];
+#endif
 	unsigned int		tn_num_iovec;
+
+	/* Force RDMA */
+	bool			tn_gpu;
 
 	/* LNet transaction payload byte count. */
 	unsigned int		tn_nob;

lnet/klnds/kfilnd/kfilnd_tn.c
@@ -154,13 +154,6 @@ static void kfilnd_tn_pack_immed_msg(struct kfilnd_transaction *tn)
 	/* Pack the protocol header and payload. */
 	lnet_hdr_to_nid4(&tn->tn_lntmsg->msg_hdr, &msg->proto.immed.hdr);
 
-	lnet_copy_kiov2flat(KFILND_IMMEDIATE_MSG_SIZE,
-			    msg,
-			    offsetof(struct kfilnd_msg,
-				     proto.immed.payload),
-			    tn->tn_num_iovec, tn->tn_kiov, 0,
-			    tn->tn_nob);
-
 	/* Pack the transport header. */
 	msg->magic = KFILND_MSG_MAGIC;
 	msg->version = tn->tn_kp->kp_version;
@@ -446,6 +439,26 @@ static void kfilnd_tn_finalize(struct kfilnd_transaction *tn, bool *tn_released)
 	/* Release the reference on the multi-receive buffer. */
 	if (tn->tn_posted_buf)
 		kfilnd_ep_imm_buffer_put(tn->tn_posted_buf);
+
+#ifdef HAVE_KFI_SGL
+	if (tn->tn_sgt_mapped) {
+		struct device *device = tn->tn_ep->end_dev->device;
+		enum dma_data_direction dmadir = tn->tn_dmadir;
+		int rc = 0;
+
+		if (tn->tn_gpu)
+			rc = lnet_rdma_unmap_sg(device, tn->tn_sgt.sgl,
+						tn->tn_sgt.nents, dmadir);
+		else
+			dma_unmap_sgtable(device, &tn->tn_sgt, dmadir, 0);
+
+		CDEBUG(D_NET,
+		       "tn %p tn_sgt %p sgl %p dir %u orig_nents %u nents %u gpu %s rc %d\n",
+		       tn, &tn->tn_sgt, tn->tn_sgt.sgl, tn->tn_dmadir,
+		       tn->tn_sgt.orig_nents, tn->tn_sgt.nents,
+		       tn->tn_gpu ? "y" : "n", rc);
+	}
+#endif
 
 	/* Finalize LNet operation. */
 	if (tn->tn_lntmsg) {
@@ -1539,6 +1552,11 @@ void kfilnd_tn_free(struct kfilnd_transaction *tn)
 	if (tn->tn_mr_key)
 		kfilnd_ep_put_key(tn->tn_ep, tn->tn_mr_key);
 
+#ifdef HAVE_KFI_SGL
+	if (tn->tn_sgt_mapped)
+		sg_free_table(&tn->tn_sgt);
+#endif
+
 	/* Free send message buffer if needed. */
 	if (tn->tn_tx_msg.msg)
 		kmem_cache_free(imm_buf_cache, tn->tn_tx_msg.msg);
@@ -1609,8 +1627,6 @@ static struct kfilnd_transaction *kfilnd_tn_alloc_common(struct kfilnd_ep *ep,
 	return tn;
 
 err_free_tn:
-	if (tn->tn_tx_msg.msg)
-		kmem_cache_free(imm_buf_cache, tn->tn_tx_msg.msg);
 	kmem_cache_free(tn_cache, tn);
 err:
 	return ERR_PTR(rc);
@@ -1775,23 +1791,94 @@ err:
 	return -ENOMEM;
 }
 
-/**
- * kfilnd_tn_set_kiov_buf() - Set the buffer used for a transaction.
- * @tn: Transaction to have buffer set.
- * @kiov: LNet KIOV buffer.
- * @num_iov: Number of IOVs.
- * @offset: Offset into IOVs where the buffer starts.
- * @len: Length of the buffer.
- *
- * This function takes the user provided IOV, offset, and len, and sets the
- * transaction buffer. The user provided IOV is an LNet KIOV. When the
- * transaction buffer is configured, the user provided offset is applied
- * when the transaction buffer is configured (i.e. the transaction buffer
- * offset is zero).
- */
-int kfilnd_tn_set_kiov_buf(struct kfilnd_transaction *tn,
-			   struct bio_vec *kiov, size_t num_iov,
-			   size_t offset, size_t len)
+#ifdef HAVE_KFI_SGL
+static int kfilnd_tn_set_sgl_buf(struct lnet_ni *ni,
+				 struct kfilnd_transaction *tn,
+				 struct bio_vec *kiov, int num_iov, int offset,
+				 int nob)
+{
+	struct kfilnd_dev *dev = ni->ni_data;
+	struct scatterlist *sg;
+	int fragnob;
+	int max_nkiov;
+	int sg_count = 0;
+	int rc = 0;
+
+	tn->tn_nob = nob;
+
+	while (offset >= kiov->bv_len) {
+		offset -= kiov->bv_len;
+		num_iov--;
+		kiov++;
+		LASSERT(num_iov > 0);
+	}
+
+	max_nkiov = num_iov;
+	rc = sg_alloc_table(&tn->tn_sgt, max_nkiov, GFP_KERNEL);
+	if (rc) {
+		CERROR("sg_alloc_table failed rc = %d\n", rc);
+		return rc;
+	}
+
+	sg = tn->tn_sgt.sgl;
+	do {
+		LASSERT(num_iov > 0);
+
+		if (!sg) {
+			CERROR("lacking enough sg entries to map tx: rc = %d\n",
+			       -EFAULT);
+			sg_free_table(&tn->tn_sgt);
+			return -EFAULT;
+		}
+
+		sg_count++;
+
+		fragnob = min_t(int, (kiov->bv_len - offset), nob);
+
+		sg_set_page(sg, kiov->bv_page, fragnob,
+			    kiov->bv_offset + offset);
+		sg = sg_next(sg);
+
+		offset = 0;
+		kiov++;
+		num_iov--;
+		nob -= fragnob;
+	} while (nob > 0);
+
+	tn->tn_dmadir = tn->sink_buffer ? DMA_FROM_DEVICE : DMA_TO_DEVICE;
+
+	tn->tn_sgt.orig_nents = sg_count;
+
+	if (tn->tn_gpu) {
+		rc = lnet_rdma_map_sg_attrs(dev->device, tn->tn_sgt.sgl,
+					    sg_count, tn->tn_dmadir);
+		if (rc > 0)
+			tn->tn_sgt.nents = rc;
+	} else {
+		rc = dma_map_sgtable(dev->device, &tn->tn_sgt, tn->tn_dmadir,
+				     0);
+	}
+
+	tn->tn_sgt_mapped = true;
+
+	/* tn_num_iovec used for convenience in some debug messages */
+	tn->tn_num_iovec = tn->tn_sgt.nents;
+
+	CDEBUG(D_NET,
+	       "tn %p tn_sgt %p sgl %p dir %u nob %d orig_nents %u nents %u gpu %s rc %d\n",
+	       tn, &tn->tn_sgt, tn->tn_sgt.sgl, tn->tn_dmadir, tn->tn_nob,
+	       tn->tn_sgt.orig_nents, tn->tn_sgt.nents, tn->tn_gpu ? "y" : "n",
+	       rc);
+
+	return rc < 0 ? rc : 0;
+}
+
+#else
+
+static int kfilnd_tn_set_kiov_buf(struct lnet_ni *ni,
+				  struct kfilnd_transaction *tn,
+				  struct bio_vec *kiov, size_t num_iov,
+				  size_t offset, size_t len)
 {
 	size_t i;
 	size_t cur_len = 0;
@@ -1832,4 +1919,29 @@ int kfilnd_tn_set_kiov_buf(struct kfilnd_transaction *tn,
 	tn->tn_nob = cur_len;
 
 	return 0;
+}
+#endif /* HAVE_KFI_SGL */
+
+/**
+ * kfilnd_tn_set_buf() - Set the buffer used for a transaction.
+ * @tn: Transaction to have buffer set.
+ * @kiov: LNet KIOV buffer.
+ * @num_iov: Number of IOVs.
+ * @offset: Offset into IOVs where the buffer starts.
+ * @len: Length of the buffer.
+ *
+ * This function takes the user provided IOV, offset, and len, and sets the
+ * transaction buffer. The user provided IOV is an LNet KIOV. When the
+ * transaction buffer is configured, the user provided offset is applied
+ * when the transaction buffer is configured (i.e. the transaction buffer
+ * offset is zero).
+ */
+int kfilnd_tn_set_buf(struct lnet_ni *ni, struct kfilnd_transaction *tn,
+		      struct bio_vec *kiov, int num_iov, int offset, int nob)
+{
+#ifdef HAVE_KFI_SGL
+	return kfilnd_tn_set_sgl_buf(ni, tn, kiov, num_iov, offset, nob);
+#else
+	return kfilnd_tn_set_kiov_buf(ni, tn, kiov, num_iov, offset, nob);
+#endif
 }

lnet/autoconf/lustre-lnet.m4
@@ -1000,6 +1000,18 @@ AC_ARG_WITH([kfi],
 			AC_DEFINE(HAVE_KFI_CXI_DOM_OPS, 1,
 				[kfi_cxi domain ops are available])
 		])
+		LB_CHECK_COMPILE([if KFI_SGL IOV type is supported],
+		KFABRIC_KFI_SGL, [
+			#include <kfabric.h>
+			#include <kfi_rma.h>
+		],[
+			struct kfi_msg_rma rma = {
+				.type = KFI_SGL,
+			};
+		],[
+			AC_DEFINE(HAVE_KFI_SGL, 1,
+				  [KFI_SGL IOV type is supported])
+		])
 	],[])
 AC_DEFINE(HAVE_KFILND, 1, [support kfabric LND])
 AC_SUBST(KFICPPFLAGS)

lnet/klnds/kfilnd/kfilnd_ep.c
@@ -342,24 +342,30 @@ int kfilnd_ep_post_tagged_recv(struct kfilnd_ep *ep,
 		return -EAGAIN;
 	}
 
+#ifdef HAVE_KFI_SGL
+	msg.type = KFI_SGL,
+	msg.msg_sgl = tn->tn_sgt.sgl,
+	msg.iov_count = tn->tn_sgt.nents,
+#else
 	msg.iov_count = tn->tn_num_iovec;
 	msg.type = KFI_BVEC;
 	msg.msg_biov = tn->tn_kiov;
+#endif
 
 	rc = kfi_trecvmsg(ep->end_rx, &msg, KFI_COMPLETION);
 	switch (rc) {
 	case 0:
 	case -EAGAIN:
 		KFILND_EP_DEBUG(ep,
-				"TN %p: %s tagged recv of %u bytes (%u frags) with tag 0x%llx: rc=%d",
+				"TN %p: %s tagged recv of %u bytes (%lu frags) with tag 0x%llx: rc=%d",
 				tn, rc ? "Failed to post" : "Posted",
-				tn->tn_nob, tn->tn_num_iovec, msg.tag, rc);
+				tn->tn_nob, msg.iov_count, msg.tag, rc);
 		break;
 
 	default:
 		KFILND_EP_ERROR(ep,
-				"TN %p: Failed to post tagged recv of %u bytes (%u frags) with tag 0x%llx: rc=%d",
-				tn, tn->tn_nob, tn->tn_num_iovec, msg.tag, rc);
+				"TN %p: Failed to post tagged recv of %u bytes (%lu frags) with tag 0x%llx: rc=%d",
+				tn, tn->tn_nob, msg.iov_count, msg.tag, rc);
 	}
 
 	return rc;
@@ -484,26 +490,32 @@ int kfilnd_ep_post_write(struct kfilnd_ep *ep, struct kfilnd_transaction *tn)
 		return -EAGAIN;
 	}
 
+#ifdef HAVE_KFI_SGL
+	rma.type = KFI_SGL;
+	rma.msg_sgl = tn->tn_sgt.sgl;
+	rma.iov_count = tn->tn_sgt.nents;
+#else
 	rma.iov_count = tn->tn_num_iovec;
 	rma.type = KFI_BVEC;
 	rma.msg_biov = tn->tn_kiov;
+#endif
 
 	rc = kfi_writemsg(ep->end_tx, &rma, KFI_TAGGED | KFI_COMPLETION);
 	switch (rc) {
 	case 0:
 	case -EAGAIN:
 		KFILND_EP_DEBUG(ep,
-				"TN ID %p: %s write of %u bytes in %u frags kp %s(%p) rma_iov.key %llu: rc=%d",
+				"TN ID %p: %s write of %u bytes in %lu frags kp %s(%p) rma_iov.key %llu: rc=%d",
 				tn, rc ? "Failed to post" : "Posted",
-				tn->tn_nob, tn->tn_num_iovec,
+				tn->tn_nob, rma.iov_count,
 				libcfs_nid2str(tn->tn_kp->kp_nid), tn->tn_kp,
 				rma_iov.key, rc);
 		break;
 
 	default:
 		KFILND_EP_ERROR(ep,
-				"TN %p: Failed to post write of %u bytes in %u frags kp %s(%p) rma_iov.key %llu: rc=%d",
-				tn, tn->tn_nob, tn->tn_num_iovec,
+				"TN %p: Failed to post write of %u bytes in %lu frags kp %s(%p) rma_iov.key %llu: rc=%d",
+				tn, tn->tn_nob, rma.iov_count,
 				libcfs_nid2str(tn->tn_kp->kp_nid), tn->tn_kp,
 				rma_iov.key, rc);
 	}
@@ -564,26 +576,32 @@ int kfilnd_ep_post_read(struct kfilnd_ep *ep, struct kfilnd_transaction *tn)
 		return -EAGAIN;
 	}
 
+#ifdef HAVE_KFI_SGL
+	rma.type = KFI_SGL;
+	rma.msg_sgl = tn->tn_sgt.sgl;
+	rma.iov_count = tn->tn_sgt.nents;
+#else
 	rma.iov_count = tn->tn_num_iovec;
 	rma.type = KFI_BVEC;
 	rma.msg_biov = tn->tn_kiov;
+#endif
 
 	rc = kfi_readmsg(ep->end_tx, &rma, KFI_TAGGED | KFI_COMPLETION);
 	switch (rc) {
 	case 0:
 	case -EAGAIN:
 		KFILND_EP_DEBUG(ep,
-				"TN %p: %s read of %u bytes in %u frags kp %s(%p) rma_iov.key %llu: rc=%d",
+				"TN %p: %s read of %u bytes in %lu frags kp %s(%p) rma_iov.key %llu: rc=%d",
 				tn, rc ? "Failed to post" : "Posted",
-				tn->tn_nob, tn->tn_num_iovec,
+				tn->tn_nob, rma.iov_count,
 				libcfs_nid2str(tn->tn_kp->kp_nid), tn->tn_kp,
 				rma_iov.key, rc);
 		break;
 
 	default:
 		KFILND_EP_ERROR(ep,
-				"TN %p: Failed to post read of %u bytes in %u frags kp %s(%p) rma_iov.key %llu: rc=%d",
-				tn, tn->tn_nob, tn->tn_num_iovec,
+				"TN %p: Failed to post read of %u bytes in %lu frags kp %s(%p) rma_iov.key %llu: rc=%d",
+				tn, tn->tn_nob, rma.iov_count,
 				libcfs_nid2str(tn->tn_kp->kp_nid), tn->tn_kp,
 				rma_iov.key, rc);
 	}

lnet/klnds/kfilnd/kfilnd_tn.h
@@ -29,7 +29,8 @@ void kfilnd_tn_event_handler(struct kfilnd_transaction *tn,
 			     enum tn_events event, int status);
 void kfilnd_tn_cleanup(void);
 int kfilnd_tn_init(void);
-int kfilnd_tn_set_kiov_buf(struct kfilnd_transaction *tn, struct bio_vec *kiov,
-		           size_t num_iov, size_t offset, size_t nob);
+int kfilnd_tn_set_buf(struct lnet_ni *ni, struct kfilnd_transaction *tn,
+			   struct bio_vec *kiov, int num_iov, int offset,
+			   int nob);
 
 #endif /* _KFILND_TN_ */

commit b93b3ec4799a4852c62c5c0f87f6a5cac3320d52

LUS-13118 kfilnd: fix sg_table free after orig_nents overwrite
The kfilnd transaction code was overwriting sg_table.orig_nents
with the actual number of populated segments to work around
dma_[un]map_sgtable() requirements. However, orig_nents must
remain the original allocation size so sg_free_table() can
determine whether to dismantle a chained multi-page table or
just issue kfree().

This caused kernel warnings in free_nonslab_page() when freeing
transactions with scatter-gather tables:

  WARNING: CPU: 1 PID: 806 at mm/slub.c:3591 free_nonslab_page+0x70/0x90
  Call Trace:
    kfree+0x20e/0x250
    __sg_free_table+0x5f/0x80
    kfilnd_tn_free+0x23a/0x3a0 [kkfilnd]

The fix introduces tn_sgt_alloc_nents to save the original
allocation size and kfilnd_tn_sgt_free() to restore orig_nents
before calling sg_free_table(). This preserves the allocation
metadata needed for proper cleanup while still allowing the
dma mapping functions to work with only the populated segments.

Additional changes:
- Move tn_num_iovec outside HAVE_KFI_SGL ifdef since it's only
  used in the non-SGL code path
- Update debug messages to use tn_sgt.nents instead of the
  removed tn_num_iovec assignment
- Add alloc_nents to debug output for better diagnostics

Fixes: 86775f2d6f ("LUS-10076 kfilnd: GDS support for kfilnd")
Test-Parameters: trivial
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: Ib9eac555f1a665991ef1baf45530905de732d5ee
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163495
Reviewed-by: Ron Gredvig <ronald.gredvig@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

lnet/klnds/kfilnd/kfilnd.c
@@ -222,7 +222,12 @@ static int kfilnd_send(struct lnet_ni *ni, void *private, struct lnet_msg *msg)
 
 	KFILND_TN_DEBUG(tn, "%s in %u bytes in %u frags",
 			msg_type_to_str(lnd_msg_type), tn->tn_nob,
-			tn->tn_num_iovec);
+#ifdef HAVE_KFI_SGL
+			tn->tn_sgt.nents
+#else
+			tn->tn_num_iovec
+#endif
+			);
 
 	/* Start the state machine processing this transaction */
 	kfilnd_tn_event_handler(tn, event, 0);
@@ -353,7 +358,12 @@ static int kfilnd_recv(struct lnet_ni *ni, void *private, struct lnet_msg *msg,
 
 	KFILND_TN_DEBUG(tn, "%s in %u bytes in %u frags",
 			msg_type_to_str(rxmsg->type), tn->tn_nob,
-			tn->tn_num_iovec);
+#ifdef HAVE_KFI_SGL
+			tn->tn_sgt.nents
+#else
+			tn->tn_num_iovec
+#endif
+			);
 
 	kfilnd_tn_event_handler(tn, event, status);
 

lnet/klnds/kfilnd/kfilnd.h
@@ -776,11 +776,13 @@ struct kfilnd_transaction {
 	/* Set to true if @tn_sgt is mapped */
 	bool			tn_sgt_mapped;
 	struct sg_table		tn_sgt;
+	/* The number of segments originally allocated */
+	unsigned int		tn_sgt_alloc_nents;
 	enum dma_data_direction	tn_dmadir;
 #else
 	struct bio_vec		tn_kiov[LNET_MAX_IOV];
-#endif
 	unsigned int		tn_num_iovec;
+#endif
 
 	/* Force RDMA */
 	bool			tn_gpu;

lnet/klnds/kfilnd/kfilnd_tn.c
@@ -1557,6 +1557,15 @@ void kfilnd_tn_event_handler(struct kfilnd_transaction *tn,
 		mutex_unlock(&tn->tn_lock);
 }
 
+#ifdef HAVE_KFI_SGL
+static void kfilnd_tn_sgt_free(struct kfilnd_transaction *tn)
+{
+	/* Restore orig_nents before free */
+	tn->tn_sgt.orig_nents = tn->tn_sgt_alloc_nents;
+	sg_free_table(&tn->tn_sgt);
+}
+#endif
+
 /**
  * kfilnd_tn_free() - Free a transaction.
  */
@@ -1573,7 +1582,7 @@ void kfilnd_tn_free(struct kfilnd_transaction *tn)
 
 #ifdef HAVE_KFI_SGL
 	if (tn->tn_sgt_mapped)
-		sg_free_table(&tn->tn_sgt);
+		kfilnd_tn_sgt_free(tn);
 #endif
 
 	/* Free send message buffer if needed. */
@@ -1881,6 +1890,12 @@ static int kfilnd_tn_set_sgl_buf(struct lnet_ni *ni,
 
 	tn->tn_dmadir = tn->sink_buffer ? DMA_FROM_DEVICE : DMA_TO_DEVICE;
 
+	/* Save orig_nents so it can be restored prior to sg_free_table() */
+	tn->tn_sgt_alloc_nents = tn->tn_sgt.orig_nents;
+
+	/* dma_[un]map_sgtable() expects all orig_nents segments to be
+	 * populated, but only sg_count of them are actually populated.
+	 */
 	tn->tn_sgt.orig_nents = sg_count;
 
 	if (tn->tn_gpu) {
@@ -1897,21 +1912,18 @@ static int kfilnd_tn_set_sgl_buf(struct lnet_ni *ni,
 		CERROR("%s: %s failed rc = %d\n", ni->ni_interface,
 		       tn->tn_gpu ? "lnet_rdma_map_sg_attrs" :
 		       "dma_map_sgtable", rc);
-		sg_free_table(&tn->tn_sgt);
+		kfilnd_tn_sgt_free(tn);
 		return rc;
 	}
 
 	/* Set tn_sgt_mapped so kfilnd_tn_free() will free tn_sgt */
 	tn->tn_sgt_mapped = true;
 
-	/* tn_num_iovec used for convenience in some debug messages */
-	tn->tn_num_iovec = tn->tn_sgt.nents;
-
 	CDEBUG(D_NET,
-	       "tn %p tn_sgt %p sgl %p dir %u nob %d orig_nents %u nents %u gpu %s rc %d\n",
+	       "tn %p tn_sgt %p sgl %p dir %u nob %d alloc_nents %u orig_nents %u nents %u gpu %s\n",
 	       tn, &tn->tn_sgt, tn->tn_sgt.sgl, tn->tn_dmadir, tn->tn_nob,
-	       tn->tn_sgt.orig_nents, tn->tn_sgt.nents, tn->tn_gpu ? "y" : "n",
-	       rc);
+	       tn->tn_sgt_alloc_nents, tn->tn_sgt.orig_nents, tn->tn_sgt.nents,
+	       tn->tn_gpu ? "y" : "n");
 
 	return 0;
 }