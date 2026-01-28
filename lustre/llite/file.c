92c4a8fe9eedd0864df7ce19fb4f7289cd0ea504
Alexey Lyashkov <alexey.lyashkov@hpe.com>
2026-01-28 14:32:56 +0300

    LUS-13172 layout: restore layout handling
    Don't have parallel lock activites with layout refresh,
    it caused problems with page cache flush and make locking
    complex.
    
    Change-Id: I66d751414eac05433e313afa6c3c9138c5386aa4
    Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
    Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163590/
    Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
    Reviewed-by: Vitaly Fertman <vitaly.fertman@hpe.com>
    Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

--- lustre/llite/file.c
+++ lustre/llite/file.c
@@ -7008,6 +7008,7 @@ int ll_layout_refresh(struct inode *inode, __u32 *gen)
 	LASSERT(fid_is_sane(ll_inode2fid(inode)));
 	LASSERT(S_ISREG(inode->i_mode));
 
+	mutex_lock(&lli->lli_layout_mutex);
 	while (1) {
 		/* mostly layout lock is caching on the local side, so try to
 		 * match it before grabbing layout lock mutex.
@@ -7024,12 +7025,11 @@ int ll_layout_refresh(struct inode *inode, __u32 *gen)
 		}
 
 		/* take layout lock mutex to enqueue layout lock exclusively. */
-		mutex_lock(&lli->lli_layout_mutex);
 		rc = ll_layout_intent(inode, &intent);
-		mutex_unlock(&lli->lli_layout_mutex);
 		if (rc != 0)
 			break;
 	}
+	mutex_unlock(&lli->lli_layout_mutex);
 
 	if (rc == 0)
 		*gen = ll_layout_version_get(lli);
