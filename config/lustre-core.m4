lustre/lustre-release

commit 35f58473619c265abac4ed98dc1092dd5afada32

LU-19186 osd-ldiskfs: use posix ACL api
Since v6.1-rc1-24-g318e66856dde, we can not use
__vfs_{get|set|remove}xattr() to work with ACL,
now we need to use the posix ACL api instead.

Test-Parameters: testlist=sanity fstype=ldiskfs serverdistro=el9.6
Test-Parameters: testlist=sanityn fstype=ldiskfs serverdistro=el9.6
Test-Parameters: testlist=sanity-sec fstype=ldiskfs serverdistro=el9.6
Test-Parameters: testlist=conf-sanity fstype=ldiskfs serverdistro=el9.6
Signed-off-by: Li Dongyang <dongyangli@ddn.com>
Change-Id: I48b99b87a90b6653388192d683ba30b8b7a447cf
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/61834
Reviewed-by: Sebastien Buisson <sbuisson@ddn.com>
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-core.m4
@@ -3071,6 +3071,26 @@ AC_DEFUN([LC_FSCRYPT_PREPARE_READDIR], [
 ]) # LC_FSCRYPT_PREPARE_READDIR
 
 #
+# LC_SET_POSIX_ACL_USER_NS
+# Linux commit v5.11-rc4-8-ge65ce2a50cf6
+#   acl: handle idmapped mounts
+#
+AC_DEFUN([LC_SRC_SET_POSIX_ACL_USER_NS], [
+	LB2_LINUX_TEST_SRC([set_posix_acl_user_ns], [
+		#include <linux/posix_acl.h>
+	],[
+		set_posix_acl((struct user_namespace *)NULL, (struct inode*)NULL, 0, NULL);
+	],[-Werror])
+])
+AC_DEFUN([LC_SET_POSIX_ACL_USER_NS], [
+	LB2_MSG_LINUX_TEST_RESULT([if set_posix_acl() has user namespace argument],
+	[set_posix_acl_user_ns], [
+		AC_DEFINE(HAVE_SET_POSIX_ACL_USER_NS, 1,
+			[set_posix_acl() has user namespace argument])
+	])
+]) # LC_SET_POSIX_ACL_USER_NS
+
+#
 # LC_BIO_SET_DEV
 #
 # Linux: v5.11-rc5-9-g309dca309fc3
@@ -4020,6 +4040,27 @@ AC_DEFUN([LC_IOP_GET_INODE_ACL], [
 			[inode_operations has .get_inode_acl member function])
 	])
 ]) # LC_IOP_GET_INODE_ACL
+
+#
+# LC_HAVE_POSIX_ACL_TYPE
+#
+# Linux commit v6.1-rc1-15-ge4cc9163032f
+#   acl: add vfs_set_acl()
+#
+AC_DEFUN([LC_SRC_HAVE_POSIX_ACL_TYPE], [
+	LB2_LINUX_TEST_SRC([posix_acl_type], [
+		#include <linux/posix_acl_xattr.h>
+	],[
+		posix_acl_type(NULL);
+	],[-Werror])
+])
+AC_DEFUN([LC_HAVE_POSIX_ACL_TYPE], [
+	LB2_MSG_LINUX_TEST_RESULT([if posix_acl_type() is available],
+	[posix_acl_type], [
+		AC_DEFINE(HAVE_POSIX_ACL_TYPE, 1,
+			[posix_acl_type() is available])
+	])
+]) # LC_HAVE_POSIX_ACL_TYPE
 
 #
 # LC_HAVE_FOLIO_MAPCOUNT
@@ -5202,6 +5243,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_FSCRYPT_PREPARE_READDIR
 
 	# 5.11
+	LC_SRC_SET_POSIX_ACL_USER_NS
 	LC_SRC_BIO_SET_DEV
 
 	# 5.12
@@ -5250,6 +5292,7 @@ AC_DEFUN([LC_PROG_LINUX_SRC], [
 	LC_SRC_NFS_FILLDIR_USE_CTX_RETURN_BOOL
 	LC_SRC_HAVE_FILEMAP_GET_FOLIOS_CONTIG
 	LC_SRC_IOP_GET_INODE_ACL
+	LC_SRC_HAVE_POSIX_ACL_TYPE
 
 	# 6.2
 	LC_SRC_HAVE_GET_RANDOM_U32_BELOW
@@ -5529,6 +5572,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_FSCRYPT_PREPARE_READDIR
 
 	# 5.11
+	LC_SET_POSIX_ACL_USER_NS
 	LC_BIO_SET_DEV
 
 	# 5.12
@@ -5579,6 +5623,7 @@ AC_DEFUN([LC_PROG_LINUX_RESULTS], [
 	LC_NFS_FILLDIR_USE_CTX_RETURN_BOOL
 	LC_HAVE_FILEMAP_GET_FOLIOS_CONTIG
 	LC_IOP_GET_INODE_ACL
+	LC_HAVE_POSIX_ACL_TYPE
 
 	# 6.2
 	LC_HAVE_GET_RANDOM_U32_BELOW

lustre/osd-ldiskfs/osd_handler.c
@@ -5412,10 +5412,7 @@ static int osd_xattr_del(const struct lu_env *env, struct dt_object *dt,
 				obj->oo_pfid_in_lma = 0;
 		}
 	} else {
-		dquot_initialize(inode);
-		dentry->d_inode = inode;
-		dentry->d_sb = inode->i_sb;
-		rc = ll_vfs_removexattr(dentry, inode, name);
+		rc = __osd_xattr_del(inode, dentry, name);
 	}
 
 	osd_trans_exec_check(env, handle, OSD_OT_XATTR_SET);

lustre/osd-ldiskfs/osd_internal.h
@@ -804,6 +804,96 @@ extern int ldiskfs_pdo;
 #define DECLARE_BVEC_ITER_ALL(iter) int iter
 #endif
 
+#ifndef HAVE_POSIX_ACL_TYPE
+static inline int posix_acl_type(const char *name)
+{
+	if (strcmp(name, XATTR_NAME_POSIX_ACL_ACCESS) == 0)
+		return ACL_TYPE_ACCESS;
+	if (strcmp(name, XATTR_NAME_POSIX_ACL_DEFAULT) == 0)
+		return ACL_TYPE_DEFAULT;
+
+	return -1;
+}
+#endif
+
+static inline int __osd_acl_get(struct inode *inode, const char *name,
+				void *buf, int len)
+{
+	struct posix_acl *acl;
+	int acl_type, error;
+
+	acl_type = posix_acl_type(name);
+	if (acl_type < 0)
+		return -EINVAL;
+
+#ifdef HAVE_IOP_GET_INODE_ACL
+	acl = get_inode_acl(inode, acl_type);
+#else
+	acl = get_acl(inode, acl_type);
+#endif
+	if (IS_ERR(acl))
+		return PTR_ERR(acl);
+	if (!acl)
+		return -ENODATA;
+
+	error = posix_acl_to_xattr(&init_user_ns, acl, buf, len);
+	posix_acl_release(acl);
+
+	return error;
+}
+
+static inline int __osd_acl_set(struct dentry *dentry, struct inode *inode,
+				const char *name, const void *buf,
+				int buflen)
+{
+	int acl_type;
+	int error;
+	struct posix_acl *acl = NULL;
+
+	acl_type = posix_acl_type(name);
+	if (acl_type < 0)
+		return -EINVAL;
+
+	if (buflen) {
+		acl = posix_acl_from_xattr(&init_user_ns, buf, buflen);
+		if (IS_ERR(acl))
+			return PTR_ERR(acl);
+	}
+
+#ifdef HAVE_MNT_IDMAP_ARG
+	error = set_posix_acl(&nop_mnt_idmap, dentry, acl_type, acl);
+#elif defined HAVE_ACL_WITH_DENTRY
+	error = set_posix_acl(&init_user_ns, dentry, acl_type, acl);
+#elif defined HAVE_SET_POSIX_ACL_USER_NS
+	error = set_posix_acl(&init_user_ns, inode, acl_type, acl);
+#else
+	error = set_posix_acl(inode, acl_type, acl);
+#endif
+	posix_acl_release(acl);
+
+	return error;
+}
+
+static inline int __osd_acl_del(struct dentry *dentry, struct inode *inode,
+				const char *name)
+{
+	int acl_type;
+
+	acl_type = posix_acl_type(name);
+	if (acl_type < 0)
+		return -EINVAL;
+
+#ifdef HAVE_MNT_IDMAP_ARG
+	return set_posix_acl(&nop_mnt_idmap, dentry, acl_type, NULL);
+#elif defined HAVE_ACL_WITH_DENTRY
+	return set_posix_acl(&init_user_ns, dentry, acl_type, NULL);
+#elif defined HAVE_SET_POSIX_ACL_USER_NS
+	return set_posix_acl(&init_user_ns, inode, acl_type, NULL);
+#else
+	return set_posix_acl(inode, acl_type, NULL);
+#endif
+}
+
 static inline int __osd_xattr_get(struct inode *inode, struct dentry *dentry,
 				  const char *name, void *buf, int len)
 {
@@ -812,7 +902,11 @@ static inline int __osd_xattr_get(struct inode *inode, struct dentry *dentry,
 
 	dentry->d_inode = inode;
 	dentry->d_sb = inode->i_sb;
-	return ll_vfs_getxattr(dentry, inode, name, buf, len);
+	if (strcmp(name, XATTR_NAME_ACL_ACCESS) == 0 ||
+	    strcmp(name, XATTR_NAME_ACL_DEFAULT) == 0)
+		return __osd_acl_get(inode, name, buf, len);
+	else
+		return ll_vfs_getxattr(dentry, inode, name, buf, len);
 }
 
 static inline int __osd_xattr_set(struct osd_thread_info *info,
@@ -824,7 +918,24 @@ static inline int __osd_xattr_set(struct osd_thread_info *info,
 	dquot_initialize(inode);
 	dentry->d_inode = inode;
 	dentry->d_sb = inode->i_sb;
-	return ll_vfs_setxattr(dentry, inode, name, buf, buflen, fl);
+	if (strcmp(name, XATTR_NAME_ACL_ACCESS) == 0 ||
+	    strcmp(name, XATTR_NAME_ACL_DEFAULT) == 0)
+		return __osd_acl_set(dentry, inode, name, buf, buflen);
+	else
+		return ll_vfs_setxattr(dentry, inode, name, buf, buflen, fl);
+}
+
+static inline int __osd_xattr_del(struct inode *inode, struct dentry *dentry,
+				  const char *name)
+{
+	dquot_initialize(inode);
+	dentry->d_inode = inode;
+	dentry->d_sb = inode->i_sb;
+	if (strcmp(name, XATTR_NAME_ACL_ACCESS) == 0 ||
+	    strcmp(name, XATTR_NAME_ACL_DEFAULT) == 0)
+		return __osd_acl_del(dentry, inode, name);
+	else
+		return ll_vfs_removexattr(dentry, inode, name);
 }
 
 static inline char *osd_oid_name(char *name, size_t name_size,

Cray/lustre

commit 45577ba6030f53f3e504219cac200ac16c5b3f27

LU-19854 ldiskfs: posix acl and IGET_NO_CHECKS
ldiskfs-6.1.38-ml.series should include
  rhel9.4/ext4-add-IGET_NO_CHECKS-flag.patch

Fix posix_acl configure tests need to include <linux/fs.h>:
  LC_SRC_SET_POSIX_ACL_USER_NS
  LC_SRC_HAVE_POSIX_ACL_TYPE

Test-Parameters: trivial
Fixes: 35f58473619 ("LU-19186 osd-ldiskfs: use posix ACL api")
Fixes: 1045c6433c5 ("LU-18195 osd-ldiskfs: use IGET_NO_CHECKS for objects under lost+found")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I3fa342c9d23f8367bb3cc253bee286d0a51d4464
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63247
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

config/lustre-core.m4
@@ -2340,6 +2340,7 @@ AC_DEFUN([LC_FSCRYPT_PREPARE_READDIR], [
 #
 AC_DEFUN([LC_SRC_SET_POSIX_ACL_USER_NS], [
 	LB2_LINUX_TEST_SRC([set_posix_acl_user_ns], [
+		#include <linux/fs.h>
 		#include <linux/posix_acl.h>
 	],[
 		set_posix_acl((struct user_namespace *)NULL, (struct inode*)NULL, 0, NULL);
@@ -3310,6 +3311,7 @@ AC_DEFUN([LC_IOP_GET_INODE_ACL], [
 #
 AC_DEFUN([LC_SRC_HAVE_POSIX_ACL_TYPE], [
 	LB2_LINUX_TEST_SRC([posix_acl_type], [
+		#include <linux/fs.h>
 		#include <linux/posix_acl_xattr.h>
 	],[
 		posix_acl_type(NULL);

ldiskfs/kernel_patches/series/ldiskfs-6.1.38-ml.series
@@ -37,3 +37,4 @@ ubuntu20.04.5/ext4-filename-encode.patch
 rhel9.1/ext4-enc-flag.patch
 rhel9.2/ext4-encdata.patch
 rhel9/ext4-add-periodic-superblock-update.patch
+rhel9.4/ext4-add-IGET_NO_CHECKS-flag.patch