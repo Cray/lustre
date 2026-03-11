Cray/lustre

commit 984a20b8c2dbfedba7bf6bf41fb24ca72575914a

LUS-12626 llite: reclaim dentries/inodes in smaller amounts
do_shrink_slab() may try to release a large amount of dentries
and inodes, especially with lower reclaim priority values.
Since reclaim is performed under shrinker_rwsem, other threads
working with it may timeout and panic.

Change-Id: I2cd887196e873660f622d25294bb2d9f100afb25
Signed-off-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163141
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/llite/llite_lib.c
@@ -1322,6 +1322,22 @@ static int super_setup_bdi_name(struct super_block *sb, char *fmt, ...)
 }
 #endif /* !HAVE_SUPER_SETUP_BDI_NAME */
 
+static unsigned long ll_count_objects(struct shrinker *shrink,
+				      struct shrink_control *sc)
+{
+	struct super_block *sb = container_of(shrink, struct super_block, s_shrink);
+	struct ll_sb_info *sbi = ll_s2sbi(sb);
+	unsigned int max = sbi->ll_max_shrink;
+	long total_objects;
+
+	total_objects = sbi->ll_count_objects(shrink, sc);
+	if (total_objects != SHRINK_EMPTY &&
+	    max && total_objects > max)
+		total_objects = max;
+
+	return total_objects;
+}
+
 int ll_fill_super(struct super_block *sb)
 {
 	struct	lustre_profile *lprof = NULL;
@@ -1464,6 +1480,10 @@ int ll_fill_super(struct super_block *sb)
 	err = client_common_fill_super(sb, md, dt);
 	if (err < 0)
 		GOTO(out_free_md, err);
+
+	sbi->ll_count_objects = sb->s_shrink.count_objects;
+	sb->s_shrink.count_objects = ll_count_objects;
+	sbi->ll_max_shrink = 0;
 
 	sbi->ll_client_common_fill_super_succeeded = 1;
 

lustre/llite/lproc_llite.c
@@ -1583,6 +1583,36 @@ static ssize_t dir_read_on_open_store(struct kobject *kobj,
 
 LUSTRE_RW_ATTR(dir_read_on_open);
 
+static ssize_t max_shrink_show(struct kobject *kobj,
+				     struct attribute *attr,
+				     char *buf)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+
+	return snprintf(buf, PAGE_SIZE, "%u\n", sbi->ll_max_shrink);
+}
+
+static ssize_t max_shrink_store(struct kobject *kobj,
+				      struct attribute *attr,
+				      const char *buffer,
+				      size_t count)
+{
+	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
+					      ll_kset.kobj);
+	unsigned int val;
+	int rc;
+
+	rc = kstrtouint(buffer, 10, &val);
+	if (rc)
+		return rc;
+
+	sbi->ll_max_shrink = val;
+
+	return count;
+}
+LUSTRE_RW_ATTR(max_shrink);
+
 static int ll_unstable_stats_seq_show(struct seq_file *m, void *v)
 {
 	struct super_block	*sb    = m->private;
@@ -1907,6 +1937,7 @@ static struct attribute *llite_attrs[] = {
 	&lustre_attr_opencache_threshold_ms.attr,
 	&lustre_attr_opencache_max_ms.attr,
 	&lustre_attr_dir_read_on_open.attr,
+	&lustre_attr_max_shrink.attr,
 	NULL,
 };
 

lustre/llite/llite_internal.h
@@ -867,6 +867,10 @@ struct ll_sb_info {
 	/* cached file security context xattr name. e.g: security.selinux */
 	char *ll_secctx_name;
 	__u32 ll_secctx_name_size;
+
+	unsigned long (*ll_count_objects)(struct shrinker *shrink,
+					  struct shrink_control *sc);
+	unsigned int		 ll_max_shrink;
 };
 
 #define SBI_DEFAULT_HEAT_DECAY_WEIGHT	((80 * 256 + 50) / 100)

commit b8634a8670a116e1813ee529a9f80f40fcd33455

LUS-13237 llite: update shrinker for el7
This effectively reverts LUS-12626 on kernels that are too old to
support it.

.../lustre/llite/rw.c:908:9: error: missing braces around \
initializer [-Werror=missing-braces]
  struct ll_readahead_state ras = { 0 };

Fixes: 984a20b8c2db ("LUS-12626 llite: reclaim dentries/inodes in smaller amounts")
Fixes: 9618a7fe3185 ("LUS-11367 readahead: simplification")
Signed-off-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Change-Id: I4e9ffb42375ca9ee37d654d02182ef824be471d3
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163572
Reviewed-by: Alexander Zarochentsev <alexander.zarochentsev@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: Vitaly Fertman <vitaly.fertman@hpe.com>

lustre/llite/llite_lib.c
@@ -1325,6 +1325,7 @@ static int super_setup_bdi_name(struct super_block *sb, char *fmt, ...)
 }
 #endif /* !HAVE_SUPER_SETUP_BDI_NAME */
 
+#if defined HAVE_SHRINKER_COUNT || defined HAVE_S_SHRINK_AS_A_POINTER
 static unsigned long ll_count_objects(struct shrinker *shrink,
 				      struct shrink_control *sc)
 {
@@ -1344,6 +1345,7 @@ static unsigned long ll_count_objects(struct shrinker *shrink,
 
 	return total_objects;
 }
+#endif
 
 int ll_fill_super(struct super_block *sb)
 {
@@ -1491,7 +1493,7 @@ int ll_fill_super(struct super_block *sb)
 #ifdef HAVE_S_SHRINK_AS_A_POINTER
 	sbi->ll_count_objects = sb->s_shrink->count_objects;
 	sb->s_shrink->count_objects = ll_count_objects;
-#else
+#elif defined HAVE_SHRINKER_COUNT
 	sbi->ll_count_objects = sb->s_shrink.count_objects;
 	sb->s_shrink.count_objects = ll_count_objects;
 #endif

lustre/llite/lproc_llite.c
@@ -1656,10 +1656,14 @@ static ssize_t max_shrink_show(struct kobject *kobj,
 				     struct attribute *attr,
 				     char *buf)
 {
+#if defined HAVE_SHRINKER_COUNT || defined HAVE_S_SHRINK_AS_A_POINTER
 	struct ll_sb_info *sbi = container_of(kobj, struct ll_sb_info,
 					      ll_kset.kobj);
 
 	return snprintf(buf, PAGE_SIZE, "%u\n", sbi->ll_max_shrink);
+#else
+	return snprintf(buf, PAGE_SIZE, "%u\n", -1);
+#endif
 }
 
 static ssize_t max_shrink_store(struct kobject *kobj,

lustre/llite/rw.c
@@ -905,7 +905,7 @@ static int ll_readpages(const struct lu_env *env, struct cl_io *io,
 {
 	int ret = 0;
 	__u64 kms;
-	struct ll_readahead_state ras = { 0 };
+	struct ll_readahead_state ras = { };
 	struct ra_io_arg *ria;
 
 	ENTRY;