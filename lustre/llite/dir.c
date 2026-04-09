Cray/lustre

commit e41b560adf0cac52b57e123e74ce54a3946cf292

LUS-7695 llite: read dir on open
Let's read some pages at directory start,
a clients needs it probably.

readdir on open enabled.

real    0m39.977s
user    0m0.121s
sys     0m7.161s

readdir on open disabled

real    1m18.106s
user    0m0.151s
sys     0m15.666s

Change-Id: Iee9bedf471e89c2acc494075ae4e6c5c05260575
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/162864/
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Reviewed-by: Andrew Perepechko <andrew.perepechko@hpe.com>
Tested-by: Elena Gryaznova <elena.gryaznova@hpe.com>

lustre/llite/dir.c
@@ -143,12 +143,25 @@
  *
  */
 struct page *ll_get_dir_page(struct inode *dir, struct md_op_data *op_data,
-			     __u64 offset, int *partial_readdir_rc)
+			     __u64 offset, bool hash64, int *partial_readdir_rc)
 {
 	struct md_readdir_info mrinfo = {
 					.mr_blocking_ast = ll_md_blocking_ast };
 	struct page *page;
+	unsigned long idx = hash_x_index(offset, hash64);
 	int rc;
+
+	/* check page first */
+	page = pagecache_get_page(dir->i_mapping, idx,
+				  FGP_NOFS | FGP_NOWAIT,
+				  mapping_gfp_mask(dir->i_mapping) |
+				  __GFP_NORETRY | __GFP_NOWARN);
+	if (page) {
+		wait_on_page_locked(page);
+		if (PageUptodate(page))
+			RETURN(page);
+		put_page(page);
+	}
 
 	rc = md_read_page(ll_i2mdexp(dir), op_data, &mrinfo, offset, &page);
 	if (rc != 0)
@@ -206,7 +219,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 			RETURN(rc);
 	}
 
-	page = ll_get_dir_page(inode, op_data, pos, partial_readdir_rc);
+	page = ll_get_dir_page(inode, op_data, pos, is_hash64, partial_readdir_rc);
 
 	while (rc == 0 && !done) {
 		struct lu_dirpage *dp;
@@ -303,7 +316,7 @@ int ll_dir_read(struct inode *inode, __u64 *ppos, struct md_op_data *op_data,
 					LDF_COLLIDE);
 			next = pos;
 			page = ll_get_dir_page(inode, op_data, pos,
-					       partial_readdir_rc);
+					       is_hash64, partial_readdir_rc);
 		}
 	}
 #ifdef HAVE_DIR_CONTEXT

lustre/include/lu_object.h
@@ -935,8 +935,16 @@ struct lu_rdpg {
         /** requested attr */
         __u32                   rp_attrs;
         /** pointers to pages */
-        struct page           **rp_pages;
+        union {
+		struct page	**rp_pages;
+		void		*rp_data;
+        };
 };
+
+/* for dt_index_walk / mdd_readpage */
+void *rdpg_page_get(const struct lu_rdpg *rdpg, unsigned int index);
+void rdpg_page_put(const struct lu_rdpg *rdpg, unsigned int index);
+
 
 enum lu_xattr_flags {
 	LU_XATTR_REPLACE = BIT(0),

lustre/include/lustre_dlm.h
@@ -1161,6 +1161,12 @@ static inline bool ldlm_has_dom(struct ldlm_lock *lock)
 		lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_DOM;
 }
 
+static inline bool ldlm_has_update(struct ldlm_lock *lock)
+{
+	return lock->l_resource->lr_type == LDLM_IBITS &&
+		lock->l_policy_data.l_inodebits.bits & MDS_INODELOCK_UPDATE;
+}
+
 static inline char *
 ldlm_ns_name(struct ldlm_namespace *ns)
 {

lustre/include/lustre_export.h
@@ -490,6 +490,11 @@ static inline int exp_connect_dom_lvb(struct obd_export *exp)
 	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_DOM_LVB);
 }
 
+static inline int exp_connect_open_readdir(struct obd_export *exp)
+{
+	return !!(exp_connect_flags2(exp) & OBD_CONNECT2_READDIR_OPEN);
+}
+
 enum {
 	/* archive_ids in array format */
 	KKUC_CT_DATA_ARRAY_MAGIC	= 0x092013cea,

lustre/include/lustre_mdc.h
@@ -110,6 +110,15 @@ static inline void cl_lov_delay_create_clear(unsigned int *flags)
 		*flags &= ~O_LOV_DELAY_CREATE_MASK;
 }
 
+static inline unsigned long hash_x_index(__u64 hash, int hash64)
+{
+	if (BITS_PER_LONG == 32 && hash64)
+		hash >>= 32;
+	/* save hash 0 with hash 1 */
+	return ~0UL - (hash + !hash);
+}
+
+
 /** @} mdc */
 
 #endif

lustre/include/obd.h
@@ -856,6 +856,7 @@ enum md_cli_flags {
 	CLI_MIGRATE	= BIT(4),
 	CLI_DIRTY_DATA	= BIT(5),
 	CLI_NO_SLOT     = BIT(6),
+	CLI_READ_ON_OPEN= BIT(7), /**< read on open (used for directory for now) */
 };
 
 enum md_op_code {

lustre/include/uapi/linux/lustre/lustre_idl.h
@@ -401,7 +401,6 @@ enum lu_dirent_attrs {
 
 	/* The following attrs are used for MDT internal only,
 	 * not visible to client */
-
 	/* Something in the record is unknown, to be verified in further. */
 	LUDA_UNKNOWN		= 0x0400,
 	/* Ignore this record, go to next directly. */
@@ -853,6 +852,8 @@ struct ptlrpc_body_v2 {
 #define OBD_CONNECT2_PCCRO	      0x800000ULL /* Read-only PCC */
 #define OBD_CONNECT2_ATOMIC_OPEN_LOCK 0x4000000ULL/* request lock on 1st open */
 #define OBD_CONNECT2_ENCRYPT_NAME     0x8000000ULL /* name encrypt */
+#define OBD_CONNECT2_READDIR_OPEN     0x8000000000ULL /* read first dir page on open */
+
 /* XXX README XXX:
  * Please DO NOT add flag values here before first ensuring that this same
  * flag value is not in use on some other branch.  Please clear any such
@@ -916,7 +917,8 @@ struct ptlrpc_body_v2 {
 				OBD_CONNECT2_LSEEK | OBD_CONNECT2_DOM_LVB |\
 				OBD_CONNECT2_REP_MBITS | \
 				OBD_CONNECT2_ATOMIC_OPEN_LOCK | \
-				OBD_CONNECT2_ENCRYPT_NAME)
+				OBD_CONNECT2_ENCRYPT_NAME |\
+				OBD_CONNECT2_READDIR_OPEN)
 
 #define OST_CONNECT_SUPPORTED  (OBD_CONNECT_SRVLOCK | OBD_CONNECT_GRANT | \
 				OBD_CONNECT_REQPORTAL | OBD_CONNECT_VERSION | \

lustre/llite/file.c
@@ -638,6 +638,65 @@ out_io:
 
 	EXIT;
 }
+void ll_dir_finish_open(struct inode *inode, struct ptlrpc_request *req)
+{
+	struct obd_export *exp = ll_i2mdexp(inode);
+	void *data;
+	struct page *page;
+	struct lu_dirpage *dp;
+	int is_hash64;
+	int rc;
+	unsigned long	offset;
+	__u64		hash;
+	unsigned int i;
+	unsigned int npages;
+	ENTRY;
+
+	if (!exp_connect_open_readdir(exp))
+		RETURN_EXIT;
+
+	if (!req_capsule_field_present(&req->rq_pill, &RMF_NIOBUF_INLINE,
+				       RCL_SERVER))
+		RETURN_EXIT;
+
+	data = req_capsule_server_get(&req->rq_pill, &RMF_NIOBUF_INLINE);
+	if (data == NULL)
+		RETURN_EXIT;
+
+	npages = req_capsule_get_size(&req->rq_pill, &RMF_NIOBUF_INLINE, RCL_SERVER);
+	if ( npages < sizeof(*dp))
+		RETURN_EXIT;
+
+	/* div rou*/
+	npages = DIV_ROUND_UP(npages, PAGE_SIZE);
+
+	for(i = 0; i < npages; i++) {
+		page = page_cache_alloc(inode->i_mapping);
+		if (!page)
+			continue;
+
+		lock_page(page);
+		SetPageUptodate(page);
+
+		dp = kmap_atomic(page);
+		memcpy(dp, data, PAGE_SIZE);
+		hash = le64_to_cpu(dp->ldp_hash_start);
+		kunmap_atomic(dp);
+
+		is_hash64 = test_bit(LL_SBI_64BIT_HASH, ll_i2sbi(inode)->ll_flags);
+		offset = hash_x_index(hash, is_hash64);
+
+		prefetchw(&page->flags);
+		rc = add_to_page_cache_lru(page, inode->i_mapping, offset,
+				   GFP_KERNEL);
+		if (rc == 0)
+			unlock_page(page);
+
+		put_page(page);
+	}
+	EXIT;
+}
+
 
 static int ll_intent_file_open(struct dentry *de, void *lmm, int lmmsize,
 				struct lookup_intent *itp)
@@ -690,6 +749,9 @@ retry:
 	op_data->op_data = lmm;
 	op_data->op_data_size = lmmsize;
 
+	if (!sbi->ll_dir_open_read && S_ISDIR(de->d_inode->i_mode))
+		op_data->op_cli_flags &= ~CLI_READ_ON_OPEN;
+
 	OBD_FAIL_TIMEOUT(OBD_FAIL_LLITE_OPEN_DELAY, cfs_fail_val);
 
 	rc = md_intent_lock(sbi->ll_md_exp, op_data, itp, &req,
@@ -737,6 +799,8 @@ retry:
 		 */
 		if (bits & MDS_INODELOCK_DOM && bits & MDS_INODELOCK_LAYOUT)
 			ll_dom_finish_open(de->d_inode, req);
+		if (bits & MDS_INODELOCK_UPDATE && S_ISDIR(de->d_inode->i_mode))
+			ll_dir_finish_open(de->d_inode, req);
 	}
 
 out:

commit bd8c743cd7978c8f722e83171ba70967d7f0cfeb

LUS-13257 llite: avoid lookup page if pagecache_get don't exist.
grab_cache_page_nowait - pagecache_get_page replacement for the el7
(3.10 kernel) unable to return unlocked page it causes an
infinite waiting. lets avoid such small optimization for readpage.

Fixes: e41b560ad ("LUS-7695 llite: read dir on open")
Change-Id: I94fd5da5923d52a6e4ff0909ffb6b9e995de5b56
Signed-off-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163624
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Alexander Boyko <alexander.boyko@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

lustre/llite/dir.c
@@ -148,8 +148,9 @@ struct page *ll_get_dir_page(struct inode *dir, struct md_op_data *op_data,
 	struct md_readdir_info mrinfo = {
 					.mr_blocking_ast = ll_md_blocking_ast };
 	struct page *page;
-	unsigned long idx = hash_x_index(offset, hash64);
 	int rc;
+#ifdef HAVE_PAGECACHE_GET_PAGE
+	unsigned long idx = hash_x_index(offset, hash64);
 
 	/* check page first */
 	page = pagecache_get_page(dir->i_mapping, idx,
@@ -162,6 +163,7 @@ struct page *ll_get_dir_page(struct inode *dir, struct md_op_data *op_data,
 			RETURN(page);
 		put_page(page);
 	}
+#endif
 
 	rc = md_read_page(ll_i2mdexp(dir), op_data, &mrinfo, offset, &page);
 	if (rc != 0)