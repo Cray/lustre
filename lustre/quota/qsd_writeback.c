lustre/lustre-release

commit d06c56d2eb61f0eeee1eed9848101d5a9d6efcfd

LU-18078 quota: check version to force_reint if needed
If the quota setting's version between QMT and QSD is mismatched,
the new quota request will be deferred and won't be applied if
the missing update with the version had been dropped.
This patch will check the version during processing the incoming
quota LDLM glimpse request and inside the QSD writeback thread,
and will trigger the force_reint if the missed version has not
been arrived in time.

Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: I1b33cfbb594e3f1595580d4190fd77efb55bc627
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/56513
Tested-by: jenkins <devops@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Sergey Cheremencev <scherementsev@ddn.com>
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/qsd_writeback.c
@@ -181,6 +181,7 @@ void qsd_bump_version(struct qsd_qtype_info *qqi, __u64 ver, bool global)
 	list    = global ? &qqi->qqi_deferred_glb : &qqi->qqi_deferred_slv;
 
 	write_lock(&qqi->qqi_qsd->qsd_lock);
+	qqi->qqi_last_version_update_time = ktime_get_seconds();
 	*idx_ver = ver;
 	if (global)
 		qqi->qqi_glb_uptodate = 1;
@@ -207,6 +208,7 @@ void qsd_upd_schedule(struct qsd_qtype_info *qqi, struct lquota_entry *lqe,
 	struct qsd_upd_rec	*upd;
 	struct qsd_instance	*qsd = qqi->qqi_qsd;
 	__u64			 cur_ver;
+	bool			 need_to_trigger_reint = false;
 	ENTRY;
 
 	CDEBUG(D_QUOTA, "%s: schedule update. global:%s, version:%llu\n",
@@ -244,16 +246,23 @@ void qsd_upd_schedule(struct qsd_qtype_info *qqi, struct lquota_entry *lqe,
 		   qqi->qqi_slv_uptodate) {
 		/* In order update, and reintegration has been done. */
 		qsd_upd_add(qsd, upd);
-	} else {
+	} else if (qqi->qqi_last_version_update_time + QSD_WB_INTERVAL >=
+		   ktime_get_seconds()) {
 		/* Out of order update (the one with smaller version hasn't
 		 * reached slave or hasn't been flushed to disk yet), or
 		 * the reintegration is in progress. Defer the update. */
 		struct list_head *list = global ? &qqi->qqi_deferred_glb :
 						  &qqi->qqi_deferred_slv;
 		qsd_add_deferred(qsd, list, upd);
+	} else {
+		qsd_upd_free(upd);
+		need_to_trigger_reint = true;
 	}
 
 	write_unlock(&qsd->qsd_lock);
+
+	if (need_to_trigger_reint)
+		qsd_start_reint_thread(qqi);
 
 	EXIT;
 }
@@ -471,6 +480,10 @@ static bool qsd_job_pending(struct qsd_instance *qsd, struct list_head *upd,
 
 		if (!qsd_type_enabled(qsd, qtype))
 			continue;
+
+		if (qqi->qqi_last_version_update_time + QSD_WB_INTERVAL <
+		    ktime_get_seconds() && !list_empty(&qqi->qqi_deferred_glb))
+			*uptodate = false;
 
 		if ((!qqi->qqi_glb_uptodate || !qqi->qqi_slv_uptodate) &&
 		     !qqi->qqi_reint)

lustre/quota/qsd_internal.h
@@ -161,6 +161,9 @@ struct qsd_qtype_info {
 	__u64			qqi_default_hardlimit;
 	__u64			qqi_default_softlimit;
 	__u64			qqi_default_gracetime;
+
+	/* the last time of updating quota index version */
+	time64_t		qqi_last_version_update_time;
 };
 
 /*

lustre/quota/qsd_reint.c
@@ -198,6 +198,8 @@ static int qsd_reint_index(const struct lu_env *env, struct qsd_qtype_info *qqi,
 			GOTO(out, rc = -ENOMEM);
 	}
 
+	qqi->qqi_last_version_update_time = ktime_get_seconds();
+
 repeat:
 	/* initialize index_info request with FID of global index */
 	memset(ii, 0, sizeof(*ii));

Cray/lustre

commit ef450e8558f4827a069e7133bc77eacde72b42f7

LU-18078 quota: clear uptodate before reintergation
The "qqi->qqi_glb_uptodate" or "qqi->qqi_slv_uptodate" should be
cleared before calling qsd_start_reint_thread to reintegration
the quota settings between QSD and QMT.

Fixes: d06c56d2eb6 ("LU-18078 quota: check version to force_reint if needed")
Signed-off-by: Hongchao Zhang <hongchao@whamcloud.com>
Change-Id: I060ba1f173f418bc2d2e5c0af2faef301940112f
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/64393
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>

lustre/quota/qsd_writeback.c
@@ -261,8 +261,14 @@ void qsd_upd_schedule(struct qsd_qtype_info *qqi, struct lquota_entry *lqe,
 
 	write_unlock(&qsd->qsd_lock);
 
-	if (need_to_trigger_reint)
+	if (need_to_trigger_reint) {
+		if (global)
+			qqi->qqi_glb_uptodate = 0;
+		else
+			qqi->qqi_slv_uptodate = 0;
+
 		qsd_start_reint_thread(qqi);
+	}
 
 	EXIT;
 }
@@ -493,8 +499,17 @@ static bool qsd_job_pending(struct qsd_instance *qsd, struct list_head *upd,
 			continue;
 
 		if (qqi->qqi_last_version_update_time + QSD_WB_INTERVAL <
-		    ktime_get_seconds() && !list_empty(&qqi->qqi_deferred_glb))
-			*uptodate = false;
+		    ktime_get_seconds()) {
+			if (!list_empty(&qqi->qqi_deferred_glb)) {
+				qqi->qqi_glb_uptodate = 0;
+				*uptodate = false;
+			}
+
+			if (!list_empty(&qqi->qqi_deferred_slv)) {
+				qqi->qqi_slv_uptodate = 0;
+				*uptodate = false;
+			}
+		}
 
 		if ((!qqi->qqi_glb_uptodate || !qqi->qqi_slv_uptodate) &&
 		     !qqi->qqi_reint)