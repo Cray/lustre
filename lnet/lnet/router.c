lustre/lustre-release

commit ed70ccb4716d3488ed1b68eddf3075e2da024d05

LU-15135 lnet: Graceful router removal and addition
Removing an LNet router from service (say, for repairs) on a
production system is somewhat difficult today.

Current best practices involve either removing all routes for a router
from all clients and servers, then waiting for the router
buffers to drain before shutting down LNet, or disabling routing on
the router node itself. The former is not ideal because a route may be
removed on one side or another at different times creating an
asymmetric route, and this can result in network failures.
The latter is not ideal because the router will immediately drop any
buffered messages as well as new messages that arrive.

This patch provides a better solution for graceful shutdown by
modifying the behavior of LNet when routing is disabled via lnetctl.

A new routing state, LNET_ROUTING_STOPPING, is defined to indicate
that the router is in the process of shutting down. While in this
state, the router forwards messages as usual, but the
LNET_PING_FEAT_RTE_DISABLED bit is set in the router's ping buffer
feature bits. When clients and servers discover the router they will
see this bit and stop using the router for future sends.

To provide time for the clients and servers to stop using the router,
the monitor thread takes over responsibility for freeing the router
buffer pools. When the thread sees that the router has entered the new
STOPPING state, and after alive_router_check_interval +
router_ping_timeout seconds have passed, it checks whether any of the
router buffer pools are still in-use. If none are in use then it will
free the buffers and transition the routing state to
LNET_ROUTING_DISABLED.

If the pools are still active after 3x alive_router_check_interval +
router_ping_timeout seconds, then the monitor will drop any pending
messages, free the buffers and transition the routing state to
DISABLED.

Similar issues may occur when bringing a router back into service. If

lnet/lnet/router.c
@@ -329,9 +329,6 @@ bool lnet_is_route_alive(struct lnet_route *route)
 	spin_lock(&gw->lp_lock);
 	if (!(gw->lp_state & LNET_PEER_ROUTER_ENABLED)) {
 		spin_unlock(&gw->lp_lock);
-		if (gw->lp_rtr_refcount > 0)
-			CERROR("peer %s is being used as a gateway but routing feature is not turned on\n",
-			       libcfs_nidstr(&gw->lp_primary_nid));
 		return false;
 	}
 	spin_unlock(&gw->lp_lock);
@@ -1082,6 +1079,50 @@ bool lnet_router_checker_active(void)
 		alive_router_check_interval > 0;
 }
 
+static void
+lnet_enable_routing(void)
+{
+	lnet_net_lock(LNET_LOCK_EX);
+	the_lnet.ln_routing = LNET_ROUTING_ENABLED;
+
+	the_lnet.ln_ping_target->pb_info.pi_features &=
+		~LNET_PING_FEAT_RTE_DISABLED;
+	lnet_net_unlock(LNET_LOCK_EX);
+	LCONSOLE_INFO("Message forwarding enabled\n");
+}
+
+static void
+lnet_disable_routing(void)
+{
+	lnet_net_lock(LNET_LOCK_EX);
+	the_lnet.ln_routing = LNET_ROUTING_DISABLED;
+	lnet_net_unlock(LNET_LOCK_EX);
+	lnet_rtrpools_free(1);
+	LCONSOLE_INFO("Message forwarding disabled\n");
+}
+
+static bool
+lnet_rtrpools_are_free(time64_t now, int cpt)
+{
+	struct lnet_rtrbufpool *rbp;
+	int cpt2;
+
+	lnet_net_unlock(cpt);
+
+	cfs_percpt_for_each(rbp, cpt2, the_lnet.ln_rtrpools) {
+		lnet_net_lock(cpt2);
+		if (rbp->rbp_credits < rbp->rbp_nbuffers) {
+			lnet_net_unlock(cpt2);
+			lnet_net_lock(cpt);
+			return false;
+		}
+		lnet_net_unlock(cpt2);
+	}
+
+	lnet_net_lock(cpt);
+	return true;
+}
+
 void
 lnet_check_routers(void)
 {
@@ -1097,8 +1138,49 @@ lnet_check_routers(void)
 	time64_t now;
 	int cpt;
 	int rc;
+	static time64_t rtr_min_wait;
+	static time64_t rtr_max_wait;
 
 	cpt = lnet_net_lock_current();
+
+	now = ktime_get_real_seconds();
+
+	if (the_lnet.ln_routing == LNET_ROUTING_STOPPING ||
+	    the_lnet.ln_routing == LNET_ROUTING_STARTING) {
+		if (!rtr_min_wait) {
+			int interval = alive_router_check_interval +
+				       router_ping_timeout;
+
+			/* Wait at least alive_router_check_interval +
+			 * router_ping_timeout, and for stopping case
+			 * no more than 3x of these intervals, before
+			 * disabling or enabling routing
+			 */
+			rtr_min_wait = now + interval;
+			rtr_max_wait = now + 3 * interval;
+		}
+
+		if (the_lnet.ln_routing == LNET_ROUTING_STARTING &&
+		    rtr_min_wait < now) {
+			lnet_net_unlock(cpt);
+			lnet_enable_routing();
+			rtr_min_wait = 0;
+			rtr_max_wait = 0;
+			push = true;
+			lnet_net_lock(cpt);
+		}
+
+		if (the_lnet.ln_routing == LNET_ROUTING_STOPPING &&
+		    ((rtr_min_wait < now && lnet_rtrpools_are_free(now, cpt)) ||
+		     rtr_max_wait < now)) {
+			lnet_net_unlock(cpt);
+			lnet_disable_routing();
+			rtr_min_wait = 0;
+			rtr_max_wait = 0;
+			lnet_net_lock(cpt);
+		}
+	}
+
 rescan:
 	version = the_lnet.ln_routers_version;
 
@@ -1194,8 +1276,8 @@ rescan:
 		}
 	}
 
-	if (lnet_routing_enabled())
-		push = lnet_update_ni_status_locked();
+	if (lnet_routing_enabled() && lnet_update_ni_status_locked())
+		push = true;
 
 	lnet_net_unlock(cpt);
 
@@ -1595,8 +1677,13 @@ lnet_rtrpools_enable(void)
 {
 	int rc = 0;
 
-	if (lnet_routing_enabled())
+	if (the_lnet.ln_routing == LNET_ROUTING_ENABLED ||
+	    the_lnet.ln_routing == LNET_ROUTING_STARTING) {
 		return 0;
+	} else if (the_lnet.ln_routing == LNET_ROUTING_STOPPING) {
+		CERROR("Cannot enable routing while it is being stopped\n");
+		return -EBUSY;
+	}
 
 	if (the_lnet.ln_rtrpools == NULL)
 		/* If routing is turned off, and we have never
@@ -1607,14 +1694,15 @@ lnet_rtrpools_enable(void)
 		rc = lnet_rtrpools_alloc(1);
 	else
 		rc = lnet_rtrpools_adjust_helper(0, 0, 0);
-	if (rc != 0)
+
+	if (rc)
 		return rc;
 
 	lnet_net_lock(LNET_LOCK_EX);
-	the_lnet.ln_routing = LNET_ROUTING_ENABLED;
-
-	the_lnet.ln_ping_target->pb_info.pi_features &=
-		~LNET_PING_FEAT_RTE_DISABLED;
+	the_lnet.ln_routing = LNET_ROUTING_STARTING;
+	LCONSOLE_INFO("Message forwarding will be enabled in %ds\n",
+		      alive_router_check_interval +
+		      router_ping_timeout);
 	lnet_net_unlock(LNET_LOCK_EX);
 
 	if (lnet_peer_discovery_disabled)
@@ -1631,7 +1719,7 @@ lnet_rtrpools_disable(void)
 		return;
 
 	lnet_net_lock(LNET_LOCK_EX);
-	the_lnet.ln_routing = LNET_ROUTING_DISABLED;
+	the_lnet.ln_routing = LNET_ROUTING_STOPPING;
 	the_lnet.ln_ping_target->pb_info.pi_features |=
 		LNET_PING_FEAT_RTE_DISABLED;
 
@@ -1639,7 +1727,9 @@ lnet_rtrpools_disable(void)
 	small_router_buffers = 0;
 	large_router_buffers = 0;
 	lnet_net_unlock(LNET_LOCK_EX);
-	lnet_rtrpools_free(1);
+	LCONSOLE_INFO("Message forwarding will stop in %ds to %ds\n",
+		      alive_router_check_interval + router_ping_timeout,
+		      3 * (alive_router_check_interval + router_ping_timeout));
 }
 
 /*

lnet/include/lnet/lib-types.h
@@ -1925,6 +1925,8 @@ struct lnet_udsp {
 /* LNet routing states */
 #define LNET_ROUTING_DISABLED		0	/* LNet routing disabled */
 #define LNET_ROUTING_ENABLED		1	/* LNet routing enabled */
+#define LNET_ROUTING_STOPPING		2	/* LNet routing stopping */
+#define LNET_ROUTING_STARTING		3	/* LNet routing starting */
 
 #define lnet_routing_disabled() (the_lnet.ln_routing == LNET_ROUTING_DISABLED)
 #define lnet_routing_enabled() (the_lnet.ln_routing != LNET_ROUTING_DISABLED)

lnet/lnet/api-ni.c
@@ -3155,6 +3155,9 @@ LNetNIInit(lnet_pid_t requested_pid)
 		rc = lnet_rtrpools_alloc(im_a_router);
 		if (rc != 0)
 			goto err_destroy_routes;
+
+		if (the_lnet.ln_routing == LNET_ROUTING_ENABLED)
+			LCONSOLE_INFO("Message forwarding enabled\n");
 	}
 
 	rc = lnet_acceptor_start();

lnet/lnet/lnet_debugfs.c
@@ -199,6 +199,10 @@ ln_routing2str(void)
 		return "Routing disabled\n";
 	case LNET_ROUTING_ENABLED:
 		return "Routing enabled\n";
+	case LNET_ROUTING_STARTING:
+		return "Routing starting\n";
+	case LNET_ROUTING_STOPPING:
+		return "Routing stopping\n";
 	default:
 		return "Routing unknown\n";
 	}

lnet/lnet/peer.c
@@ -3290,7 +3290,7 @@ static int lnet_peer_merge_data(struct lnet_peer *lp,
 
 	/*
 	 * Cache the routing feature for the peer; whether it is enabled
-	 * for disabled as reported by the remote peer.
+	 * or disabled as reported by the remote peer.
 	 */
 	spin_lock(&lp->lp_lock);
 	if (!(pbuf->pb_info.pi_features & LNET_PING_FEAT_RTE_DISABLED))

lnet/utils/lnetconfig/liblnetconfig.c
@@ -3263,7 +3263,7 @@ out:
 	return rc;
 }
 
-int lustre_lnet_enable_routing(int enable, int seq_no, struct cYAML **err_rc)
+int lustre_lnet_config_routing(int enable, int seq_no, struct cYAML **err_rc)
 {
 	struct lnet_ioctl_config_data data;
 	int rc = LUSTRE_CFG_RC_NO_ERR;
@@ -5803,7 +5803,7 @@ static int handle_yaml_config_routing(struct cYAML *tree,
 	enable = cYAML_get_object_item(tree, "enable");
 
 	if (enable) {
-		rc = lustre_lnet_enable_routing(enable->cy_valueint,
+		rc = lustre_lnet_config_routing(enable->cy_valueint,
 						(seq_no) ?
 						    seq_no->cy_valueint : -1,
 						err_rc);
@@ -5836,7 +5836,7 @@ static int handle_yaml_del_routing(struct cYAML *tree, struct cYAML **show_rc,
 
 	seq_no = cYAML_get_object_item(tree, "seq_no");
 
-	return lustre_lnet_enable_routing(0, (seq_no) ?
+	return lustre_lnet_config_routing(0, (seq_no) ?
 						seq_no->cy_valueint : -1,
 					err_rc);
 }

lnet/utils/lnetconfig/liblnetconfig.h
@@ -293,14 +293,14 @@ int lustre_lnet_show_net(char *nw, int detail, int seq_no,
 			 bool backup);
 
 /*
- * lustre_lnet_enable_routing
+ * lustre_lnet_config_routing
  *   Send down an IOCTL to enable or diable routing
  *
  *   enable - 1 to enable routing, 0 to disable routing
  *   seq_no - sequence number of the request
  *   err_rc - [OUT] struct cYAML tree describing the error. Freed by caller
  */
-int lustre_lnet_enable_routing(int enable, int seq_no,
+int lustre_lnet_config_routing(int enable, int seq_no,
 			       struct cYAML **err_rc);
 
 /*

lnet/utils/lnetctl.c
@@ -929,7 +929,7 @@ static int jt_set_routing(int argc, char **argv)
 		return -1;
 	}
 
-	rc = lustre_lnet_enable_routing(value, -1, &err_rc);
+	rc = lustre_lnet_config_routing(value, -1, &err_rc);
 
 	if (rc != LUSTRE_CFG_RC_NO_ERR)
 		cYAML_print_tree2file(stderr, err_rc);

Cray/lustre

commit 2ddd37dcac4522040834532b62d99f92b731f19d

LU-20039 lnet: preserve router buffer sizing on re-enable
When sanity-lnet test 257 disables routing and then enables it
again, the router can try to allocate the default large buffer
pool instead of the small buffer counts configured at module
load time. On small-memory test nodes this can trigger an
allocation failure like:

lnet: error allocating 1024x16 page router buffers on CPT 0: rc = -12

The disable and re-enable path was losing the configured router
buffer sizes. lnet_rtrpools_disable() reset tiny_router_buffers,
small_router_buffers, and large_router_buffers to zero, and
lnet_rtrpools_enable() rebuilt existing pools by calling
lnet_rtrpools_adjust_helper() with 0, 0, 0. In this interface, zero
means to fall back to the default sizing, not to preserve the current
values.

Fix this by keeping the configured buffer counts across routing
disable, and by having lnet_rtrpools_enable() rebuild the
existing pools with the saved tiny, small, and large router
buffer values. This makes routing re-enable follow the same
effective sizing as the initial router pool allocation.

Test-Parameters: trivial
Fixes: ed70ccb471 ("LU-15135 lnet: Graceful router removal and addition")
Signed-off-by: Chris Horn <chris.horn@hpe.com>
Change-Id: I6edff22744275bfe2fc95496d8a31d802803cb76
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65153
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Cyril Bordage <cbordage@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Tested-by: jenkins <devops@whamcloud.com>

lnet/lnet/router.c
@@ -1685,7 +1685,9 @@ lnet_rtrpools_enable(void)
 		 * time. */
 		rc = lnet_rtrpools_alloc(1);
 	else
-		rc = lnet_rtrpools_adjust_helper(0, 0, 0);
+		rc = lnet_rtrpools_adjust_helper(tiny_router_buffers,
+						 small_router_buffers,
+						 large_router_buffers);
 
 	if (rc)
 		return rc;
@@ -1714,10 +1716,6 @@ lnet_rtrpools_disable(void)
 	the_lnet.ln_routing = LNET_ROUTING_STOPPING;
 	the_lnet.ln_ping_target->pb_info.pi_features |=
 		LNET_PING_FEAT_RTE_DISABLED;
-
-	tiny_router_buffers = 0;
-	small_router_buffers = 0;
-	large_router_buffers = 0;
 	lnet_net_unlock(LNET_LOCK_EX);
 	LCONSOLE_INFO("Message forwarding will stop in %ds to %ds\n",
 		      alive_router_check_interval + router_ping_timeout,