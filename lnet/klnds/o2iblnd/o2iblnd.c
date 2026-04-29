lustre/lustre-release

commit aebedbe75007ad5747d10a3fc620644aa4074e6a

LU-19707 lnet: fix RTNL assert in LNet on v6.15
Testing on Linux v6.15:

RTNL: assertion failed at include/net/netdev_lock.h (56)
WARNING: CPU: 8 PID: 970 at include/net/netdev_lock.h:56 __linkwatch_sync_dev+0x226/0x280
...
Call Trace:
 <TASK>
 ethtool_op_get_link+0x12/0x60
 lnet_get_link_status+0x1f7/0x420 [lnet]
 ksocknal_startup+0x1269/0x22a0 [ksocklnd]
 ? ksocknal_startup+0xdbc/0x22a0 [ksocklnd]
 lnet_startup_lndnet+0x73e/0x25e0 [lnet]
 ? list_move_tail+0x34/0x50
 LNetNIInit+0x9cb/0x1120 [lnet]
 ptlrpc_init_portals+0xb6/0x380 [ptlrpc]
 init_module+0x2f8/0x1000 [ptlrpc]
 do_one_initcall+0x119/0x410
 ? llcrypt_msg+0x1090/0x1090 [libcfs]
 ? stack_depot_save_flags+0x41/0x830
 ? kasan_save_track+0x40/0x70
 ? kasan_save_track+0x2f/0x70
 ? __kasan_kmalloc+0x72/0x80
 ? do_init_module+0x8e/0x7c0
 ? __x64_sys_finit_module+0x3b5/0x5b0
 ? do_syscall_64+0x6c/0x140
 ? entry_SYSCALL_64_after_hwframe+0x4b/0x53
 ? pcpu_alloc_noprof+0xadd/0xf70
 ? codetag_module_init+0x273/0x350
 ? load_module+0x3489/0x4390
 ? lock_acquire+0xe7/0x1f0
 ? __kmalloc_cache_noprof+0x34/0x270
 ? kasan_unpoison+0x48/0x70
 ? kasan_unpoison+0x48/0x70
 ? __asan_register_globals+0x59/0x70
 do_init_module+0x2ce/0x7c0
 __x64_sys_finit_module+0x3b5/0x5b0
 do_syscall_64+0x6c/0x140
 ? arch_exit_to_user_mode_prepare+0x9/0x40
 entry_SYSCALL_64_after_hwframe+0x4b/0x53
...
 </TASK>

Solve this by holding the need locks in the order
typical in the kernel:

rtnl_lock();
netdev_lock_ops(dev);
...
netdev_unlock_ops(dev);
rtnl_unlock();

Implement the needed configure checks as well.

Test-Parameters: trivial
Signed-off-by: Timothy Day <timday@amazon.com>
Change-Id: Ideab7fb1bac89ab1c729ae823dd97cc47ee61a5b
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/63038
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Andreas Dilger <adilger@thelustrecollective.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/o2iblnd/o2iblnd.c
@@ -3208,7 +3208,7 @@ kiblnd_handle_link_state_change(struct net_device *dev,
 		if (link_down) {
 			ni_state_before = lnet_set_link_fatal_state(ni, 1);
 		} else {
-			state = (lnet_get_link_status(dev) == 0);
+			state = (lnet_get_link_status_locked(dev) == 0);
 			ni_state_before = lnet_set_link_fatal_state(ni,
 								    state);
 		}

config/lustre-lnet.m4
@@ -1283,6 +1283,26 @@ AC_DEFUN([LN_HAVE_IN_DEV_FOR_EACH_IFA_RTNL], [
 ]) # LN_HAVE_IN_DEV_FOR_EACH_IFA_RTNL
 
 #
+# LN_SRC_HAVE_NETDEV_LOCK_OPS
+#
+# netdev_lock_ops() added in v6.14-rc4-1113-gd4c22ec680 to lock ethtool changes
+#
+AC_DEFUN([LN_SRC_HAVE_NETDEV_LOCK_OPS], [
+	LB2_LINUX_TEST_SRC([netdev_lock_ops], [
+		#include <net/netdev_lock.h>
+	],[
+		netdev_lock_ops(NULL);
+	],[-Werror])
+])
+AC_DEFUN([LN_HAVE_NETDEV_LOCK_OPS], [
+	LB2_MSG_LINUX_TEST_RESULT([if 'netdev_lock_ops' exist],
+	[netdev_lock_ops], [
+		AC_DEFINE(HAVE_NETDEV_LOCK_OPS, 1,
+			['netdev_lock_ops' is present])
+	])
+]) # LN_SRC_HAVE_NETDEV_LOCK_OPS
+
+#
 # LN_USR_RDMA
 #
 #
@@ -1341,6 +1361,8 @@ AC_DEFUN([LN_PROG_LINUX_SRC], [
 	LN_SRC_HAVE_IN_DEV_FOR_EACH_IFA_RTNL
 	# 5.9
 	LN_SRC_CONFIG_SENDPAGE_OK
+	# 6.15
+	LN_SRC_HAVE_NETDEV_LOCK_OPS
 ])
 
 AC_DEFUN([LN_PROG_LINUX_RESULTS], [
@@ -1361,6 +1383,8 @@ AC_DEFUN([LN_PROG_LINUX_RESULTS], [
 	LN_HAVE_IN_DEV_FOR_EACH_IFA_RTNL
 	# 5.9
 	LN_CONFIG_SENDPAGE_OK
+	# 6.15
+	LN_HAVE_NETDEV_LOCK_OPS
 ])
 
 #

include/lustre_compat/net/netdev_lock.h
@@ -1,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __LIBCFS_LINUX_NETDEV_LOCK_H__
+#define __LIBCFS_LINUX_NETDEV_LOCK_H__
+
+#ifndef HAVE_NETDEV_LOCK_OPS
+static inline void netdev_lock_ops(struct net_device *dev)
+{
+}
+
+static inline void netdev_unlock_ops(struct net_device *dev)
+{
+}
+#else
+#include <net/netdev_lock.h>
+#endif
+
+#endif /* __LIBCFS_LINUX_NETDEV_LOCK_H__ */

lnet/include/lnet/lib-lnet.h
@@ -1001,6 +1001,7 @@ int lnet_ping_info_validate(struct lnet_ping_info *pinfo);
 struct lnet_ping_buffer *lnet_ping_buffer_alloc(int bytes, gfp_t gfp);
 void lnet_ping_buffer_free(struct kref *kref);
 int lnet_get_link_status(struct net_device *dev);
+int lnet_get_link_status_locked(struct net_device *dev);
 __u32 lnet_set_link_fatal_state(struct lnet_ni *ni, unsigned int link_state);
 
 struct lnet_ping_iter {

lnet/klnds/socklnd/socklnd.c
@@ -2063,7 +2063,7 @@ ksocknal_handle_link_state_change(struct net_device *dev,
 		if (link_down) {
 			ni_state_before = lnet_set_link_fatal_state(ni, 1);
 		} else {
-			state = (lnet_get_link_status(dev) == 0);
+			state = (lnet_get_link_status_locked(dev) == 0);
 			ni_state_before = lnet_set_link_fatal_state(ni,
 								    state);
 		}

lnet/lnet/config.c
@@ -13,8 +13,12 @@
 #include <linux/ctype.h>
 #include <linux/nsproxy.h>
 #include <linux/ethtool.h>
+#include <linux/rtnetlink.h>
 #include <net/net_namespace.h>
+
 #include <lnet/lib-lnet.h>
+
+#include <lustre_compat/net/netdev_lock.h>
 
 /* tmp struct for parsing routes */
 struct lnet_text_buf {
@@ -1506,7 +1510,7 @@ __u32 lnet_set_link_fatal_state(struct lnet_ni *ni, unsigned int link_state)
 }
 EXPORT_SYMBOL(lnet_set_link_fatal_state);
 
-int lnet_get_link_status(struct net_device *dev)
+int lnet_get_link_status_locked(struct net_device *dev)
 {
 	int ret = -1;
 
@@ -1516,14 +1520,29 @@ int lnet_get_link_status(struct net_device *dev)
 	if (!netif_running(dev)) {
 		ret = 0;
 		CDEBUG(D_NET, "device idx %d not running\n", dev->ifindex);
-	}
-	/* Some devices may not be providing link settings */
-	else if (dev->ethtool_ops->get_link) {
+	} else if (dev->ethtool_ops->get_link) {
+		/* Some devices may not be providing link settings */
 		ret = dev->ethtool_ops->get_link(dev);
 		CDEBUG(D_NET, "device idx %d get_link %u\n",
-		       ret,
-		       dev->ifindex);
+		       ret, dev->ifindex);
 	}
+
+	return ret;
+}
+EXPORT_SYMBOL(lnet_get_link_status_locked);
+
+int lnet_get_link_status(struct net_device *dev)
+{
+	int ret = -1;
+
+	if (!dev)
+		return -1;
+
+	rtnl_lock();
+	netdev_lock_ops(dev);
+	ret = lnet_get_link_status_locked(dev);
+	netdev_unlock_ops(dev);
+	rtnl_unlock();
 
 	return ret;
 }

Cray/lustre

commit e3a0dfd046fb57b0b540ea1360f539dc0cfb46ea

LU-20102 o2iblnd: do not call rtnl_lock() in RCU read section
Change-Id #63038 ("aebedbe750 LU-19707 lnet: fix RTNL assert in
LNet on v6.15") has introduced a regression allowing rtnl_lock()
from inside a RCU read-side critical section.

To avoid this wrong behaviour, now only a reference will be taken on
net device inside the RCU section, and all others actions/locks will
be done outside.

Fixes: aebedbe750 ("LU-19707 lnet: fix RTNL assert in LNet on v6.15")
Signed-off-by: Bruno Faccini <bfaccini@nvidia.com>
Change-Id: I286369834d5d3c6e2a1d174201fba85ebd717525
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65174
Tested-by: jenkins <devops@whamcloud.com>
Reviewed-by: Timothy Day <timday@thelustrecollective.com>
Reviewed-by: Shaun Tancheff <shaun.tancheff@hpe.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: James Simmons <jsimmons@infradead.org>
Tested-by: Maloo <maloo@whamcloud.com>

lnet/klnds/o2iblnd/o2iblnd.c
@@ -3078,11 +3078,10 @@ kiblnd_dev_failover(struct kib_dev *dev, struct net *ns)
 		dev->ibd_failed_failover = 0;
 
 		if (set_fatal) {
-			rcu_read_lock();
-			netdev = dev_get_by_name_rcu(ns, dev->ibd_ifname);
+			netdev = dev_get_by_name(ns, dev->ibd_ifname);
 			if (netdev && (lnet_get_link_status(netdev) == 1))
 				kiblnd_set_ni_fatal_on(dev->ibd_hdev, 0);
-			rcu_read_unlock();
+			dev_put(netdev);
 		}
 	}
 
@@ -3797,16 +3796,15 @@ kiblnd_startup(struct lnet_ni *ni)
 	if (ibdev->ibd_hdev->ibh_state == IBLND_DEV_PORT_DOWN)
 		kiblnd_set_ni_fatal_on(ibdev->ibd_hdev, 1);
 
-	rcu_read_lock();
-	netdev = dev_get_by_name_rcu(ni->ni_net_ns, net->ibn_dev->ibd_ifname);
+	netdev = dev_get_by_name(ni->ni_net_ns, net->ibn_dev->ibd_ifname);
+
 	if (netdev &&
 	    ((netdev->reg_state == NETREG_UNREGISTERING) ||
 	     (netdev->operstate != IF_OPER_UP) ||
 	    (lnet_get_link_status(netdev) == 0))) {
 		kiblnd_set_ni_fatal_on(ibdev->ibd_hdev, 1);
 	}
-	rcu_read_unlock();
-
+	dev_put(netdev);
 	write_unlock_irqrestore(&kiblnd_data.kib_global_lock, flags);
 
 	net->ibn_init = IBLND_INIT_ALL;