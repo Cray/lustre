lustre/lustre-release

commit b850dbb71d5d8182393074e27c09001d9d7010d4

LU-16738 utils: add mgsname mount option for custom hostname display
Add support for mgsname= mount option that allows specifying a custom
MGS hostname to display in /proc/mounts instead of the resolved NID
list.

Example usage:
mount -t lustre -o mgsname=my-mgs server@tcp:/lustre /mnt/lustre shows
"my-mgs@tcp:/lustre" in /proc/mounts instead of "1.2.3.4@tcp:/lustre".

Signed-off-by: Chakshu Kansal <ckansal@ddn.com>
Change-Id: Ib9541ca0cbfe315642824becabaa0a93e3bfa410
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/59934
Reviewed-by: Andreas Dilger <adilger@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Mikhail Pershin <mpershin@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

lustre/utils/mount_lustre.c
@@ -83,6 +83,7 @@ static void usage(FILE *out)
 		"\t<cliopt>: one or more comma separated client options:\n"
 		"\t\texclude=<ostname>[:<ostname>]: list of inactive OSTs (e.g. lustre-OST0001)\n"
 		"\t\tlocalflock: enable POSIX flock only on local client\n"
+		"\tmgsname=HOSTNAME: MGS hostname for display in /proc/mounts\n"
 		"\t\tretry=<num>: number of times mount is retried by client\n"
 #ifdef HAVE_GSS
 		"\t\tskpath=<file|directory>: path of keys to load into kernel keyring\n"
@@ -258,19 +259,21 @@ static int parse_options(struct mount_opts *mop, char *orig_options,
 		 * the form of param=value. We should pay attention not to
 		 * remove those mount options, see bug 22097.
 		 */
-		if (val && strncmp(arg, "max_sectors_kb", 14) == 0) {
+		if (strcmp(opt, "force") == 0) {
+			/* XXX special check for 'force' option */
+			++mop->mo_force;
+			printf("force: %d\n", mop->mo_force);
+		} else if (val && strncmp(arg, "max_sectors_kb", 14) == 0) {
 			fprintf(stderr,
 				"%s: max_sectors_kb is ignored\n",
 				progname);
 		} else if (val &&
 			   strncmp(arg, "md_stripe_cache_size", 20) == 0) {
 			mop->mo_md_stripe_cache_size = atoi(val + 1);
-		} else if (val && strncmp(arg, "retry", 5) == 0) {
-			mop->mo_retry = atoi(val + 1);
-			if (mop->mo_retry > MAX_RETRIES)
-				mop->mo_retry = MAX_RETRIES;
-			else if (mop->mo_retry < 0)
-				mop->mo_retry = 0;
+		} else if (val && strncmp(arg, "mgsname", 7) == 0) {
+			rc = append_option(options, options_len, opt, NULL);
+			if (rc != 0)
+				goto out_options;
 		} else if (val && strncmp(arg, "mgssec", 6) == 0) {
 			rc = append_option(options, options_len, opt, NULL);
 			if (rc != 0)
@@ -280,10 +283,12 @@ static int parse_options(struct mount_opts *mop, char *orig_options,
 			rc = append_option(options, options_len, opt, NULL);
 			if (rc != 0)
 				goto out_options;
-		} else if (strcmp(opt, "force") == 0) {
-			/* XXX special check for 'force' option */
-			++mop->mo_force;
-			printf("force: %d\n", mop->mo_force);
+		} else if (val && strncmp(arg, "retry", 5) == 0) {
+			mop->mo_retry = atoi(val + 1);
+			if (mop->mo_retry > MAX_RETRIES)
+				mop->mo_retry = MAX_RETRIES;
+			else if (mop->mo_retry < 0)
+				mop->mo_retry = 0;
 #ifdef HAVE_GSS
 		} else if (val && strncmp(opt, "skpath=", 7) == 0) {
 			if (strlen(val) + 1 >= sizeof(mop->mo_skpath)) {
@@ -648,12 +653,14 @@ static void set_defaults(struct mount_opts *mop)
 	mop->mo_nosvc = 0;
 }
 
-static int parse_opts(int argc, char *const argv[], struct mount_opts *mop)
+static int parse_opts(int argc, char *const argv[], struct mount_opts *mop,
+		      char *options, size_t options_len)
 {
 	static struct option long_opts[] = {
 	{ .val = 1,	.name = "force",	.has_arg = no_argument },
 	{ .val = 'f',	.name = "fake",		.has_arg = no_argument },
 	{ .val = 'h',	.name = "help",		.has_arg = no_argument },
+	{ .val = 'M',	.name = "mgsname",	.has_arg = required_argument },
 	{ .val = 'n',	.name = "nomtab",	.has_arg = no_argument },
 	{ .val = 'o',	.name = "options",	.has_arg = required_argument },
 	{ .val = 'v',	.name = "verbose",	.has_arg = no_argument },
@@ -666,7 +673,7 @@ static int parse_opts(int argc, char *const argv[], struct mount_opts *mop)
 	char *ptr;
 	int opt, rc;
 
-	while ((opt = getopt_long(argc, argv, "fhno:vV",
+	while ((opt = getopt_long(argc, argv, "fhM:no:vV",
 				  long_opts, NULL)) != EOF){
 		switch (opt) {
 		case 1:
@@ -679,6 +686,12 @@ static int parse_opts(int argc, char *const argv[], struct mount_opts *mop)
 			break;
 		case 'h':
 			usage(stdout);
+			break;
+		case 'M':
+			rc = append_option(options, options_len, "mgsname=",
+					   optarg);
+			if (rc != 0)
+				return rc;
 			break;
 		case 'n':
 			++mop->mo_nomtab;
@@ -934,9 +947,17 @@ int main(int argc, char *const argv[])
 	}
 	maxopt_len = MIN(g_pagesize, 64 * 1024);
 
-	rc = parse_opts(argc, argv, &mop);
+	options = malloc(maxopt_len);
+	if (!options) {
+		fprintf(stderr, "can't allocate memory for options\n");
+		rc = ENOMEM;
+		goto out_mo_source;
+	}
+	options[0] = '\0';
+
+	rc = parse_opts(argc, argv, &mop, options, maxopt_len);
 	if (rc || version)
-		return rc;
+		goto out_options;
 
 	if (verbose) {
 		for (i = 0; i < argc; i++)
@@ -945,13 +966,6 @@ int main(int argc, char *const argv[])
 		       mop.mo_source, mop.mo_target);
 		printf("options(%zu/%zu) = %s\n", strlen(mop.mo_orig_options),
 		       maxopt_len, mop.mo_orig_options);
-	}
-
-	options = malloc(maxopt_len);
-	if (!options) {
-		fprintf(stderr, "can't allocate memory for options\n");
-		rc = ENOMEM;
-		goto out_mo_source;
 	}
 
 	if (strlen(mop.mo_orig_options) >= maxopt_len) {
@@ -1022,6 +1036,29 @@ int main(int argc, char *const argv[])
 			progname, mop.mo_usource);
 		goto out_options;
 #endif
+	}
+
+	/*
+	 * Auto-generate mgsname from device string if not already specified,
+	 * and convert_hostname() has replaced the hostname with an IP address.
+	 */
+	if (client && !strstr(options, "mgsname=") &&
+	    strchr(mop.mo_usource, '@')) {
+		char *end = strpbrk(mop.mo_usource, ",:");
+
+		if (end && strncmp(mop.mo_usource, mop.mo_source,
+				   end - mop.mo_usource) != 0) {
+			char sep = *end;
+
+			*end = '\0'; /* temporarily NUL terminate mgsname */
+			rc = append_option(options, maxopt_len, "mgsname=",
+					   mop.mo_usource);
+			if (verbose)
+				printf("add option mgsname=%s: rc = %d\n",
+				       mop.mo_usource, rc);
+			*end = sep;
+			rc = 0;
+		}
 	}
 
 	/*

lustre/utils/mount_utils.c
@@ -171,6 +171,29 @@ char *strscpy(char *dst, char *src, int buflen)
 	return strscat(dst, src, buflen);
 }
 
+/*
+ * Check if filesystem is already mounted by comparing filesystem names.
+ * For Lustre client mounts, extract and compare the filesystem name part
+ * (after ":/" in the mount source) to handle cases where hostnames differ
+ * but refer to the same filesystem.
+ */
+static int compare_lustre_sources(const char *src1, const char *src2)
+{
+	const char *fs1, *fs2;
+
+	/* Find filesystem part after ":/" */
+	fs1 = strstr(src1, ":/");
+	fs2 = strstr(src2, ":/");
+
+	/* If both have ":/" pattern, compare filesystem names */
+	if (fs1 && fs2) {
+		src1 = fs1 + 2; /* skip ":/" */
+		src2 = fs2 + 2; /* skip ":/" */
+	}
+
+	return strcmp(src1, src2) == 0;
+}
+
 int check_mtab_entry(char *spec1, char *spec2, char *mtpt, char *type)
 {
 	FILE *fp;
@@ -181,8 +204,8 @@ int check_mtab_entry(char *spec1, char *spec2, char *mtpt, char *type)
 		return 0;
 
 	while ((mnt = getmntent(fp)) != NULL) {
-		if ((strcmp(mnt->mnt_fsname, spec1) == 0 ||
-		     strcmp(mnt->mnt_fsname, spec2) == 0) &&
+		if ((compare_lustre_sources(mnt->mnt_fsname, spec1) ||
+		     compare_lustre_sources(mnt->mnt_fsname, spec2)) &&
 		    (!mtpt || strcmp(mnt->mnt_dir, mtpt) == 0) &&
 		    (!type || strcmp(mnt->mnt_type, type) == 0)) {
 			endmntent(fp);

lustre/doc/mount.lustre.8
@@ -237,6 +237,23 @@ Warning! 'network' option is incompatible with LNet Dynamic Peer Discovery.
 If you want to restrict client NID, please make sure LNet Dynamic Peer Discovery
 is disabled.
 .TP
+.BI mgsname= MGSNAME
+Specify a custom hostname for the MGS that will be displayed in
+.BR /proc/mounts
+instead of the resolved IP address. The network type (e.g.,
+.BR @tcp ,
+.BR @o2ib )
+is automatically extracted from the first MGS NID in the device string and
+should be included in the hostname.
+If this option is not specified and the device string contains a hostname
+(not an IP address), the hostname will be automatically extracted and used
+to generate the mgsname option.
+This option is purely cosmetic and does not affect the actual connection to the
+MGS, which is still determined by the device string. This can be useful for
+making mount entries more readable in system monitoring tools and if MGSNAME is
+specified as a hostname with DNS resolution that resolves to multiple NIDs
+instead of numeric addresses.
+.TP
 .B test_dummy_encryption
 Enable test dummy encryption mode.
 .RE
@@ -373,6 +390,31 @@ process for the case that clients are known to be unavailable:
 .RS
 .EX
 .B # mount -t lustre_tgt -L testfs-MDT0000 -o abort_recov /mnt/test/mdt
+.EE
+.RE
+.PP
+Mount a client filesystem with a custom MGS name for display purposes.
+The network type
+.B @o2ib
+will be automatically appended to
+.B mgs-cluster
+in
+.BR /proc/mounts :
+.RS
+.EX
+.B # mount -t lustre -o mgsname=cfs21@o2ib 10.0.0.2,10.0.0.4@o2ib:/testfs /mnt/myfs
+.EE
+.RE
+.PP
+Mount a client filesystem with automatic MGS name generation. The hostname
+.B test-mgs
+will be automatically extracted from the device string and
+.B mgsname=test-mgs@tcp
+will be appended to the list of mount options for display in
+.BR /proc/mounts :
+.RS
+.EX
+.B # mount -t lustre cfs21@o2ib:/testfs /mnt/myfilesystem
 .EE
 .RE
 .SH AVAILABILITY

lustre/include/lustre_disk.h
@@ -104,6 +104,7 @@ struct lustre_mount_data {
 	char   *lmd_params;	/* lustre params */
 	u32    *lmd_exclude;	/* array of OSTs to ignore */
 	char   *lmd_mgs;	/* MGS nid */
+	char   *lmd_mgsname;	/* MGS hostname for display */
 	char   *lmd_osd_type;	/* OSD type */
 	char   *lmd_nidnet;     /* network to restrict this client to */
 };

lustre/llite/super25.c
@@ -79,6 +79,23 @@ static int ll_drop_inode(struct inode *inode)
 	return drop;
 }
 
+static int ll_show_devname(struct seq_file *m, struct dentry *root)
+{
+	struct lustre_sb_info *lsi = s2lsi(root->d_sb);
+	struct lustre_mount_data *lmd = lsi->lsi_lmd;
+
+	if (lmd && lmd->lmd_mgsname) {
+		struct ll_sb_info *sbi = lsi->lsi_llsbi;
+
+		seq_printf(m, "%s:/%s", lmd->lmd_mgsname, sbi->ll_fsname);
+	} else if (lmd && lmd->lmd_dev) {
+		seq_puts(m, lmd->lmd_dev);
+	} else {
+		seq_puts(m, "<unknown>");
+	}
+	return 0;
+}
+
 /* exported operations */
 const struct super_operations lustre_super_operations = {
 	.alloc_inode   = ll_alloc_inode,
@@ -90,6 +107,7 @@ const struct super_operations lustre_super_operations = {
 	.umount_begin  = ll_umount_begin,
 	.remount_fs    = ll_remount_fs,
 	.show_options  = ll_show_options,
+	.show_devname  = ll_show_devname,
 };
 
 /**

lustre/obdclass/obd_mount.c
@@ -657,6 +657,8 @@ static int lustre_free_lsi(struct lustre_sb_info *lsi)
 				lsi->lsi_lmd->lmd_exclude_count);
 		OBD_FREE(lsi->lsi_lmd->lmd_mgs,
 			 strlen(lsi->lsi_lmd->lmd_mgs) + 1);
+		OBD_FREE(lsi->lsi_lmd->lmd_mgsname,
+			 strlen(lsi->lsi_lmd->lmd_mgsname) + 1);
 		OBD_FREE(lsi->lsi_lmd->lmd_osd_type,
 			 strlen(lsi->lsi_lmd->lmd_osd_type) + 1);
 		OBD_FREE(lsi->lsi_lmd->lmd_params, 4096);
@@ -1110,9 +1112,10 @@ static int lmd_parse_mgs(struct lustre_mount_data *lmd, char *ptr, char **tail)
 enum lmd_mnt_flags {
 	LMD_OPT_RECOVERY_TIME_SOFT	= LMD_FLG_NUM_FLAGS + 1,
 	LMD_OPT_RECOVERY_TIME_HARD,
+	LMD_OPT_EXCLUDE,
+	LMD_OPT_MGSNAME,
 	LMD_OPT_MGSNODE,
 	LMD_OPT_MGSSEC,
-	LMD_OPT_EXCLUDE,
 	LMD_OPT_SVNAME,
 	LMD_OPT_PARAM,
 	LMD_OPT_OSD,
@@ -1144,9 +1147,10 @@ static const match_table_t lmd_flags_table = {
 
 	{LMD_OPT_RECOVERY_TIME_SOFT,	"recovery_time_soft=%u"},
 	{LMD_OPT_RECOVERY_TIME_HARD,	"recovery_time_hard=%u"},
+	{LMD_OPT_EXCLUDE,		"exclude=%s"},
+	{LMD_OPT_MGSNAME,		"mgsname=%s"},
 	{LMD_OPT_MGSNODE,		"mgsnode=%s"},
 	{LMD_OPT_MGSSEC,		"mgssec=%s"},
-	{LMD_OPT_EXCLUDE,		"exclude=%s"},
 	{LMD_OPT_SVNAME,		"svname=%s"},
 	{LMD_OPT_PARAM,			"param=%s"},
 	{LMD_OPT_OSD,			"osd=%s"},
@@ -1421,6 +1425,9 @@ int lmd_parse(char *options, struct lustre_mount_data *lmd)
 			if (rc == 0)
 				lmd->lmd_recovery_time_hard = max_t(int, tmp,
 								    time_min);
+			break;
+		case LMD_OPT_MGSNAME:
+			rc = lmd_parse_string(&lmd->lmd_mgsname, args->from);
 			break;
 		case LMD_OPT_MGSNODE:
 			/* Assume the next mount opt is the first

Cray/lustre

commit d3ea87a17f209afdf3d6b289f64afccfcdcc4eb6

LU-16738 utils: fix auto mgsname= mount option
When mounting a client using a TCP hostname that does not contain
an "@nettype" component, the "@tcp0" nettype is assumed.  However,
this was not being handled properly when adding an "mgsnode="
mount option to capture the original hostname during mounting.

Only compare if the original mount device does not match the new
mount device before deciding whether to add an "mgsnode=" option.

Fix check for already-mounted filesystems to use "lustre_tgt"
instead of "lustre" when mounting server targets.

Simplify check_mtab_entry() and callers to avoid comparing devices
multiple times before deciding if the filesystem is mounted. Check
fstype and mountpoint before devname, and only pass mo_device once.

Update the mount.lustre.8 man page to better describe the options.

Test-Parameters: trivial
Fixes: b850dbb71d ("LU-16738 utils: add mgsname mount option for custom hostname display")
Fixes: 4b2eca7095 ("LU-12514 utils: try lustre_tgt filesystem for mount")
Signed-off-by: Andreas Dilger <adilger@thelustrecollective.com>
Change-Id: I9d284ec6ecaf66ef1cb1b68422bf108436500c1e
Reviewed-on: https://review.whamcloud.com/c/fs/lustre-release/+/65362
Tested-by: Maloo <maloo@whamcloud.com>
Reviewed-by: Chakshu Kansal <ckansal@ddn.com>
Reviewed-by: Jian Yu <yujian@whamcloud.com>
Reviewed-by: Oleg Drokin <green@whamcloud.com>
Tested-by: jenkins <devops@whamcloud.com>

Documentation/man8/mount.lustre.8
@@ -33,35 +33,65 @@ or
 .B lustre_tgt
 filesystem type is used. Lustre clients and targets are stopped by using the
 .BR umount (8)
-command. When mounted, Lustre clients will have parameters set according to
-what is described in
-.BR /etc/lustre/mount.client.params ,
-and/or the
-.BI /etc/lustre/mount. FSNAME .params
-file, with lines of the same style as
-.BR lctl-set_param (8),
-with lines of the form
-.IR PARAM = VALUE .
+command.
 .P
 There are two forms for the device option passed to the
 .I mount
 command, depending on whether a client or a target service is started:
 .TP
-.IR MGSNAME :/ FSNAME [/ SUBDIR ]
+.IB MGSNAME :/ FSNAME\fR[ / SUBDIR \fR]
 mounts the Lustre client filesystem named
 .I FSNAME
 (optionally starting at subdirectory
 .I SUBDIR
-within the filesystem, if specified) on the client at the directory
-.IR CLIENT_MOUNTPOINT ,
+within the filesystem, if specified)
 by contacting the Management Service at
 .IR MGSNAME .
-The format for
-.I MGSNAME
-is defined below. A client filesystem will typically be listed in
+.TP
+.I CLIENT_MOUNTPOINT
+is the directory where the client filesystem will be mounted and accessed.
+A client filesystem will typically be listed in
 .BR fstab (5)
 for automatic mount at boot time, is usable like any local filesystem, and
 provides a full POSIX-compilant interface.
+.TP
+.IB MGSNAME := MGSNODE\fR[ : MGSNODE \fR]
+The
+.I MGSNAME
+may be a colon-separated list of
+.I MGSNODE
+servers where the MGS service may run. Multiple
+.I MGSNODE
+values can be specified if the MGS service is configured for High Availability
+and may be running on any one of the nodes.
+.TP
+.IB MGSNODE := MGSNID\fR[ , MGSNID \fR]
+Each
+.I MGSNODE
+may specify a comma-separated list of NIDs,
+if there are different LNet interfaces for the same
+.IR MGSNODE .
+.TP
+.IB MGSNID := HOST @ NETTYPE \c
+.RI [ NET_NUMBER ]
+Each
+.I MGSNID
+contains a hostname that can be resolved via the
+.BR hosts (5)
+name service in
+.BR nsswitch.conf (5),
+or a numeric IP, IB, or other address that does not need resolution.
+The
+.BI @ NETTYPE
+is the LNet network type, such as
+.BR @tcp ", " @o2ib 
+or other supported LND type,
+.B @tcp0
+if unspecified.  The optional
+.I NET_NUMBER
+is the network number for that
+.IR NETTYPE ,
+or 0 if unspecified.
 .TP
 .I BLOCK_DEVICE
 specifies the physical disk or ZFS dataset uses to start the target service
@@ -79,31 +109,34 @@ option. Care should be taken if there is a device-level backup of
 the target filesystem, which would have a duplicate label and UUID if it is
 not changed with
 .BR tune2fs (8)
-or similar. The mounted target service filesystem at
-.I MOUNTPOINT
-is only useful for
+or similar.
+.TP
+.I SERVER_MOUNTPOINT
+is the directory where the target filesystem will be mounted,
+but is only useful for
 .BR df (1)
-operations to show the current space usage, and appears in
+to show the current space usage, and
+.BR ioctl (3)
+for block device operations such as
+.BR resize2fs (8)
+or
+.BR fstrim (8).
+It appears in
 .BR /proc/mounts
-to show the device is in use.
+to show that
+.I BLOCK_DEVICE
+is in use.
+.PP
+After successful mount, Lustre clients may have additional parameters set
+according to those listed in
+.BR /etc/lustre/mount.client.params ,
+and/or the
+.BI /etc/lustre/mount. FSNAME .params
+files, with lines in the same style as command-line arguments to
+.BR lctl-set_param (8),
+with the form
+.IR PARAM = VALUE .
 .SH OPTIONS
-.TP
-.IB MGSNAME := MGSNODE [: MGSNODE ]
-The
-.I MGSNAME
-may be a colon-separated list of
-.I MGSNODE
-names where the MGS service may run. Multiple
-.I MGSNODE
-values can be specified if the MGS service is configures for HA failover
-and may be running on any one of the nodes.
-.TP
-.IB MGSNODE := MGSNID [, MGSNID ]
-Each
-.I MGSNODE
-may specify a comma-separated list of NIDs, if there are different
-LNet interfaces for the same
-.IR MGSNODE .
 .TP
 .BI mgssec= FLAVOR
 Specifies the encryption flavour for the initial network RPC connection to
@@ -343,6 +376,57 @@ The default soft recovery timeout is set to 300 seconds (5 minutes).
 The server will be allowed to incrementally extend its timeout up to a hard
 maximum of 'timeout' seconds.
 The default hard recovery timeout is set to 900 seconds (15 minutes).
+.SH COMMAND-LINE OPTIONS
+Some command-line options for
+.B mount.lustre
+itself may passed from the
+.BR mount (8)
+command-line, but are mostly only available for debugging if the
+.BR mount.lustre (8)
+command is run directly.
+These are separate from the mountpoint options passed to
+.BR mount (2)
+for the mountpoint itself, which are listed in the
+.B OPTIONS
+section.
+.TP
+.BR -f ", " --fake
+Fake filesystem mount, setting client-specific parameters and adding it to
+.B /etc/fstab
+without actually calling the
+.BR mount (2)
+system call.  Together with the
+.B -v
+option this will show what mount options would be set for the filesystem.
+.TP
+.BR --force
+Force mounting the filesystem even if it appears to already be mounted.
+.TP
+.BR -h ", " --help
+Print usage message and exit.
+.TP
+.BR -n ", " --nomtab
+Do not update the
+.B /etc/fstab
+file after the mount has successfully completed.
+.TP
+.BR -o ", " --options
+Specify the list of
+.B OPTIONS
+to pass to the kernel for
+.IR MOUNTPOINT .
+These are normally passed from the
+.BR mount
+command, but can be specified directly for debugging purposes.
+.TP
+.BR -v ", " --verbose
+Print extra information during mounting.  This will be passed on from
+.BR mount .
+.TP
+.BR -V ", " --version
+Print the build version of the
+.BR mount.lustre
+binary and exit.
 .SH BUGS
 Not very many mount options can be changed with
 .BR "-o remount" .
@@ -355,7 +439,7 @@ The Management Service is running on a node reachable via NID
 .BR cfs21@tcp0 :
 .RS
 .EX
-.B # mount -t lustre cfs21@tcp0:/testfs /mnt/myfilesystem
+.RB client# " mount -t lustre cfs21@tcp0:/testfs /mnt/myfilesystem"
 .EE
 .RE
 .PP
@@ -364,7 +448,7 @@ Like above example, but mount subdirectory
 as fileset:
 .RS
 .EX
-.B # mount -t lustre cfs21@tcp0:/testfs/dir /mnt/myfilesystem
+.RB client# " mount -t lustre cfs21@tcp0:/testfs/dir /mnt/myfilesystem"
 .EE
 .RE
 .PP
@@ -373,14 +457,15 @@ nodes
 .B mgs1
 and
 .BR mgs2 ,
-which are two different hosts separated by a colon and
-served as a failover pair. Lustre tries the first one, and if that fails, it
-tries the second one. On each service node, the comma-separated NIDs refer to
+are two different hosts separated by a colon and served as a failover pair,
+and have both TCP and IB addresses.
+Lustre tries the first one, and if that fails, it tries the second one.
+On each service node, the comma-separated NIDs refer to
 different interfaces on the same host, and the Lustre client chooses the best
 one for communication based on which network interfaces are available locally:
 .RS
 .EX
-.B # mount -t lustre mgs1@tcp0,mgs1ib@o2ib0:mgs2@tcp0,mgs2ib@o2ib0:/testfs /mnt/fs
+.RB client# " mount -t lustre mgs1@tcp0,mgs1ib@o2ib0:mgs2@tcp0,mgs2ib@o2ib0:/testfs /mnt/fs"
 .EE
 .RE
 .PP
@@ -390,7 +475,7 @@ on mountpoint
 .BR /mnt/test/mdt :
 .RS
 .EX
-.B # mount -t lustre_tgt /dev/sda1 /mnt/test/mdt
+.RB mds# " mount -t lustre_tgt /dev/sda1 /mnt/test/mdt"
 .EE
 .RE
 .PP
@@ -400,20 +485,20 @@ service (by using the disk label), but aborts the Lustre client recovery
 process for the case that clients are known to be unavailable:
 .RS
 .EX
-.B # mount -t lustre_tgt -L testfs-MDT0000 -o abort_recov /mnt/test/mdt
+.RB mds# " mount -t lustre_tgt -L testfs-MDT0000 -o abort_recov /mnt/test/mdt"
 .EE
 .RE
 .PP
 Mount a client filesystem with a custom MGS name for display purposes.
 The network type
 .B @o2ib
-will be automatically appended to
-.B mgs-cluster
+will be automatically appended to the MGS hostname
+.B cfs-mgs
 in
 .BR /proc/mounts :
 .RS
 .EX
-.B # mount -t lustre -o mgsname=cfs21@o2ib 10.0.0.2,10.0.0.4@o2ib:/testfs /mnt/myfs
+.RB client# " mount -t lustre -o mgsname=cfs-mgs@o2ib 10.0.0.2@o2ib,10.0.0.4@o2ib:/testfs /myfs"
 .EE
 .RE
 .PP
@@ -425,7 +510,7 @@ will be appended to the list of mount options for display in
 .BR /proc/mounts :
 .RS
 .EX
-.B # mount -t lustre cfs21@o2ib:/testfs /mnt/myfilesystem
+.RB client# " mount -t lustre test-mgs@o2ib:/testfs /mnt/myfs"
 .EE
 .RE
 .SH AVAILABILITY
@@ -436,15 +521,15 @@ are part of the
 .BR lustre (7)
 filesystem package.
 .B mount.lustre
-was added in release 0.10.0
+was added in release 0.10.0, and
 .\" Added in commit 0.9.1
 .B mount.lustre_tgt
-was added in release 2.13.0
+was added in release 2.13.0.
 .\" Added in commit v2_12_58-71-g510aea4a37
 .SH SEE ALSO
 .BR lfs (1),
 .BR lustre (7),
-.BR lctl (8),
+.BR lctl-set_param (8),
 .BR mkfs.lustre (8),
 .BR mount (8),
 .BR tunefs.lustre (8)

lib/libcfs/nidstrings.c
@@ -1012,7 +1012,7 @@ libcfs_strnid(struct lnet_nid *nid, const char *str)
 		nf = libcfs_str2net_internal(sep + 1, &net);
 		if (nf == NULL)
 			return -EINVAL;
-	} else {
+	} else /* assume @tcp0 if NID does not specify @nettype */ {
 		sep = str + strlen(str);
 		net = LNET_MKNET(SOCKLND, 0);
 		nf = libcfs_lnd2netstrfns(SOCKLND);

lustre/utils/mkfs_lustre.c
@@ -1043,7 +1043,7 @@ int main(int argc, char *const argv[])
 		goto out;
 	}
 
-	if (check_mtab_entry(mop.mo_device, mop.mo_device, NULL, NULL)) {
+	if (check_mtab_entry(mop.mo_device, NULL, NULL, NULL)) {
 		fprintf(stderr, "%s: is currently mounted, exiting without any change\n",
 			mop.mo_device);
 		return EEXIST;

lustre/utils/mount_lustre.c
@@ -937,6 +937,7 @@ int main(int argc, char *const argv[])
 	bool client;
 	size_t maxopt_len;
 	size_t g_pagesize;
+	char *fstype = "lustre";
 
 	progname = strrchr(argv[0], '/');
 	progname = progname ? progname + 1 : argv[0];
@@ -990,9 +991,15 @@ int main(int argc, char *const argv[])
 		goto out_options;
 	}
 
+	/* Prefer filesystem type given on mount command line
+	 * so it appears correctly in the /proc/mounts output.
+	 */
+	if (strstr(argv[0], "mount.lustre_tgt"))
+		fstype = "lustre_tgt";
+
 	if (!mop.mo_force) {
 		rc = check_mtab_entry(mop.mo_usource, mop.mo_source,
-				      mop.mo_target, "lustre");
+				      mop.mo_target, fstype);
 		if (rc && !(flags & MS_REMOUNT)) {
 			fprintf(stderr,
 				"%s: according to %s %s is already mounted on %s\n",
@@ -1080,7 +1087,7 @@ int main(int argc, char *const argv[])
 	 * and convert_hostname() has replaced the hostname with an IP address.
 	 */
 	if (client && !strstr(options, "mgsname=") &&
-	    strchr(mop.mo_usource, '@')) {
+	    isalpha(mop.mo_usource[0])) {
 		char *end = strpbrk(mop.mo_usource, ",:");
 
 		if (end && strncmp(mop.mo_usource, mop.mo_source,
@@ -1099,7 +1106,7 @@ int main(int argc, char *const argv[])
 	}
 
 	/*
-	 * In Linux 2.4, the target device doesn't get passed to any of our
+	 * Since Linux 2.4, the target device doesn't get passed to any of our
 	 * functions.  So we'll stick it on the end of the options.
 	 */
 	rc = append_option(options, maxopt_len, "device=", mop.mo_source);
@@ -1120,15 +1127,6 @@ int main(int argc, char *const argv[])
 #endif
 
 	if (!mop.mo_fake) {
-		char *fstype;
-
-		/* Prefer filesystem type given on mount command line
-		 * so it appears correctly in the /proc/mounts output.
-		 */
-		if (strstr(argv[0], "mount.lustre_tgt"))
-			fstype = "lustre_tgt";
-		else
-			fstype = "lustre";
 		/*
 		 * flags and target get to lustre_get_sb(), but not
 		 * lustre_fill_super().  Lustre ignores the flags, but mount
@@ -1147,6 +1145,7 @@ int main(int argc, char *const argv[])
 						mop.mo_retry - i);
 				}
 
+#if LUSTRE_VERSION_CODE > OBD_OCD_VERSION(2, 18, 53, 0)
 				/* Pre-2.13 Lustre without 'lustre_tgt' type?
 				 * Try with 'lustre' instead.  Eventually this
 				 * can be removed (e.g. 2.18 or whenever).
@@ -1157,6 +1156,7 @@ int main(int argc, char *const argv[])
 					i--;
 					continue;
 				}
+#endif
 
 				if (mop.mo_retry) {
 					int limit = i / 2 > 5 ? i / 2 : 5;
@@ -1187,38 +1187,38 @@ int main(int argc, char *const argv[])
 
 		fprintf(stderr, "%s: mount %s at %s failed: %s\n", progname,
 			mop.mo_usource, mop.mo_target, strerror(errno));
-		if (errno == EBUSY)
+		if (errno == EBUSY || errno == EINPROGRESS)
 			fprintf(stderr,
-				"Is the backend filesystem mounted?\n Check /etc/mtab and /proc/mounts\n");
-		if (errno == ENODEV)
+				"Is the backend filesystem mounted?\n Check /proc/mounts\n");
+		else if (errno == ENODEV)
 			fprintf(stderr,
 				"Are the lustre modules loaded?\n Check /etc/modprobe.conf and /proc/filesystems\n");
-		if (errno == ENOTBLK)
+		else if (errno == ENOTBLK)
 			fprintf(stderr, "Do you need -o loop?\n");
-		if (errno == ENOMEDIUM)
+		else if (errno == ENOMEDIUM)
 			fprintf(stderr,
 				"This filesystem needs at least 1 OST\n");
-		if (errno == ENOENT) {
+		else if (errno == ENOENT) {
 			fprintf(stderr, "Is the MGS specification correct?\n");
 			fprintf(stderr, "Is the filesystem name correct?\n");
 			fprintf(stderr,
 				"If upgrading, is the copied client log valid? (see upgrade docs)\n");
 		}
-		if (errno == EALREADY)
+		else if (errno == EALREADY)
 			fprintf(stderr,
 				"The target service is already running. (%s)\n",
 				mop.mo_usource);
-		if (errno == ENXIO)
+		else if (errno == ENXIO)
 			fprintf(stderr,
 				"The target service failed to start (bad config log?) (%s).  See /var/log/messages.\n",
 				mop.mo_usource);
-		if (errno == EIO)
+		else if (errno == EIO)
 			fprintf(stderr, "Is the MGS running?\n");
-		if (errno == EADDRINUSE)
+		else if (errno == EADDRINUSE)
 			fprintf(stderr,
 				"The target service's index is already in use. (%s)\n",
 				mop.mo_usource);
-		if (errno == EINVAL) {
+		else if (errno == EINVAL) {
 			fprintf(stderr, "This may have multiple causes.\n");
 			if (cli)
 				fprintf(stderr,
@@ -1249,7 +1249,7 @@ int main(int argc, char *const argv[])
 		 * '/etc/lustre/mount.params' or '/etc/lustre/FSNAME.params'
 		 * and are set here.
 		 */
-		if (strstr(mop.mo_usource, ":/") != NULL) {
+		if (client) {
 			update_utab_entry(&mop);
 			rc = set_client_params(mop.mo_fsname);
 		}

lustre/utils/mount_utils.c
@@ -173,8 +173,10 @@ char *strscpy(char *dst, char *src, int buflen)
  * For Lustre client mounts, extract and compare the filesystem name part
  * (after ":/" in the mount source) to handle cases where hostnames differ
  * but refer to the same filesystem.
+ *
+ * Return true if sources match, false otherwise.
  */
-static int compare_lustre_sources(const char *src1, const char *src2)
+static bool compare_lustre_sources(const char *src1, const char *src2)
 {
 	const char *fs1, *fs2;
 
@@ -201,10 +203,12 @@ int check_mtab_entry(char *spec1, char *spec2, char *mtpt, char *type)
 		return 0;
 
 	while ((mnt = getmntent(fp)) != NULL) {
-		if ((compare_lustre_sources(mnt->mnt_fsname, spec1) ||
-		     compare_lustre_sources(mnt->mnt_fsname, spec2)) &&
-		    (!mtpt || strcmp(mnt->mnt_dir, mtpt) == 0) &&
-		    (!type || strcmp(mnt->mnt_type, type) == 0)) {
+		if (type && strcmp(mnt->mnt_type, type) != 0)
+			continue;
+		if (mtpt && strcmp(mnt->mnt_dir, mtpt) != 0)
+			continue;
+		if (compare_lustre_sources(mnt->mnt_fsname, spec1) ||
+		    (spec2 && compare_lustre_sources(mnt->mnt_fsname, spec2))) {
 			endmntent(fp);
 			return EEXIST;
 		}