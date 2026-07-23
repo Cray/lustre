lustre/lustre-release

commit 2dc19f20ba9fcc1bcac6ae7ee5169ce10caab882

LU-7372 mgs: reprocess all locks at device fini
This is to avoid a case that IR lock revocation is going on when
the obd is being stopped, an extra ldlm_reprocess_recovery_done() is
required to make revocation process move forward.

Turn off 'set -e' in rundbench. Otherwise killing dbench process will
return an error to wait(1) in rundbench. Since test-framework has
turned on error on exit, it will set test result as failure, which
is actually a false alarm.

Test-Parameters: envdefinitions=SLOW=yes,ONLY=26 testlist=replay-dual,replay-dual,replay-dual,replay-dual
Signed-off-by: Jinshan Xiong <jinshan.xiong@intel.com>
Signed-off-by: Bobi Jam <bobijam.xu@intel.com>
Change-Id: I43ab1df9c8fe5aea15da6c90175fd08a0b099ea2
Reviewed-on: https://review.whamcloud.com/17853
Tested-by: Jenkins
Tested-by: Maloo <hpdd-maloo@intel.com>
Reviewed-by: Fan Yong <fan.yong@intel.com>
Reviewed-by: Bobi Jam <bobijam@hotmail.com>
Reviewed-by: Oleg Drokin <oleg.drokin@intel.com>

lustre/ldlm/ldlm_lock.c
@@ -2528,6 +2528,11 @@ int ldlm_export_cancel_locks(struct obd_export *exp)
 	       "left on hash table %d.\n", exp, ecl.ecl_loop,
 	       atomic_read(&exp->exp_lock_hash->hs_count));
 
+	if (ecl.ecl_loop > 0 &&
+	    atomic_read(&exp->exp_lock_hash->hs_count) == 0 &&
+	    exp->exp_obd->obd_stopping)
+		ldlm_reprocess_recovery_done(exp->exp_obd->obd_namespace);
+
 	return ecl.ecl_loop;
 }
 

lustre/tests/rundbench
@@ -82,6 +82,10 @@ rm -rf dbench $LIBS71 client.txt
 exit 0
 ' TERM
 
+# turn off errexit. rundbench will return error code on failure
+trap '' ERR
+set +e
+
 cd $DIR
 echo "running 'dbench $@' $PREFIX $PWD at `date`"
 

lustre/tests/test-framework.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-trap 'print_summary && touch $TF_FAIL && \
+trap 'print_summary && print_stack_trace | tee $TF_FAIL && \
     echo "$TESTSUITE: FAIL: test-framework exiting on error"' ERR
 set -e
 #set -x

Cray/lustre

commit 51103a047d84342d91ab6a066cd9da360e568008

LUS-13096 ldlm: recovery_done is to be used after recovery
ldlm_reprocess_recovery_done() is to be used only after recovery as
it tries to reprocess all the locks in the waiting queues. In other
cases LDLM_PROCESS_RECOVERY is excessive and LDLM_PROCESS_RESCAN is
to be used instead - what is usually called on a lock cancel.

This reduces the amount of time spent on reprocessing.

Fixes: 2dc19f20ba ("LU-7372 mgs: reprocess all locks at device fini")
Fixes: 05e6ccd344 ("LU-13456 ldlm: fix reprocessing of locks with more bits")
Signed-off-by: Vitaly Fertman <vitaly.fertman@hpe.com>
Change-Id: I78bdb8a471e0662c53b2557533e7243f3793c192
Reviewed-on: https://es-gerrit.hpc.amslabs.hpecorp.net/163540
Reviewed-by: Andriy Skulysh <andriy.skulysh@hpe.com>
Reviewed-by: Alexey Lyashkov <alexey.lyashkov@hpe.com>
Tested-by: Alexander Lezhoev <alexander.lezhoev@hpe.com>

lustre/include/lustre_dlm.h
@@ -1667,6 +1667,7 @@ void ldlm_lock_mode_downgrade(struct ldlm_lock *lock, enum ldlm_mode new_mode);
 void ldlm_lock_cancel(struct ldlm_lock *lock);
 void ldlm_reprocess_all(struct ldlm_resource *res, __u64 hint);
 void ldlm_reprocess_recovery_done(struct ldlm_namespace *ns);
+void ldlm_reprocess_ns(struct ldlm_namespace *ns);
 void ldlm_lock_dump_handle(int level, const struct lustre_handle *lockh);
 void ldlm_unlink_lock_skiplist(struct ldlm_lock *req);
 

lustre/ldlm/ldlm_lock.c
@@ -2427,22 +2427,32 @@ static int ldlm_reprocess_res(struct cfs_hash *hs, struct cfs_hash_bd *bd,
 			      struct hlist_node *hnode, void *arg)
 {
 	struct ldlm_resource *res = cfs_hash_object(hs, hnode);
+	int intention = *(int *)arg;
 
 	/* This is only called once after recovery done. LU-8306. */
-	__ldlm_reprocess_all(res, LDLM_PROCESS_RECOVERY, 0);
+	__ldlm_reprocess_all(res, intention, 0);
 	return 0;
+}
+
+static void __ldlm_reprocess_ns(struct ldlm_namespace *ns, int intention)
+{
+	ENTRY;
+	if (ns != NULL) {
+		cfs_hash_for_each_nolock(ns->ns_rs_hash,
+					 ldlm_reprocess_res, &intention, 0);
+	}
+	EXIT;
 }
 
 /* Iterate on all resources on namespace attempting to grant waiting locks. */
 void ldlm_reprocess_recovery_done(struct ldlm_namespace *ns)
 {
-	ENTRY;
+	__ldlm_reprocess_ns(ns, LDLM_PROCESS_RECOVERY);
+}
 
-	if (ns != NULL) {
-		cfs_hash_for_each_nolock(ns->ns_rs_hash,
-					 ldlm_reprocess_res, NULL, 0);
-	}
-	EXIT;
+void ldlm_reprocess_ns(struct ldlm_namespace *ns)
+{
+	__ldlm_reprocess_ns(ns, LDLM_PROCESS_RESCAN);
 }
 
 /* Helper to call blocking AST for LDLM lock \a lock in a "cancelling" mode. */
@@ -2671,7 +2681,7 @@ int ldlm_export_cancel_locks(struct obd_export *exp)
 	if (ecl.ecl_loop > 0 &&
 	    atomic_read(&exp->exp_lock_hash->hs_count) == 0 &&
 	    exp->exp_obd->obd_stopping)
-		ldlm_reprocess_recovery_done(exp->exp_obd->obd_namespace);
+		ldlm_reprocess_ns(exp->exp_obd->obd_namespace);
 
 	lu_env_fini(&env);
 

lustre/ldlm/ldlm_lockd.c
@@ -2768,7 +2768,7 @@ void ldlm_revoke_export_locks(struct obd_export *exp)
 			  LDLM_WORK_REVOKE_AST);
 
 	if (rc == -ERESTART)
-		ldlm_reprocess_recovery_done(exp->exp_obd->obd_namespace);
+		ldlm_reprocess_ns(exp->exp_obd->obd_namespace);
 
 	EXIT;
 }

lustre/ldlm/ldlm_reclaim.c
@@ -204,7 +204,7 @@ static void ldlm_reclaim_res(struct ldlm_namespace *ns, int *count,
 
 	rc  = ldlm_run_ast_work(ns, &data.rcd_rpc_list, LDLM_WORK_REVOKE_AST);
 	if (rc == -ERESTART)
-		ldlm_reprocess_recovery_done(ns);
+		ldlm_reprocess_ns(ns);
 
 	*count -= data.rcd_added;
 	EXIT;